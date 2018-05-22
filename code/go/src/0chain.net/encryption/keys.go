d346ee298ae407f7bebe4ebdd9df26cb14641e9c
Kenny <Kenwes13@gmail.com>
2018-05-22 03:39:14 -0700

    updated keys

--- code/go/src/0chain.net/encryption/keys.go
+++ code/go/src/0chain.net/encryption/keys.go
@@ -1,30 +1,34 @@
 package encryption
 
 import (
-	"fmt"
-	"math/rand"
+	"crypto/rand"
+	"encoding/hex"
+	"golang.org/x/crypto/ed25519"
 )
-
-var r = rand.New(rand.NewSource(99))
 
 // TODO: Implement this the right way
 //Generatekeys - Generate assymetric private/public keys
 func GenerateKeys() (privateKey string, publicKey string) {
-	id := r.Int63()
-	k1 := fmt.Sprintf("0chain.net key %v", id)
-	id = r.Int63()
-	k2 := fmt.Sprintf("0chain.net key %v", id)
-	return Hash(k1), Hash(k2)
+	public, private, err := ed25519.GenerateKey(rand.Reader)
+	if err == nil {
+		return hex.EncodeToString(public), hex.EncodeToString(private)
+	}
+	return "", ""
 }
 
 // TODO: Implement this the right way
 //Sign - given a private key and data, compute it's signature
 func Sign(privateKey string, hash string) string {
-	return Hash(hash)
+	private := []byte(privateKey)
+	data := []byte(hash)
+	return hex.EncodeToString(ed25519.Sign(private, data))
 }
 
 // TODO: Implement this the right way
 //Verify - given a public key and a signature,
 func Verify(publicKey string, signature string, hash string) bool {
-	return Hash(hash) == signature
+	public := []byte(publicKey)
+	sign := []byte(signature)
+	data := []byte(hash)
+	return ed25519.Verify(public, data, sign)
 }
