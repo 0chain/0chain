f9bcf314800a482b93b32fbf85d059fe5f6d255f
Siva Dirisala <siva@0chain.net>
2018-05-28 09:20:02 -0700

    Need to pass the server chain id as that's what is used to create the transactions

--- code/go/src/0chain.net/block/block_test.go
+++ code/go/src/0chain.net/block/block_test.go
@@ -8,6 +8,7 @@ import (
 
 	"0chain.net/client"
 	"0chain.net/common"
+	"0chain.net/config"
 	"0chain.net/datastore"
 	"0chain.net/encryption"
 	"0chain.net/node"
@@ -24,7 +25,7 @@ func TestBlockGeneration(t *testing.T) {
 	ctx = datastore.WithConnection(ctx)
 	BLOCK_SIZE = 1
 	a := Provider().(*Block)
-	a.GenerateBlock(ctx, "This is my chain. There are many who like it, but this one is mine.", nil)
+	a.GenerateBlock(ctx, config.GetServerChainID(), nil)
 	buf := new(bytes.Buffer)
 	json.NewEncoder(buf).Encode(a)
 	fmt.Printf("%v\n", buf)
