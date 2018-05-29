caab89688eeb91b1cc2b1559b94a4185e4d51b45
Siva Dirisala <siva@0chain.net>
2018-05-29 16:28:57 -0700

    Adjusted the interface API to avoid name conflict. Adde WithConnection APIs for persistance connection as well

--- code/go/src/0chain.net/persistencestore/connection.go
+++ code/go/src/0chain.net/persistencestore/connection.go
@@ -1,6 +1,9 @@
 package persistencestore
 
 import (
+	"context"
+
+	"0chain.net/common"
 	"github.com/gocql/gocql"
 )
 
@@ -19,7 +22,31 @@ func getCluster() *gocql.ClusterConfig {
 func initSession() error {
 	var err error
 	if session == nil || session.Closed() {
-		session, err := getCluster().CreateSession()
+		session, err = getCluster().CreateSession()
 	}
 	return err
+}
+
+/*GetConnection - returns a connection from the Pool
+* Should always use right after getting the connection to avoid leaks
+* defer c.Close()
+ */
+func GetConnection() *gocql.Session {
+	return session
+}
+
+/*CONNECTION - key used to get the connection object from the context */
+const CONNECTION common.ContextKey = "pconnection"
+
+/*WithConnection takes a context and adds a connection value to it */
+func WithConnection(ctx context.Context) context.Context {
+	return context.WithValue(ctx, CONNECTION, GetConnection())
+}
+
+/*GetCon returns a connection stored in the context which got created via WithConnection */
+func GetCon(ctx context.Context) *gocql.Session {
+	if ctx == nil {
+		return GetConnection()
+	}
+	return ctx.Value(CONNECTION).(*gocql.Session)
 }
--- code/go/src/0chain.net/persistencestore/entity.go
+++ code/go/src/0chain.net/persistencestore/entity.go
@@ -2,7 +2,6 @@ package persistencestore
 
 import (
 	"context"
-	"fmt"
 
 	"0chain.net/common"
 	"0chain.net/datastore"
@@ -22,50 +21,10 @@ func GetProvider(entityName string) common.EntityProvider {
 	return providers[entityName]
 }
 
+/*PersistenceEntity - Persistence Entity */
 type PersistenceEntity interface {
 	datastore.Entity
-	Read(ctx context.Context, key datastore.Key) error
-	Write(ctx context.Context) error
-	Delete(ctx context.Context) error
-}
-
-func GetEntityKey(entity PersistenceEntity) datastore.Key {
-	var key interface{} = entity.GetKey()
-	switch v := key.(type) {
-	case string:
-		return datastore.ToKey(fmt.Sprintf("%s:%v", entity.GetEntityName(), v))
-	case []byte:
-		return datastore.ToKey(append(append([]byte(entity.GetEntityName()), ':'), v...))
-	default:
-		return datastore.EmptyKey
-	}
-}
-
-func Write(w http.ResponseWriter, r *http.Request) {
-    var errs []string
-    sharder, errs := FormToSharder(r)
-
-    // have we created a sharder correctly
-    var created bool = false
-
-    // if we had no errors from FormToSharder, we will
-    // attempt to save our data to Cassandra
-    if len(errs) == 0 {
-        fmt.Println("creating a new sharder")
-        // write data to Cassandra
-        if err := session.Query(`
-            INSERT INTO block (block_hash, prev_block_hash, block_signature, miner_id, timestamp, round) VALUES (?, ?, ?, ?, ?, ?)`,
-            sharder.Block_hash, sharder.Prev_block_hash, sharder.Block_signature, sharder.Miner_id, sharder.Timestamp, sharder.Round).Exec(); err != nil {
-            errs = append(errs, err.Error())
-        } else {
-            created = true
-        }
-    }
-
-    if created {
-        fmt.Println("Data inserted")
-    } else {
-        fmt.Println("errors", errs)
-        json.NewEncoder(w).Encode(ErrorResponse{Errors: errs})
-    }
+	PRead(ctx context.Context, key datastore.Key) error
+	PWrite(ctx context.Context) error
+	PDelete(ctx context.Context) error
 }
