018eea5394c3e5349ac89c037a39aa9a6a2338c5
Nidhi Sharma <nidhi.ardent@gmail.com>
2018-05-30 16:01:41 -0700

    Added sharder entity.go and also updated the persistencestore

--- code/go/src/0chain.net/persistencestore/entity.go
+++ code/go/src/0chain.net/persistencestore/entity.go
@@ -1,7 +1,10 @@
 package persistencestore
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"0chain.net/common"
 	"0chain.net/datastore"
@@ -27,4 +30,55 @@ type PersistenceEntity interface {
 	PRead(ctx context.Context, key datastore.Key) error
 	PWrite(ctx context.Context) error
 	PDelete(ctx context.Context) error
+}
+
+func PRead(ctx context.Context, key datastore.Key, entity PersistenceEntity) error {
+	buffer := bytes.NewBuffer(make([]byte, 0, 256))
+	err := json.NewDecoder(buffer).Decode(entity)
+	c := GetCon(ctx)
+	var errs []string
+
+	fmt.Println("here : ", entity)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := c.Query(
+		"SELECT JSON * from block").Exec(); err != nil {
+		errs = append(errs, err.Error())
+	}
+	return nil
+}
+
+func PWrite(ctx context.Context, entity PersistenceEntity) error {
+	return PWriteAux(ctx, entity, true)
+}
+
+func PWriteAux(ctx context.Context, entity PersistenceEntity, overwrite bool) error {
+	buffer := bytes.NewBuffer(make([]byte, 0, 256))
+	json.NewEncoder(buffer).Encode(entity)
+	c := GetCon(ctx)
+	var errs []string
+
+	fmt.Println("here : ", entity)
+
+	b, err := json.Marshal(entity)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := c.Query(
+		"INSERT INTO block JSON ?", b).Exec(); err != nil {
+		errs = append(errs, err.Error())
+	} else {
+		overwrite = true
+	}
+	return nil
+}
+
+func PDelete(ctx context.Context, entity PersistenceEntity) error {
+	c := GetCon(ctx)
+	c.Query("Delete from block")
+	return nil
 }
