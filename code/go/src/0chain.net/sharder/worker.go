ddc172541e961d3bb16fd2f4a131a90c13c4a0a7
Siva Dirisala <siva@0chain.net>
2018-06-13 21:49:12 -0700

    Added block generation time instrumentation

--- code/go/src/0chain.net/sharder/worker.go
+++ code/go/src/0chain.net/sharder/worker.go
@@ -2,12 +2,16 @@ package sharder
 
 import (
 	"context"
+	"log"
+	"os"
+	"time"
 
 	"0chain.net/block"
 	"0chain.net/common"
 	"0chain.net/datastore"
 	. "0chain.net/logging"
 	"0chain.net/round"
+	metrics "github.com/rcrowley/go-metrics"
 	"go.uber.org/zap"
 )
 
@@ -16,7 +20,10 @@ func SetupWorkers() {
 	ClearWorkerState()
 	ctx := common.GetRootContext()
 	go GetSharderChain().BlockWorker(ctx)
+	go metrics.LogScaled(metrics.DefaultRegistry, 60*time.Second, time.Millisecond, log.New(os.Stderr, "metrics: ", log.Lmicroseconds))
 }
+
+var timer metrics.Timer
 
 //TODO: The blocks and rounds data structures are temporary for debugging.
 var blocks map[string]*block.Block
@@ -27,11 +34,15 @@ func ClearWorkerState() {
 	Logger.Info("clearing worker state")
 	blocks = make(map[string]*block.Block)
 	rounds = make(map[int64]*round.Round)
+	if timer != nil {
+		metrics.Unregister("block_time")
+	}
+	timer = metrics.GetOrRegisterTimer("block_time", nil)
 }
 
 /*BlockWorker - stores the blocks */
 func (sc *Chain) BlockWorker(ctx context.Context) {
-
+	var ts time.Time
 	for true {
 		select {
 		case <-ctx.Done():
@@ -55,6 +66,10 @@ func (sc *Chain) BlockWorker(ctx context.Context) {
 				er.RandomSeed = b.RoundRandomSeed
 				rounds[er.Number] = er
 			}
+			if b.Round != 1 {
+				timer.UpdateSince(ts)
+			}
+			ts = time.Now()
 			er.AddNotarizedBlock(b)
 			StoreBlock(ctx, b)
 		}
