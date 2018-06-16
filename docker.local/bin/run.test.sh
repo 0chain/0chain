22af4edd86cf10779e12e0a35b2423d85b665dad
Siva <siva@0chain.net>
2018-06-16 00:11:10 -0700

    Using curl instead of wget to reduce dependencies

--- docker.local/bin/run.test.sh
+++ docker.local/bin/run.test.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 num_txns=${1:-1000}
+echo Submitting $num_txns transactions to each miner
 docker.local/bin/generate_txns.sh $num_txns
-wget -q -O - http://localhost:7171/_start
-wget -q -O - http://localhost:7071/_start
+
+echo Clearing the sharder state
+curl http://localhost:7171/_start -o -
+
+echo Starting the block generation process
+curl http://localhost:7071/_start -o -
