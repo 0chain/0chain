9197e601541a402172b290cf61f21718a681d5b5
Siva <siva@0chain.net>
2018-06-16 13:14:14 -0700

    Updating logging so it prints debug statements

--- code/go/src/0chain.net/logging/logger.go
+++ code/go/src/0chain.net/logging/logger.go
@@ -17,15 +17,8 @@ func InitLogging(mode string) {
 }
 
 /*LoggerInit - initialize the logger */
-func LoggerInit(logMode, logFile string) *zap.Logger {
-	conf := zap.NewDevelopmentEncoderConfig()
-	writer := zapcore.AddSync(&lumberjack.Logger{
-		Filename:   "log/0chain.log",
-		MaxSize:    10, // megabytes
-		MaxBackups: 3,
-		MaxAge:     28, // days
-	})
-
+func LoggerInit(logMode, logFile string) {
+	var conf zapcore.EncoderConfig
 	if logMode == "development" {
 		conf = zap.NewDevelopmentEncoderConfig()
 	} else if logMode == "production" {
@@ -33,26 +26,37 @@ func LoggerInit(logMode, logFile string) *zap.Logger {
 	} else {
 		conf = zap.NewDevelopmentEncoderConfig()
 	}
-	conf = zapcore.EncoderConfig{
-		TimeKey:        "@timestamp",
-		LevelKey:       "level",
-		NameKey:        "logger",
-		CallerKey:      "caller",
-		MessageKey:     "msg",
-		StacktraceKey:  "stacktrace",
-		EncodeLevel:    zapcore.LowercaseLevelEncoder,
-		EncodeTime:     zapcore.ISO8601TimeEncoder,
-		EncodeDuration: zapcore.NanosDurationEncoder,
+	/*
+		conf = zapcore.EncoderConfig{
+			TimeKey:        "@timestamp",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			MessageKey:     "msg",
+			StacktraceKey:  "stacktrace",
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.NanosDurationEncoder,
+		} */
+	writer := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   logFile,
+		MaxSize:    10, // megabytes
+		MaxBackups: 5,
+		MaxAge:     28, // days
+	})
+	if logMode == "development" {
+		core := zapcore.NewCore(
+			zapcore.NewJSONEncoder(conf),
+			writer,
+			zap.DebugLevel,
+		)
+		Logger = zap.New(core, zap.AddCaller())
+	} else {
+		core := zapcore.NewCore(
+			zapcore.NewJSONEncoder(conf),
+			writer,
+			zap.InfoLevel,
+		)
+		Logger = zap.New(core)
 	}
-	core := zapcore.NewCore(
-		zapcore.NewJSONEncoder(conf),
-		writer,
-		zap.InfoLevel,
-	)
-	Logger = zap.New(core)
-
-	zapLogger := zap.New(core, zap.AddCaller())
-
-	return zapLogger
-
 }
