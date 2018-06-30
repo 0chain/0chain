7e59742952bc92fd50da0bb48cf1db0db0a2e710
Nidhi Sharma <nidhi.ardent@gmail.com>
2018-06-30 10:40:35 -0700

    update to map all cassandra keyspaces and table from sql directory

--- docker.local/config/cassandra/cassandra-init.sh
+++ docker.local/config/cassandra/cassandra-init.sh
@@ -3,7 +3,10 @@ chmod +x /0chain/cassandra/wait-for-it.sh
 
 /0chain/cassandra/wait-for-it.sh -t 0 cassandra:9042 -- echo "CASSANDRA Node started"
 
+cd ../0chain/sql
 
-cqlsh -f /0chain/cassandra/init.cql cassandra
+cqlsh -f zerochain_keyspace.cql cassandra
+cqlsh -f block_summary.cql cassandra
+cqlsh -f txn_summary.cql cassandra
 
 echo "### CASSANDRA INITIALISED! ###"
