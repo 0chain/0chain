b7e311a31950bfd6644800c7bd7e70efad2f2ca8
Siva <siva@0chain.net>
2018-07-05 16:05:37 -0700

    Each redis db should be in it's own directory

--- docker.local/bin/init.setup.sh
+++ docker.local/bin/init.setup.sh
@@ -2,13 +2,16 @@
 
 for i in $(seq 1 3)
 do
-  mkdir -p docker.local/miner$i/data/db/redis
+  mkdir -p docker.local/miner$i/data/redis/state
+  mkdir -p docker.local/miner$i/data/redis/transactions
+  mkdir -p docker.local/miner$i/data/rocksdb
   mkdir -p docker.local/miner$i/log
 done
 
 for i in $(seq 1 3)
 do
   mkdir -p docker.local/sharder$i/data/blocks
+  mkdir -p docker.local/miner$i/data/rocksdb
   mkdir -p docker.local/sharder$i/data/cassandra
   mkdir -p docker.local/sharder$i/log
 done
