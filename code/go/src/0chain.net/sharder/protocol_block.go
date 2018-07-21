4542444c4bb00ec6f1b87a0fcdf7716548dfe826
KoppakaRavali <ravali@0chain.net>
2018-07-20 17:48:52 -0700

    Get Round Object before further processing

--- code/go/src/0chain.net/sharder/protocol_block.go
+++ code/go/src/0chain.net/sharder/protocol_block.go
@@ -19,6 +19,7 @@ func (sc *Chain) UpdatePendingBlock(ctx context.Context, b *block.Block, txns []
 
 /*UpdateFinalizedBlock - updates the finalized block */
 func (sc *Chain) UpdateFinalizedBlock(ctx context.Context, b *block.Block) {
+	fr := sc.GetRound(b.Round)
 	Logger.Info("update finalized block", zap.Int64("round", b.Round), zap.String("block", b.Hash), zap.Any("lf_round", sc.LatestFinalizedBlock.Round), zap.Any("current_round", sc.CurrentRound), zap.Any("blocks_size", len(sc.Blocks)), zap.Any("rounds_size", len(sc.rounds)))
 	// Sort transactions by their hash - useful for quick search
 	sort.SliceStable(b.Txns, func(i, j int) bool { return b.Txns[i].Hash < b.Txns[j].Hash })
@@ -26,7 +27,7 @@ func (sc *Chain) UpdateFinalizedBlock(ctx context.Context, b *block.Block) {
 	if err != nil {
 		Logger.Error("block save", zap.Any("round", b.Round), zap.Any("hash", b.Hash), zap.Error(err))
 	}
-	fr := sc.GetRound(b.Round)
+
 	if fr != nil {
 		fr.Finalize(b)
 		err := sc.StoreRound(ctx, fr)
@@ -35,5 +36,7 @@ func (sc *Chain) UpdateFinalizedBlock(ctx context.Context, b *block.Block) {
 		}
 		sc.GetRoundChannel() <- fr
 		sc.DeleteRoundsBelow(ctx, fr.Number)
+	} else {
+		Logger.Debug("round - missed", zap.Int64("round", b.Round))
 	}
 }
