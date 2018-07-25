2d205ade74f00e063a5cadda7eb3f3a1fcd6534b
Siva <siva@0chain.net>
2018-07-24 18:22:17 -0700

    Updated typo of miner to sharder

--- docker.local/bin/init.setup.sh
+++ docker.local/bin/init.setup.sh
@@ -11,7 +11,7 @@ done
 for i in $(seq 1 3)
 do
   mkdir -p docker.local/sharder$i/data/blocks
-  mkdir -p docker.local/miner$i/data/rocksdb
+  mkdir -p docker.local/sharder$i/data/rocksdb
   mkdir -p docker.local/sharder$i/data/cassandra
   mkdir -p docker.local/sharder$i/log
 done
