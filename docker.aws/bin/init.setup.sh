598637b672fa31f8a6c2db2bbd75d66d1cbf9ed2
sachin <sachin@0chain.net>
2018-07-30 00:18:50 -0700

    Absolute file path

--- docker.aws/bin/init.setup.sh
+++ docker.aws/bin/init.setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-mkdir -p docker.aws/miner/data/redis/state
-mkdir -p docker.aws/miner/data/redis/transactions
-mkdir -p docker.aws/miner/data/rocksdb
-mkdir -p docker.aws/miner/log
+mkdir -p /0chain/miner/data/redis/state
+mkdir -p /0chain/miner/data/redis/transactions
+mkdir -p /0chain/miner/data/rocksdb
+mkdir -p /0chain/miner/log
 
-mkdir -p docker.aws/sharder/data/blocks
-mkdir -p docker.aws/sharder/data/rocksdb
-mkdir -p docker.aws/sharder/data/cassandra
-mkdir -p docker.aws/sharder/log
+mkdir -p /0chain/sharder/data/blocks
+mkdir -p /0chain/sharder/data/rocksdb
+mkdir -p /0chain/sharder/data/cassandra
+mkdir -p /0chain/sharder/log
