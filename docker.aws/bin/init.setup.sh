db9e9e1dbc2bcf71da8e0236ab215f8d766f6f5a
sachin <sachin@0chain.net>
2018-08-02 16:56:05 -0700

    Fix recursive diretory write

--- docker.aws/bin/init.setup.sh
+++ docker.aws/bin/init.setup.sh
@@ -3,7 +3,7 @@ mkdir -p /0chain/miner/data/redis/state
 mkdir -p /0chain/miner/data/redis/transactions
 mkdir -p /0chain/miner/data/rocksdb
 mkdir -p /0chain/miner/log
-sudo chmod 777  /0chain/miner/data
+sudo chmod -R 777  /0chain/miner/data
 
 mkdir -p /0chain/sharder/data/blocks
 mkdir -p /0chain/sharder/data/rocksdb
