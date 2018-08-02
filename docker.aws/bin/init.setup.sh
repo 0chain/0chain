44edd222d15672ec6690205c1364320cb55a33df
sachin <sachin@0chain.net>
2018-08-02 00:34:25 -0700

    Update volumes around root

--- docker.aws/bin/init.setup.sh
+++ docker.aws/bin/init.setup.sh
@@ -3,8 +3,10 @@ mkdir -p /0chain/miner/data/redis/state
 mkdir -p /0chain/miner/data/redis/transactions
 mkdir -p /0chain/miner/data/rocksdb
 mkdir -p /0chain/miner/log
+sudo chown -R root:root /0chain/miner
 
 mkdir -p /0chain/sharder/data/blocks
 mkdir -p /0chain/sharder/data/rocksdb
 mkdir -p /0chain/sharder/data/cassandra
 mkdir -p /0chain/sharder/log
+sudo chown -R root:root /0chain/sharder
