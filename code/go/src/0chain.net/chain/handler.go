feb0933dc0ce1e71a4a8231c8d80a52304e5af08
Siva <siva@0chain.net>
2018-08-07 00:03:55 -0700

    Add log links by detail

--- code/go/src/0chain.net/chain/handler.go
+++ code/go/src/0chain.net/chain/handler.go
@@ -167,8 +167,8 @@ func HomePageHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<li><a href='/_chain_stats'>/_chain_stats</a></li>")
 		fmt.Fprintf(w, "<li><a href='/_diagnostics/info'>/_diagnostics/info</a></li>")
 		fmt.Fprintf(w, "<li><a href='/_diagnostics/n2n/info'>/_diagnostics/n2n/info</a></li>")
-		fmt.Fprintf(w, "<li><a href='/_diagnostics/logs'>/_diagnostics/logs</a></li>")
-		fmt.Fprintf(w, "<li><a href='/_diagnostics/n2n_logs'>/_diagnostics/n2n_logs</a></li>")
+		fmt.Fprintf(w, "<li>/_diagnostics/logs [Level <a href='/_diagnostics/logs?detail=1'>1</a>, <a href='/_diagnostics/logs?detail=2'>2</a>, <a href='/_diagnostics/logs?detail=3'>3</a>]</li>")
+		fmt.Fprintf(w, "<li>/_diagnostics/n2n_logs [Level <a href='/_diagnostics/n2n_logs?detail=1'>1</a>, <a href='/_diagnostics/n2n_logs?detail=2'>2</a>, <a href='/_diagnostics/n2n_logs?detail=3'>3</a>]</li>")
 		fmt.Fprintf(w, "<li><a href='/debug/pprof/'>/debug/pprof/</a></li>")
 		fmt.Fprintf(w, "</ul>")
 	}
