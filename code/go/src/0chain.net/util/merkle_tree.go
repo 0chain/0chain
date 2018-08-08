00f142f52276182c2bfe60a31914b0ec868c0156
Kenny <Kenwes13@gmail.com>
2018-08-08 16:01:05 -0700

    Changed how ComputeTree handles one hashable

--- code/go/src/0chain.net/util/merkle_tree.go
+++ code/go/src/0chain.net/util/merkle_tree.go
@@ -35,7 +35,11 @@ func (mt *MerkleTree) ComputeTree(hashes []Hashable) {
 	for idx, hashable := range hashes {
 		mt.tree[idx] = hashable.GetHash()
 	}
-	for pl0, plsize := 0, mt.leavesCount; mt.tree[len(mt.tree)-1] == ""; pl0, plsize = pl0+plsize, (plsize+1)/2 {
+	if len(hashes) == 1 {
+		mt.tree[1] = MHash(mt.tree[0], mt.tree[0])
+		return
+	}
+	for pl0, plsize := 0, mt.leavesCount; plsize > 1; pl0, plsize = pl0+plsize, (plsize+1)/2 {
 		l0 := pl0 + plsize
 		for i, j := 0, 0; i < plsize; i, j = i+2, j+1 {
 			mt.tree[pl0+plsize+j] = MHash(mt.tree[pl0+i], mt.tree[pl0+i+1])
