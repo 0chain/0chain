e72176d9cc3392dfe2a7c02a1bd77c79909eb21a
Siva <siva@0chain.net>
2018-08-20 22:53:59 -0700

    Added benchmarking data

--- code/go/src/0chain.net/wallet/wallet_test.go
+++ code/go/src/0chain.net/wallet/wallet_test.go
@@ -24,10 +24,13 @@ const (
 
 func TestMPTWithWalletTxns(t *testing.T) {
 	clients := 1000
-	transactions := 1000
+	transactions := 10000
 	var rs = rand.NewSource(randTime)
 	prng = rand.New(rs)
 	wallets := createWallets(clients)
+	prng = rand.New(rs)
+	fmt.Printf("using in no db\n")
+	testWithMPT(nil, wallets, transactions)
 	prng = rand.New(rs)
 	fmt.Printf("using in memory db\n")
 	testWithMPT(GetMPT(MEMORY), wallets, transactions)
@@ -39,9 +42,11 @@ func TestMPTWithWalletTxns(t *testing.T) {
 	ts := time.Now()
 	lmpt.GetChangeCollector().UpdateChanges(pmpt.GetNodeDB(), util.Origin(2010), false)
 	fmt.Printf("time taken to persist: %v\n", time.Since(ts))
-	prng = rand.New(rs)
-	fmt.Printf("using persist db\n")
-	testWithMPT(pmpt, wallets, transactions)
+	/*
+		prng = rand.New(rs)
+		fmt.Printf("using persist db\n")
+		testWithMPT(pmpt, wallets, transactions)
+	*/
 }
 
 func GetMPT(dbType int) util.MerklePatriciaTrieI {
@@ -70,22 +75,26 @@ func testWithMPT(mpt util.MerklePatriciaTrieI, wallets []*Wallet, transactions i
 	if debug {
 		fmt.Printf("INFO: random source seed %d\n", randTime)
 	}
-	for idx, w := range wallets {
-		balance := state.Balance(w.Balance)
-		mpt.Insert(util.Path(w.ClientID), &state.State{Balance: balance})
-		state, err := getState(mpt, w.ClientID)
-		if err != nil {
-			panic(err)
-		}
-		if debug {
-			fmt.Printf("INFO:(%v) id:%v balance:%v (%v)\n", idx, w.ClientID, w.Balance, state.Balance)
+	if mpt != nil {
+		for idx, w := range wallets {
+			balance := state.Balance(w.Balance)
+			mpt.Insert(util.Path(w.ClientID), &state.State{Balance: balance})
+			state, err := getState(mpt, w.ClientID)
+			if err != nil {
+				panic(err)
+			}
+			if debug {
+				fmt.Printf("INFO:(%v) id:%v balance:%v (%v)\n", idx, w.ClientID, w.Balance, state.Balance)
+			}
 		}
 	}
-	fmt.Printf("wallet creation - num changes: %v\n", len(mpt.GetChangeCollector().GetChanges()))
+	if mpt != nil {
+		fmt.Printf("wallet creation - num changes: %v\n", len(mpt.GetChangeCollector().GetChanges()))
+	}
 	if transactions == 0 {
 		return
 	}
-	mpt.ResetChangeCollector(nil)
+	//mpt.ResetChangeCollector(nil)
 	ts := time.Now()
 	for count := 1; count <= transactions; count++ {
 		var wf, wt *Wallet
@@ -104,35 +113,48 @@ func testWithMPT(mpt util.MerklePatriciaTrieI, wallets []*Wallet, transactions i
 		value := prng.Int63n(wf.Balance) + 1
 		wf.Balance -= value
 		wt.Balance += value
-		//fmt.Printf("INFO: from:%v to:%v amt:%v\n", wf.ClientID, wt.ClientID, value)
 		if wf.Balance == 0 {
 			if debug {
 				fmt.Printf("INFO: deleting wallet of %v as balance is zero\n", wf.ClientID)
 			}
-			mpt.Delete(util.Path(wf.ClientID))
+			if mpt != nil {
+				mpt.Delete(util.Path(wf.ClientID))
+			}
 		} else {
 			if debug {
 				fmt.Printf("INFO: moving balance %v from %v to %v\n", value, wf.ClientID, wt.ClientID)
 			}
-			s, err := getState(mpt, wf.ClientID)
+			if mpt != nil {
+				s, err := getState(mpt, wf.ClientID)
+				if err != nil {
+					panic(err)
+				}
+				s.Balance -= state.Balance(value)
+				mpt.Insert(util.Path(wf.ClientID), s)
+			}
+		}
+		if mpt != nil {
+			s, err := getState(mpt, wt.ClientID)
 			if err != nil {
 				panic(err)
 			}
-			s.Balance -= state.Balance(value)
-			mpt.Insert(util.Path(wf.ClientID), s)
+			s.Balance += state.Balance(value)
+			mpt.Insert(util.Path(wt.ClientID), s)
 		}
-		s, err := getState(mpt, wt.ClientID)
-		if err != nil {
-			panic(err)
-		}
-		s.Balance += state.Balance(value)
-		mpt.Insert(util.Path(wt.ClientID), s)
 	}
-	fmt.Printf("transactions - num changes: %v\n", len(mpt.GetChangeCollector().GetChanges()))
+	if mpt != nil {
+		fmt.Printf("transactions - num changes: %v\n", len(mpt.GetChangeCollector().GetChanges()))
+	}
 	fmt.Printf("transactions - time taken: %v\n", time.Since(ts))
-
+	if mpt == nil {
+		return
+	}
+	zbcount := 0
 	for index := 0; index < len(wallets); index++ {
 		w := wallets[index]
+		if w.Balance == 0 {
+			zbcount++
+		}
 		s, err := getState(mpt, w.ClientID)
 		if err != nil {
 			if err == util.ErrNodeNotFound {
@@ -146,6 +168,7 @@ func testWithMPT(mpt util.MerklePatriciaTrieI, wallets []*Wallet, transactions i
 			}
 		}
 	}
+	fmt.Printf("zero balance clients %v\n", zbcount)
 }
 
 func createWallets(num int) []*Wallet {
