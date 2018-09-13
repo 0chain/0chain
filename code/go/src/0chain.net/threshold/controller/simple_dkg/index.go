4a261312936df8935fdd7a8df6637738628eff96
Paul Merrill <pdm@pdm.me>
2018-09-13 13:49:42 -0700

    controller/simple_dkg: Add output types and timeouts

--- code/go/src/0chain.net/threshold/controller/simple_dkg/index.go
+++ code/go/src/0chain.net/threshold/controller/simple_dkg/index.go
@@ -1,6 +1,9 @@
 package simple_dkg
 
 import (
+	"context"
+	"time"
+
 	"0chain.net/node"
 	. "0chain.net/threshold/controller"
 	. "0chain.net/threshold/model"
@@ -8,37 +11,73 @@ import (
 	"0chain.net/threshold/model/simple_dkg"
 )
 
+type Timeouts struct {
+	retransmit time.Duration
+}
+
 type ShareMsg struct {
 	m Key
 	v VerificationKey
 }
 
+type Output interface{}
+type Canceled struct{}
+type IncorrectShare *node.Node
+type Success party.Party
+
 type Protocol struct {
-	info *NodeInfo
-	dkg  simple_dkg.DKG
+	net      *NodeInfo
+	dkg      simple_dkg.DKG
+	timeouts Timeouts
+	output   chan Output
 }
 
-func New(info *NodeInfo, t T) Protocol {
+func newProtocol(net *NodeInfo, t T, timeouts Timeouts) Protocol {
 	return Protocol{
-		info: info,
-		dkg:  simple_dkg.New(t, N(len(info.Peers.Nodes))),
+		net:      net,
+		dkg:      simple_dkg.New(t, N(len(net.Peers.Nodes))),
+		timeouts: timeouts,
+		output:   make(chan Output, 10),
 	}
 }
-func (p *Protocol) close() {}
-func (p *Protocol) GetShareMsgFor(to *node.Node) ShareMsg {
-	i := p.info.PeerIds[to.Host]
+func (p *Protocol) newShareMsg(to *node.Node) ShareMsg {
+	i := p.net.PeerIds[to.Host]
 	m, v := p.dkg.GetShareFor(i)
 	return ShareMsg{
 		m: m,
 		v: v,
 	}
 }
+func (p *Protocol) transmitAll() {
+	for _, peer := range p.net.Peers.Nodes {
+		m := p.newShareMsg(peer)
+		_ = m
+		// TODO: Send m to peer.
+	}
+}
 func (p *Protocol) receive(from *node.Node, m ShareMsg) {
-	i := p.info.PeerIds[from.Host]
+	i := p.net.PeerIds[from.Host]
 	p.dkg.ReceiveShare(i, m.m, m.v)
 }
 
-func Run(info *NodeInfo, t T) chan party.Party {
-	New(info, t)
-	return make(chan party.Party, 1)
+func (p *Protocol) run(ctx context.Context) {
+	retransmit, _ := context.WithTimeout(ctx, p.timeouts.retransmit)
+	p.transmitAll()
+	for {
+		select {
+		case <-ctx.Done():
+			p.output <- Canceled{}
+			return
+		case <-retransmit.Done():
+			p.transmitAll()
+			continue
+			// TODO: Receive share from a peer. Optinally quit.
+		}
+	}
+}
+
+func Run(ctx context.Context, net *NodeInfo, t T, timeouts Timeouts) <-chan Output {
+	p := newProtocol(net, t, timeouts)
+	go p.run(ctx)
+	return p.output
 }
