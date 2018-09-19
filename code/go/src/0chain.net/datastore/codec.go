b1ad151a66a6b33e3463e3f2f4b07419c6c1c504
Siva <siva@0chain.net>
2018-09-19 01:34:19 -0700

    Moving codec into common and retaining a wrapper in datastore

--- code/go/src/0chain.net/datastore/codec.go
+++ code/go/src/0chain.net/datastore/codec.go
@@ -2,14 +2,9 @@ package datastore
 
 import (
 	"bytes"
-	"encoding/json"
-	"fmt"
 	"io"
 
 	"0chain.net/common"
-	. "0chain.net/logging"
-	"github.com/vmihailenco/msgpack"
-	"go.uber.org/zap"
 )
 
 const (
@@ -19,50 +14,22 @@ const (
 
 /*ToJSON - given an entity, get the json of that entity as a buffer */
 func ToJSON(entity Entity) *bytes.Buffer {
-	buffer := bytes.NewBuffer(make([]byte, 0, 256))
-	json.NewEncoder(buffer).Encode(entity)
-	return buffer
+	return common.ToJSON(entity)
 }
 
 /*WriteJSON - writes the entity json to a stream */
 func WriteJSON(w io.Writer, entity Entity) error {
-	return json.NewEncoder(w).Encode(entity)
+	return common.WriteJSON(w, entity)
 }
 
 /*ToMsgpack - msgpack encoding */
 func ToMsgpack(entity Entity) *bytes.Buffer {
-	buffer := bytes.NewBuffer(make([]byte, 0, 256))
-	encoder := msgpack.NewEncoder(buffer)
-	encoder.UseJSONTag(true)
-	encoder.Encode(entity)
-	return buffer
-}
-
-func ToJSONPipe(entity Entity) *io.PipeReader {
-	pr, pw := io.Pipe()
-	go func() {
-		defer pw.Close()
-		err := json.NewEncoder(pw).Encode(entity)
-		if err != nil {
-			Logger.Error("TOJSONPipe:error", zap.Error(err))
-		}
-	}()
-	return pr
+	return common.ToMsgpack(entity)
 }
 
 /*FromJSON - read data into an entity */
 func FromJSON(data interface{}, entity Entity) error {
-	var err error
-	switch jsondata := data.(type) {
-	case []byte:
-		err = json.Unmarshal(jsondata, entity)
-	case string:
-		err = json.Unmarshal([]byte(jsondata), entity)
-	case io.Reader:
-		err = json.NewDecoder(jsondata).Decode(entity)
-	default:
-		return common.NewError("unknown_data_type", fmt.Sprintf("unknown data type for reading entity from json: %T, %v\n", data, data))
-	}
+	err := common.FromJSON(data, entity)
 	if err != nil {
 		return err
 	}
@@ -72,28 +39,12 @@ func FromJSON(data interface{}, entity Entity) error {
 
 /*ReadJSON - read entity json from a stream */
 func ReadJSON(r io.Reader, entity Entity) error {
-	return json.NewDecoder(r).Decode(entity)
+	return common.ReadJSON(r, entity)
 }
 
 /*FromMsgpack - read data into an entity */
 func FromMsgpack(data interface{}, entity Entity) error {
-	var err error
-	switch jsondata := data.(type) {
-	case []byte:
-		decoder := msgpack.NewDecoder(bytes.NewBuffer(jsondata))
-		decoder.UseJSONTag(true)
-		err = decoder.Decode(entity)
-	case string:
-		decoder := msgpack.NewDecoder(bytes.NewBuffer([]byte(jsondata)))
-		decoder.UseJSONTag(true)
-		err = decoder.Decode(entity)
-	case io.Reader:
-		decoder := msgpack.NewDecoder(jsondata)
-		decoder.UseJSONTag(true)
-		err = decoder.Decode(entity)
-	default:
-		return common.NewError("unknown_data_type", fmt.Sprintf("unknown data type for reading entity from json: %T, %v\n", data, data))
-	}
+	err := common.FromMsgpack(data, entity)
 	if err != nil {
 		return err
 	}
