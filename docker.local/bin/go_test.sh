34d68f4ad1aebf7cac352c20636b744bb99b08e2
Paul Merrill <pdm@pdm.me>
2018-09-24 19:05:24 -0700

    docker: Disable caching in go_test

--- docker.local/bin/go_test.sh
+++ docker.local/bin/go_test.sh
@@ -2,4 +2,5 @@
 docker build -f docker.local/build.go_test/Dockerfile code/go/src -t zchain_go_test
 
 # Allocate interactive TTY to allow Ctrl-C.
-docker run -it zchain_go_test go test "${@:-./...}"
+# Use "-count 1" to disable caching since we're just going to throw the container away after.
+docker run -it zchain_go_test go test -count 1 "${@:-./...}"
