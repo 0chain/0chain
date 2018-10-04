10abc8c97c6c66a370e8596fdfb022240d2c766a
Siva <siva@0chain.net>
2018-10-03 21:37:00 -0700

    Replacing Cassandra with Scylladb

--- bin/cassandra-init.sh
+++ bin/cassandra-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/0chain/bin/cassandra-wait.sh -t 0 cassandra:9042 -- echo "cassandra started"
+/0chain/bin/wait-for-service.sh -t 0 cassandra:9042 -- echo "cassandra started"
 cqlsh -f /0chain/sql/zerochain_keyspace.sql cassandra
 cqlsh -f /0chain/sql/txn_summary.sql cassandra
 echo "cassandra initialized"
