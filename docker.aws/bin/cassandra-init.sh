5df5293c209dc9446684e0893728df8830332418
sachin <sachin@0chain.net>
2018-11-01 18:24:38 -0700

    Cleanup config

--- docker.aws/bin/cassandra-init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/0chain/bin/wait-for-service.sh -t 0 cassandra:9042 -- echo "cassandra started"
-cqlsh -f /0chain/sql/zerochain_keyspace.sql cassandra
-cqlsh -f /0chain/sql/txn_summary.sql cassandra
-echo "cassandra initialized"
--- docker.aws/bin/scylla-init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/0chain/bin/wait-for-service.sh -t 0 scylla:9042 -- echo "scylla started"
-cqlsh -f /0chain/sql/zerochain_keyspace.sql scylla
-cqlsh -f /0chain/sql/txn_summary.sql scylla
-echo "scylla initialized"
