0chain/0chain

commit 917d25a051c8ba965b5d2fc42e9c742aedf41fc0

Code cleanup for get/put client

code/go/src/0chain.net/client/entity.go
@@ -27,6 +27,11 @@ type Client struct {
 	PublicKeyBytes encryption.HashBytes `json:"-"`
 }
 
+//NewClient - create a new client object
+func NewClient() *Client {
+	return datastore.GetEntityMetadata("client").Instance().(*Client)
+}
+
 var clientEntityMetadata *datastore.EntityMetadataImpl
 
 /*GetEntityMetadata - implementing the interface */
@@ -41,28 +46,6 @@ func (c *Client) Validate(ctx context.Context) error {
 	}
 	if !datastore.IsEqual(c.ID, datastore.ToKey(encryption.Hash(c.PublicKeyBytes))) {
 		return common.InvalidRequest("client id is not a SHA3-256 hash of the public key")
-	}
-	return nil
-}
-
-/*GetClient - gets client from either cache or database*/
-func (c *Client) GetClient(ctx context.Context, key datastore.Key) error {
-	var ok bool
-	var co *Client
-	ico, cerr := cacher.Get(key)
-	if cerr == nil {
-		co, ok = ico.(*Client)
-	}
-	if !ok {
-		err := c.Read(ctx, key)
-		if err == nil {
-			cacher.Add(key, c)
-		}
-		return err
-	} else {
-		c.ID = co.ID
-		c.PublicKey = co.PublicKey
-		c.SetPublicKey(c.PublicKey)
 	}
 	return nil
 }
@@ -104,11 +87,11 @@ func Provider() datastore.Entity {
 
 /*ComputeProperties - implement interface */
 func (c *Client) ComputeProperties() {
-	c.computePublicKeyBytes(c.PublicKey)
+	c.computePublicKeyBytes()
 }
 
-func (c *Client) computePublicKeyBytes(key string) {
-	b, _ := hex.DecodeString(key)
+func (c *Client) computePublicKeyBytes() {
+	b, _ := hex.DecodeString(c.PublicKey)
 	if len(b) > len(c.PublicKeyBytes) {
 		b = b[len(b)-encryption.HASH_LENGTH:]
 	}
@@ -118,7 +101,7 @@ func (c *Client) computePublicKeyBytes(key string) {
 /*SetPublicKey - set the public key */
 func (c *Client) SetPublicKey(key string) {
 	c.PublicKey = key
-	c.computePublicKeyBytes(key)
+	c.computePublicKeyBytes()
 	c.ID = encryption.Hash(c.PublicKeyBytes)
 }
 
@@ -167,4 +150,32 @@ func GetClients(ctx context.Context, clients map[string]*Client) {
 			clients[clientIDs[i]] = cEntities[j].(*Client)
 		}
 	}
+}
+
+/*GetClient - gets client from either cache or database*/
+func GetClient(ctx context.Context, key datastore.Key) (*Client, error) {
+	if co, cerr := cacher.Get(key); cerr == nil {
+		return co.(*Client), nil
+	}
+	co := NewClient()
+	err := co.Read(ctx, key)
+	if err != nil {
+		return nil, err
+	}
+	cacher.Add(key, co)
+	return nil, err
+}
+
+/*PutClient - Given a client data, it stores it */
+func PutClient(ctx context.Context, entity datastore.Entity) (interface{}, error) {
+	co, ok := entity.(*Client)
+	if !ok {
+		return nil, common.NewError("entity_invalid_type", "Invald entity type")
+	}
+	response, err := datastore.PutEntityHandler(ctx, entity)
+	if err != nil {
+		return nil, err
+	}
+	cacher.Add(co.GetKey(), co)
+	return response, nil
 }

code/go/src/0chain.net/transaction/entity.go
@@ -70,7 +70,7 @@ func (t *Transaction) ComputeClientID() {
 		if t.ClientID == "" {
 			// Doing this is OK because the transaction signature has ClientID
 			// that won't pass verification if some other client's public is put in
-			co := &client.Client{}
+			co := client.NewClient()
 			co.SetPublicKey(t.PublicKey)
 			t.ClientID = co.ID
 		}
@@ -152,8 +152,7 @@ func (t *Transaction) GetHashBytes() []byte {
 
 /*GetClient - get the Client object associated with the transaction */
 func (t *Transaction) GetClient(ctx context.Context) (*client.Client, error) {
-	co := &client.Client{}
-	err := co.GetClient(ctx, t.ClientID)
+	co, err := client.GetClient(ctx, t.ClientID)
 	if err != nil {
 		return nil, err
 	}
@@ -183,7 +182,7 @@ func (t *Transaction) VerifyHash(ctx context.Context) error {
 /*VerifySignature - verify the transaction hash */
 func (t *Transaction) VerifySignature(ctx context.Context) error {
 	var err error
-	co := datastore.GetEntityMetadata("client").Instance().(*client.Client)
+	co := client.NewClient()
 	if t.PublicKey == "" {
 		co, err = t.GetClient(ctx)
 		if err != nil {

code/go/src/0chain.net/datastore/entity.go
@@ -7,7 +7,7 @@ import (
 var (
 	/*EntityNotFound code should be used to check whether an entity is found or not */
 	EntityNotFound = "entity_not_found"
-	/*EntityDuplicate codee should be used to check if an entity is already present */
+	/*EntityDuplicate code should be used to check if an entity is already present */
 	EntityDuplicate = "duplicate_entity"
 )
 
@@ -23,6 +23,7 @@ type Entity interface {
 	Delete(ctx context.Context) error
 }
 
+//AllocateEntities - allocate entities for the given entity type
 func AllocateEntities(size int, entityMetadata EntityMetadata) []Entity {
 	entities := make([]Entity, size)
 	for i := 0; i < size; i++ {

code/go/src/0chain.net/client/handler.go
@@ -11,16 +11,11 @@ import (
 
 /*SetupHandlers sets up the necessary API end points */
 func SetupHandlers() {
-	http.HandleFunc("/v1/client/get", common.ToJSONResponse(memorystore.WithConnectionHandler(GetClient)))
+	http.HandleFunc("/v1/client/get", common.ToJSONResponse(memorystore.WithConnectionHandler(GetClientHandler)))
 	http.HandleFunc("/v1/client/put", datastore.ToJSONEntityReqResponse(datastore.DoAsyncEntityJSONHandler(PutClient, ClientEntityChannel), clientEntityMetadata))
 }
 
-/*GetClient - given an id returns the client information */
-func GetClient(ctx context.Context, r *http.Request) (interface{}, error) {
+/*GetClientHandler - given an id returns the client information */
+func GetClientHandler(ctx context.Context, r *http.Request) (interface{}, error) {
 	return datastore.GetEntityHandler(ctx, r, clientEntityMetadata, "id")
-}
-
-/*PutClient - Given a client data, it stores it */
-func PutClient(ctx context.Context, entity datastore.Entity) (interface{}, error) {
-	return datastore.PutEntityHandler(ctx, entity)
 }

code/go/src/0chain.net/node/n2n_send.go
@@ -436,9 +436,9 @@ func pullEntityHandler(ctx context.Context, nd *Node, uri string, handler datast
 	params := make(map[string]string)
 	params["_puri"] = uri
 	params["id"] = datastore.ToString(entityID)
-	N2n.Info("message pull", zap.String("uri", uri), zap.String("entity", entityName), zap.String("id", entityID))
 	rhandler := pullDataRequestor(params, phandler)
-	rhandler(nd)
+	result := rhandler(nd)
+	N2n.Info("message pull", zap.String("uri", uri), zap.String("entity", entityName), zap.String("id", entityID), zap.Bool("result", result))
 }
 
 var pullDataRequestor EntityRequestor

commit 2450f89cc17887daf9f3415d567ab207cc7ba2fd

Fixing a bug introduced in commit 917d25a...

code/go/src/0chain.net/client/entity.go
@@ -15,7 +15,7 @@ import (
 var cacher cache.Cache
 
 func init() {
-	cacher = cache.NewLFUCache(10 * 1024)
+	cacher = cache.NewLFUCache(1)
 }
 
 /*Client - data structure that holds the client data */
@@ -163,7 +163,7 @@ func GetClient(ctx context.Context, key datastore.Key) (*Client, error) {
 		return nil, err
 	}
 	cacher.Add(key, co)
-	return nil, err
+	return co, nil
 }
 
 /*PutClient - Given a client data, it stores it */

code/go/src/0chain.net/transaction/entity.go
@@ -182,13 +182,14 @@ func (t *Transaction) VerifyHash(ctx context.Context) error {
 /*VerifySignature - verify the transaction hash */
 func (t *Transaction) VerifySignature(ctx context.Context) error {
 	var err error
-	co := client.NewClient()
+	var co *client.Client
 	if t.PublicKey == "" {
 		co, err = t.GetClient(ctx)
 		if err != nil {
 			return err
 		}
 	} else {
+		co = client.NewClient()
 		co.ID = t.ClientID
 		co.PublicKey = t.PublicKey
 		co.SetPublicKey(co.PublicKey)