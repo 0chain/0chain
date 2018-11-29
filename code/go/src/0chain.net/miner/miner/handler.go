d1d7ee2f7a796f71e2865c527960345ff1206421
Siva <siva@0chain.net>
2018-11-29 13:03:26 -0800

    Moving server_chain.block.generation.transactions to development.txn_generation.transactions
    Features not meant for production should go into development.

--- code/go/src/0chain.net/miner/miner/handler.go
+++ code/go/src/0chain.net/miner/miner/handler.go
@@ -66,7 +66,7 @@ func updateConfig(w http.ResponseWriter, r *http.Request, updateUrl string) {
 	newGenTxnRate, _ := strconv.ParseInt(r.FormValue("generate_txn"), 10, 32)
 	if newGenTxnRate > 0 {
 		SetTxnGenRate(int32(newGenTxnRate))
-		viper.Set("server_chain.block.generation.transactions", newGenTxnRate)
+		viper.Set("development.txn_generation.transactions", newGenTxnRate)
 	}
 	newTxnWaitTime, _ := strconv.Atoi(r.FormValue("txn_wait_time"))
 	if newTxnWaitTime > 0 {
@@ -76,8 +76,8 @@ func updateConfig(w http.ResponseWriter, r *http.Request, updateUrl string) {
 	w.Header().Set("Content-Type", "text/html;charset=UTF-8")
 	fmt.Fprintf(w, "<form action='%s' method='post'>", updateUrl)
 	fmt.Fprintf(w, "Generation Timeout (time till a miner makes a block with less than max blocksize): <input type='text' name='generate_timeout' value='%v'><br>", viper.Get("server_chain.block.generation.timeout"))
-	fmt.Fprintf(w, "Transaction Generation Rate (rate the miner will add transactions to create a block): <input type='text' name='generate_txn' value='%v'><br>", viper.Get("server_chain.block.generation.transactions"))
 	fmt.Fprintf(w, "Retry Wait Time (time miner waits if there aren't enough transactions to reach max blocksize): <input type='text' name='txn_wait_time' value='%v'><br>", viper.Get("server_chain.block.generation.retry_wait_time"))
+	fmt.Fprintf(w, "Transaction Generation Rate (rate the miner will add transactions to create a block): <input type='text' name='generate_txn' value='%v'><br>", viper.Get("development.txn_generation.transactions"))
 	fmt.Fprintf(w, "<input type='submit' value='Submit'>")
 	fmt.Fprintf(w, "</form>")
 }
