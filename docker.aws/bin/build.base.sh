26b4b04313030cacb2b5c9c94d4152f2ca9b5652
Kenwes13 <kenwes13@gmail.com>
2018-11-28 18:18:10 -0800

    Updated docker.aws files

--- docker.aws/bin/build.base.sh
+++ docker.aws/bin/build.base.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build -f docker.aws/build.base/Dockerfile docker.aws/build.base -t zchain_base
+docker build -f docker.aws/build.base/Dockerfile docker.aws/build.base -t zchain_base $*
