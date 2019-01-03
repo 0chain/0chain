f72f8b80d46083330ec8eeec6eeb9b5d1513c67c
Kenwes13 <kenwes13@gmail.com>
2019-01-02 18:57:44 -0800

    Update pool.go

--- code/go/src/0chain.net/pool/pool.go
+++ code/go/src/0chain.net/pool/pool.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 
 	"0chain.net/common"
+	"0chain.net/datastore"
 	"0chain.net/state"
+	"0chain.net/transaction"
 )
 
 type Pool struct {
-	ID      string        `json:"id"`
-	Balance state.Balance `json:"amount"`
+	id      datastore.Key `json:"id"`
+	balance state.Balance `json:"balance"`
 }
 
 func (p *Pool) Encode() []byte {
@@ -22,15 +24,36 @@ func (p *Pool) Decode(input []byte) error {
 	return err
 }
 
+//Pool to pool transfer
 func (p *Pool) TransferTo(op *Pool, value state.Balance) error {
-	if value > p.Balance {
+	if value > p.balance {
 		return common.NewError("pool transfer failed", "value exceeds balance")
 	}
-	op.Balance += value
-	p.Balance -= value
+	op.balance += value
+	p.balance -= value
 	return nil
 }
 
-func (p *Pool) EmptyPool() {
-	p.Balance = 0
+func (p *Pool) EmptyPool(fromClientID, toClientID datastore.Key) *state.Transfer {
+	transfer := state.NewTransfer(fromClientID, toClientID, p.balance)
+	p.balance = 0
+	return transfer
+}
+
+func (p *Pool) DrainPool(fromClientID, toClientID datastore.Key, value state.Balance) (*state.Transfer, error) {
+	if value > p.balance {
+		return nil, common.NewError("pool drain failed", "value exceeds balance")
+	}
+	transfer := state.NewTransfer(fromClientID, toClientID, value)
+	p.balance -= value
+	return transfer, nil
+}
+
+func (p *Pool) FillPool(txn *transaction.Transaction) {
+	p.balance += txn.Value
+}
+
+func DigPool(id datastore.Key, txn *transaction.Transaction) *Pool {
+	p := &Pool{id, txn.Value}
+	return p
 }
