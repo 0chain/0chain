cf227d107615e8b9e920101749df79af0425dd0b
jay-at-0chain <jay@0chain.net>
2019-01-22 12:45:27 -0800

    Generate keys and save it into a given file

--- code/go/src/0chain.net/encryption/keys/main.go
+++ code/go/src/0chain.net/encryption/keys/main.go
@@ -12,19 +12,21 @@ import (
 
 func main() {
 	clientSigScheme := flag.String("signature_scheme", "", "ed25519 or bls0chain")
-	keysFile := flag.String("keys_file", "keys.txt", "keys_file")
+	keysFileName := flag.String("keys_file_name", "keys.txt", "keys_file_name")
+	path := flag.String("keys_file_path", "keys.txt", "keys_file_path")
 	data := flag.String("data", "", "data")
 	timestamp := flag.Bool("timestamp", true, "timestamp")
 	generateKeys := flag.Bool("generate_keys", false, "generate_keys")
 	flag.Parse()
+	keysFile := fmt.Sprintf("%s/%s", *path, *keysFileName)
 	var sigScheme = encryption.GetSignatureScheme(*clientSigScheme)
 	if *generateKeys {
 		err := sigScheme.GenerateKeys()
 		if err != nil {
 			panic(err)
 		}
-		if len(*keysFile) > 0 {
-			writer, err := os.OpenFile(*keysFile, os.O_RDWR|os.O_CREATE, 0644)
+		if len(keysFile) > 0 {
+			writer, err := os.OpenFile(keysFile, os.O_RDWR|os.O_CREATE, 0644)
 			if err != nil {
 				panic(err)
 			}
@@ -34,14 +36,14 @@ func main() {
 			sigScheme.WriteKeys(os.Stdout)
 		}
 	}
-	if len(*keysFile) == 0 {
+	if len(keysFile) == 0 {
 		return
 	}
-	reader, err := os.Open(*keysFile)
+	reader, err := os.Open(keysFile)
 	if err != nil {
 		panic(err)
 	}
-	_, publicKey, privateKey := encryption.ReadKeys(reader)
+	_, publicKey, _ := encryption.ReadKeys(reader)
 	pubKeyBytes, err := hex.DecodeString(publicKey)
 	if err != nil {
 		panic(err)
@@ -50,7 +52,6 @@ func main() {
 	reader.Close()
 	time := common.Now()
 	fmt.Printf("data: %v\n", *data)
-	fmt.Printf("keys file: %v\n", *keysFile)
 	fmt.Printf("public_key: %v\n", publicKey)
 	fmt.Printf("timestamp: %v\n", time)
 	fmt.Printf("client_id: %v\n", clientID)
@@ -63,7 +64,7 @@ func main() {
 	fmt.Printf("hashdata: %v", hashdata)
 	hash := encryption.Hash(hashdata)
 	fmt.Printf("hash: %v\n", hash)
-	sign, err := encryption.Sign(privateKey, hash)
+	sign, err := sigScheme.Sign(hash)
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 	} else {
