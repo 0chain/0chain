c04f1543cdec4045b8e2b370d66c9a4a3acc7615
Paul Merrill <pdm@pdm.me>
2019-02-11 14:07:52 -0800

    Docker: Remove all references to BLOCK_SIZE

--- docker.local/bin/build.miners.sh
+++ docker.local/bin/build.miners.sh
@@ -5,7 +5,7 @@ docker build -f docker.local/build.miner/Dockerfile . -t miner
 
 for i in $(seq 1 3);
 do
-  MINER=$i BLOCK_SIZE=5000 docker-compose -p miner$i -f docker.local/build.miner/docker-compose.yml build --force-rm
+  MINER=$i docker-compose -p miner$i -f docker.local/build.miner/docker-compose.yml build --force-rm
 done
 
 docker.local/bin/sync_clock.sh
