40281b0d756003b70f76f50581fd380d7821689b
Siva <siva@0chain.net>
2019-04-24 11:40:40 -0700

    Avoid updating start to finish metric when the time to finish is too long to be meaningful

--- code/go/0chain.net/chaincore/chain/protocol_block.go
+++ code/go/0chain.net/chaincore/chain/protocol_block.go
@@ -152,7 +152,9 @@ func (c *Chain) finalizeBlock(ctx context.Context, fb *block.Block, bsh BlockSta
 	if time.Since(ssFTs) < 20*time.Second {
 		SteadyStateFinalizationTimer.UpdateSince(ssFTs)
 	}
-	StartToFinalizeTimer.UpdateSince(fb.ToTime())
+	if time.Since(fb.ToTime()) < 100*time.Second {
+		StartToFinalizeTimer.UpdateSince(fb.ToTime())
+	}
 	ssFTs = time.Now()
 	c.UpdateChainInfo(fb)
 	c.SaveChanges(ctx, fb)
