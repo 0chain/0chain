05cdf16acfcb6afe0321c00b124bb8a7b35044da
jay-at-0chain <jay@0chain.net>
2019-05-29 14:06:41 -0700

    handy script to init and clean miner4
    Useful for non-genesis miner

---
+++ docker.local/bin/clean_miner4.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm docker.local/miner4/log/*
+rm -rf docker.local/miner4/data/redis/state/*
+rm -rf docker.local/miner4/data/redis/transactions/*
+rm -rf docker.local/miner4/data/rocksdb/*
---
+++ docker.local/bin/init_miner4.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir -p docker.local/miner4/data/redis/state
+mkdir -p docker.local/miner4/data/redis/transactions
+mkdir -p docker.local/miner4/data/rocksdb
+mkdir -p docker.local/miner4/log
+
