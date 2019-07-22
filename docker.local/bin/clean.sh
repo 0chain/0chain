a845d949eadf1d93ea799e68746c89d5e0802e7f
Siva <siva@0chain.net>
2019-07-22 11:44:11 -0700

    Updates to readme and providing log of actions done when running clean.sh

--- docker.local/bin/clean.sh
+++ docker.local/bin/clean.sh
@@ -2,16 +2,27 @@
 
 for i in $(seq 1 3)
 do
+  echo "deleting miner$i logs"
   rm -rf docker.local/miner$i/log/*
+  echo "deleting miner$i redis db"
   rm -rf docker.local/miner$i/data/redis/state/*
   rm -rf docker.local/miner$i/data/redis/transactions/*
+  echo "deleting miner$i rocksdb db"
   rm -rf docker.local/miner$i/data/rocksdb/*
 done
 
 for i in $(seq 1 3)
 do
+  echo "deleting sharder$i logs"
   rm -rf docker.local/sharder$i/log/*
+  echo "deleting sharder$i cassandra db"
   rm -rf docker.local/sharder$i/data/cassandra/*
-  rm -rf docker.local/sharder$i/data/blocks/*
+  echo "deleting sharder$i rocksdb db"
   rm -rf docker.local/sharder$i/data/rocksdb/*
+done
+
+for i in $(seq 1 3)
+do
+  echo "deleting sharder$i blocks on the file system"
+  rm -rf docker.local/sharder$i/data/blocks/*
 done
