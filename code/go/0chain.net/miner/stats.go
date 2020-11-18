0cf88e7218d76b4d8d18bce9f9e23e19fb7dd9e2
Krishna <kupadhyaya@techracers.io>
2020-11-18 18:50:38 +0530

    Added current txn fee to miner stats API

--- code/go/0chain.net/miner/stats.go
+++ code/go/0chain.net/miner/stats.go
@@ -3,13 +3,14 @@ package miner
 import "time"
 
 type ExplorerStats struct {
-	BlockFinality      float64                  `json:"block_finality"`
-	LastFinalizedRound int64                    `json:"last_finalized_round"`
-	BlocksFinalized    int64                    `json:"blocks_finalized"`
-	StateHealth        int64                    `json:"state_health"`
-	CurrentRound       int64                    `json:"current_round"`
-	RoundTimeout       int64                    `json:"round_timeout"`
-	Timeouts           int64                    `json:"timeouts"`
-	AverageBlockSize   int                      `json:"average_block_size"`
-	NetworkTime        map[string]time.Duration `json:"network_times"`
+	BlockFinality         float64                  `json:"block_finality"`
+	LastFinalizedRound    int64                    `json:"last_finalized_round"`
+	BlocksFinalized       int64                    `json:"blocks_finalized"`
+	StateHealth           int64                    `json:"state_health"`
+	CurrentRound          int64                    `json:"current_round"`
+	RoundTimeout          int64                    `json:"round_timeout"`
+	Timeouts              int64                    `json:"timeouts"`
+	AverageBlockSize      int                      `json:"average_block_size"`
+	NetworkTime           map[string]time.Duration `json:"network_times"`
+	CurrentTransactionFee int64                    `json:"current_transaction_fee"`
 }
