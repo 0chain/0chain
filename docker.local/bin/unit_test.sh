63278094060e3076933c1e39b00382dd6444c07e
Kirill Taran <kirill@taran.space>
2020-12-19 18:29:30 +0100

    Automatic unit tests running

--- docker.local/bin/unit_test.sh
+++ docker.local/bin/unit_test.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 set -e
 
+# Allocate interactive TTY to allow Ctrl-C.
+INTERACTIVE="-it"
+PACKAGE=""
+
+if [[ "$1" == "--ci" ]]
+then
+    # But we need non-interactive mode for CI
+    INTERACTIVE=""
+else
+    PACKAGE="$1"
+fi
+
 docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
 
-# Allocate interactive TTY to allow Ctrl-C.
-if [[ -n "$1" ]]; then
+if [[ -n "$PACKAGE" ]]; then
     # Run tests from a single package.
-    # Assume that $1 looks something like: 0chain.net/chaincore/threshold/bls
-    docker run -it zchain_unit_test sh -c "cd $1; go test -tags bn256"
+    # Assume that $PACKAGE looks something like: 0chain.net/chaincore/threshold/bls
+    docker run $INTERACTIVE zchain_unit_test sh -c "cd $PACKAGE; go test -tags bn256"
 else
     # Run all tests.
-    docker run -it zchain_unit_test sh -c '
+    docker run $INTERACTIVE zchain_unit_test sh -c '
         for mod_file in $(find * -name go.mod); do
             mod_dir=$(dirname $mod_file)
             (cd $mod_dir; go test -tags bn256 $mod_dir/...)
