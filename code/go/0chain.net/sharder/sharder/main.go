2d56b72aed75609add16ceca14751b3d55479023
Krishna <kupadhyaya@techracers.io>
2021-01-15 18:09:54 +0530

    Allow blank path in non-genesis nodes

--- code/go/0chain.net/sharder/sharder/main.go
+++ code/go/0chain.net/sharder/sharder/main.go
@@ -329,7 +329,7 @@ func readNonGenesisHostAndPort(keysFile *string) (string, string, int, string, e
 
 	result = scanner.Scan()
 	if result == false {
-		return "", "", 0, "", errors.New("error reading path")
+		return h, n2nh, p, "", nil
 	}
 
 	path := scanner.Text()
