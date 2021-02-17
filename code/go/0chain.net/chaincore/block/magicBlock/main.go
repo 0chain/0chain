412dbb124a0424b0c521c9cc0fa47b3b326cda52
YurGas <gasparyan.yur@gmail.com>
2021-02-17 20:22:48 +0300

    Improved code of chaincore/magic-block

--- code/go/0chain.net/chaincore/block/magicBlock/main.go
+++ code/go/0chain.net/chaincore/block/magicBlock/main.go
@@ -1,131 +1,130 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "gopkg.in/yaml.v2"
-    "io/ioutil"
-    "log"
-    "flag"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
 
-    "0chain.net/chaincore/block"
-    "0chain.net/chaincore/node"
-    "0chain.net/chaincore/threshold/bls"
-    "0chain.net/core/common"
+	"0chain.net/chaincore/block"
+	"0chain.net/chaincore/node"
+	"0chain.net/chaincore/threshold/bls"
 )
 
+type cmdMagicBlock struct {
+	// magic block instance
+	block *block.MagicBlock
+	// yaml config instance
+	yml *configYaml
+	// dkgs collection
+	dkgs map[string]*bls.DKG
+}
+
+func new() *cmdMagicBlock {
+	return &cmdMagicBlock{dkgs: map[string]*bls.DKG{}}
+}
+
+// setupYaml method initalizes a configuration file based on yaml
+func (cmd *cmdMagicBlock) setupYaml(config string) error {
+	c := newYaml()
+	fPath := fmt.Sprintf("/0chain/go/0chain.net/docker.local/config/%v.yaml", config)
+	if err := c.readYaml(fPath); err != nil {
+		return err
+	}
+	return nil
+}
+
+// setupBlock method creates a new blank magic block and then fill
+// all data from yaml config file
+func (cmd *cmdMagicBlock) setupBlock() {
+	mb := block.NewMagicBlock()
+	mb.Miners = node.NewPool(node.NodeTypeMiner)
+	mb.Sharders = node.NewPool(node.NodeTypeSharder)
+
+	mb.MagicBlockNumber = cmd.yml.MagicBlockNumber
+	mb.StartingRound = cmd.yml.StartingRound
+	mb.N = len(cmd.yml.Miners)
+	mb.T = int(float64(mb.N) * (float64(cmd.yml.TPercent) / 100.0))
+	mb.K = int(float64(mb.N) * (float64(cmd.yml.KPercent) / 100.0))
+	cmd.block = mb
+}
+
+// setupMPKS setups
+func (cmd *cmdMagicBlock) setupMPKS() {
+	cmd.block.Mpks = block.NewMpks()
+	for id := range cmd.block.Miners.NodesMap {
+		cmd.dkgs[id] = bls.MakeDKG(cmd.block.T, cmd.block.N, id)
+		mpk := &block.MPK{ID: id}
+		for _, v := range cmd.dkgs[id].Mpk {
+			mpk.Mpk = append(mpk.Mpk, v.SerializeToHexStr())
+		}
+		cmd.block.Mpks.Mpks[id] = mpk
+	}
+}
+
+// createShareOrSigns method add new group of share or sign
+func (cmd *cmdMagicBlock) createShareOrSigns() {
+	cmd.block.ShareOrSigns = block.NewGroupSharesOrSigns()
+	for mid, _ := range cmd.block.Miners.NodesMap {
+		ss := block.NewShareOrSigns()
+		ss.ID = mid
+		var privateKey bls.Key
+		privateKey.SetHexString(cmd.yml.MinersMap[mid].PrivateKey)
+		for id := range cmd.block.Miners.NodesMap {
+			otherPartyId := bls.ComputeIDdkg(id)
+			share, _ := cmd.dkgs[mid].ComputeDKGKeyShare(otherPartyId)
+			ss.ShareOrSigns[id] = &bls.DKGKeyShare{
+				Message: cmd.yml.Message,
+				Share:   share.GetHexString(),
+				Sign:    privateKey.Sign(cmd.yml.Message).SerializeToHexStr()}
+		}
+		cmd.block.ShareOrSigns.Shares[mid] = ss
+	}
+}
+
+// setupBlockHash generates a new hash for a block
+func (cmd *cmdMagicBlock) setupBlockHash() {
+	cmd.block.Hash = cmd.block.GetHash()
+}
+
+// jsonMB method return indented json of block
+func (cmd *cmdMagicBlock) jsonMB() ([]byte, error) {
+	return json.MarshalIndent(cmd.block, "", " ")
+}
+
+// saveBlock method saves a magic block on file storage
+func (cmd *cmdMagicBlock) saveBlock() error {
+	file, err := json.MarshalIndent(cmd.block, "", " ")
+	if err != nil {
+		return err
+	}
+	path := fmt.Sprintf("/0chain/go/0chain.net/docker.local/config/%v.json", cmd.yml.MagicBlockFilename)
+	if err := ioutil.WriteFile(path, file, 0644); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
-    magicBlockConfig := flag.String("config_file", "", "config_file")
-    flag.Parse()
-    if *magicBlockConfig != "" {
-        c := NewConfigYaml()
-        mb := block.NewMagicBlock()
-        dkgs := make(map[string]*bls.DKG)
-        err := c.readYaml(fmt.Sprintf("/0chain/go/0chain.net/docker.local/config/%v.yaml",*magicBlockConfig))
-        if err == nil {
-            setupMagicBlock(mb, c)
-            setupNodes(mb, c)
-            createMPKS(mb, dkgs)
-            createShareOrSigns(mb, dkgs, c)
+	magicBlockConfig := flag.String("config_file", "", "config_file")
+	flag.Parse()
 
-            mb.Hash = mb.GetHash()
-            file, _ := json.MarshalIndent(mb, "", " ")
-            err := ioutil.WriteFile(fmt.Sprintf("/0chain/go/0chain.net/docker.local/config/%v.json", c.MagicBlockFilename), file, 0644)
-            if err != nil {
-                log.Printf("Error writing json file: %v\n", err.Error())
-            }
-        } else {
-            log.Printf("Failed to read configuration file (%v) for magicBlock. Error: %v\n", *magicBlockConfig, err)
-        }
-    } else {
-        log.Println("Failed to find configuration file for magicBlock")
-    }
-}
+	cmd := new()
+	if err := cmd.setupYaml(*magicBlockConfig); err != nil {
+		log.Printf("Failed to read configuration file (%v) for magicBlock. Error: %v\n", *magicBlockConfig, err)
+		return
+	}
 
-func setupMagicBlock(mb *block.MagicBlock, c *configYaml) {
-    mb.Miners = node.NewPool(node.NodeTypeMiner)
-    mb.Sharders = node.NewPool(node.NodeTypeSharder)
+	cmd.setupBlock()
+	cmd.setupMPKS()
+	cmd.createShareOrSigns()
 
-    mb.MagicBlockNumber = c.MagicBlockNumber
-    mb.StartingRound = c.StartingRound
-    mb.N = len(c.Miners)
-    mb.T = int(float64(mb.N) * (float64(c.TPercent) / 100.0))
-    mb.K = int(float64(mb.N) * (float64(c.KPercent) / 100.0))
-}
+	cmd.setupBlockHash()
 
-func setupNodes(mb *block.MagicBlock, c *configYaml) {
-    for _, v := range c.Miners {
-        c.MinersMap[v.ID] = v
-        v.CreationDate = common.Now()
-        v.Type = mb.Miners.Type
-        mb.Miners.AddNode(&v.Node)
-    }
-    for _, v := range c.Sharders {
-        c.ShardersMap[v.ID] = v
-        v.CreationDate = common.Now()
-        v.Type = mb.Sharders.Type
-        mb.Sharders.AddNode(&v.Node)
-    }
-}
-
-func createMPKS(mb *block.MagicBlock, dkgs map[string]*bls.DKG) {
-    mb.Mpks = block.NewMpks()
-    for id := range mb.Miners.NodesMap {
-        dkgs[id] = bls.MakeDKG(mb.T, mb.N, id)
-        mpk := &block.MPK{ID: id}
-        for _, v := range dkgs[id].Mpk {
-            mpk.Mpk = append(mpk.Mpk, v.SerializeToHexStr())
-        }
-        mb.Mpks.Mpks[id] = mpk
-    }
-}
-
-func createShareOrSigns(mb *block.MagicBlock, dkgs map[string]*bls.DKG, c *configYaml) {
-    mb.ShareOrSigns = block.NewGroupSharesOrSigns()
-    for mid, _ := range mb.Miners.NodesMap {
-        ss := block.NewShareOrSigns()
-        ss.ID = mid
-        var privateKey bls.Key
-        privateKey.SetHexString(c.MinersMap[mid].PrivateKey)
-        for id := range mb.Miners.NodesMap {
-            otherPartyId := bls.ComputeIDdkg(id)
-            share, _ := dkgs[mid].ComputeDKGKeyShare(otherPartyId)
-            ss.ShareOrSigns[id] = &bls.DKGKeyShare{Message: c.Message, Share: share.GetHexString(), Sign: privateKey.Sign(c.Message).SerializeToHexStr()}
-        }
-        mb.ShareOrSigns.Shares[mid] = ss
-    }
-}
-
-type configYaml struct {
-    Miners             []*yamlNode `yaml:"miners"`
-    MinersMap          map[string]*yamlNode
-    Sharders           []*yamlNode `yaml:"sharders"`
-    ShardersMap        map[string]*yamlNode
-    Message            string `yaml:"message"`
-    MagicBlockNumber   int64  `yaml:"magic_block_number"`
-    StartingRound      int64  `yaml:"starting_round"`
-    TPercent           int    `yaml:"t_percent"`
-    KPercent           int    `yaml:"k_percent"`
-    MagicBlockFilename string `yaml:"magic_block_filename"`
-}
-
-func NewConfigYaml() *configYaml {
-    return &configYaml{MinersMap: make(map[string]*yamlNode), ShardersMap: make(map[string]*yamlNode)}
-}
-
-func (c *configYaml) readYaml(file string) error {
-    yamlFile, err := ioutil.ReadFile(file)
-    if err != nil {
-        return err
-    }
-    err = yaml.Unmarshal(yamlFile, c)
-    if err != nil {
-        return err
-    }
-    return nil
-}
-
-type yamlNode struct {
-    node.Node        `yaml:",inline"`
-    PrivateKey string `yaml:"private_key"`
+	if err := cmd.saveBlock(); err != nil {
+		log.Printf("Error writing json file: %v\n", err.Error())
+		return
+	}
+	log.Printf("Success: Magic block already created")
 }
---
+++ code/go/0chain.net/chaincore/block/magicBlock/yaml.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+
+	"0chain.net/chaincore/node"
+)
+
+type yamlNode struct {
+	node.Node  `yaml:",inline"`
+	PrivateKey string `yaml:"private_key"`
+}
+
+// yaml config file structure
+type configYaml struct {
+	Miners             []*yamlNode `yaml:"miners"`
+	MinersMap          map[string]*yamlNode
+	Sharders           []*yamlNode `yaml:"sharders"`
+	ShardersMap        map[string]*yamlNode
+	Message            string `yaml:"message"`
+	MagicBlockNumber   int64  `yaml:"magic_block_number"`
+	StartingRound      int64  `yaml:"starting_round"`
+	TPercent           int    `yaml:"t_percent"`
+	KPercent           int    `yaml:"k_percent"`
+	MagicBlockFilename string `yaml:"magic_block_filename"`
+}
+
+func newYaml() *configYaml {
+	return &configYaml{MinersMap: make(map[string]*yamlNode), ShardersMap: make(map[string]*yamlNode)}
+}
+
+func (c *configYaml) readYaml(file string) error {
+	yamlFile, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		return err
+	}
+	return nil
+}
