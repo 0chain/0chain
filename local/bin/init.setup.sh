fd5039d863fbbf2424befad431e3cdadb912dc94
piers <piers.shepperson@btinternet.com>
2021-02-25 05:43:30 +0000

    Remove unnecessary config file copies.

--- local/bin/init.setup.sh
+++ local/bin/init.setup.sh
@@ -5,8 +5,6 @@ mkdir -p miner/log
 mkdir -p miner/config
 cp config/0chain.yaml miner/config/0chain.yaml
 cp config/sc.yaml miner/config/sc.yaml
-cp config/b0mnode1_keys.txt miner/config/b0mnode1_keys.txt
-cp config/n2n_delay.yaml miner/config/n2n_delay.yaml
 cp config/b0magicBlock_2_miners_1_sharder.json miner/config/b0magicBlock_2_miners_1_sharder.json
 
 mkdir -p sharder/data/blocks
