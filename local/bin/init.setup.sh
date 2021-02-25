6c290c43061f64ee4816379065be9be3c2a92e40
piers <piers.shepperson@btinternet.com>
2021-02-25 03:27:12 +0000

    only one miner and sharder

--- local/bin/init.setup.sh
+++ local/bin/init.setup.sh
@@ -1,24 +1,18 @@
 #!/bin/sh
 
-for i in $(seq 1 8)
-do
-  mkdir -p local/miner$i/data/rocksdb
-  mkdir -p local/miner$i/log
-  mkdir -p local/miner$i/config
-  cp local/config/0chain.yaml local/miner$i/config/0chain.yaml
-  cp local/config/sc.yaml local/miner$i/config/sc.yaml
-  cp local/config/b0mnode1_keys.txt local/miner$i/config/b0mnode1_keys.txt
-  cp local/config/n2n_delay.yaml local/miner$i/config/n2n_delay.yaml
-  cp local/config/b0magicBlock_2_miners_1_sharder.json local/miner$i/config/b0magicBlock_2_miners_1_sharder.json
-done
+mkdir -p miner/data/rocksdb
+mkdir -p miner/log
+mkdir -p miner/config
+cp config/0chain.yaml miner/config/0chain.yaml
+cp config/sc.yaml miner/config/sc.yaml
+cp config/b0mnode1_keys.txt miner/config/b0mnode1_keys.txt
+cp config/n2n_delay.yaml miner/config/n2n_delay.yaml
+cp config/b0magicBlock_2_miners_1_sharder.json miner/config/b0magicBlock_2_miners_1_sharder.json
 
-for i in $(seq 1 3)
-do
-  mkdir -p local/sharder$i/data/blocks
-  mkdir -p local/sharder$i/data/rocksdb
-  mkdir -p local/sharder$i/log
-  mkdir -p local/sharder$i/config
-  cp local/config/0chain.yaml local/sharder$i/config/0chain.yaml
-  cp local/config/sc.yaml local/sharder$i/config/sc.yaml
-  cp local/config/b0magicBlock_2_miners_1_sharder.json local/sharder$i/config/b0magicBlock_2_miners_1_sharder.json
-done
+mkdir -p sharder/data/blocks
+mkdir -p sharder/data/rocksdb
+mkdir -p sharder/log
+mkdir -p sharder/config
+cp config/0chain.yaml sharder/config/0chain.yaml
+cp config/sc.yaml sharder/config/sc.yaml
+cp config/b0magicBlock_2_miners_1_sharder.json sharder/config/b0magicBlock_2_miners_1_sharder.json
