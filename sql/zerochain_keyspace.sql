ab3a220cd405512834a90badc97780b06ec0a79f
Piers Shepperson <piers.shepperson@btinternet.com>
2021-03-02 15:13:59 +0000

    New 3 1 gen magic block

--- sql/zerochain_keyspace.sql
+++ sql/zerochain_keyspace.sql
@@ -1,3 +1,5 @@
+-- noinspection SqlDialectInspectionForFile
+
 CREATE  KEYSPACE IF NOT EXISTS zerochain
 WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }
 AND DURABLE_WRITES = true;
