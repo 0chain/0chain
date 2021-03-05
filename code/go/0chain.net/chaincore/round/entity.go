dc348fa64ad60320ffafc1ee53aff718de7a91be
Piers Shepperson <piers.shepperson@btinternet.com>
2021-03-05 11:27:41 +0000

    Add cap to yaml

--- code/go/0chain.net/chaincore/round/entity.go
+++ code/go/0chain.net/chaincore/round/entity.go
@@ -3,6 +3,7 @@ package round
 import (
 	"context"
 	"fmt"
+	"github.com/spf13/viper"
 	"math/rand"
 	"os"
 	"runtime/pprof"
@@ -90,7 +91,8 @@ func (tc *timeoutCounter) IncrementTimeoutCount(prrs int64, miners *node.Pool) {
 }
 
 func (tc *timeoutCounter) checkCap() {
-	if tc.count > timeoutCap {
+	timeoutCap := viper.GetInt("server_chain.round_timeouts.timeout_cap")
+	if timeoutCap > 0 && tc.count > timeoutCap {
 		tc.count = timeoutCap
 	}
 }
