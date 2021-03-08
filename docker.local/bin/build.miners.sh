bfa8e307608f1bb374d43609ae7da236e4850f87
Kirill Taran <kirill@taran.space>
2021-03-08 10:09:27 +0100

    Double-quoting for path-like variables in Bash

--- docker.local/bin/build.miners.sh
+++ docker.local/bin/build.miners.sh
@@ -13,28 +13,28 @@ if [[ "$@" == *"--dev"* ]]
 then
     echo -e "\nDevelopment mode: building miner locally\n"
 
-    cd $ROOT/code/go/0chain.net/miner/miner
+    cd "$ROOT/code/go/0chain.net/miner/miner"
     go build -v -tags "bn256 development" \
         -ldflags "-X 0chain.net/core/build.BuildTag=$GIT_COMMIT"
 
-    sed 's,%COPY%,COPY ./code,g' $DOCKERFILE.template > $DOCKERFILE
+    sed 's,%COPY%,COPY ./code,g' "$DOCKERFILE.template" > "$DOCKERFILE"
 
-    cd $ROOT
+    cd "$ROOT"
     docker build --build-arg GIT_COMMIT=$GIT_COMMIT \
-        -f $DOCKERFILE . -t miner --build-arg DEV=yes
+        -f "$DOCKERFILE" . -t miner --build-arg DEV=yes
 else
     echo -e "\nProduction mode: building miner in Docker\n"
 
-    sed 's,%COPY%,COPY --from=miner_build $APP_DIR,g' $DOCKERFILE.template > $DOCKERFILE
+    sed 's,%COPY%,COPY --from=miner_build $APP_DIR,g' "$DOCKERFILE.template" > "$DOCKERFILE"
 
-    cd $ROOT
+    cd "$ROOT"
     docker build --build-arg GIT_COMMIT=$GIT_COMMIT \
-        -f $DOCKERFILE . -t miner --build-arg DEV=no
+        -f "$DOCKERFILE" . -t miner --build-arg DEV=no
 fi
 
 for i in $(seq 1 5);
 do
-    MINER=$i docker-compose -p miner$i -f $DOCKERCOMPOSE build --force-rm
+    MINER=$i docker-compose -p miner$i -f "$DOCKERCOMPOSE" build --force-rm
 done
 
-$ROOT/docker.local/bin/sync_clock.sh
+"$ROOT"/docker.local/bin/sync_clock.sh
