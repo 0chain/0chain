bd293e2ebf52b0837c8d8fc563e1017f03f1f29f
Piers Shepperson <piers.shepperson@btinternet.com>
2021-03-21 10:41:32 +0000

    added write test

--- code/go/0chain.net/smartcontract/storagesc/formulae_test.go
+++ code/go/0chain.net/smartcontract/storagesc/formulae_test.go
@@ -6,28 +6,40 @@ import (
 )
 
 type formulae struct {
-	blobber    mock0ChainBlobberYml
-	sc         scConfig
-	ar         newAllocationRequest
-	readMarker ReadMarker
+	blobber     mock0ChainBlobberYml
+	sc          scConfig
+	ar          newAllocationRequest
+	readMarker  ReadMarker
+	writeMarker WriteMarker
+}
+
+// write formulae
+//
+
+func (f formulae) ChallangePoolBalance() (cpBalance state.Balance) {
+	var size = sizeInGB(f.writeMarker.Size)
+	var price = float64(convertZcnToValue(f.blobber.WritePrice))
+	var duration = f.allocRestOfDurationInTUs(f.writeMarker.Timestamp)
+	return state.Balance(size * price * duration)
+
 }
 
 // read formulae
 //
 
-func (f *formulae) RmRewardsCharge() (blobberCharge state.Balance) {
+func (f formulae) RmRewardsCharge() (blobberCharge state.Balance) {
 	return state.Balance(f.blobber.ServiceCharge * float64(f.RmValue()))
 }
 
-func (f *formulae) RmRewardsBlobber() (blobberCharge state.Balance) {
+func (f formulae) RmRewardsBlobber() (blobberCharge state.Balance) {
 	return state.Balance((1 - f.blobber.ServiceCharge) * float64(f.RmValue()))
 }
 
-func (f *formulae) RmRewardsValidator() (validatorCharge state.Balance) {
+func (f formulae) RmRewardsValidator() (validatorCharge state.Balance) {
 	return 0 // todo implement validators
 }
 
-func (f *formulae) RmValue() (value state.Balance) {
+func (f formulae) RmValue() (value state.Balance) {
 	rp := float64(convertZcnToValue(f.blobber.ReadPrice))
 	return state.Balance(sizeInGB(f.readMarker.ReadCounter*CHUNK_SIZE) * rp)
 }
@@ -35,36 +47,31 @@ func (f *formulae) RmValue() (value state.Balance) {
 // Allocation formulae
 //
 
-func (f *formulae) AllocRestMinLockDemandTotal2(value state.Balance, now common.Timestamp) state.Balance {
+func (f formulae) AllocRestMinLockDemandTotal2(value state.Balance, now common.Timestamp) state.Balance {
 	return f.AllocRestMinLockDemandTotal(now) - value
 }
 
-func (f *formulae) AllocRestMinLockDemandTotal(now common.Timestamp) state.Balance {
-	var gbSize = f.allocPerBlobber()
-	var mlockShard = f.allocLockDemandPerBlobber(gbSize, now)
-	a := f.ar.DataShards
-	a = a
-	b := f.ar.ParityShards
-	b = b
-	c := state.Balance(f.ar.DataShards+f.ar.ParityShards) * mlockShard
-	c = c
-	return state.Balance(f.ar.DataShards+f.ar.ParityShards) * mlockShard
+func (f formulae) AllocRestMinLockDemandTotal(now common.Timestamp) state.Balance {
+	var lockPerBlobber = f.allocLockDemandPerBlobber(f.allocPerBlobber(), now)
+	return state.Balance(f.ar.DataShards+f.ar.ParityShards) * lockPerBlobber
 }
 
-func (f *formulae) allocPerBlobber() (restGB float64) {
+func (f formulae) allocPerBlobber() (restGB float64) {
 	var shards = int64(f.ar.DataShards + f.ar.ParityShards)
 	var bsize = (f.ar.Size + shards - 1) / shards
 	return sizeInGB(bsize)
 }
 
-func (f *formulae) allocLockDemandPerBlobber(gbSize float64, now common.Timestamp) (mdl state.Balance) {
+func (f formulae) allocLockDemandPerBlobber(gbSize float64, now common.Timestamp) (mdl state.Balance) {
 	var writePrice = float64(convertZcnToValue(f.blobber.WritePrice))
-	var remaining = f.allocDurationInTimeUnits(now)
+	var remaining = f.allocRestOfDurationInTUs(now)
 	return state.Balance(writePrice * gbSize * remaining * f.blobber.MinLockDemand)
 }
 
-// time left before expiration in fractions of a time unit.
-func (f *formulae) allocDurationInTimeUnits(now common.Timestamp) (rdtu float64) {
-	rdtu = float64((f.ar.Expiration - now).Duration()) / float64(f.sc.TimeUnit)
-	return
+func (f *formulae) allocRestOfDurationInTUs(now common.Timestamp) (rdtu float64) {
+	return f.toTimeUnits(f.ar.Expiration - now)
+}
+
+func (f *formulae) toTimeUnits(duration common.Timestamp) (dtu float64) {
+	return float64(duration.Duration()) / float64(f.sc.TimeUnit)
 }
