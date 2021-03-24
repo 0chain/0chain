0458a8cdfda1ec86defc2202fe2b91a2b4fdac20
Piers Shepperson <piers.shepperson@btinternet.com>
2021-03-24 14:35:26 +0000

    debug test

--- docker.local/bin/linter.sh
+++ docker.local/bin/linter.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-docker build -f docker.local/build.lint_test/Dockerfile . -t zchain_unit_test
+docker build -f docker.local/build.lint_test/Dockerfile . -t zchain_lint_test
 
-docker run zchain_unit_test sh -c '
+docker run zchain_lint_test sh -c '
     for mod_file in $(find * -name go.mod); do
         mod_dir=$(dirname $mod_file)
         (cd $mod_dir; go mod download; golangci-lint run)
