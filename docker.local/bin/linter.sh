155eaebdaa0eeb7f63e79bde354f79e7520b3aca
Piers Shepperson <piers.shepperson@btinternet.com>
2021-03-24 08:55:46 +0000

    add lint dockerfile

--- docker.local/bin/linter.sh
+++ docker.local/bin/linter.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
+docker build -f docker.local/build.lint_test/Dockerfile . -t zchain_unit_test
 
 docker run zchain_unit_test sh -c '
     for mod_file in $(find * -name go.mod); do
