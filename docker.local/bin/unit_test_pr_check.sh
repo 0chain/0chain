f2bff9010e2f27d0ff040135470fc6891abbd995
Piers Shepperson <piers.shepperson@btinternet.com>
2021-03-24 14:49:31 +0000

    debug test

--- docker.local/bin/unit_test_pr_check.sh
+++ docker.local/bin/unit_test_pr_check.sh
@@ -3,10 +3,10 @@ set -e
 
 docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
 
-test=docker run zchain_unit_test sh -c "'
+docker run zchain_unit_test sh -c "'
     for mod_file in $(find * -name go.mod); do
         mod_dir=$(dirname $mod_file)
         (cd $mod_dir; go test -tags bn256 $mod_dir/...)
     done
     '"
-echo "finished all unit tests returned" $test
+echo "finished all unit tests"
