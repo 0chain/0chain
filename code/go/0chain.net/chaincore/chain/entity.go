bd7f538579b1ad36b9d2ac0fafb8e539306ea095
peterlimg <peterlimg@protonmail.com>
2021-03-24 16:58:57 +1100

    Call method to set chain's current round
    Instead of assigning to the member variable directly

--- code/go/0chain.net/chaincore/chain/entity.go
+++ code/go/0chain.net/chaincore/chain/entity.go
@@ -446,7 +446,7 @@ func Provider() datastore.Entity {
 
 /*Initialize - intializes internal datastructures to start again */
 func (c *Chain) Initialize() {
-	c.CurrentRound = 0
+	c.setCurrentRound(0)
 	c.SetLatestFinalizedBlock(nil)
 	c.BlocksToSharder = 1
 	c.VerificationTicketsTo = AllMiners
@@ -986,7 +986,7 @@ func (c *Chain) SetRandomSeed(r round.RoundI, randomSeed int64) bool {
 	r.SetRandomSeed(randomSeed, c.GetMiners(r.GetRoundNumber()).Size())
 	roundNumber := r.GetRoundNumber()
 	if roundNumber > c.CurrentRound {
-		c.CurrentRound = roundNumber
+		c.setCurrentRound(roundNumber)
 	}
 	return true
 }
@@ -1002,6 +1002,10 @@ func (c *Chain) GetCurrentRound() int64 {
 func (c *Chain) SetCurrentRound(round int64) {
 	c.roundsMutex.Lock()
 	defer c.roundsMutex.Unlock()
+	c.setCurrentRound(round)
+}
+
+func (c *Chain) setCurrentRound(round int64) {
 	c.CurrentRound = round
 }
 
