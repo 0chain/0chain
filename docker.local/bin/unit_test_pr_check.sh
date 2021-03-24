d379d4f042c687f8854467150a8e1048c6545b90
Piers Shepperson <piers.shepperson@btinternet.com>
2021-03-24 16:13:20 +0000

    force error

--- docker.local/bin/unit_test_pr_check.sh
+++ docker.local/bin/unit_test_pr_check.sh
@@ -2,10 +2,13 @@
 
 docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
 
+RC=0
 docker run zchain_unit_test sh -c '
     for mod_file in $(find * -name go.mod); do
         mod_dir=$(dirname $mod_file)
         (cd $mod_dir; go test -tags bn256 $mod_dir/...)
     done
     '
-echo "finished all unit tests"
+
+exit $RC
+echo "finished all unit tests exit " $RC
