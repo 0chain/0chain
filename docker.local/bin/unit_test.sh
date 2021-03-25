187b70dbb2b540a34b359adec92c874353893046
Kirill Taran <kirill@taran.space>
2021-03-26 00:51:29 +0100

    Merging general and SC unit tests scripts

--- docker.local/bin/unit_test.sh
+++ docker.local/bin/unit_test.sh
@@ -4,27 +4,70 @@ set -e
 # Allocate interactive TTY to allow Ctrl-C.
 INTERACTIVE="-it"
 PACKAGE=""
+IMAGE=""
 
-if [[ "$1" == "--ci" ]]
+
+GENERAL_IMAGE="zchain_unit_test"
+SC_IMAGE="zchain_sc_unit_test"
+
+if [[ "$@" == *"--ci"* ]]
 then
-    # But we need non-interactive mode for CI
+    # We need non-interactive mode for CI
     INTERACTIVE=""
+
+    echo "Building both general and SC test images"
+    docker build -f docker.local/build.sc_unit_test/Dockerfile . -t zchain_sc_unit_test
+    docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
 else
     PACKAGE="$1"
+
+    if [[ "$@" == *"--sc"* ]]
+    then
+        IMAGE="$SC_IMAGE"
+        echo "Building SC test image"
+        docker build -f docker.local/build.sc_unit_test/Dockerfile . -t $IMAGE
+    else
+        IMAGE="$GENERAL_IMAGE"
+        echo "Building general test image"
+        docker build -f docker.local/build.unit_test/Dockerfile . -t $IMAGE
+    fi
 fi
 
-docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
+GO_TEST="go test -v -cover -tags bn256"
 
 if [[ -n "$PACKAGE" ]]; then
     # Run tests from a single package.
-    # Assume that $PACKAGE looks something like: 0chain.net/chaincore/threshold/bls
-    docker run $INTERACTIVE zchain_unit_test sh -c "cd $PACKAGE; go test -tags bn256"
+
+    # assume that $PACKAGE looks something like: 0chain.net/chaincore/threshold/bls
+    echo "Running unit tests from $PACKAGE:"
+    docker run $INTERACTIVE $IMAGE sh -c "cd $PACKAGE; $GO_TEST"
 else
     # Run all tests.
-    docker run $INTERACTIVE zchain_unit_test sh -c '
-        for mod_file in $(find * -name go.mod); do
-            mod_dir=$(dirname $mod_file)
-            (cd $mod_dir; go test -tags bn256 $mod_dir/...)
-        done
-    '
+
+    # avoid expanding outside of Docker container
+    cd_mod='cd 0chain.net/$mod'
+    all_in_mod='0chain.net/$mod/...'
+
+    GENERAL_MODS="core \
+        chaincore chaincore/block/magicBlock \
+        conductor conductor/conductor \
+        smartcontract smartcontract/multisigsc/test \
+        miner miner/miner \
+        sharder sharder/sharder \
+    "
+
+    echo "Running general unit tests:"
+    docker run $INTERACTIVE $GENERAL_IMAGE sh -c "\
+        for mod in $GENERAL_MODS; do \
+            $cd_mod; \
+            $GO_TEST $all_in_mod; \
+            cd -; \
+        done \
+    "
+
+    echo "Running smart contract unit tests:"
+    docker run $INTERACTIVE $SC_IMAGE sh -c "\
+        cd 0chain.net/smartcontract/minersc; $GO_TEST; \ cd -;
+        cd 0chain.net/smartcontract/storagesc; $GO_TEST \ cd -;
+    "
 fi
