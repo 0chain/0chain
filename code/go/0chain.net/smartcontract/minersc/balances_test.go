a6c78518731837c71fb01aa6ef6c40798e63c682
peterlimg <peterlimg@protonmail.com>
2021-03-26 16:45:26 +1100

    Fix testBalances error
    - Add GetChainCurrentMagicBlock
    - Rename test files

--- code/go/0chain.net/smartcontract/minersc/balances_test.go
+++ code/go/0chain.net/smartcontract/minersc/balances_test.go
@@ -8,11 +8,9 @@ import (
 	"0chain.net/core/datastore"
 	"0chain.net/core/encryption"
 	"0chain.net/core/util"
+	"github.com/stretchr/testify/require"
+	"testing"
 )
-
-//
-// helper for tests implements chainState.StateContextI
-//
 
 type testBalances struct {
 	balances      map[datastore.Key]state.Balance
@@ -31,12 +29,60 @@ func newTestBalances() *testBalances {
 	}
 }
 
+func (tb *testBalances) zeroize() {
+	tb.balances = make(map[string]state.Balance)
+}
+
 func (tb *testBalances) setBalance(key datastore.Key, b state.Balance) {
 	tb.balances[key] = b
 }
 
 func (tb *testBalances) setLFMB(lfmb *block.Block) {
 	tb.lfmb = lfmb
+}
+
+func (tb *testBalances) requireAllBeZeros(t *testing.T) {
+	for id, value := range tb.balances {
+		if id == ADDRESS {
+			continue
+		}
+		require.Zerof(t, value, "%s has non-zero balance: %d", id, value)
+	}
+}
+
+func (tb *testBalances) requireSpecifiedBeEqual(t *testing.T,
+	clients []*Client, value state.Balance, message string) {
+
+	for _, client := range clients {
+		require.EqualValues(t, value, tb.balances[client.id], message)
+	}
+}
+
+func (tb *testBalances) requireTotalAmountBeEqual(t *testing.T,
+	expected state.Balance) {
+
+	var total state.Balance
+	for id, value := range tb.balances {
+		if id == ADDRESS {
+			continue
+		}
+		total += value
+	}
+
+	require.EqualValues(t, expected, total, "total amount of tokens is wrong")
+}
+
+func (tb *testBalances) requireNodeAndStakersSumUpTo(t *testing.T,
+	node *Client, stakers []*Client, expected state.Balance) {
+
+	var total state.Balance
+	for _, staker := range stakers {
+		total += tb.balances[staker.id]
+	}
+	total += tb.balances[node.id]
+
+	require.EqualValues(t, expected, total,
+		"total amount distributed among node and its stakers is wrong")
 }
 
 func (tb *testBalances) GetBlock() *block.Block {
@@ -109,11 +155,11 @@ func (tb *testBalances) InsertTrieNode(key datastore.Key,
 }
 
 func (tb *testBalances) AddTransfer(t *state.Transfer) error {
-	if t.ClientID != tb.txn.ClientID && t.ClientID != tb.txn.ToClientID {
+	if t.Sender != tb.txn.ClientID && t.Sender != tb.txn.ToClientID {
 		return state.ErrInvalidTransfer
 	}
-	tb.balances[t.ClientID] -= t.Amount
-	tb.balances[t.ToClientID] += t.Amount
+	tb.balances[t.Sender] -= t.Amount
+	tb.balances[t.Receiver] += t.Amount
 	tb.transfers = append(tb.transfers, t)
 	return nil
 }
@@ -122,6 +168,10 @@ func (tb *testBalances) AddMint(mint *state.Mint) error {
 	if mint.Minter != ADDRESS {
 		panic("invalid miner: " + mint.Minter)
 	}
-	tb.balances[mint.ToClientID] += mint.Amount // mint!
+	tb.balances[mint.Receiver] += mint.Amount // mint!
+	return nil
+}
+
+func (tb *testBalances) GetChainCurrentMagicBlock() *block.MagicBlock {
 	return nil
 }
