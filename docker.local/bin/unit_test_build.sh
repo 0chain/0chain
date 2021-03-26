3a3b34777be9d6ea8770496466d98c9c594fbac7
Piers Shepperson <piers.shepperson@btinternet.com>
2021-03-26 21:35:40 +0000

    add verbose

--- docker.local/bin/unit_test_build.sh
+++ docker.local/bin/unit_test_build.sh
@@ -6,7 +6,7 @@ docker run zchain_unit_test sh -c '
   echo running zchain_unit_test
   for mod_file in $(find * -name go.mod -maxdepth 2); do
       mod_dir=$(dirname $mod_file)
-      (cd $mod_dir; go test -tags bn256 $mod_dir/...)
+      (cd $mod_dir; go test -tags bn256 -v $mod_dir/...)
       if [ $? -ne 0 ]; then
         exit 1
       fi
