f05f0bfdc9ea337f72d542b801c8b40ccf692d8f
Piers Shepperson <piers.shepperson@btinternet.com>
2021-03-28 11:28:46 +0100

    change message

--- docker.local/bin/unit_test_build.sh
+++ docker.local/bin/unit_test_build.sh
@@ -7,7 +7,7 @@
 docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
 
 docker run zchain_unit_test sh -c '
-  echo running zchain_unit_test
+  echo running unit tests
   for mod_file in $(find * -name go.mod -maxdepth 2); do
       mod_dir=$(dirname $mod_file)
       (cd $mod_dir; go test -tags bn256 $mod_dir/...)
