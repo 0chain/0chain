2c76c2f2abc2b8a56dc57246247a1f86b96385ad
bbist <pro.bbist@gmail.com>
2021-04-05 18:48:26 +0545

    --dev build miner inside container; uses vendoring

--- docker.local/bin/build.miners.sh
+++ docker.local/bin/build.miners.sh
@@ -9,32 +9,37 @@ DOCKERDIR="$ROOT/docker.local/build.miner"
 DOCKERFILE="$DOCKERDIR/Dockerfile"
 DOCKERCOMPOSE="$DOCKERDIR/docker-compose.yml"
 
+APP_DIR="$ROOT/code/go/0chain.net/miner/miner"
+
 if [[ "$@" == *"--dev"* ]]
 then
-    echo -e "\nDevelopment mode: building miner locally\n"
+    cd $APP_DIR
+    echo "Building: --dev mode: vendoring dependencies"
+    rm -rf vendor
+    go mod vendor
+    # libzstd: start: to rebuild inside container
+    dstdir="vendor/github.com/valyala"
+    rm -r $dstdir/*
+    srcdir="$GOPATH/pkg/mod/github.com/valyala"
+    cp -r "$srcdir/$(ls $srcdir | tail -n1)" $dstdir/gozstd
+    chmod -R +w vendor
+    # libzstd: end
+    cd $ROOT
+fi
 
-    cd "$ROOT/code/go/0chain.net/miner/miner"
-    go build -v -tags "bn256 development" \
-        -ldflags "-X 0chain.net/core/build.BuildTag=$GIT_COMMIT"
+docker build --build-arg GIT_COMMIT=$GIT_COMMIT -f $DOCKERFILE -t miner .
 
-    sed 's,%COPY%,COPY ./code,g' "$DOCKERFILE.template" > "$DOCKERFILE"
-
-    cd "$ROOT"
-    docker build --build-arg GIT_COMMIT=$GIT_COMMIT \
-        -f "$DOCKERFILE" . -t miner --build-arg DEV=yes
-else
-    echo -e "\nProduction mode: building miner in Docker\n"
-
-    sed 's,%COPY%,COPY --from=miner_build $APP_DIR,g' "$DOCKERFILE.template" > "$DOCKERFILE"
-
-    cd "$ROOT"
-    docker build --build-arg GIT_COMMIT=$GIT_COMMIT \
-        -f "$DOCKERFILE" . -t miner --build-arg DEV=no
+if [[ "$@" == *"--dev"* ]]
+then
+    echo "Build complete: cleaning vendored dependencies"
+    cd $APP_DIR
+    rm -rf vendor
+    cd $ROOT
 fi
 
 for i in $(seq 1 5);
 do
-    MINER=$i docker-compose -p miner$i -f "$DOCKERCOMPOSE" build --force-rm
+  MINER=$i docker-compose -p miner$i -f $DOCKERCOMPOSE build --force-rm
 done
 
-"$ROOT"/docker.local/bin/sync_clock.sh
+docker.local/bin/sync_clock.sh
