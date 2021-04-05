6afeade201237987141334b5acf62d924367ea7e
Alex Guo <chessnut@outlook.com>
2021-04-05 23:41:08 +0000

    Enhance bls0chain scheme to handle MIRACL public key input.

--- code/go/0chain.net/core/encryption/bls0chain.go
+++ code/go/0chain.net/core/encryption/bls0chain.go
@@ -73,6 +73,29 @@ func (b0 *BLS0ChainScheme) ReadKeys(reader io.Reader) error {
 	return nil
 }
 
+// Converts public key 'pk' to format that the herumi/bls library likes.
+// It's possible to get a MIRACL PublicKey which is of much longer format
+// (See below example), as wallets are using MIRACL library not herumi lib.
+// If 'pk' is not in MIRACL format, we just return the original 'pk' then.
+//
+// This is an example of the raw public key we expect from MIRACL
+var miraclExamplePK = `0418a02c6bd223ae0dfda1d2f9a3c81726ab436ce5e9d17c531ff0a385a13a0b491bdfed3a85690775ee35c61678957aaba7b1a1899438829f1dc94248d87ed36817f6dfafec19bfa87bf791a4d694f43fec227ae6f5a867490e30328cac05eaff039ac7dfc3364e851ebd2631ea6f1685609fc66d50223cc696cb59ff2fee47ac`
+//
+// This is an example of the same MIRACL public key serialized with ToString().
+// pk ([1bdfed3a85690775ee35c61678957aaba7b1a1899438829f1dc94248d87ed368,18a02c6bd223ae0dfda1d2f9a3c81726ab436ce5e9d17c531ff0a385a13a0b49],[039ac7dfc3364e851ebd2631ea6f1685609fc66d50223cc696cb59ff2fee47ac,17f6dfafec19bfa87bf791a4d694f43fec227ae6f5a867490e30328cac05eaff])
+func MiraclToHerumiPK(pk string) string {
+	if len(pk) != len(miraclExamplePK) {
+		return pk
+	}
+	n1 := pk[2:66]
+	n2 := pk[66:(66+64)]
+	n3 := pk[(66+64):(66+64+64)]
+	n4 := pk[(66+64+64):(66+64+64+64)]
+	var p bls.PublicKey
+	p.SetHexString("1 " + n2 + " " + n1 + " " + n4 + " " + n3)
+	return p.SerializeToHexStr()
+}
+
 //WriteKeys - implement interface
 func (b0 *BLS0ChainScheme) WriteKeys(writer io.Writer) error {
 	publicKey := hex.EncodeToString(b0.publicKey)
@@ -86,6 +109,8 @@ func (b0 *BLS0ChainScheme) SetPublicKey(publicKey string) error {
 	if len(b0.privateKey) > 0 {
 		return errors.New("cannot set public key when there is a private key")
 	}
+
+	publicKey = MiraclToHerumiPK(publicKey)
 	publicKeyBytes, err := hex.DecodeString(publicKey)
 	if err != nil {
 		return err
