c7394648adda5d0f910e6bfeddf68e8666ba94f4
Tyrone <avanaur@gmail.com>
2021-04-06 20:27:31 +1000

    Conductor tests updates (#139)
    * Conductor update to fix issues with running miners, sharders, and blobber yamls
    Added tests for zbox `start-repair`
    
    * Updated conductor for view-change tests
    
    * Reenable view-change conductor tests
    
    * Added comment on the commented code

--- docker.local/bin/conductor/upload_no_tokens.sh
+++ docker.local/bin/conductor/upload_no_tokens.sh
@@ -8,5 +8,5 @@ head -c 52428800 < /dev/urandom > random.bin
 # upload it
 ./zboxcli/zbox --wallet testing.json upload \
     --allocation "$(cat ~/.zcn/allocation.txt)" \
-    --localpath=upload.bin \
+    --localpath=random.bin \
     --remotepath=/remote/upload.bin
