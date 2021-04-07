b6da19cbc9bf2a346e1363c34b9bfcb34a9165bc
Vlad Gorokhov <vlad@witchery.io>
2021-04-07 20:14:17 +0300

    Unite Test Script Fix

--- docker.local/bin/unit_test.sh
+++ docker.local/bin/unit_test.sh
@@ -4,11 +4,6 @@ set -e
 # Allocate interactive TTY to allow Ctrl-C.
 INTERACTIVE="-it"
 PACKAGE=""
-IMAGE=""
-
-
-GENERAL_IMAGE="zchain_unit_test"
-SC_IMAGE="zchain_sc_unit_test"
 
 if [[ "$@" == *"--ci"* ]]
 then
@@ -16,40 +11,22 @@ then
     INTERACTIVE=""
 
     echo "Building both general and SC test images"
-    docker build -f docker.local/build.sc_unit_test/Dockerfile . -t zchain_sc_unit_test
     docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
 else
     PACKAGE="$1"
 
-    if [[ "$@" == *"--sc"* ]]
-    then
-        IMAGE="$SC_IMAGE"
-        echo "Building SC test image"
-        docker build -f docker.local/build.sc_unit_test/Dockerfile . -t $IMAGE
-    else
-        IMAGE="$GENERAL_IMAGE"
-        echo "Building general test image"
-        docker build -f docker.local/build.unit_test/Dockerfile . -t $IMAGE
-    fi
+    echo "Building general test image"
+    docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
 fi
-
-GO_TEST="go test -v -cover -tags bn256"
 
 if [[ -n "$PACKAGE" ]]; then
     # Run tests from a single package.
 
     # assume that $PACKAGE looks something like: 0chain.net/chaincore/threshold/bls
     echo "Running unit tests from $PACKAGE:"
-    docker run $INTERACTIVE $IMAGE sh -c "cd /0chain/go/$PACKAGE; go test -tags bn256 -cover ./..."
+    docker run $INTERACTIVE zchain_unit_test sh -c "cd /0chain/go/$PACKAGE; go test -tags bn256 -cover ./..."
 else
     # Run all tests.
-
     echo "Running general unit tests:"
-    docker run $INTERACTIVE $GENERAL_IMAGE sh -c "cd 0chain.net; go test -tags bn256 -cover ./..."
-
-    echo "Running smart contract unit tests:"
-    docker run $INTERACTIVE $SC_IMAGE sh -c "\
-        cd 0chain.net/smartcontract/minersc; $GO_TEST; \ cd -;
-        cd 0chain.net/smartcontract/storagesc; $GO_TEST \ cd -;
-    "
+    docker run $INTERACTIVE zchain_unit_test sh -c "cd 0chain.net; go test -tags bn256 -cover ./..."
 fi
