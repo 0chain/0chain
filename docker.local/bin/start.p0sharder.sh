c6a2a3db57a3caaa1395c3a147c541922685e45a
anish-squareops <anish@squareops.com>
2021-04-09 10:35:58 +0000

    minor fixes

--- docker.local/bin/start.p0sharder.sh
+++ docker.local/bin/start.p0sharder.sh
@@ -7,4 +7,4 @@ SHARDER_ID=`echo $SHARDER_DIR | sed -e 's/.*\(.\)$/\1/'`
 
 echo Starting sharder$SHARDER_ID in daemon mode ...
 
-SHARDER=$SHARDER_ID docker-compose -p sharder$SHARDER_ID -f ../build.sharder/podocker-compose.yml up -d
+SHARDER=$SHARDER_ID docker-compose -p sharder$SHARDER_ID -f ../build.sharder/p0docker-compose.yml up -d
