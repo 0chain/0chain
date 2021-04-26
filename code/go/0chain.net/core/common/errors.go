155e168477f98e2852de4ba592140bbed888074c
murashovven <murashovven@gmail.com>
2021-04-26 11:51:51 +0300

    Move errors interface from smartcontract package to common

--- code/go/0chain.net/core/common/errors.go
+++ code/go/0chain.net/core/common/errors.go
@@ -1,10 +1,25 @@
 package common
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 )
 
-var ErrTemporaryFailure = NewError("temporary_failure", "Please retry again later")
+var (
+	ErrTemporaryFailure = NewError("temporary_failure", "Please retry again later")
+
+	// ErrNoResource represents error corresponds to http.StatusNotFound.
+	ErrNoResource = NewError("resource_not_found", "can't retrieve resource")
+
+	// ErrBadRequest represents error corresponds to http.StatusBadRequest.
+	ErrBadRequest = NewError("bad_request", "request is invalid")
+
+	// ErrInternal represents error corresponds to http.StatusInternalServerError.
+	ErrInternal = NewError("internal", "internal server error")
+
+	ErrDecoding = errors.New("decoding error")
+)
 
 /*Error type for a new application error */
 type Error struct {
@@ -29,4 +44,31 @@ func NewErrorf(code string, format string, args ...interface{}) *Error {
 /*InvalidRequest - create error messages that are needed when validating request input */
 func InvalidRequest(msg string) error {
 	return NewError("invalid_request", fmt.Sprintf("Invalid request (%v)", msg))
+}
+
+// WrapErrInternal wraps ErrInternal in new error with provided messages and returns resulted error.
+func WrapErrInternal(msgs ...string) error {
+	if len(msgs) == 0 {
+		return ErrNoResource
+	}
+
+	return fmt.Errorf("%w: %s", ErrInternal, strings.Join(msgs, ": "))
+}
+
+// WrapErrNoResource wraps ErrNoResource in new error with provided messages and returns resulted error.
+func WrapErrNoResource(msgs ...string) error {
+	if len(msgs) == 0 {
+		return ErrNoResource
+	}
+
+	return fmt.Errorf("%w: %s", ErrNoResource, strings.Join(msgs, ": "))
+}
+
+// WrapErrBadRequest wraps ErrBadRequest in new error with provided messages and returns resulted error.
+func WrapErrBadRequest(msgs ...string) error {
+	if len(msgs) == 0 {
+		return ErrBadRequest
+	}
+
+	return fmt.Errorf("%w: %s", ErrBadRequest, strings.Join(msgs, ": "))
 }
