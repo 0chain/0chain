12ae5699a184d0814dd223f52f39834ae36da622
Piers Shepperson <piers.shepperson@btinternet.com>
2021-05-05 20:08:41 +0100

    Test ling count 278

--- docker.local/bin/linter.sh
+++ docker.local/bin/linter.sh
@@ -5,7 +5,7 @@ docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
 
 #Set this to a value higher than the current number of linter errors. We should lower this number over time
 docker run zchain_unit_test bash -c '
-    current_lint_count=279
+    current_lint_count=278
     apk add curl
     curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.38.0
     (cd 0chain.net; golangci-lint run --build-tags bn256 --timeout 10m0s | tee lint_report.out)
