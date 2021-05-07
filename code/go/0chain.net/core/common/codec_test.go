d13a28fddaf1a262970aedbc9eb8c9387b0575fe
Piers Shepperson <piers.shepperson@btinternet.com>
2021-05-07 15:34:27 +0100

    remove race errors from codec_test.go

--- code/go/0chain.net/core/common/codec_test.go
+++ code/go/0chain.net/core/common/codec_test.go
@@ -7,28 +7,50 @@ import (
 
 type CodecTestStruct struct {
 	Numbers []int `json:"numbers" msgpack:"nums"`
+	mutex   sync.RWMutex
+}
+
+func (c *CodecTestStruct) getNumbers() []int {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+	return c.Numbers
+}
+
+func (c *CodecTestStruct) setNumbers(numbers []int) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.Numbers = numbers
+}
+
+func (c *CodecTestStruct) DoReadLock() {
+	c.mutex.RLock()
+}
+
+func (c *CodecTestStruct) DoReadUnlock() {
+	c.mutex.RUnlock()
 }
 
 func TestConcurrentCodec(t *testing.T) {
 	var o CodecTestStruct
 	var wg sync.WaitGroup
 	count := 0
-	for idx := 0; idx < 100; idx++ {
-		o.Numbers = append(o.Numbers, 1)
-		for i := 0; i < 100; i++ {
+	for idx := 0; idx < 10; idx++ {
+		o.setNumbers(append(o.getNumbers(), 1))
+		for i := 0; i < 10; i++ {
 			var mi = i
 			go func() {
 				wg.Add(1)
-				nums := o.Numbers
+				var nums = []int{}
+				nums = append(nums, o.getNumbers()...)
 				for j := 0; j < 1; j++ {
 					nums = append(nums, 100*mi+j)
 				}
-				o.Numbers = nums
+				o.setNumbers(nums)
 				wg.Done()
 			}()
 		}
-		for i := 0; i < 100; i++ {
-			encoded := ToMsgpack(o)
+		for i := 0; i < 10; i++ {
+			encoded := ToMsgpack(&o)
 			if encoded.Len() > 16 {
 				count++
 			}
