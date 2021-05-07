32a2f0c3c78dc0f5848a90546d6a7fba5b0484ba
murashovven <murashovven@gmail.com>
2021-05-07 13:54:34 +0300

    Add datastore unit tests

---
+++ code/go/0chain.net/core/datastore/codec_test.go
@@ -0,0 +1,280 @@
+package datastore_test
+
+import (
+	"0chain.net/chaincore/block"
+	"0chain.net/core/common"
+	"0chain.net/core/datastore"
+	"0chain.net/core/memorystore"
+	"bytes"
+	"encoding/json"
+	"github.com/vmihailenco/msgpack"
+	"io"
+	"reflect"
+	"testing"
+)
+
+func init() {
+	block.SetupEntity(memorystore.GetStorageProvider())
+}
+
+func TestToJSON(t *testing.T) {
+	t.Parallel()
+
+	b := block.NewBlock("", 1)
+
+	type args struct {
+		entity datastore.Entity
+	}
+	tests := []struct {
+		name string
+		args args
+		want *bytes.Buffer
+	}{
+		{
+			name: "Test_ToJSON_OK",
+			args: args{entity: b},
+			want: common.ToJSON(b),
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			if got := datastore.ToJSON(tt.args.entity); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ToJSON() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestWriteJSON(t *testing.T) {
+	t.Parallel()
+
+	b := block.NewBlock("", 1)
+	buf := new(bytes.Buffer)
+	if err := common.WriteJSON(buf, b); err != nil {
+		t.Error(err)
+	}
+
+	type args struct {
+		entity datastore.Entity
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantW   string
+		wantErr bool
+	}{
+		{
+			name:    "Test_WriteJSON_OK",
+			args:    args{entity: b},
+			wantW:   buf.String(),
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			w := &bytes.Buffer{}
+			err := datastore.WriteJSON(w, tt.args.entity)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("WriteJSON() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotW := w.String(); gotW != tt.wantW {
+				t.Errorf("WriteJSON() gotW = %v, want %v", gotW, tt.wantW)
+			}
+		})
+	}
+}
+
+func TestToMsgpack(t *testing.T) {
+	t.Parallel()
+
+	b := block.NewBlock("", 1)
+	buf := datastore.ToMsgpack(b)
+
+	type args struct {
+		entity datastore.Entity
+	}
+	tests := []struct {
+		name string
+		args args
+		want *bytes.Buffer
+	}{
+		{
+			name: "Test_ToMsgpack_OK",
+			args: args{entity: b},
+			want: buf,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			if got := datastore.ToMsgpack(tt.args.entity); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ToMsgpack() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFromJSON(t *testing.T) {
+	t.Parallel()
+
+	b := block.NewBlock("", 1)
+	byt, err := json.Marshal(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	type args struct {
+		data   interface{}
+		entity datastore.Entity
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantB   datastore.Entity
+		wantErr bool
+	}{
+		{
+			name:    "Test_FromJSON_OK",
+			args:    args{data: byt, entity: &block.Block{}},
+			wantB:   b,
+			wantErr: false,
+		},
+		{
+			name:    "Test_FromJSON_ERR",
+			args:    args{data: "}{", entity: &block.Block{}},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			if err := datastore.FromJSON(tt.args.data, tt.args.entity); (err != nil) != tt.wantErr {
+				t.Errorf("FromJSON() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if !tt.wantErr {
+				got := tt.args.entity.(*block.Block)
+				want := tt.wantB.(*block.Block)
+				if !reflect.DeepEqual(got.Hash, want.Hash) {
+					t.Errorf("FromJSON() got = %#v, want %#v", got.Hash, want.Hash)
+				}
+			}
+		})
+	}
+}
+
+func TestReadJSON(t *testing.T) {
+	t.Parallel()
+
+	b := block.NewBlock("", 1)
+	byt, err := json.Marshal(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	type args struct {
+		r      io.Reader
+		entity datastore.Entity
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+		wantB   datastore.Entity
+	}{
+		{
+			name:    "Test_ReadJSON_OK",
+			args:    args{r: bytes.NewBuffer(byt), entity: &block.Block{}},
+			wantErr: false,
+			wantB:   b,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			if err := datastore.ReadJSON(tt.args.r, tt.args.entity); (err != nil) != tt.wantErr {
+				t.Errorf("ReadJSON() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if !tt.wantErr {
+				got := tt.args.entity.(*block.Block)
+				want := tt.wantB.(*block.Block)
+				if !reflect.DeepEqual(got.Hash, want.Hash) {
+					t.Errorf("ReadJSON() got = %#v, want %#v", got.Hash, want.Hash)
+				}
+			}
+		})
+	}
+}
+
+func TestFromMsgpack(t *testing.T) {
+	t.Parallel()
+
+	b := block.NewBlock("", 1)
+	buf := bytes.Buffer{}
+	encoder := msgpack.NewEncoder(&buf)
+	encoder.UseJSONTag(true)
+	if err := encoder.Encode(b); err != nil {
+		t.Fatal(err)
+	}
+
+	type args struct {
+		data   interface{}
+		entity datastore.Entity
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+		wantB   datastore.Entity
+	}{
+		{
+			name: "Test_FromMsgpack_OK",
+			args: args{
+				data:   buf.Bytes(),
+				entity: &block.Block{},
+			},
+			wantErr: false,
+			wantB:   b,
+		},
+		{
+			name: "Test_FromMsgpack_ERR",
+			args: args{
+				data:   1,
+				entity: &block.Block{},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			if err := datastore.FromMsgpack(tt.args.data, tt.args.entity); (err != nil) != tt.wantErr {
+				t.Errorf("FromMsgpack() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if !tt.wantErr {
+				got := tt.args.entity.(*block.Block)
+				want := tt.wantB.(*block.Block)
+				if !reflect.DeepEqual(got.Hash, want.Hash) {
+					t.Errorf("FromMsgpack() got = %#v, want %#v", got.Hash, want.Hash)
+				}
+			}
+		})
+	}
+}
---
+++ code/go/0chain.net/core/datastore/handler_test.go
@@ -0,0 +1,330 @@
+package datastore_test
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+
+	"0chain.net/chaincore/block"
+	"0chain.net/chaincore/client"
+	"0chain.net/chaincore/node"
+	"0chain.net/core/datastore"
+	"0chain.net/core/encryption"
+	"0chain.net/core/memorystore"
+	mocks "0chain.net/mocks/core/datastore"
+)
+
+func init() {
+	block.SetupEntity(memorystore.GetStorageProvider())
+	client.SetClientSignatureScheme("ed25519")
+	log.SetOutput(bytes.NewBuffer(nil))
+}
+
+func TestToJSONEntityReqResponse(t *testing.T) {
+	t.Parallel()
+
+	handler := func(ctx context.Context, entity datastore.Entity) (interface{}, error) {
+		return nil, nil
+	}
+	em := makeTestEntityMetadataImpl()
+
+	type args struct {
+		handler        datastore.JSONEntityReqResponderF
+		entityMetadata datastore.EntityMetadata
+		r              *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+		want http.ResponseWriter
+	}{
+		{
+			name: "Test_ToJSONEntityReqResponse_CO_FALSE_OK",
+			args: func() args {
+				r := httptest.NewRequest(http.MethodGet, "/", nil)
+				r.Header.Add("Origin", "invalid origin")
+
+				return args{
+					handler: handler,
+					r:       r,
+				}
+			}(),
+			want: httptest.NewRecorder(),
+		},
+		{
+			name: "Test_ToJSONEntityReqResponse_Options_OK",
+			args: func() args {
+				r := httptest.NewRequest(http.MethodOptions, "/", nil)
+				r.Header.Add("Origin", "file://localhost:8080/")
+
+				return args{
+					handler: handler,
+					r:       r,
+				}
+			}(),
+			want: func() http.ResponseWriter {
+				w := httptest.NewRecorder()
+				w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+				w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Accept-Encoding")
+				w.Header().Set("Access-Control-Allow-Origin", "file://localhost:8080/")
+				return w
+			}(),
+		},
+		{
+			name: "Test_ToJSONEntityReqResponse_Not_Application_JSON_ERR",
+			args: func() args {
+				r := httptest.NewRequest(http.MethodGet, "/", nil)
+				r.Header.Add("Origin", "file://localhost:8080/")
+
+				return args{
+					handler: handler,
+					r:       r,
+				}
+			}(),
+			want: func() http.ResponseWriter {
+				w := httptest.NewRecorder()
+				w.Header().Set("Access-Control-Allow-Origin", "file://localhost:8080/")
+				http.Error(w, "Header Content-type=application/json not found", 400)
+				return w
+			}(),
+		},
+		{
+			name: "Test_ToJSONEntityReqResponse_Decoding_ERR",
+			args: func() args {
+				r := httptest.NewRequest(http.MethodGet, "/", bytes.NewBuffer(nil))
+				r.Header.Add("Origin", "file://localhost:8080/")
+				r.Header.Add("Content-type", "application/json")
+
+				return args{
+					handler:        handler,
+					entityMetadata: &em,
+					r:              r,
+				}
+			}(),
+			want: func() http.ResponseWriter {
+				w := httptest.NewRecorder()
+				w.Header().Set("Access-Control-Allow-Origin", "file://localhost:8080/")
+				http.Error(w, "Error decoding json", 500)
+				return w
+			}(),
+		},
+		{
+			name: "Test_ToJSONEntityReqResponse_OK",
+			args: func() args {
+				data := map[string]interface{}{
+					"key": "value",
+				}
+				buf := bytes.NewBuffer(nil)
+				if err := json.NewEncoder(buf).Encode(data); err != nil {
+					t.Fatal(err)
+				}
+
+				r := httptest.NewRequest(http.MethodGet, "/", buf)
+				r.Header.Add("Origin", "file://localhost:8080/")
+				r.Header.Add("Content-type", "application/json")
+
+				return args{
+					handler:        handler,
+					entityMetadata: &em,
+					r:              r,
+				}
+			}(),
+			want: func() http.ResponseWriter {
+				w := httptest.NewRecorder()
+				w.Header().Set("Access-Control-Allow-Origin", "file://localhost:8080/")
+				w.WriteHeader(http.StatusNoContent)
+				return w
+			}(),
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			w := httptest.NewRecorder()
+			handler := datastore.ToJSONEntityReqResponse(tt.args.handler, tt.args.entityMetadata)
+			handler(w, tt.args.r)
+
+			if !reflect.DeepEqual(w, tt.want) {
+				t.Errorf("ToJSONEntityReqResponse() = %#v, want %#v", w, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetEntityHandler(t *testing.T) {
+	e := mocks.Entity{}
+	e.On("Read", context.TODO(), mock.AnythingOfType("string")).Return(
+		func(ctx context.Context, _ datastore.Key) error {
+			return nil
+		},
+	)
+	e.On("Read", context.Context(nil), mock.AnythingOfType("string")).Return(
+		func(ctx context.Context, _ datastore.Key) error {
+			return errors.New("")
+		},
+	)
+
+	em := mocks.EntityMetadata{}
+	em.On("Instance").Return(
+		func() datastore.Entity {
+			return &e
+		},
+	)
+
+	type args struct {
+		ctx            context.Context
+		r              *http.Request
+		entityMetadata datastore.EntityMetadata
+		idparam        string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    interface{}
+		wantErr bool
+	}{
+		{
+			name: "Test_GetEntityHandler_Empty_ID_Param_ERR",
+			args: args{
+				r:       httptest.NewRequest(http.MethodGet, "/", nil),
+				idparam: "param",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Test_GetEntityHandler_Read_ERR",
+			args: func() args {
+				r := httptest.NewRequest(http.MethodGet, "/", nil)
+				id := "id"
+				q := r.URL.Query()
+				q.Add(id, "param")
+				r.URL.RawQuery = q.Encode()
+
+				return args{
+					r:              r,
+					entityMetadata: &em,
+					idparam:        id,
+				}
+			}(),
+			wantErr: true,
+		},
+		{
+			name: "Test_GetEntityHandler_OK",
+			args: func() args {
+				r := httptest.NewRequest(http.MethodGet, "/", nil)
+				id := "id"
+				q := r.URL.Query()
+				q.Add(id, "param")
+				r.URL.RawQuery = q.Encode()
+
+				return args{
+					ctx:            context.TODO(),
+					r:              r,
+					entityMetadata: &em,
+					idparam:        id,
+				}
+			}(),
+			want:    &e,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := datastore.GetEntityHandler(tt.args.ctx, tt.args.r, tt.args.entityMetadata, tt.args.idparam)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetEntityHandler() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetEntityHandler() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPutEntityHandler(t *testing.T) {
+	t.Parallel()
+
+	// Test_PutEntityHandler_Sync_Ctx_OK
+	ctx := context.TODO()
+	ch := make(chan datastore.QueuedEntity)
+	ctx = datastore.WithAsyncChannel(ctx, ch)
+
+	n := node.Node{}
+	n.ID = "123"
+	node.RegisterNode(&n)
+
+	b := block.NewBlock("", 1)
+	b.MinerID = n.ID
+	b.Hash = b.ComputeHash()
+	pbKey, prKey, err := encryption.GenerateKeys()
+	if err != nil {
+		t.Fatal(err)
+	}
+	b.Signature, err = encryption.Sign(prKey, b.Hash)
+	if err != nil {
+		t.Fatal(err)
+	}
+	n.PublicKey = pbKey
+
+	type args struct {
+		ctx    context.Context
+		object interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    interface{}
+		qeCh    chan datastore.QueuedEntity
+		wantErr bool
+	}{
+		{
+			name:    "Test_PutEntityHandler_Object_Is_Not_Entity_ERR",
+			args:    args{ctx: context.TODO(), object: "not an entity"},
+			wantErr: true,
+		},
+		{
+			name:    "Test_PutEntityHandler_Invalid_Entity_ERR",
+			args:    args{ctx: context.TODO(), object: block.NewBlock("", 1)},
+			wantErr: true,
+		},
+		{
+			name:    "Test_PutEntityHandler_Sync_Ctx_OK",
+			args:    args{ctx: ctx, object: b},
+			qeCh:    ch,
+			want:    b,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			if tt.qeCh != nil {
+				go func() {
+					<-tt.qeCh
+				}()
+			}
+
+			got, err := datastore.PutEntityHandler(tt.args.ctx, tt.args.object)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PutEntityHandler() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("PutEntityHandler() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
