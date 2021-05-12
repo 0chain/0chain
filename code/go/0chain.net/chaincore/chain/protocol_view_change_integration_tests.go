a5d741d66aa4029f8f80cde04232d26864d7cfb4
bbist <pro.bbist@gmail.com>
2021-05-12 16:29:54 +0545

    fixed integration test start_lock issue

---
+++ code/go/0chain.net/chaincore/chain/protocol_view_change_integration_tests.go
@@ -0,0 +1,66 @@
+// +build integration_tests
+
+package chain
+
+import (
+	"context"
+	"time"
+
+	crpc "0chain.net/conductor/conductrpc"
+	. "0chain.net/core/logging"
+	"go.uber.org/zap"
+)
+
+func (mc *Chain) SetupSC(ctx context.Context) {
+	Logger.Info("SetupSC start...")
+	tm := time.NewTicker(5 * time.Second)
+
+	state := crpc.Client().State()
+
+	for {
+		select {
+		case <-ctx.Done():
+			Logger.Debug("SetupSC is done")
+			return
+		case <-tm.C:
+			if state.IsLock {
+				continue
+			}
+
+			Logger.Debug("SetupSC - check if node is registered")
+			isRegisteredC := make(chan bool)
+			go func() {
+				if mc.isRegistered() {
+					Logger.Debug("SetupSC - node is already registered")
+					isRegisteredC <- true
+					return
+				}
+				isRegisteredC <- false
+			}()
+
+			select {
+			case reg := <-isRegisteredC:
+				if reg {
+					continue
+				}
+			case <-time.NewTimer(3 * time.Second).C:
+				Logger.Debug("SetupSC - check node registered timeout")
+			}
+
+			Logger.Debug("Request to register node")
+			txn, err := mc.RegisterNode()
+			if err != nil {
+				Logger.Warn("failed to register node in SC -- init_setup_sc",
+					zap.Error(err))
+				continue
+			}
+
+			if txn != nil && mc.ConfirmTransaction(txn) {
+				Logger.Debug("Register node transaction confirmed")
+				continue
+			}
+
+			Logger.Debug("Register node transaction not confirmed yet")
+		}
+	}
+}
