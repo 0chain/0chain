87fcbdf6b54b7f9ab7d4daecdab544e940d5c4cc
bbist <pro.bbist@gmail.com>
2021-05-12 16:29:54 +0545

    fixed integration test start_lock issue

--- code/go/0chain.net/chaincore/chain/protocol_view_change.go
+++ code/go/0chain.net/chaincore/chain/protocol_view_change.go
@@ -1,7 +1,6 @@
 package chain
 
 import (
-	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -40,53 +39,6 @@ const (
 	scRestAPIGetSharderList     = "/getSharderList"
 	scRestAPIGetSharderKeepList = "/getSharderKeepList"
 )
-
-func (mc *Chain) SetupSC(ctx context.Context) {
-	Logger.Info("SetupSC start...")
-	tm := time.NewTicker(5 * time.Second)
-	for {
-		select {
-		case <-ctx.Done():
-			Logger.Debug("SetupSC is done")
-			return
-		case <-tm.C:
-			Logger.Debug("SetupSC - check if node is registered")
-			isRegisteredC := make(chan bool)
-			go func() {
-				if mc.isRegistered() {
-					Logger.Debug("SetupSC - node is already registered")
-					isRegisteredC <- true
-					return
-				}
-				isRegisteredC <- false
-			}()
-
-			select {
-			case reg := <-isRegisteredC:
-				if reg {
-					continue
-				}
-			case <-time.NewTimer(3 * time.Second).C:
-				Logger.Debug("SetupSC - check node registered timeout")
-			}
-
-			Logger.Debug("Request to register node")
-			txn, err := mc.RegisterNode()
-			if err != nil {
-				Logger.Warn("failed to register node in SC -- init_setup_sc",
-					zap.Error(err))
-				continue
-			}
-
-			if txn != nil && mc.ConfirmTransaction(txn) {
-				Logger.Debug("Register node transaction confirmed")
-				continue
-			}
-
-			Logger.Debug("Register node transaction not confirmed yet")
-		}
-	}
-}
 
 // RegisterClient registers client on BC.
 func (mc *Chain) RegisterClient() {
