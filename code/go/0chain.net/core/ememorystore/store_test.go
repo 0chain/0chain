b5fdf3de7c13ee4c9d567ed430200d9af7678b6f
murashovven <murashovven@gmail.com>
2021-05-12 10:17:18 +0300

    Remove parallel running from tests: - TestStore_Write

--- code/go/0chain.net/core/ememorystore/store_test.go
+++ code/go/0chain.net/core/ememorystore/store_test.go
@@ -184,8 +184,6 @@ func refreshDBs() error {
 }
 
 func TestStore_Write(t *testing.T) {
-	t.Parallel()
-
 	r := round.NewRound(1)
 	r.BlockHash = encryption.Hash("data")
 
@@ -213,10 +211,7 @@ func TestStore_Write(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-
 			if err := refreshDBs(); err != nil {
 				t.Error(err)
 			}
