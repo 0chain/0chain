8681adde15b68d69d40fe21650853d5bccc68d09
murashovven <murashovven@gmail.com>
2021-05-13 16:40:46 +0300

    Remove parallel running from TestInitLogging

--- code/go/0chain.net/core/logging/logger_test.go
+++ code/go/0chain.net/core/logging/logger_test.go
@@ -9,8 +9,6 @@ import (
 )
 
 func TestInitLogging(t *testing.T) {
-	t.Parallel()
-
 	type args struct {
 		mode string
 	}
@@ -24,10 +22,7 @@ func TestInitLogging(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-
 			InitLogging(tt.args.mode)
 		})
 	}
