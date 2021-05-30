6b0c91867dcbd67494920960e3d4700222d85920
Vlad Gorokhov <vlad@witchery.io>
2021-05-30 17:54:28 +0300

    :white_check_mark: Update unit test build.sh

--- docker.local/bin/unit_test_build.sh
+++ docker.local/bin/unit_test_build.sh
@@ -4,6 +4,8 @@
 # of code/go/0chain.net.
 # Returns 0 if all of the tests pass and 1 if any one of the tests fail.
 
+./generate_mocks.sh
+
 docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
 docker run $INTERACTIVE zchain_unit_test sh -c "cd 0chain.net; go test -tags bn256 -cover ./..."
 
