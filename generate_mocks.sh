453d8df2f76af2f31b3fe77c7c328cdb9265dd5f
Vlad Gorokhov <vlad@witchery.io>
2021-05-30 14:49:25 +0300

    :white_check_mark: Keeptree To Avoid Cyclic

--- generate_mocks.sh
+++ generate_mocks.sh
@@ -5,21 +5,21 @@ BASEDIR=$(pwd)
 echo "Making mocks..."
 
 cd $BASEDIR/code/go/0chain.net/core || exit
-mockery --output=../core/mocks --all
+mockery --output=../core/mocks --all --keeptree
 
 cd $BASEDIR/code/go/0chain.net/miner || exit
-mockery --output=../miner/mocks --all
+mockery --output=../miner/mocks --all --keeptree
 
 cd $BASEDIR/code/go/0chain.net/chaincore || exit
-mockery --output=../chaincore/mocks --all
+mockery --output=../chaincore/mocks --all --keeptree
 
 cd $BASEDIR/code/go/0chain.net/conductor || exit
-mockery --output=../conductor/mocks --all
+mockery --output=../conductor/mocks --all --keeptree
 
 cd $BASEDIR/code/go/0chain.net/sharder || exit
-mockery --output=../sharder/mocks --all
+mockery --output=../sharder/mocks --all --keeptree
 
 cd $BASEDIR/code/go/0chain.net/smartcontract || exit
-mockery --output=../smartcontract/mocks --all
+mockery --output=../smartcontract/mocks --all --keeptree
 
 echo "Mocks files are generated."
