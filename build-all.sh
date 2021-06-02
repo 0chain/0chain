235f51efea33f4175ba23ab4cacfe58950826f47
Shravan Shetty <shravanshetty322@gmail.com>
2021-06-02 11:17:27 +0530

    removing uneeded files

--- build-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-./docker.local/bin/build.base.sh
-./docker.local/bin/build.miners.sh
-./docker.local/bin/build.sharders.sh
-./docker.local/bin/sync_clock.sh
--- clean-up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker stop $(docker ps -a -q)
-./docker.local/bin/clean.sh
-./docker.local/bin/init.setup.sh
-./docker.local/bin/sync_clock.sh
