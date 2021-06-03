278aea7e892a434e858355deaa785d61ec6b4ae9
Vlad Gorokhov <vlad@witchery.io>
2021-06-03 03:59:13 +0300

    :white_check_mark: block notarization unit test

---
+++ code/go/0chain.net/miner/block_notarization_entity_test.go
@@ -0,0 +1,56 @@
+package miner
+
+import (
+	"0chain.net/chaincore/block"
+	"0chain.net/core/mocks"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type BlockNotarizationEntityTestSuite struct {
+	suite.Suite
+}
+
+func TestBlockNotarizationEntityTestSuiteSuite(t *testing.T) {
+	suite.Run(t, &BlockNotarizationEntityTestSuite{})
+}
+
+func (s *BlockNotarizationEntityTestSuite) TestNotarizationProvider() {
+	NotarizationProvider()
+}
+
+func (s *BlockNotarizationEntityTestSuite) TestNotarizationDoReadLock() {
+	n := NotarizationProvider().(*Notarization)
+	store := &mocks.Store{}
+
+	block.SetupEntity(store)
+
+	b := block.NewBlock("1", 1)
+	n.Block = b
+	n.DoReadLock()
+}
+
+func (s *BlockNotarizationEntityTestSuite) TestNotarizationDoReadUnlock() {
+	n := NotarizationProvider().(*Notarization)
+	store := &mocks.Store{}
+
+	block.SetupEntity(store)
+
+	b := block.NewBlock("1", 1)
+	n.Block = b
+	//n.DoReadUnlock()
+}
+
+func (s *BlockNotarizationEntityTestSuite) TestNotarizationGetEntityMetadata() {
+	n := NotarizationProvider().(*Notarization)
+	n.GetEntityMetadata()
+}
+
+func (s *BlockNotarizationEntityTestSuite) TestNotarizationGetKey() {
+	n := NotarizationProvider().(*Notarization)
+	n.GetKey()
+}
+
+func (s *BlockNotarizationEntityTestSuite) TestSetupNotarizationEntity() {
+	SetupNotarizationEntity()
+}
