bd24365a48a4ee5605b1c94fce5f547a9d06dde2
Dmitry <dmitry@centralex.com>
2021-06-09 08:46:14 +0300

    Dockerfile changed

--- docker.local/bin/build.base.apple.sh
+++ docker.local/bin/build.base.apple.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-docker buildx build --platform linux/amd64 -f docker.local/build.base/Dockerfile.build_base . -t zchain_build_base
-docker buildx build --platform linux/amd64 -f docker.local/build.base/Dockerfile.run_base docker.local/build.base -t zchain_run_base
+docker buildx build --platform linux/amd64 -f docker.local/build.base/Dockerfile.build_base.apple . -t zchain_build_base
+docker buildx build --platform linux/amd64 -f docker.local/build.base/Dockerfile.run_base.apple docker.local/build.base -t zchain_run_base
