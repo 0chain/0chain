4395c1bcb3279ecfdf338590a4aad8ee2480cd61
Dmitry <dmitry@centralex.com>
2021-06-10 09:32:42 +0300

    Code review fix

--- docker.local/bin/build.magic_block.apple.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker buildx build --platform linux/amd64 -f docker.local/build.magicBlock/Dockerfile . -t magicblock
-docker-compose -p magic_block -f docker.local/build.magicBlock/docker-compose.yml build --force-rm
