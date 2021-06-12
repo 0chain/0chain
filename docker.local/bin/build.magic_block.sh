6f93968c098adef972b074e3c6f0f005ca6094f3
Dima <dima.dyachkov@gmail.com>
2021-06-12 17:19:20 +0300

    docker apple m1 cherry pick

--- docker.local/bin/build.magic_block.sh
+++ docker.local/bin/build.magic_block.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
-docker build -f docker.local/build.magicBlock/Dockerfile . -t magicblock
+
+cmd="build"
+
+for arg in "$@"
+do
+    case $arg in
+        -m1|--m1|m1)
+        echo "The build will be performed for Apple M1 chip"
+        cmd="buildx build --platform linux/amd64"
+        shift
+        ;;
+    esac
+done
+
+docker $cmd -f docker.local/build.magicBlock/Dockerfile . -t magicblock
 docker-compose -p magic_block -f docker.local/build.magicBlock/docker-compose.yml build --force-rm
