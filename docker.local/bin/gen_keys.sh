585c96dcb51a56b1739e557d3cdc894447e3086b
Dima <dima.dyachkov@gmail.com>
2021-06-12 17:33:02 +0300

    Updated remaining docker images

--- docker.local/bin/gen_keys.sh
+++ docker.local/bin/gen_keys.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
-docker build -f docker.local/build.genkeys/Dockerfile . -t zchain_genkeys
+
+cmd="build"
+
+for arg in "$@"
+do
+    case $arg in
+        -m1|--m1|m1)
+        echo "The build will be performed for Apple M1 chip"
+        cmd="buildx build --platform linux/amd64"
+        shift
+        ;;
+    esac
+done
+
+docker $cmd -f docker.local/build.genkeys/Dockerfile . -t zchain_genkeys
 
 if [ "$#" -ne 3 ]; 
     then echo "illegal number of parameters usage: docker.local/bin/gen_keys.sh signatureScheme [bls0chain or ed25519] absolute_path_to_keyfiles_folder key_file_name"
     exit 
 fi
-
-
 
 docker run -v $2:/mykeys -it zchain_genkeys go run encryption/keys/main.go   --signature_scheme "$1" --keys_file_name "$3" --keys_file_path "/mykeys" --generate_keys true  --timestamp true 
 
--- docker.local/bin/linter.sh
+++ docker.local/bin/linter.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 set -e
 
+cmd="build"
 
-docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
+for arg in "$@"
+do
+    case $arg in
+        -m1|--m1|m1)
+        echo "The build will be performed for Apple M1 chip"
+        cmd="buildx build --platform linux/amd64"
+        shift
+        ;;
+    esac
+done
+
+docker $cmd -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
 
 #Set this to a value higher than the current number of linter errors. We should lower this number over time
 docker run zchain_unit_test bash -c '
