3c451c009ab969309b1fea6106d82a1afb00a310
Dima <dima.dyachkov@gmail.com>
2021-06-13 10:40:43 +0300

    Revert "unit test docker m1 fix"
    This reverts commit 86e06302cf52dc2711c0902f09320b13924e09a4.

--- docker.local/bin/unit_test_build.sh
+++ docker.local/bin/unit_test_build.sh
@@ -2,17 +2,13 @@
 set -e
 
 cmd="build"
-dockerfile="docker.local/build.unit_test/Dockerfile"
-platform=""
 
 for arg in "$@"
 do
     case $arg in
         -m1|--m1|m1)
         echo "The build will be performed for Apple M1 chip"
-        cmd="buildx build --platform linux/arm64"
-        dockerfile="docker.local/build.unit_test/Dockerfile.m1"
-        platform="--platform=linux/amd64"
+        cmd="buildx build --platform linux/amd64"
         shift
         ;;
     esac
@@ -22,5 +18,5 @@ done
 # of code/go/0chain.net.
 # Returns 0 if all of the tests pass and 1 if any one of the tests fail.
 
-docker $cmd -f $dockerfile . -t zchain_unit_test
-docker run $platform $INTERACTIVE zchain_unit_test sh -c "cd 0chain.net; go test -tags bn256 -cover ./..."
+docker $cmd -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
+docker run $INTERACTIVE zchain_unit_test sh -c "cd 0chain.net; go test -tags bn256 -cover ./..."
