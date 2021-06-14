c8b57d0c0719155738d0312d49379aada46a2071
Vlad Gorokhov <vlad@witchery.io>
2021-06-14 06:04:40 +0300

    Volume Mount Added

--- docker.local/bin/unit_test_build.sh
+++ docker.local/bin/unit_test_build.sh
@@ -6,5 +6,7 @@ set -e
 # Returns 0 if all of the tests pass and 1 if any one of the tests fail.
 
 docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
+
 docker run -v `pwd`/code/go/0chain.net:/0chain/code/go/0chain.net zchain_unit_test sh -c "sh /0chain/generate_mocks.sh"
-docker run $INTERACTIVE zchain_unit_test sh -c "cd 0chain.net; go test -tags bn256 -cover ./..."
+
+docker run -v `pwd`/code/go/0chain.net:/0chain/code/go/0chain.net zchain_unit_test sh -c "cd 0chain.net; go test -tags bn256 -cover ./..."
