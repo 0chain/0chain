341e1ce6edd26ea26000466399e274e92c926289
Vlad Gorokhov <vlad@witchery.io>
2021-06-14 04:49:44 +0300

    Update Script To Use Volumes

--- generate_mocks.sh
+++ generate_mocks.sh
@@ -4,25 +4,25 @@ BASEDIR=$(pwd)
 
 echo "Making mocks..."
 
-cd $BASEDIR/code/go/0chain.net/core || exit
+cd $BASEDIR/0chain.net/core || exit
 mockery --output=../core/mocks --all
 
-cd $BASEDIR/code/go/0chain.net/miner || exit
+cd $BASEDIR/0chain.net/miner || exit
 mockery --output=../miner/mocks --all
 
-cd $BASEDIR/code/go/0chain.net/chaincore || exit
+cd $BASEDIR/0chain.net/chaincore || exit
 mockery --output=../chaincore/mocks --all
 
-cd $BASEDIR/code/go/0chain.net/conductor || exit
+cd $BASEDIR/0chain.net/conductor || exit
 mockery --output=../conductor/mocks --all
 
-cd $BASEDIR/code/go/0chain.net/sharder || exit
+cd $BASEDIR/0chain.net/sharder || exit
 mockery --output=../sharder/mocks --all
 
-cd $BASEDIR/code/go/0chain.net/smartcontract || exit
+cd $BASEDIR/0chain.net/smartcontract || exit
 mockery --output=../smartcontract/mocks --all
 
-cd $BASEDIR/code/go/0chain.net/chaincore/chain/state || exit
+cd $BASEDIR/0chain.net/chaincore/chain/state || exit
 mockery --name=StateContextI --output=../../../mocks --filename=state-context-i.go
 
 echo "Mocks files are generated."
