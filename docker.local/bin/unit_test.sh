03f2b2a5026ba90d0af43e1a3eb3ac5aacb634bd
Vlad Gorokhov <vlad@witchery.io>
2021-06-14 03:45:56 +0300

    Docker Volume

--- docker.local/bin/unit_test.sh
+++ docker.local/bin/unit_test.sh
@@ -3,7 +3,7 @@ set -e
 
 # Allocate interactive TTY to allow Ctrl-C.
 INTERACTIVE="-it"
-PACKAGE=""
+PACKAGE="$1"
 
 if [[ "$1" == *"--ci"* ]]
 then
@@ -12,19 +12,20 @@ then
     echo "Building both general and SC test images"
     docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
 else
-    PACKAGE="$1"
-
     echo "Building general test image"
     docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
 fi
 
+docker run -v `pwd`/code/go/0chain.net:/0chain/code/go/0chain.net zchain_unit_test sh -c "sh /0chain/generate_mocks.sh"
+
+
 if [[ -n "$PACKAGE" ]]; then
-    # Run tests from a single package.
-    # assume that $PACKAGE looks something like: 0chain.net/chaincore/threshold/bls
+#     Run tests from a single package.
+#     assume that $PACKAGE looks something like: 0chain.net/chaincore/threshold/bls
     echo "Running unit tests from $PACKAGE:"
-    docker run $INTERACTIVE zchain_unit_test sh -c "cd /0chain/go/code/$PACKAGE; go test -tags bn256 -cover ./..."
+    docker run -v `pwd`/code/go/0chain.net:/0chain/code/go/0chain.net zchain_unit_test sh -c "cd $PACKAGE; go test -tags bn256 -cover ./..."
 else
     # Run all tests.
     echo "Running general unit tests:"
-    docker run $INTERACTIVE zchain_unit_test sh -c "cd 0chain.net; go test -tags bn256 -cover ./..."
+    docker run -v `pwd`/code/go/0chain.net:/0chain/code/go/0chain.net zchain_unit_test sh -c "cd 0chain.net; go test -tags bn256 -cover ./..."
 fi
