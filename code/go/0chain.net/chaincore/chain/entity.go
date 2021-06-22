45b0c013bcdb2fd78194ca80fb5dc46b2441f267
peterlimg <peterlimg@protonmail.com>
2021-06-22 16:21:24 +1000

    Fix logic error on verify chain history
    Do not set round seed when round timeout count is equal to block round timeout count.

--- code/go/0chain.net/chaincore/chain/entity.go
+++ code/go/0chain.net/chaincore/chain/entity.go
@@ -621,7 +621,7 @@ func (c *Chain) AddNotarizedBlockToRound(r round.RoundI, b *block.Block) (*block
 
 	// Only for blocks with greater RTC (elder blocks)
 	if r.GetRandomSeed() != b.GetRoundRandomSeed() ||
-		r.GetTimeoutCount() <= b.RoundTimeoutCount {
+		r.GetTimeoutCount() < b.RoundTimeoutCount {
 
 		logging.Logger.Info("AddNotarizedBlockToRound round and block random seed different",
 			zap.Int64("Round", r.GetRoundNumber()),
