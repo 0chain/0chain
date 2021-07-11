e183d5d4d02714dcb0f97c0fccc172d40fe5ed30
peterlimg <peterlimg@protonmail.com>
2021-07-11 14:07:15 +1000

    Fix error in unit_test.sh

--- docker.local/bin/unit_test.sh
+++ docker.local/bin/unit_test.sh
@@ -22,7 +22,7 @@ if [[ -n "$PACKAGE" ]]; then
     # Run tests from a single package.
     # assume that $PACKAGE looks something like: 0chain.net/chaincore/threshold/bls
     echo "Running unit tests from $PACKAGE:"
-    docker run $INTERACTIVE zchain_unit_test sh -c "cd /0chain/go/$PACKAGE; go test -tags bn256 -cover ./..."
+    docker run $INTERACTIVE zchain_unit_test sh -c "cd /0chain/code/go/$PACKAGE; go test -tags bn256 -cover ./..."
 else
     # Run all tests.
     echo "Running general unit tests:"
