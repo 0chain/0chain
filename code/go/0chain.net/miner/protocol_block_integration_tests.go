dabc4d2e6953af4035ed19695e33e96085f1ba4a
peterlimg <peterlimg@protonmail.com>
2021-07-12 21:26:06 +1000

    Execute smart contract with timeout
    It used to execute a smart contract with no limit time if the block is notarized or
    the SmartContractTimeout is 0.

--- code/go/0chain.net/miner/protocol_block_integration_tests.go
+++ code/go/0chain.net/miner/protocol_block_integration_tests.go
@@ -140,7 +140,7 @@ func (mc *Chain) GenerateBlock(ctx context.Context, b *block.Block,
 				return false
 			}
 		}
-		if err := mc.UpdateState(b, txn); err != nil {
+		if err := mc.UpdateState(ctx, b, txn); err != nil {
 			if debugTxn {
 				logging.Logger.Error("generate block (debug transaction) update state", zap.String("txn", txn.Hash), zap.Int32("idx", idx), zap.String("txn_object", datastore.ToJSON(txn).String()), zap.Error(err))
 			}
