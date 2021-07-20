8ea68ccce0f87d046305af6856cd7d39ea6878a2
Dmitry <dima.dyachkov@gmail.com>
2021-07-20 17:05:58 +0300

    Test fix

--- code/go/0chain.net/smartcontract/zcnsc/mint_test.go
+++ code/go/0chain.net/smartcontract/zcnsc/mint_test.go
@@ -20,16 +20,17 @@ func init() {
 }
 
 func Test_MintPayload_Encode_Decode(t *testing.T) {
-	expected := createMintPayload("1", "2", "3")
+	expected, _, err := createMintPayload("1", "2", "3")
+	require.NoError(t, err)
 	actual := &mintPayload{}
-	err := actual.Decode(expected.Encode())
+	err = actual.Decode(expected.Encode())
 	require.NoError(t, err)
 	require.Equal(t, expected.Nonce, actual.Nonce)
 	require.Equal(t, expected.Amount, actual.Amount)
 	require.Equal(t, expected.EthereumTxnID, actual.EthereumTxnID)
 	require.Equal(t, expected.ReceivingClientID, actual.ReceivingClientID)
 	require.Equal(t, len(expected.Signatures), len(actual.Signatures))
-	for i, _ := range actual.Signatures {
+	for i := range actual.Signatures {
 		require.Equal(t, expected.Signatures[i].ID, actual.Signatures[i].ID)
 		require.Equal(t, expected.Signatures[i].Signature, actual.Signatures[i].Signature)
 	}
@@ -38,23 +39,35 @@ func Test_MintPayload_Encode_Decode(t *testing.T) {
 // TBD
 func Test_FuzzyMintTest(t *testing.T) {
 	contract := CreateZCNSmartContract()
-	tr := CreateDefaultTransactionToZcnsc()
+	tr1 := CreateTransactionToZcnsc(clientId, tokens)
+	//tr2 := CreateTransactionToZcnsc(clientId + "1", tokens)
+	//tr3 := CreateTransactionToZcnsc(clientId + "2", tokens)
+
 	ctx := CreateMockStateContext(clientId)
 
 	addAuthorizer(t, contract, ctx, clientId)
-	addAuthorizer(t, contract, ctx, clientId + "1")
-	addAuthorizer(t, contract, ctx, clientId + "2")
+	//addAuthorizer(t, contract, ctx, clientId + "1")
+	//addAuthorizer(t, contract, ctx, clientId + "2")
 
-	payload := createMintPayload(clientId, clientId + "1", clientId + "2")
+	payload, _, err := createMintPayload(clientId, clientId + "1", clientId + "2")
+	require.NoError(t, err)
 
-	response, err := contract.mint(tr, payload.Encode(), ctx)
+	response, err := contract.mint(tr1, payload.Encode(), ctx)
 	require.NoError(t, err)
 	require.NotNil(t, response)
 	require.NotEmpty(t, response)
+	//response, err = contract.mint(tr2, payload.Encode(), ctx)
+	//require.NoError(t, err)
+	//require.NotNil(t, response)
+	//require.NotEmpty(t, response)
+	//response, err = contract.mint(tr3, payload.Encode(), ctx)
+	//require.NoError(t, err)
+	//require.NotNil(t, response)
+	//require.NotEmpty(t, response)
 }
 
 // TBD
-func MintPayloadNonceShouldBeHigherByOneThanUserNonce(t *testing.T) {
+func Test_MintPayloadNonceShouldBeHigherByOneThanUserNonce(t *testing.T) {
 	payload := createBurnPayload()
 	tr := CreateDefaultTransactionToZcnsc()
 	contract := CreateZCNSmartContract()
