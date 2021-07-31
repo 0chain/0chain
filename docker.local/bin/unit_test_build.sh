c083b4e5e4a2d007dba39e38db3bf1f3cf2ca960
NoSkillGuy <rsivapraveen001@gmail.com>
2021-07-31 13:31:07 +0530

    running unit tests in m1 machine

--- docker.local/bin/unit_test_build.sh
+++ docker.local/bin/unit_test_build.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 set -e
+
+cmd="build"
+build_dockerfile="docker.local/build.unit_test/Dockerfile"
+
+for arg in "$@"
+do
+    case $arg in
+        -m1|--m1|m1)
+        echo "The build will be performed for Apple M1 chip"
+        cmd="buildx build --platform linux/amd64"
+        build_dockerfile="docker.local/build.unit_test/Dockerfile.m1"
+        shift
+        ;;
+    esac
+done
 
 # Runs each unit test in batches corresponding to each subdirectory
 # of code/go/0chain.net.
 # Returns 0 if all of the tests pass and 1 if any one of the tests fail.
 
-docker build -f docker.local/build.unit_test/Dockerfile . -t zchain_unit_test
+docker build -f $build_dockerfile . -t zchain_unit_test
 docker run $INTERACTIVE zchain_unit_test sh -c "cd 0chain.net; go test -tags bn256 -cover ./..."
