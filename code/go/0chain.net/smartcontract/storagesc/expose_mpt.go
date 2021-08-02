ca65b1eb2670428de0ae27f1de21fb803b48a5ee
Piers Shepperson <piers.shepperson@btinternet.com>
2021-08-02 17:30:49 +0100

    expose mpt

---
+++ code/go/0chain.net/smartcontract/storagesc/expose_mpt.go
@@ -0,0 +1,32 @@
+package storagesc
+
+import (
+	"0chain.net/chaincore/chain/state"
+	"0chain.net/core/common"
+	"context"
+	"net/url"
+)
+
+func (ssc *StorageSmartContract) GetMptKey(
+	_ context.Context,
+	params url.Values,
+	balances state.StateContextI,
+) (interface{}, error) {
+	var err error
+	var conf *scConfig
+	if conf, err = ssc.getConfig(balances, false); err != nil {
+		return nil, common.NewError("get_mpt_key",
+			"can't get SC configurations: "+err.Error())
+	}
+	if !conf.ExposeMpt {
+		return nil, common.NewError("get_mpt_key",
+			"exposed mpt not enabled")
+	}
+
+	var key = params.Get("key")
+	val, err := balances.GetTrieNode(stakePoolKey(ssc.ID, key))
+	if err != nil {
+		return nil, err
+	}
+	return string(val.Encode()), nil
+}
