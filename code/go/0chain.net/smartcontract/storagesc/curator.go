1efaf30ebde5ea5671bd31e32f57aeb547cefc74
Piers Shepperson <piers.shepperson@btinternet.com>
2021-08-24 11:03:42 +0100

    Remove curator (#470)

---
+++ code/go/0chain.net/smartcontract/storagesc/curator.go
@@ -0,0 +1,114 @@
+package storagesc
+
+import (
+	chainstate "0chain.net/chaincore/chain/state"
+	"0chain.net/chaincore/transaction"
+	"0chain.net/core/common"
+	"encoding/json"
+)
+
+type curatorInput struct {
+	CuratorId    string `json:"curator_id"`
+	AllocationId string `json:"allocation_id"`
+}
+
+func (aci *curatorInput) decode(input []byte) error {
+	return json.Unmarshal(input, aci)
+}
+
+func (sc *StorageSmartContract) removeCurator(
+	txn *transaction.Transaction,
+	input []byte,
+	balances chainstate.StateContextI,
+) (err error) {
+	var rci curatorInput
+	if err = rci.decode(input); err != nil {
+		return common.NewError("remove_curator_failed",
+			"error unmarshalling input: "+err.Error())
+	}
+
+	var alloc *StorageAllocation
+	alloc, err = sc.getAllocation(rci.AllocationId, balances)
+	if err != nil {
+		return common.NewError("remove_curator_failed alloc_cancel_failed", err.Error())
+	}
+
+	if alloc.Owner != txn.ClientID {
+		return common.NewError("remove_curator_failed",
+			"only owner can remove a curator")
+	}
+
+	var found = false
+	for i, curator := range alloc.Curators {
+		if curator == rci.CuratorId {
+			// we don't care about orderif
+			if len(alloc.Curators) == 1 {
+				alloc.Curators = []string{}
+			} else {
+				alloc.Curators[i] = alloc.Curators[len(alloc.Curators)-1]
+				alloc.Curators = alloc.Curators[:len(alloc.Curators)-1]
+			}
+			found = true
+			break
+		}
+	}
+	if !found {
+		return common.NewError("remove_curator_failed",
+			"cannot find curator: "+rci.CuratorId)
+	}
+
+	_, err = balances.InsertTrieNode(alloc.GetKey(sc.ID), alloc)
+	if err != nil {
+		return common.NewError("remove_curator_failed",
+			"cannot save allocation"+err.Error())
+	}
+
+	return nil
+}
+
+func (sc *StorageSmartContract) addCurator(
+	txn *transaction.Transaction,
+	input []byte,
+	balances chainstate.StateContextI,
+) (err error) {
+	var aci curatorInput
+	if err = aci.decode(input); err != nil {
+		return common.NewError("add_curator_failed",
+			"error unmarshalling input: "+err.Error())
+	}
+
+	var alloc *StorageAllocation
+	alloc, err = sc.getAllocation(aci.AllocationId, balances)
+	if err != nil {
+		return common.NewError("alloc_cancel_failed", err.Error())
+	}
+
+	if alloc.Owner != txn.ClientID {
+		return common.NewError("add_curator_failed",
+			"only owner can add a curator")
+	}
+
+	if alloc.isCurator(aci.CuratorId) {
+		return common.NewError("add_curator_failed",
+			"already a curator: "+aci.CuratorId)
+	}
+
+	alloc.Curators = append(alloc.Curators, aci.CuratorId)
+
+	_, err = balances.InsertTrieNode(alloc.GetKey(sc.ID), alloc)
+	if err != nil {
+		return common.NewError("add_curator_failed",
+			"cannot save allocation"+err.Error())
+	}
+
+	return nil
+}
+
+func (sa StorageAllocation) isCurator(id string) bool {
+	for _, curator := range sa.Curators {
+		if curator == id {
+			return true
+		}
+	}
+	return false
+}
---
+++ code/go/0chain.net/smartcontract/storagesc/curator_test.go
@@ -0,0 +1,232 @@
+package storagesc
+
+import (
+	chainState "0chain.net/chaincore/chain/state"
+	"0chain.net/chaincore/mocks"
+	sci "0chain.net/chaincore/smartcontractinterface"
+	"0chain.net/chaincore/transaction"
+	"encoding/json"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestAddCurator(t *testing.T) {
+	t.Parallel()
+	const (
+		mockClientId     = "mock client id"
+		mockCuratorId    = "mock curator id"
+		mockAllocationId = "mock allocation id"
+	)
+	type args struct {
+		ssc      *StorageSmartContract
+		txn      *transaction.Transaction
+		input    []byte
+		balances chainState.StateContextI
+	}
+	type parameters struct {
+		clientId         string
+		info             curatorInput
+		existingCurators []string
+	}
+	type want struct {
+		err    bool
+		errMsg string
+	}
+	var setExpectations = func(t *testing.T, name string, p parameters, want want) args {
+		var balances = &mocks.StateContextI{}
+		var txn = &transaction.Transaction{
+			ClientID: p.clientId,
+		}
+		var ssc = &StorageSmartContract{
+			SmartContract: sci.NewSC(ADDRESS),
+		}
+		input, err := json.Marshal(p.info)
+		require.NoError(t, err)
+
+		var sa = StorageAllocation{
+			ID:    p.info.AllocationId,
+			Owner: p.clientId,
+		}
+		for _, curator := range p.existingCurators {
+			sa.Curators = append(sa.Curators, curator)
+		}
+		balances.On("GetTrieNode", sa.GetKey(ssc.ID)).Return(&sa, nil).Once()
+
+		balances.On(
+			"InsertTrieNode",
+			sa.GetKey(ssc.ID),
+			mock.MatchedBy(func(sa *StorageAllocation) bool {
+				if len(sa.Curators) < 1 {
+					return false
+				}
+				for i, curator := range p.existingCurators {
+					if curator != sa.Curators[i] {
+						return false
+					}
+				}
+				if p.info.CuratorId != sa.Curators[len(sa.Curators)-1] {
+					return false
+				}
+				return sa.ID == p.info.AllocationId && sa.Owner == p.clientId
+			})).Return("", nil).Once()
+
+		return args{ssc, txn, input, balances}
+	}
+
+	testCases := []struct {
+		name       string
+		parameters parameters
+		want       want
+	}{
+		{
+			name: "ok",
+			parameters: parameters{
+				clientId: mockClientId,
+				info: curatorInput{
+					CuratorId:    mockCuratorId,
+					AllocationId: mockAllocationId,
+				},
+			},
+		},
+	}
+	for _, test := range testCases {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			args := setExpectations(t, test.name, test.parameters, test.want)
+
+			err := args.ssc.addCurator(args.txn, args.input, args.balances)
+
+			require.EqualValues(t, test.want.err, err != nil)
+			if err != nil {
+				require.EqualValues(t, test.want.errMsg, err.Error())
+				return
+			}
+			require.True(t, mock.AssertExpectationsForObjects(t, args.balances))
+		})
+	}
+}
+
+func TestRemoveCurator(t *testing.T) {
+	t.Parallel()
+	const (
+		mockClientId     = "mock client id"
+		mockCuratorId    = "mock curator id"
+		mockCuratorId2   = "mock curator id 2"
+		mockCuratorId3   = "mock curator id 3"
+		mockAllocationId = "mock allocation id"
+	)
+	type args struct {
+		ssc      *StorageSmartContract
+		txn      *transaction.Transaction
+		input    []byte
+		balances chainState.StateContextI
+	}
+	type parameters struct {
+		clientId         string
+		info             curatorInput
+		existingCurators []string
+	}
+	type want struct {
+		err    bool
+		errMsg string
+	}
+	var setExpectations = func(t *testing.T, name string, p parameters, want want) args {
+		var balances = &mocks.StateContextI{}
+		var txn = &transaction.Transaction{
+			ClientID: p.clientId,
+		}
+		var ssc = &StorageSmartContract{
+			SmartContract: sci.NewSC(ADDRESS),
+		}
+		input, err := json.Marshal(p.info)
+		require.NoError(t, err)
+
+		var sa = StorageAllocation{
+			ID:    p.info.AllocationId,
+			Owner: p.clientId,
+		}
+		for _, curator := range p.existingCurators {
+			sa.Curators = append(sa.Curators, curator)
+		}
+		balances.On("GetTrieNode", sa.GetKey(ssc.ID)).Return(&sa, nil).Once()
+
+		balances.On(
+			"InsertTrieNode",
+			sa.GetKey(ssc.ID),
+			mock.MatchedBy(func(sa *StorageAllocation) bool {
+				if len(sa.Curators) != len(p.existingCurators)-1 {
+					return false
+				}
+				for _, curator := range sa.Curators {
+					if p.info.CuratorId == curator {
+						return false
+					}
+				}
+				return sa.ID == p.info.AllocationId && sa.Owner == p.clientId
+			})).Return("", nil).Once()
+
+		return args{ssc, txn, input, balances}
+	}
+
+	testCases := []struct {
+		name       string
+		parameters parameters
+		want       want
+	}{
+		{
+			name: "ok_1_curators",
+			parameters: parameters{
+				clientId: mockClientId,
+				info: curatorInput{
+					CuratorId:    mockCuratorId,
+					AllocationId: mockAllocationId,
+				},
+				existingCurators: []string{mockCuratorId2, mockCuratorId, mockCuratorId3},
+			},
+		},
+		{
+			name: "ok_3_curators",
+			parameters: parameters{
+				clientId: mockClientId,
+				info: curatorInput{
+					CuratorId:    mockCuratorId,
+					AllocationId: mockAllocationId,
+				},
+				existingCurators: []string{mockCuratorId},
+			},
+		},
+		{
+			name: "error_not_curator",
+			parameters: parameters{
+				clientId: mockClientId,
+				info: curatorInput{
+					CuratorId:    mockCuratorId,
+					AllocationId: mockAllocationId,
+				},
+				existingCurators: []string{mockCuratorId2, mockCuratorId3},
+			},
+			want: want{
+				err:    true,
+				errMsg: "remove_curator_failed: cannot find curator: " + mockCuratorId,
+			},
+		},
+	}
+	for _, test := range testCases {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			args := setExpectations(t, test.name, test.parameters, test.want)
+
+			err := args.ssc.removeCurator(args.txn, args.input, args.balances)
+
+			require.EqualValues(t, test.want.err, err != nil)
+			if err != nil {
+				require.EqualValues(t, test.want.errMsg, err.Error())
+				return
+			}
+			require.True(t, mock.AssertExpectationsForObjects(t, args.balances))
+		})
+	}
+}
