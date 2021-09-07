e5c65c23c3994b1295ed7a4c6d2c6c5d993146c0
Piers Shepperson <piers.shepperson@btinternet.com>
2021-09-07 14:42:20 +0100

    add format varable

--- code/go/0chain.net/smartcontract/minersc/sc.go
+++ code/go/0chain.net/smartcontract/minersc/sc.go
@@ -144,7 +144,7 @@ func (msc *MinerSmartContract) Execute(t *transaction.Transaction,
 	}
 	scFunc, found := msc.smartContractFunctions[funcName]
 	if !found {
-		return common.NewErrorf("failed execution", "no miner smart contract method with name", funcName).Error(), nil
+		return common.NewErrorf("failed execution", "no miner smart contract method with name: %v", funcName).Error(), nil
 	}
 	return scFunc(t, input, gn, balances)
 }
