5a072e2c972c6cef9337fab606bd81b0ec06345c
MI <hello@superiss.com>
2021-10-13 15:05:22 +0100

    added Sign grpc handler

--- code/go/0chain.net/miner/server/grpc/handlers/sign.go
+++ code/go/0chain.net/miner/server/grpc/handlers/sign.go
@@ -2,13 +2,39 @@ package handlers
 
 import (
 	"context"
+	"encoding/hex"
+	"fmt"
 
+	"0chain.net/core/encryption"
 	minerproto "0chain.net/miner/proto/api/src/proto"
 )
 
 // Sign
 func (m *minerGRPCService) Sign(ctx context.Context, req *minerproto.SignRequest) (*minerproto.SignResponse, error) {
-	// implement new logic
-	//
-	return &minerproto.SignResponse{}, nil
+	privateKey := req.PrivateKey
+	publicKey := req.PublicKey
+	data := req.Data
+	timestamp := req.TimeStamp
+	key, err := hex.DecodeString(publicKey)
+	if err != nil {
+		return nil, err
+	}
+	clientID := encryption.Hash(key)
+	var hashdata string
+	if timestamp != "" {
+		hashdata = fmt.Sprintf("%v:%v:%v", clientID, timestamp, data)
+	} else {
+		hashdata = fmt.Sprintf("%v:%v", clientID, data)
+	}
+	hash := encryption.Hash(hashdata)
+	signature, err := encryption.Sign(privateKey, hash)
+	if err != nil {
+		return nil, err
+	}
+
+	return &minerproto.SignResponse{
+		ClientId:  clientID,
+		Hash:      hash,
+		Signature: signature,
+	}, nil
 }
