2faa1ade1ca765ef08a9bf048425934826a94844
MI <hello@superiss.com>
2021-10-13 22:14:58 +0100

    removed unnecessary variable assignment

--- code/go/0chain.net/miner/server/grpc/handlers/sign.go
+++ code/go/0chain.net/miner/server/grpc/handlers/sign.go
@@ -7,29 +7,26 @@ import (
 
 	"0chain.net/core/encryption"
 	minerproto "0chain.net/miner/proto/api/src/proto"
+	"github.com/0chain/errors"
 )
 
 // Sign
 func (m *minerGRPCService) Sign(ctx context.Context, req *minerproto.SignRequest) (*minerproto.SignResponse, error) {
-	privateKey := req.PrivateKey
-	publicKey := req.PublicKey
-	data := req.Data
-	timestamp := req.TimeStamp
-	key, err := hex.DecodeString(publicKey)
+	key, err := hex.DecodeString(req.PublicKey)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "failed to decode string")
 	}
 	clientID := encryption.Hash(key)
 	var hashdata string
-	if timestamp != "" {
-		hashdata = fmt.Sprintf("%v:%v:%v", clientID, timestamp, data)
+	if req.TimeStamp != "" {
+		hashdata = fmt.Sprintf("%v:%v:%v", clientID, req.TimeStamp, req.Data)
 	} else {
-		hashdata = fmt.Sprintf("%v:%v", clientID, data)
+		hashdata = fmt.Sprintf("%v:%v", clientID, req.Data)
 	}
 	hash := encryption.Hash(hashdata)
-	signature, err := encryption.Sign(privateKey, hash)
+	signature, err := encryption.Sign(req.PrivateKey, hash)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "unable to sign data")
 	}
 
 	return &minerproto.SignResponse{
