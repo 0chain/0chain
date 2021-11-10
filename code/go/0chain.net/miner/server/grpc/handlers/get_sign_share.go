2a8155f74afc45a88916fc3aade7fad8de1c0f1b
MI <hello@superiss.com>
2021-11-10 18:53:39 +0100

    added GetSignShare method to implement minerGRPCService

---
+++ code/go/0chain.net/miner/server/grpc/handlers/get_sign_share.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"context"
+
+	minerproto "0chain.net/miner/proto/api/src/proto"
+)
+
+// GetSignShare -
+func (m *minerGRPCService) GetSignShare(ctx context.Context, req *minerproto.GetSignShareRequest) (*minerproto.GetSignShareResponse, error) {
+
+	return &minerproto.GetSignShareResponse{
+		DkgKeyShare: &minerproto.DKGKeyShare{
+			// Id:      message.ID,
+			// Message: message.Message,
+			// Share:   message.Share,
+			// Sign:    message.Sign,
+		},
+	}, nil
+}
