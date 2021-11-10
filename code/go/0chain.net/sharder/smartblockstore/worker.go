66620c0e5bd91d1f66d0fc6f9eb696d35e85662d
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2021-11-10 07:28:03 +0545

    Add block movement to cold tier function

---
+++ code/go/0chain.net/sharder/smartblockstore/worker.go
@@ -0,0 +1,99 @@
+package smartblockstore
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	. "0chain.net/core/logging"
+)
+
+func SetupWorkers(ctx context.Context)
+
+/*
+Todo assign correct index for selectDir function; restartVolumes, recoverMetadata
+Todo Think more about caching
+Todo
+*/
+
+func moveToColdTier(smartStore *SmartStore, ctx context.Context) {
+	pollInterval := time.Hour * time.Duration(cTier.pollInterval)
+	t := time.NewTicker(pollInterval)
+	errCh := make(chan error, 1)
+	for {
+		select {
+		case <-ctx.Done():
+			break
+		case err := <-errCh:
+			Logger.Error(fmt.Sprintf("Error occurred while moving objects to cold tier. Error: %v", err))
+			break
+		case <-t.C:
+			Logger.Info("Moving blocks to cold tier")
+			minPrefix := []byte(time.Now().Add(-pollInterval).Format(time.RFC3339))
+			var newColdPath string
+
+			guideChannel := make(chan struct{}, 10)
+			wg := sync.WaitGroup{}
+
+			var ubr *UnmovedBlockRecord
+			var prevKey []byte
+			var err error
+			for ubr, prevKey, err = GetUnmovedBlock(prevKey, minPrefix); ubr == nil || err != nil; {
+				guideChannel <- struct{}{}
+				wg.Add(1)
+
+				go func() {
+					defer func() {
+						<-guideChannel
+						wg.Done()
+					}()
+
+					bwr, err := GetBlockWhereRecord(ubr.Hash)
+					if err != nil {
+						Logger.Error(fmt.Sprintf("Unexpected error; Error: %v", err))
+						return
+					}
+
+					newColdPath, err = cTier.moveBlock(bwr.Hash, bwr.BlockPath)
+					if err != nil {
+						Logger.Error(err.Error())
+						return
+					}
+
+					Logger.Info(fmt.Sprintf("Block %v is moved to %v", bwr.Hash, newColdPath))
+					switch bwr.Tiering {
+					case HotTier:
+						bwr.Tiering = newTiering(HotTier, HotTier, cTier.deleteLocal)
+					case WarmTier:
+						bwr.Tiering = newTiering(WarmTier, WarmTier, cTier.deleteLocal)
+					case CacheAndWarmTier:
+						bwr.Tiering = newTiering(CacheAndWarmTier, WarmTier, cTier.deleteLocal)
+					case CacheAndHotTier:
+						bwr.Tiering = newTiering(CacheAndHotTier, HotTier, cTier.deleteLocal)
+					}
+
+					if cTier.deleteLocal {
+						bwr.BlockPath = ""
+					}
+					bwr.CloudPath = newColdPath
+
+					if err := bwr.AddOrUpdate(); err != nil {
+						Logger.Error(fmt.Sprintf("Block %v is moved to %v but could not update meta record. Error: %v", bwr.Hash, newColdPath, err))
+						errCh <- err
+					}
+				}()
+			}
+
+			wg.Wait()
+		}
+	}
+}
+
+func newTiering(previousTiering WhichTier, skipOrSubtractTier WhichTier, deleteLocal bool) (nt WhichTier) {
+	nt = previousTiering + ColdTier
+	if deleteLocal {
+		nt -= skipOrSubtractTier
+	}
+	return
+}
