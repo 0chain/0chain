a0f9c0b22b340cdb2e4ae94ae703db883e566581
dabasov <yuderbasov@gmail.com>
2021-12-26 21:52:01 +0300

    added more logging

--- code/go/0chain.net/smartcontract/storagesc/handler.go
+++ code/go/0chain.net/smartcontract/storagesc/handler.go
@@ -56,6 +56,7 @@ func (ssc *StorageSmartContract) GetBlobbersHandler(
 	params url.Values, balances cstate.StateContextI,
 ) (interface{}, error) {
 	if balances.GetEventDB() == nil {
+		logging.Logger.Error("no event_db error")
 		return nil, smartcontract.NewErrNoResourceOrErrInternal(
 			util.ErrValueNotPresent, true, "cannot find event database",
 		)
