431dbf2a8647e9c5860b197c0c8d7074ba4e7d1d
Sam Tholiya <tholiya.sam@outlook.in>
2022-01-05 00:34:34 +0100

    Fix #759 added transaction table

---
+++ code/go/0chain.net/smartcontract/dbs/event/transaction.go
@@ -0,0 +1,37 @@
+package event
+
+import (
+	"gorm.io/gorm"
+)
+
+// Transaction model to save the transaction data
+type Transaction struct {
+	gorm.Model
+	Hash              string
+	BlockHash         string
+	Version           string
+	ClientId          string
+	ToClientId        string
+	TransactionData   string
+	Value             int64
+	Signature         string
+	CreationDate      int64
+	Fee               int64
+	TransactionType   int
+	TransactionOutput string
+	OutputHash        string
+	Status            string
+}
+
+// AddTransaction Saves the transaction to the event db
+func (edb *EventDb) AddTransaction(transaction Transaction) error {
+	res := edb.Store.Get().Create(&transaction)
+	return res.Error
+}
+
+// GetTransactionByHash finds the transaction record by hash
+func (edb *EventDb) GetTransactionByHash(hash string) (Transaction, error) {
+	tr := Transaction{}
+	res := edb.Store.Get().Table("transactions").Where("hash = ?", hash).Scan(&tr)
+	return tr, res.Error
+}
