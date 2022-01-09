c7e5c419ea5cba11126c616fd306ef3e39fa7a60
Sumit Sardana <sardana.sumit08@gmail.com>
2022-01-09 14:58:08 +0530

    - user table implemented GetUser, AddOrOverwrite functions

--- code/go/0chain.net/smartcontract/dbs/event/user.go
+++ code/go/0chain.net/smartcontract/dbs/event/user.go
@@ -2,6 +2,7 @@ package event
 
 import (
 	"0chain.net/chaincore/state"
+	"errors"
 	"fmt"
 	"gorm.io/gorm"
 )
@@ -14,6 +15,19 @@ type User struct {
 
 	// Non sql fields
 	Amount state.Balance `gorm:"-"`
+}
+
+func (edb *EventDb) GetUser(id string) (*User, error) {
+	var user User
+	result := edb.Store.Get().
+		Model(&User{}).
+		Where(&User{UserID: id}).
+		First(&user)
+	if result.Error != nil {
+		return nil, fmt.Errorf("error retrieving user %v, error %v", id, result.Error)
+	}
+
+	return &user, nil
 }
 
 func (edb *EventDb) updateIncreaseUserBalanceByAmount(u User) error {
@@ -64,6 +78,30 @@ func (edb *EventDb) updateDecreaseUserBalanceByAmount(u User) error {
 	return result.Error
 }
 
+func (edb *EventDb) overwriteUser(user User) error {
+	result := edb.Store.Get().
+		Model(&User{}).
+		Where(&User{UserID: user.UserID}).
+		Updates(User{
+			TransactionID: user.TransactionID,
+			Balance:       user.Balance,
+		})
+	return result.Error
+}
+
+func (edb *EventDb) addOrOverwriteUser(user User) error {
+	exists, err := user.exists(edb)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return edb.overwriteUser(user)
+	}
+
+	result := edb.Store.Get().Create(&user)
+	return result.Error
+}
+
 func (u *User) exists(edb *EventDb) (bool, error) {
 
 	var user User
@@ -72,7 +110,9 @@ func (u *User) exists(edb *EventDb) (bool, error) {
 		Where(&User{UserID: u.UserID}).
 		Take(&user)
 
-	if result.Error != nil {
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return false, nil
+	} else if result.Error != nil {
 		return false, fmt.Errorf("error searching for user %v, error %v",
 			u.UserID, result.Error)
 	}
