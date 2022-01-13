d40cd2272df8a82073c041b450f9c26110926cfc
Sumit Sardana <sardana.sumit08@gmail.com>
2022-01-13 20:39:42 +0530

    - nil eventdb error handling

--- code/go/0chain.net/smartcontract/storagesc/handler.go
+++ code/go/0chain.net/smartcontract/storagesc/handler.go
@@ -366,6 +366,9 @@ func (ssc *StorageSmartContract) GetAllocationsHandler(ctx context.Context,
 func (ssc *StorageSmartContract) GetActiveAllocationsCountHandler(ctx context.Context,
 	params url.Values, balances cstate.StateContextI) (interface{}, error) {
 
+	if balances.GetEventDB() == nil {
+		return nil, common.NewErrNoResource("db is not initialized")
+	}
 	count, err := balances.GetEventDB().GetActiveAllocationsCount()
 	if err != nil {
 		return nil, smartcontract.NewErrNoResourceOrErrInternal(err, true,
@@ -381,6 +384,10 @@ func (ssc *StorageSmartContract) GetActiveAllocationsCountHandler(ctx context.Co
 
 func (ssc *StorageSmartContract) GetBlobberAllocationsCountHandler(ctx context.Context,
 	params url.Values, balances cstate.StateContextI) (interface{}, error) {
+
+	if balances.GetEventDB() == nil {
+		return nil, common.NewErrNoResource("db is not initialized")
+	}
 
 	count, err := balances.GetEventDB().GetBlobberAllocationCount()
 	if err != nil {
