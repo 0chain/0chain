5a7b529a3eb8076b16cb5115da63b64d7814a22f
Sam Tholiya <tholiya.sam@outlook.in>
2022-01-15 19:23:40 +0100

    Fix #792 testcase updated

--- code/go/0chain.net/chaincore/smartcontract/handler_test.go
+++ code/go/0chain.net/chaincore/smartcontract/handler_test.go
@@ -212,7 +212,7 @@ func TestGetSmartContract(t *testing.T) {
 		{
 			name:       "storage",
 			address:    storagesc.ADDRESS,
-			restpoints: 21,
+			restpoints: 22,
 		},
 		{
 			name:       "interest",
