b11af33ee90c46d78e9d14acb782449295f9ad42
Sam Tholiya <tholiya.sam@outlook.in>
2022-01-15 22:27:07 +0100

    Fix #890 updated unit test case

--- code/go/0chain.net/chaincore/smartcontract/handler_test.go
+++ code/go/0chain.net/chaincore/smartcontract/handler_test.go
@@ -212,7 +212,7 @@ func TestGetSmartContract(t *testing.T) {
 		{
 			name:       "storage",
 			address:    storagesc.ADDRESS,
-			restpoints: 19,
+			restpoints: 20,
 		},
 		{
 			name:       "interest",
