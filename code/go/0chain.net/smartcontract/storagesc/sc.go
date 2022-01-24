80ba4241184b0f06f4e3615c296929badb83aad8
Sam Tholiya <tholiya.sam@outlook.in>
2022-01-24 22:49:37 +0100

    Fix #890 updated rest api handler mapping

--- code/go/0chain.net/smartcontract/storagesc/sc.go
+++ code/go/0chain.net/smartcontract/storagesc/sc.go
@@ -118,7 +118,7 @@ func (ssc *StorageSmartContract) setSC(sc *sci.SmartContract, bcContext sci.BCCo
 	ssc.SmartContract.RestHandlers["/getChallengePoolStat"] = ssc.getChallengePoolStatHandler
 	// events db
 	ssc.SmartContract.RestHandlers["/transaction"] = ssc.GetTransactionByHashHandler
-	ssc.SmartContract.RestHandlers["/transactions"] = ssc.GetTransactionByClientHandler
+	ssc.SmartContract.RestHandlers["/transactions"] = ssc.GetTransactionByFilterHandler
 	ssc.SmartContractExecutionStats["/get_block_by_hash"] = ssc.GetBlockByHashHandler
 	ssc.SmartContractExecutionStats["/get_blocks"] = ssc.GetBlocksHandler
 }
