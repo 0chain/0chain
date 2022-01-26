36d2c068f46071469b070e96a3de5608c17e240e
amanjain <amanjain5221@gmail.com>
2022-01-26 23:22:28 +0530

    Fix tests

--- code/go/0chain.net/smartcontract/storagesc/curator_test.go
+++ code/go/0chain.net/smartcontract/storagesc/curator_test.go
@@ -76,7 +76,7 @@ func TestAddCurator(t *testing.T) {
 
 		balances.On(
 			"EmitEvent",
-			event.TypeStats, event.TagAddCurator, mock.Anything, mock.Anything,
+			event.TypeStats, event.TagAddCurator, mock.Anything,
 		).Return().Maybe()
 
 		return args{ssc, txn, input, balances}
@@ -178,7 +178,7 @@ func TestRemoveCurator(t *testing.T) {
 
 		balances.On(
 			"EmitEvent",
-			event.TypeStats, event.TagRemoveCurator, mock.Anything, mock.Anything,
+			event.TypeStats, event.TagRemoveCurator, mock.Anything,
 		).Return().Maybe()
 
 		return args{ssc, txn, input, balances}
