f3eea822776172efeb0a638b18003d6f14350b65
Sumit <sardana.sumit08@gmail.com>
2022-02-04 04:04:43 +0530

    - updated blobber_block_reward_test

--- code/go/0chain.net/smartcontract/storagesc/block_reward_test.go
+++ code/go/0chain.net/smartcontract/storagesc/block_reward_test.go
@@ -1,1 +1,97 @@
 package storagesc
+
+import (
+	"0chain.net/chaincore/state"
+	"0chain.net/smartcontract/partitions"
+	"0chain.net/smartcontract/stakepool"
+	"github.com/stretchr/testify/require"
+	"strconv"
+	"testing"
+)
+
+func TestStorageSmartContract_blobberBlockRewards(t *testing.T) {
+	balances := newTestBalances(t, true)
+	type params struct {
+		numBlobbers       int
+		wp                []state.Balance
+		successChallenges []int
+		delegatesBal      [][]state.Balance
+	}
+
+	type result struct {
+		blobberRewards          []state.Balance
+		blobberDelegatesRewards [][]state.Balance
+	}
+
+	setupRewards := func(t *testing.T, p params, sc *StorageSmartContract) {
+		setConfig(t, balances)
+		allBR, err := getActivePassedBlobbersList(balances)
+		require.NoError(t, err)
+		for i := 0; i < p.numBlobbers; i++ {
+			bID := "blobber" + strconv.Itoa(i)
+			_, err = allBR.Add(&partitions.BlobberRewardNode{
+				Id:                bID,
+				SuccessChallenges: p.successChallenges[i],
+				WritePrice:        p.wp[i],
+			}, balances)
+			require.NoError(t, err)
+
+			sp := newStakePool()
+			sp.Settings.DelegateWallet = bID
+			for j, bal := range p.delegatesBal[i] {
+				dp := new(stakepool.DelegatePool)
+				dp.Balance = bal
+				sp.Pools["delegate"+strconv.Itoa(j)] = dp
+			}
+			_, err = balances.InsertTrieNode(stakePoolKey(sc.ID, bID), sp)
+			require.NoError(t, err)
+		}
+		err = allBR.Save(balances)
+		require.NoError(t, err)
+	}
+
+	compareResult := func(t *testing.T, p params, r result, ssc *StorageSmartContract) {
+		for i := 0; i < p.numBlobbers; i++ {
+			bID := "blobber" + strconv.Itoa(i)
+			sp, err := ssc.getStakePool(bID, balances)
+			require.NoError(t, err)
+			require.EqualValues(t, r.blobberRewards[i], sp.Reward)
+		}
+		allBR, err := getActivePassedBlobbersList(balances)
+		require.NoError(t, err)
+		err = allBR.Migrate("dump", balances)
+		require.NoError(t, err)
+	}
+
+	tests := []struct {
+		name    string
+		wantErr bool
+		params  params
+		result  result
+	}{
+		{
+			name: "test 1 blobber",
+			params: params{
+				numBlobbers:       1,
+				wp:                []state.Balance{2},
+				successChallenges: []int{10},
+				delegatesBal:      [][]state.Balance{{1, 0, 3}},
+			},
+			result: result{
+				blobberRewards:          []state.Balance{1000},
+				blobberDelegatesRewards: [][]state.Balance{{250, 0, 750}},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ssc := newTestStorageSC()
+			setupRewards(t, tt.params, ssc)
+			err := ssc.blobberBlockRewards(balances)
+			require.EqualValues(t, tt.wantErr, err != nil)
+			compareResult(t, tt.params, tt.result, ssc)
+
+		})
+	}
+}
