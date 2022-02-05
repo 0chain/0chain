dab550bdc857f2f314f9be5b3d68d17a3234a09e
Roman Shevchenko <rrrooommmaaa@mail.ru>
2022-02-05 10:56:39 +0000

    fix

--- code/go/0chain.net/chaincore/chain/entity_main.go
+++ code/go/0chain.net/chaincore/chain/entity_main.go
@@ -3,8 +3,12 @@
 package chain
 
 import (
+	"context"
+
+	"0chain.net/chaincore/block"
 	"0chain.net/chaincore/node"
 	"0chain.net/chaincore/round"
+	"0chain.net/chaincore/transaction"
 )
 
 //IsRoundGenerator - is this miner a generator for this round
