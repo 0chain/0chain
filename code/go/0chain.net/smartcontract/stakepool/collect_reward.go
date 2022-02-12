c3f043cf8ceab546e2c3d4af828b45f7cc1337d5
Piers Shepperson <piers.shepperson@btinternet.com>
2022-02-12 11:47:53 +0000

    review issues

--- code/go/0chain.net/smartcontract/stakepool/collect_reward.go
+++ code/go/0chain.net/smartcontract/stakepool/collect_reward.go
@@ -2,10 +2,6 @@ package stakepool
 
 import (
 	"encoding/json"
-
-	cstate "0chain.net/chaincore/chain/state"
-	"0chain.net/chaincore/state"
-	"0chain.net/core/common"
 )
 
 type CollectRewardRequest struct {
@@ -15,44 +11,4 @@ type CollectRewardRequest struct {
 
 func (spr *CollectRewardRequest) Decode(p []byte) error {
 	return json.Unmarshal(p, spr)
-}
-
-func CollectReward(
-	client string,
-	input []byte,
-	balances cstate.StateContextI,
-) (state.Balance, error) {
-	var prr CollectRewardRequest
-	if err := prr.Decode(input); err != nil {
-		return 0, common.NewErrorf("pay_reward_failed",
-			"can't decode request: %v", err)
-	}
-
-	var usp *UserStakePools
-	usp, err := GetUserStakePool(prr.ProviderType, client, balances)
-	if err != nil {
-		return 0, common.NewErrorf("pay_reward_failed",
-			"can't get related user stake pools: %v", err)
-	}
-
-	providerId := usp.Find(prr.PoolId)
-	if len(providerId) == 0 {
-		return 0, common.NewErrorf("pay_reward_failed",
-			"user %v does not own stake pool %v", client, prr.PoolId)
-	}
-
-	sp, err := GetStakePool(prr.ProviderType, providerId, balances)
-	if err != nil {
-		return 0, common.NewErrorf("pay_reward_failed",
-			"can't get related stake pool: %v", err)
-	}
-
-	total, err := sp.MintRewards(
-		client, prr.PoolId, providerId, prr.ProviderType, usp, balances)
-	if err != nil {
-		return 0, common.NewErrorf("pay_reward_failed",
-			"error emptying account, %v", err)
-	}
-
-	return total, nil
 }
