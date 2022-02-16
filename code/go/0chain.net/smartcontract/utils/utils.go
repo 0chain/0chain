d00d757816107aa3460212b20be45ac4125f122e
Sumit <sardana.sumit08@gmail.com>
2022-02-17 04:33:19 +0530

    - get current round/ get previous round checks added

--- code/go/0chain.net/smartcontract/utils/utils.go
+++ code/go/0chain.net/smartcontract/utils/utils.go
@@ -1,11 +1,22 @@
 package utils
 
 func GetCurrentRewardRound(currentRound, period int64) int64 {
-	extra := currentRound % period
-	return currentRound - extra
+	if period > 0 {
+		extra := currentRound % period
+		if currentRound >= extra {
+			return currentRound - extra
+		} else {
+			return 0
+		}
+	}
+	return 0
 }
 
 func GetPreviousRewardRound(currentRound, period int64) int64 {
 	crr := GetCurrentRewardRound(currentRound, period)
-	return crr - period
+	if crr >= period {
+		return crr - period
+	} else {
+		return 0
+	}
 }
