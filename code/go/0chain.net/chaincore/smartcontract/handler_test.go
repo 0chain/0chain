470072c1e4ea915e3b4280859fdb41b583e4767c
Sumit <sardana.sumit08@gmail.com>
2022-02-21 20:35:17 +0530

    - fix minersc unit test

--- code/go/0chain.net/chaincore/smartcontract/handler_test.go
+++ code/go/0chain.net/chaincore/smartcontract/handler_test.go
@@ -458,7 +458,7 @@ func TestExecuteSmartContract(t *testing.T) {
 					InputData:    blob,
 				},
 			},
-			want:    "{\"simple_miner\":{\"id\":\"\",\"n2n_host\":\"\",\"host\":\"\",\"port\":0,\"geolocation\":{\"latitude\":0,\"longitude\":0},\"path\":\"\",\"public_key\":\"\",\"short_name\":\"\",\"build_tag\":\"\",\"total_stake\":0,\"delete\":false,\"delegate_wallet\":\"\",\"service_charge\":0,\"number_of_delegates\":0,\"min_stake\":0,\"max_stake\":0,\"stat\":{},\"last_health_check\":0}}",
+			want:    "{\"simple_miner\":{\"id\":\"\",\"n2n_host\":\"\",\"host\":\"\",\"port\":0,\"geolocation\":{\"latitude\":0,\"longitude\":0},\"path\":\"\",\"public_key\":\"\",\"short_name\":\"\",\"build_tag\":\"\",\"total_stake\":0,\"delete\":false,\"delegate_wallet\":\"\",\"service_charge\":0,\"number_of_delegates\":0,\"min_stake\":0,\"max_stake\":0,\"stat\":{},\"last_health_check\":0,\"last_setting_update_round\":0}}",
 			wantErr: false,
 		},
 	}
