fd040cdedf01a7ebb485aa1f9f77f6fda948fffd
Sam Tholiya <tholiya.sam@outlook.in>
2022-02-22 22:06:49 +0100

    Fix #760 pool filter updated

--- code/go/0chain.net/smartcontract/dbs/event/allocation_pool.go
+++ code/go/0chain.net/smartcontract/dbs/event/allocation_pool.go
@@ -1,6 +1,9 @@
 package event
 
-import "gorm.io/gorm"
+import (
+	"github.com/guregu/null"
+	"gorm.io/gorm"
+)
 
 type AllocationPool struct {
 	gorm.Model
@@ -14,4 +17,25 @@ type AllocationPool struct {
 
 func (edb *EventDb) addAllocationPool(allocationPool AllocationPool) error {
 	return edb.Get().Create(&allocationPool).Error
+}
+
+type AllocationPoolFilter struct {
+	gorm.Model
+	AllocationID  null.String
+	TransactionId null.String
+	UserID        null.String
+	Balance       null.Int
+	IsWritePool   null.Bool
+}
+
+func (edb *EventDb) GetAllocationPoolWithFilterAndPagination(filter AllocationPoolFilter, offset, limit int) ([]AllocationPool, error) {
+	query := edb.Get().Model(&AllocationPool{}).Where(&filter)
+	if offset != -1 {
+		query = query.Offset(offset)
+	}
+	if limit != -1 {
+		query = query.Limit(limit)
+	}
+	var allocationPools []AllocationPool
+	return allocationPools, query.Scan(&allocationPools).Error
 }
--- code/go/0chain.net/smartcontract/dbs/event/allocation_pool_test.go
+++ code/go/0chain.net/smartcontract/dbs/event/allocation_pool_test.go
@@ -2,10 +2,12 @@ package event
 
 import (
 	"os"
+	"strconv"
 	"testing"
 	"time"
 
 	"0chain.net/smartcontract/dbs"
+	"github.com/guregu/null"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -67,4 +69,75 @@ func TestWriteAllocationPool(t *testing.T) {
 		},
 	})
 	assert.Error(t, err, "there should be an error")
+}
+
+func TestWriteAllocationPoolFilter(t *testing.T) {
+	access := dbs.DbAccess{
+		Enabled:         true,
+		Name:            os.Getenv("POSTGRES_DB"),
+		User:            os.Getenv("POSTGRES_USER"),
+		Password:        os.Getenv("POSTGRES_PASSWORD"),
+		Host:            os.Getenv("POSTGRES_HOST"),
+		Port:            os.Getenv("POSTGRES_PORT"),
+		MaxIdleConns:    100,
+		MaxOpenConns:    200,
+		ConnMaxLifetime: 20 * time.Second,
+	}
+	eventDb, err := NewEventDb(access)
+	if err != nil {
+		t.Skip("only for local debugging, requires local postgresql")
+	}
+	defer eventDb.Close()
+	err = eventDb.AutoMigrate()
+	defer eventDb.drop()
+	assert.NoError(t, err, "error while migrating")
+	createAllocationPool(t, eventDb, 20)
+	t.Run("return only read allocation", func(t *testing.T) {
+		allocations, err := eventDb.GetAllocationPoolWithFilterAndPagination(AllocationPoolFilter{
+			IsWritePool: null.BoolFrom(false),
+		}, 0, 10)
+		assert.NoError(t, err, "There should be no error")
+		assert.Equal(t, 10, len(allocations), "not all read allocations were returned")
+		for _, allocation := range allocations {
+			assert.Equal(t, false, allocation.IsWritePool, "write pool should not be returned")
+		}
+	})
+	t.Run("return only write allocation", func(t *testing.T) {
+		allocations, err := eventDb.GetAllocationPoolWithFilterAndPagination(AllocationPoolFilter{
+			IsWritePool: null.BoolFrom(true),
+		}, 0, 10)
+		assert.NoError(t, err, "There should be no error")
+		assert.Equal(t, 10, len(allocations), "not all write allocations were returned")
+		for _, allocation := range allocations {
+			assert.Equal(t, true, allocation.IsWritePool, "read pool should not be returned")
+		}
+	})
+}
+
+func createAllocationPool(t *testing.T, eventDb *EventDb, count int) {
+	for i := 0; i < count; i++ {
+		indexString := strconv.Itoa(i)
+		err := eventDb.addAllocationPool(
+			AllocationPool{
+				AllocationID:  "allocation" + indexString,
+				TransactionId: "transaction" + indexString,
+				UserID:        "userid" + indexString,
+				Balance:       int64(i),
+				IsWritePool:   i%2 == 0,
+				Blobbers: []BlobberPool{
+					{
+						AllocationPoolID: "allocation" + indexString,
+						BlobberID:        "blobber1",
+						Balance:          2,
+					},
+					{
+						AllocationPoolID: "allocation" + indexString,
+						BlobberID:        "blobber2",
+						Balance:          2,
+					},
+				},
+			},
+		)
+		assert.NoError(t, err, "error while creating allocation")
+	}
 }
