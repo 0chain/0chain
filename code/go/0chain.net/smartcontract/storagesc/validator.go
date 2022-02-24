bea004f947fd5c5dff7ca7ed94150a99cb977fc7
kushthedude <kushthedude@gmail.com>
2022-02-25 02:15:12 +0530

    fix
    Signed-off-by: kushthedude <kushthedude@gmail.com>

--- code/go/0chain.net/smartcontract/storagesc/validator.go
+++ code/go/0chain.net/smartcontract/storagesc/validator.go
@@ -1,7 +1,7 @@
 package storagesc
 
 import (
-	c_state "0chain.net/chaincore/chain/state"
+	cstate "0chain.net/chaincore/chain/state"
 	"0chain.net/chaincore/transaction"
 	"0chain.net/core/common"
 	"0chain.net/core/util"
@@ -11,7 +11,7 @@ import (
 
 const allValidatorsPartitionSize = 50
 
-func getValidatorsList(balances c_state.StateContextI) (partitions.RandPartition, error) {
+func getValidatorsList(balances cstate.StateContextI) (partitions.RandPartition, error) {
 	all, err := partitions.GetRandomSelector(ALL_VALIDATORS_KEY, balances)
 	if err != nil {
 		if err != util.ErrValueNotPresent {
@@ -28,7 +28,7 @@ func getValidatorsList(balances c_state.StateContextI) (partitions.RandPartition
 	return all, nil
 }
 
-func (sc *StorageSmartContract) addValidator(t *transaction.Transaction, input []byte, balances c_state.StateContextI) (string, error) {
+func (sc *StorageSmartContract) addValidator(t *transaction.Transaction, input []byte, balances cstate.StateContextI) (string, error) {
 	newValidator := &ValidationNode{}
 	err := newValidator.Decode(input) //json.Unmarshal(input, &newBlobber)
 	if err != nil {
@@ -102,4 +102,73 @@ func (sc *StorageSmartContract) addValidator(t *transaction.Transaction, input [
 
 	buff := newValidator.Encode()
 	return string(buff), nil
+}
+
+func (sc *StorageSmartContract) updateValidatorSettings(t *transaction.Transaction,
+	input []byte, balances cstate.StateContextI,
+) (resp string, err error) {
+	// get smart contract configuration
+	conf, err := sc.getConfig(balances, true)
+	if err != nil {
+		return "", common.NewError("update_validator_settings_failed",
+			"can't get config: "+err.Error())
+	}
+
+	var validators *StorageNodes
+	if blobbers, err = sc.getBlobbersList(balances); err != nil {
+		return "", common.NewError("update_blobber_settings_failed",
+			"failed to get blobber list: "+err.Error())
+	}
+
+	var updatedBlobber = new(StorageNode)
+	if err = updatedBlobber.Decode(input); err != nil {
+		return "", common.NewError("update_blobber_settings_failed",
+			"malformed request: "+err.Error())
+	}
+
+	var blobber *StorageNode
+	sc.get
+	if blobber, err = sc.getBlobber(updatedBlobber.ID, balances); err != nil {
+		return "", common.NewError("update_blobber_settings_failed",
+			"can't get the blobber: "+err.Error())
+	}
+
+	var sp *stakePool
+	if sp, err = sc.getStakePool(updatedBlobber.ID, balances); err != nil {
+		return "", common.NewError("update_blobber_settings_failed",
+			"can't get related stake pool: "+err.Error())
+	}
+
+	if sp.Settings.DelegateWallet == "" {
+		return "", common.NewError("update_blobber_settings_failed",
+			"blobber's delegate_wallet is not set")
+	}
+
+	if t.ClientID != sp.Settings.DelegateWallet {
+		return "", common.NewError("update_blobber_settings_failed",
+			"access denied, allowed for delegate_wallet owner only")
+	}
+
+	blobber.Terms = updatedBlobber.Terms
+	blobber.Capacity = updatedBlobber.Capacity
+
+	if err = sc.updateBlobber(t, conf, blobber, blobbers, balances); err != nil {
+		return "", common.NewError("update_blobber_settings_failed", err.Error())
+	}
+
+	// save all the blobbers
+	_, err = balances.InsertTrieNode(ALL_BLOBBERS_KEY, blobbers)
+	if err != nil {
+		return "", common.NewError("update_blobber_settings_failed",
+			"saving all blobbers: "+err.Error())
+	}
+
+	// save blobber
+	_, err = balances.InsertTrieNode(blobber.GetKey(sc.ID), blobber)
+	if err != nil {
+		return "", common.NewError("update_blobber_settings_failed",
+			"saving blobber: "+err.Error())
+	}
+
+	return string(blobber.Encode()), nil
 }
