96a696c29ec5c165e6c082d20019d0f0a931eac6
Piers Shepperson <piers.shepperson@btinternet.com>
2022-02-26 10:52:23 +0000

    temp logging

--- code/go/0chain.net/smartcontract/minersc/sc.go
+++ code/go/0chain.net/smartcontract/minersc/sc.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 	"sync"
 
+	"go.uber.org/zap"
+
 	"0chain.net/chaincore/smartcontract"
 
 	cstate "0chain.net/chaincore/chain/state"
@@ -153,7 +155,13 @@ func (msc *MinerSmartContract) Execute(t *transaction.Transaction,
 	if !found {
 		return common.NewErrorf("failed execution", "no miner smart contract method with name: %v", funcName).Error(), nil
 	}
-	return scFunc(t, input, gn, balances)
+	resp, err := scFunc(t, input, gn, balances)
+	if err != nil {
+		Logger.Info("piers Execute",
+			zap.String("func name", funcName), zap.Error(err))
+	}
+	return resp, err
+	//return scFunc(t, input, gn, balances)
 }
 
 func getHostnameAndPort(burl string) (string, int, error) {
