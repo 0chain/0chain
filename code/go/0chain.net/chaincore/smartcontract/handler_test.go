e46fa10d06e4f179f349617b0eaa6bb35380d043
kushthedude <kushthedude@gmail.com>
2022-03-12 21:12:23 +0530

    fix: sc
    Signed-off-by: kushthedude <kushthedude@gmail.com>

--- code/go/0chain.net/chaincore/smartcontract/handler_test.go
+++ code/go/0chain.net/chaincore/smartcontract/handler_test.go
@@ -212,7 +212,7 @@ func TestGetSmartContract(t *testing.T) {
 		{
 			name:       "storage",
 			address:    storagesc.ADDRESS,
-			restpoints: 28,
+			restpoints: 29,
 		},
 		{
 			name:       "interest",
