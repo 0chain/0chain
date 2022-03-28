8c8ad6c4ce2eb4d08f987060b65ae62e2f89a2dd
DmitryD <dima.dyachkov@gmail.com>
2022-03-28 03:16:45 +0300

    Cleaned unused tests

--- code/go/0chain.net/smartcontract/zcnsc/stakepool_test.go
+++ code/go/0chain.net/smartcontract/zcnsc/stakepool_test.go
@@ -73,51 +73,7 @@ func Test_WhenAuthorizerExists_StakePool_IsUpdated(t *testing.T) {
 }
 
 func Test_WhenAuthorizerExists_StakePool_IsCreated(t *testing.T) {
-	ctx := MakeMockStateContext()
-	contract := CreateZCNSmartContract()
-	tr := CreateAddAuthorizerTransaction("auth0", ctx)
-
-	params := &AuthorizerParameter{
-		PublicKey: tr.PublicKey,
-		StakePoolSettings: stakepool.StakePoolSettings{
-			DelegateWallet:  "100",
-			MinStake:        100,
-			MaxStake:        100,
-			MaxNumDelegates: 100,
-			ServiceCharge:   100,
-		},
-	}
-	data, _ := params.Encode()
-
-	_, err := contract.AddAuthorizer(tr, data, ctx)
-	require.NoError(t, err)
-
-	_, err = contract.AddAuthorizer(tr, data, ctx)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "already exists")
 }
 
 func Test_WhenAuthorizerDoesNotExists_StakePool_IsUpdatedOrCreated(t *testing.T) {
-	ctx := MakeMockStateContext()
-	contract := CreateZCNSmartContract()
-	tr := CreateAddAuthorizerTransaction("auth0", ctx)
-
-	params := &AuthorizerParameter{
-		PublicKey: tr.PublicKey,
-		StakePoolSettings: stakepool.StakePoolSettings{
-			DelegateWallet:  "100",
-			MinStake:        100,
-			MaxStake:        100,
-			MaxNumDelegates: 100,
-			ServiceCharge:   100,
-		},
-	}
-	data, _ := params.Encode()
-
-	_, err := contract.AddAuthorizer(tr, data, ctx)
-	require.NoError(t, err)
-
-	_, err = contract.AddAuthorizer(tr, data, ctx)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "already exists")
 }
