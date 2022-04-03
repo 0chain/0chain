64187afe5951793c1552231d592d9d6a60f2dbb0
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-04-03 22:13:09 +0530

    add 0 validation

--- code/go/0chain.net/smartcontract/storagesc/writepool.go
+++ code/go/0chain.net/smartcontract/storagesc/writepool.go
@@ -255,7 +255,7 @@ func (ssc *StorageSmartContract) writePoolLock(t *transaction.Transaction,
 			"missing allocation ID in request")
 	}
 
-	if t.Value < conf.MinLock {
+	if t.Value < conf.MinLock || t.Value <= 0 {
 		return "", common.NewError("write_pool_lock_failed",
 			"insufficient amount to lock")
 	}
