0f381785d4f8e4d25d93939a89a412379307cf7a
Piers Shepperson <piers.shepperson@btinternet.com>
2022-04-07 10:29:02 +0100

    msgp

--- code/go/0chain.net/rest/storage_model_gen.go
+++ code/go/0chain.net/rest/storage_model_gen.go
@@ -522,3 +522,47 @@ func (z *storageStakePool) Msgsize() (s int) {
 	s = 1 + 10 + z.StakePool.Msgsize() + 12 + z.TotalOffers.Msgsize() + 13 + z.TotalUnStake.Msgsize()
 	return
 }
+
+// MarshalMsg implements msgp.Marshaler
+func (z userPoolStat) MarshalMsg(b []byte) (o []byte, err error) {
+	o = msgp.Require(b, z.Msgsize())
+	// map header, size 0
+	o = append(o, 0x80)
+	return
+}
+
+// UnmarshalMsg implements msgp.Unmarshaler
+func (z *userPoolStat) UnmarshalMsg(bts []byte) (o []byte, err error) {
+	var field []byte
+	_ = field
+	var zb0001 uint32
+	zb0001, bts, err = msgp.ReadMapHeaderBytes(bts)
+	if err != nil {
+		err = msgp.WrapError(err)
+		return
+	}
+	for zb0001 > 0 {
+		zb0001--
+		field, bts, err = msgp.ReadMapKeyZC(bts)
+		if err != nil {
+			err = msgp.WrapError(err)
+			return
+		}
+		switch msgp.UnsafeString(field) {
+		default:
+			bts, err = msgp.Skip(bts)
+			if err != nil {
+				err = msgp.WrapError(err)
+				return
+			}
+		}
+	}
+	o = bts
+	return
+}
+
+// Msgsize returns an upper bound estimate of the number of bytes occupied by the serialized message
+func (z userPoolStat) Msgsize() (s int) {
+	s = 1
+	return
+}
