7804f5e24b2089c25fc20e42fa3f77f126b66c9a
Piers Shepperson <piers.shepperson@btinternet.com>
2022-04-08 17:24:00 +0100

    comments

--- code/go/0chain.net/smartcontract/interestpoolsc/sc.go
+++ code/go/0chain.net/smartcontract/interestpoolsc/sc.go
@@ -81,9 +81,9 @@ func (ipsc *InterestPoolSmartContract) GetCost(t *transaction.Transaction, funcN
 
 func (ipsc *InterestPoolSmartContract) setSC(sc *smartcontractinterface.SmartContract, bcContext smartcontractinterface.BCContextI) {
 	ipsc.SmartContract = sc
-	//ipsc.SmartContract.RestHandlers["/getPoolsStats"] = ipsc.getPoolsStats
-	//ipsc.SmartContract.RestHandlers["/getLockConfig"] = ipsc.getLockConfig
-	//ipsc.SmartContract.RestHandlers["/getConfig"] = ipsc.getConfig
+	ipsc.SmartContract.RestHandlers["/getPoolsStats"] = ipsc.getPoolsStats
+	ipsc.SmartContract.RestHandlers["/getLockConfig"] = ipsc.getLockConfig
+	ipsc.SmartContract.RestHandlers["/getConfig"] = ipsc.getConfig
 	ipsc.SmartContractExecutionStats["lock"] = metrics.GetOrRegisterTimer(fmt.Sprintf("sc:%v:func:%v", ipsc.ID, "lock"), nil)
 	ipsc.SmartContractExecutionStats["unlock"] = metrics.GetOrRegisterTimer(fmt.Sprintf("sc:%v:func:%v", ipsc.ID, "unlock"), nil)
 	ipsc.SmartContractExecutionStats["updateVariables"] = metrics.GetOrRegisterTimer(fmt.Sprintf("sc:%v:func:%v", ipsc.ID, "updateVariables"), nil)
