0a362119cb0e1445f74ace8c85540c6e691b44e5
Sumit <sardana.sumit08@gmail.com>
2022-05-17 19:38:44 +0530

    - conversion unit added

--- code/go/0chain.net/pkg/tokens/units.go
+++ code/go/0chain.net/pkg/tokens/units.go
@@ -3,3 +3,9 @@ package tokens
 //go:generate msgp -io=false -tests=false -v
 //SAS - any quantity that is represented as an integer in the lowest denomination
 type SAS int64
+
+const (
+	ZCN  SAS = 1e10
+	mZCN SAS = 1e7
+	uZCN SAS = 1e4
+)
