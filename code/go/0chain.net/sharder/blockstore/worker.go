23ad7d38bdd2feb2e6b2a3d2e473194aeb17a139
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2022-05-20 07:54:03 +0545

    Add worker

---
+++ code/go/0chain.net/sharder/blockstore/worker.go
@@ -0,0 +1,34 @@
+package blockstore
+
+import (
+	"context"
+	"time"
+
+	"0chain.net/core/logging"
+)
+
+func setupVolumeRevivingWorker(ctx context.Context) {
+	logging.Logger.Info("Setting volume reviving worker")
+	mainStore := Store.(*blockStore)
+	// interval := 2 * time.Minute
+	t := time.NewTicker(mainStore.blockMovementInterval)
+
+	var dTier *diskTier
+
+	for {
+		select {
+		case <-ctx.Done():
+			break
+		case <-t.C:
+			logging.Logger.Info("Checking if volume is able to store blocks")
+			for vPath, volume := range unableVolumes {
+				if volume.isAbleToStoreBlock(dTier) {
+					dTier.Mu.Lock()
+					dTier.Volumes = append(dTier.Volumes, volume)
+					delete(unableVolumes, vPath)
+					dTier.Mu.Unlock()
+				}
+			}
+		}
+	}
+}
