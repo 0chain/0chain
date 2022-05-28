ee195312241001a763c536cd94e974e308cebf90
Piers Shepperson <piers.shepperson@btinternet.com>
2022-05-28 13:43:06 +0100

    add missing tests

--- code/go/0chain.net/smartcontract/minersc/benchmark_tests_test.go
+++ code/go/0chain.net/smartcontract/minersc/benchmark_tests_test.go
@@ -1,6 +1,8 @@
 package minersc
 
 import (
+	"0chain.net/chaincore/smartcontract"
+	sci "0chain.net/chaincore/smartcontractinterface"
 	"0chain.net/smartcontract/benchmark"
 	"0chain.net/smartcontract/benchmark/mocks"
 	"github.com/stretchr/testify/mock"
@@ -14,11 +16,16 @@ func TestMinerBenchmarkTests(t *testing.T) {
 	mockSigScheme.On("SetPrivateKey", mock.Anything).Return()
 	mockSigScheme.On("Sign", mock.Anything).Return("", nil)
 
-	msc := NewMinerSmartContract()
+	var msc = &MinerSmartContract{
+		SmartContract: sci.NewSC(ADDRESS),
+		bcContext:     &smartcontract.BCContext{},
+	}
+	msc.smartContractFunctions = make(map[string]smartContractFunction)
+	msc.initSC()
 
 	require.EqualValues(
 		t,
-		len(msc.GetExecutionStats()),
+		len(msc.smartContractFunctions),
 		len(BenchmarkTests(benchmark.MockBenchData, mockSigScheme).Benchmarks),
 	)
 }
