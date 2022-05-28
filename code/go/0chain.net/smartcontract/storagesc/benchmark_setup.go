b370d4ba380101d04c0d73cf9f8dc74f188319bc
dabasov <yuderbasov@gmail.com>
2022-05-28 23:16:57 +0300

    fixed comments

--- code/go/0chain.net/smartcontract/storagesc/benchmark_setup.go
+++ code/go/0chain.net/smartcontract/storagesc/benchmark_setup.go
@@ -2,7 +2,6 @@ package storagesc
 
 import (
 	"0chain.net/core/datastore"
-	"context"
 	"encoding/json"
 	"log"
 	"strconv"
@@ -568,37 +567,6 @@ func AddMockBlobbers(
 		panic(err)
 	}
 	return rtvBlobbers
-}
-
-func UpdateHealthCheck(balances cstate.StateContextI) {
-	var sscId = StorageSmartContract{
-		SmartContract: sci.NewSC(ADDRESS),
-	}.ID
-
-	for i := 0; i < viper.GetInt(sc.NumBlobbers); i++ {
-		err := executor.Run(context.Background(), func(i int) func() error {
-			return func() error {
-				id := getMockBlobberId(i)
-				blobber := &StorageNode{ID: id}
-				err := balances.GetTrieNode(blobber.GetKey(sscId), blobber)
-
-				if err != nil {
-					return err
-				}
-				blobber.LastHealthCheck = common.Timestamp(time.Now().Unix())
-
-				if _, err := balances.InsertTrieNode(blobber.GetKey(sscId), blobber); err != nil {
-					return err
-				}
-				return nil
-
-			}
-		}(i))
-
-		if err != nil {
-			panic(err)
-		}
-	}
 }
 
 func AddMockValidators(
