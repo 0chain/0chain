2b6132d06ca2f86321e5aa826b634e357e9b62e5
Lz <imlangzi@qq.com>
2022-06-20 08:33:58 +0800

    fix(git): merge staging

--- code/go/0chain.net/smartcontract/dbs/event/blobber.go
+++ code/go/0chain.net/smartcontract/dbs/event/blobber.go
@@ -205,10 +205,6 @@ func (edb *EventDb) GetBlobberIdsFromUrls(urls []string) ([]string, error) {
 
 func (edb *EventDb) GetBlobbersFromParams(allocation AllocationQuery) ([]string, error) {
 	dbStore := edb.Store.Get().Model(&Blobber{})
-<<<<<<< HEAD
-	//dbStore = dbStore.Where("challenge_completion_time <= ?", allocation.MaxChallengeCompletionTime.Nanoseconds())
-=======
->>>>>>> staging
 	dbStore = dbStore.Where("read_price between ? and ?", allocation.ReadPriceRange.Min, allocation.ReadPriceRange.Max)
 	dbStore = dbStore.Where("write_price between ? and ?", allocation.WritePriceRange.Min, allocation.WritePriceRange.Max)
 	dbStore = dbStore.Where("max_offer_duration >= ?", allocation.MaxOfferDuration.Nanoseconds())
