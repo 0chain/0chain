9faefb16fb4014f104ecee4f7c4ecc56d769cf0b
0xhrsh <harsh1429@gmail.com>
2022-06-26 06:18:49 +0530

    validating txns

--- code/go/0chain.net/miner/protocol_block_main_test.go
+++ code/go/0chain.net/miner/protocol_block_main_test.go
@@ -1,13 +1,28 @@
 package miner
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"os"
 	"testing"
 
+	"0chain.net/chaincore/chain"
+	"0chain.net/chaincore/client"
+	"0chain.net/chaincore/config"
+	"0chain.net/chaincore/node"
 	"0chain.net/chaincore/transaction"
+	"0chain.net/core/common"
 	"0chain.net/core/datastore"
-	"github.com/stretchr/testify/require"
+	"0chain.net/core/encryption"
+	"0chain.net/core/memorystore"
+
+	"github.com/alicebob/miniredis/v2"
+	"github.com/go-redis/redis"
+	// "github.com/stretchr/testify/require"
 )
 
+/*
 func TestTxnIterInfo_checkForCurrent(t *testing.T) {
 	type fields struct {
 		pastTxns    []datastore.Entity
@@ -276,4 +291,137 @@ func TestTxnIterInfo_checkForInvalidTxns(t *testing.T) {
 			require.Equal(t, tt.want, invalidTxns)
 		})
 	}
+}
+
+*/
+
+func setupTempRocksDBDir() func() {
+	if err := os.MkdirAll("data/rocksdb/state", 0766); err != nil {
+		panic(err)
+	}
+
+	return func() {
+		if err := os.RemoveAll("data/rocksdb/state"); err != nil {
+			panic(err)
+		}
+
+		if err := os.RemoveAll("log"); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func TestChain_deletingTxns(t *testing.T) {
+
+	txs1 := []*transaction.Transaction{
+		{ClientID: "1", Nonce: 0, TransactionData: "false"},
+		{ClientID: "1", Nonce: 1, TransactionData: ""},
+		{ClientID: "1", Nonce: 2, TransactionData: ""},
+		{ClientID: "1", Nonce: 3, TransactionData: ""},
+		{ClientID: "1", Nonce: 4, TransactionData: ""},
+	}
+
+	type fields struct {
+		ctx  context.Context
+		txns []datastore.Entity
+	}
+
+	tests := []struct {
+		name   string
+		fields fields
+		arg    *transaction.Transaction
+		want   []datastore.Entity
+	}{
+		{
+			name: "test_sample",
+			fields: fields{
+				ctx:  context.Background(),
+				txns: []datastore.Entity{txs1[0], txs1[1]},
+			},
+			arg:  txs1[4],
+			want: []datastore.Entity{txs1[0], txs1[1]},
+		},
+	}
+
+	mr, err := miniredis.Run()
+	if err != nil {
+		log.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
+	redisClient.FlushDB()
+	// memorystore.AddPool("txndb", memorystore.DefaultPool)
+
+	mc := GetMinerChain()
+	setupTempRocksDBDir()
+	common.SetupRootContext(node.GetNodeContext())
+	config.SetServerChainID(config.GetMainChainID())
+	transaction.SetupEntity(memorystore.GetStorageProvider())
+	client.SetupEntity(memorystore.GetStorageProvider())
+	chain.SetupEntity(memorystore.GetStorageProvider(), "")
+
+	sigScheme := encryption.GetSignatureScheme("bls0chain")
+	err = sigScheme.GenerateKeys()
+	if err != nil {
+		println(err.Error())
+		panic(err)
+	}
+
+	c := &client.Client{}
+	err = c.SetPublicKey(sigScheme.GetPublicKey())
+	if err != nil {
+		println(err.Error())
+		panic(err)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			println("Starting test: " + tt.name)
+			// storing txns
+			for _, txn := range tt.fields.txns {
+				txn.(*transaction.Transaction).CreationDate = common.Now()
+				txn.(*transaction.Transaction).PublicKey = c.PublicKey
+				txn.(*transaction.Transaction).Hash = txn.(*transaction.Transaction).ComputeHash()
+
+				sig, err := txn.(*transaction.Transaction).Sign(sigScheme)
+				if err != nil {
+					fmt.Printf("Error signing\n")
+				}
+
+				txn.(*transaction.Transaction).Signature = sig
+
+				if err != nil {
+					println(err.Error())
+				}
+
+				out, err := transaction.PutTransaction(tt.fields.ctx, txn)
+				println("==> heyo")
+				if err != nil {
+					println(err)
+				} else {
+					if out != nil {
+						println("t is not nil!!!")
+						t := out.(*transaction.Transaction)
+						println("Stored txn: " + t.ClientID)
+					} else {
+						println("t is nil!!!")
+					}
+
+				}
+
+			}
+			println("Stored txns")
+
+			// deleting txns
+			mc.deleteTxns(tt.fields.txns)
+			println("Deleted txns")
+
+			// checking if txns are deleted
+			// memorystore.
+		})
+
+	}
+
 }
