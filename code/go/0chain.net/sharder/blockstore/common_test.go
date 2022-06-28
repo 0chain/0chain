899248ecb0d83dd2fe269ffb32309262124ff1c5
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2022-06-28 09:13:50 +0545

    Add test for lock

--- code/go/0chain.net/sharder/blockstore/common_test.go
+++ code/go/0chain.net/sharder/blockstore/common_test.go
@@ -3,8 +3,13 @@ package blockstore
 import (
 	"testing"
 
+	"0chain.net/core/logging"
 	"github.com/stretchr/testify/require"
 )
+
+func init() {
+	logging.InitLogging("debug", ".")
+}
 
 func TestGetUint64ValueFromYamlConfig(t *testing.T) {
 	m := map[uint64]interface{}{
@@ -60,4 +65,39 @@ func TestGetIntValueFromYamlConfig(t *testing.T) {
 		_, err := getintValueFromYamlConfig(v)
 		require.NotNil(t, err)
 	}
+}
+
+func TestLock(t *testing.T) {
+	mu := make(Mutex, 1)
+	t.Log("Locking")
+	mu.Lock()
+
+	select {
+	case mu <- struct{}{}:
+		t.Fail()
+	default:
+	}
+
+	require.NotPanics(t, func() {
+		t.Log("Unlocking")
+		mu.Unlock()
+		t.Log("Unlocked")
+	})
+
+	require.Panics(t, func() {
+		t.Log("Unlocking unlocked lock")
+		mu.Unlock()
+	})
+
+	t.Log("Locking")
+	mu.Lock()
+
+	require.Equal(t, false, mu.TryLock())
+	mu.Unlock()
+	require.Equal(t, true, mu.TryLock())
+	require.Equal(t, false, mu.TryLock())
+
+	require.NotPanics(t, func() {
+		mu.Unlock()
+	})
 }
