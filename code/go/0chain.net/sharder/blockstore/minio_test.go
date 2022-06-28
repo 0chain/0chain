0c352ff6f89ef43f2b2be056285c3c9fa1df301e
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2022-06-28 09:16:06 +0545

    Fix test issues

--- code/go/0chain.net/sharder/blockstore/minio_test.go
+++ code/go/0chain.net/sharder/blockstore/minio_test.go
@@ -5,30 +5,37 @@ import (
 	"errors"
 	"fmt"
 	"testing"
+	"time"
 
+	"0chain.net/core/logging"
 	"0chain.net/core/viper"
 	"github.com/stretchr/testify/require"
 )
 
+func init() {
+	logging.InitLogging("debug", ".")
+}
+
 func getMinioViperConfig(t *testing.T, strategy string) *viper.Viper {
 	v := viper.New()
-	coldStorageConfig := `
+	coldStorageConfigF := `
 strategy: %s
 cloud_storages:
   - storage_service_url: "s3.amazonaws.com"
     access_id: "ABCD"
-	secret_access_key: "EFGH"
-	bucket_name: "bkt1"
-	allowed_block_numbers: "10^15"
-	allowed_block_size: "10^5"
-	
+    secret_access_key: "EFGH"
+    bucket_name: "bkt1"
+    allowed_block_numbers: "10^15"
+    allowed_block_size: "10^5"
+
   - storage_service_url: "s3.amazonaws.com"
     access_id: "ABCD"
-	secret_access_key: "EFGH"
-	bucket_name: "bkt2"
-	allowed_block_numbers: "%10^15"
-	allowed_block_size: "10^5"
+    secret_access_key: "EFGH"
+    bucket_name: "bkt2"
+    allowed_block_numbers: "10^15"
+    allowed_block_size: "10^5"
 `
+	coldStorageConfig := fmt.Sprintf(coldStorageConfigF, strategy)
 	v.SetConfigType("yaml")
 	err := v.ReadConfig(bytes.NewBuffer([]byte(coldStorageConfig)))
 	require.Nil(t, err)
@@ -48,26 +55,6 @@ func TestInitCold(t *testing.T) {
 		})
 	})
 }
-func TestSelectNextStorage(t *testing.T) {
-	t.Run("Next storage must have been selected", func(t *testing.T) {
-		cT := initCold(getMinioViperConfig(t, ""), "start")
-		cT.Mu.Lock()
-		cT.Mu.Unlock()
-		for i := 1; i < 10; i++ {
-			ind := i % len(cT.ColdStorages)
-			bktName := fmt.Sprintf("lpobkt%d", ind)
-			select {
-			case selectedColdStorage := <-cT.SelectedStorageChan:
-				require.NotNil(t, selectedColdStorage)
-				mc := selectedColdStorage.coldStorage.(*minioClient)
-				require.Equal(t, bktName, mc.bucketName)
-			default:
-				t.Fail()
-			}
-			cT.SelectNextStorage(cT.ColdStorages, cT.PrevInd)
-		}
-	})
-}
 
 type mockMinioClient struct {
 	mb func() (string, error)
@@ -81,22 +68,34 @@ func (mc *mockMinioClient) getBlock(hash string) ([]byte, error) {
 }
 
 func TestMoveBlock(t *testing.T) {
+	timeout := time.After(time.Minute * 3)
+	doneCh := make(chan bool)
+	go func(t *testing.T) {
+		select {
+		case <-timeout:
+			fmt.Println("Test timed out")
+			t.Fail()
+		case <-doneCh:
+		}
+	}(t)
+
 	type input struct {
-		name                    string
-		cTier                   *coldTier
-		minioClients            []*mockMinioClient
-		selectedColdStorageChan chan selectedColdStorage
-		wantErr                 bool
-		setup                   func(t *testing.T, in *input)
-		furtherTets             func(t *testing.T, in *input)
+		name                string
+		cTier               *coldTier
+		minioClients        []*mockMinioClient
+		storageSelectorChan chan selectedColdStorage
+		wantErr             bool
+		setup               func(t *testing.T, in *input)
+		furtherTets         func(t *testing.T, in *input)
 	}
 
-	tests := []input{
+	tests := []*input{
 		{
 			name: "Should select next cold storage",
 			cTier: &coldTier{
 				Mu: make(Mutex, 1),
 			},
+			storageSelectorChan: make(chan selectedColdStorage, 1),
 			minioClients: []*mockMinioClient{
 				{
 					mb: func() (string, error) { return "minio:bkt1/hash", nil },
@@ -105,15 +104,16 @@ func TestMoveBlock(t *testing.T) {
 				},
 			},
 			setup: func(t *testing.T, in *input) {
-				in.cTier.SelectNextStorage = getColdRBStrategyFunc(in.cTier, in.selectedColdStorageChan)
+				in.cTier.SelectNextStorage = getColdRBStrategyFunc(in.cTier, in.storageSelectorChan)
+				in.cTier.StorageSelectorChan = in.storageSelectorChan
 				for _, m := range in.minioClients {
 					in.cTier.ColdStorages = append(in.cTier.ColdStorages, m)
 				}
-				in.cTier.SelectNextStorage(in.cTier.ColdStorages, in.cTier.PrevInd)
+				go in.cTier.SelectNextStorage(in.cTier.ColdStorages, in.cTier.PrevInd)
 			},
 			furtherTets: func(t *testing.T, in *input) {
 				prevInd := in.cTier.PrevInd
-				selectedStorage := <-in.cTier.SelectedStorageChan
+				selectedStorage := <-in.cTier.StorageSelectorChan
 				require.NotEqual(t, prevInd, selectedStorage.prevInd)
 			},
 		}, {
@@ -121,16 +121,19 @@ func TestMoveBlock(t *testing.T) {
 			cTier: &coldTier{
 				Mu: make(Mutex, 1),
 			},
+			storageSelectorChan: make(chan selectedColdStorage, 1),
 			minioClients: []*mockMinioClient{
 				{
 					mb: func() (string, error) { return "", errors.New("minio error") },
 				},
 			},
 			setup: func(t *testing.T, in *input) {
+				in.cTier.SelectNextStorage = getColdRBStrategyFunc(in.cTier, in.storageSelectorChan)
+				in.cTier.StorageSelectorChan = in.storageSelectorChan
 				for _, m := range in.minioClients {
 					in.cTier.ColdStorages = append(in.cTier.ColdStorages, m)
 				}
-				in.cTier.SelectNextStorage(in.cTier.ColdStorages, in.cTier.PrevInd)
+				go in.cTier.SelectNextStorage(in.cTier.ColdStorages, in.cTier.PrevInd)
 			},
 			wantErr: true,
 		},
@@ -139,6 +142,7 @@ func TestMoveBlock(t *testing.T) {
 			cTier: &coldTier{
 				Mu: make(Mutex, 1),
 			},
+			storageSelectorChan: make(chan selectedColdStorage, 1),
 			minioClients: []*mockMinioClient{
 				{
 					mb: func() (string, error) { return "", errors.New("minio error") },
@@ -148,27 +152,28 @@ func TestMoveBlock(t *testing.T) {
 				},
 			},
 			setup: func(t *testing.T, in *input) {
-				in.cTier.SelectNextStorage = getColdRBStrategyFunc(in.cTier, in.selectedColdStorageChan)
+				in.cTier.SelectNextStorage = getColdRBStrategyFunc(in.cTier, in.storageSelectorChan)
+				in.cTier.StorageSelectorChan = in.storageSelectorChan
 				for _, m := range in.minioClients {
 					in.cTier.ColdStorages = append(in.cTier.ColdStorages, m)
 				}
-				in.cTier.SelectNextStorage(in.cTier.ColdStorages, in.cTier.PrevInd)
+				go in.cTier.SelectNextStorage(in.cTier.ColdStorages, in.cTier.PrevInd)
 			},
 			furtherTets: func(t *testing.T, in *input) {
 				for i := 0; i < 10; i++ {
-					selectedColdStorage := <-in.selectedColdStorageChan
-					require.Equal(t, len(in.cTier.ColdStorages)-1, selectedColdStorage.prevInd)
-					in.cTier.SelectNextStorage(in.cTier.ColdStorages, in.cTier.PrevInd)
+					selectedColdStorage := <-in.cTier.StorageSelectorChan
+					require.Equal(t, 0, selectedColdStorage.prevInd)
+					go in.cTier.SelectNextStorage(in.cTier.ColdStorages, in.cTier.PrevInd)
 				}
 			},
 		},
 	}
 
 	for _, test := range tests {
-		test := test
 		t.Run(test.name, func(t *testing.T) {
+			t.Logf("running test: %s", test.name)
 			if test.setup != nil {
-				test.setup(t, &test)
+				test.setup(t, test)
 			}
 
 			_, err := test.cTier.moveBlock("hash", "/a/b")
@@ -179,8 +184,10 @@ func TestMoveBlock(t *testing.T) {
 			}
 
 			if test.furtherTets != nil {
-				test.furtherTets(t, &test)
+				test.furtherTets(t, test)
 			}
 		})
 	}
+
+	doneCh <- true
 }
