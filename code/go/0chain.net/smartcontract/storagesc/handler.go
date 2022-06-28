650da29bb00f9953fa55b7f8896dbbd37104b45d
Prateek Rathod <emaillawrathod@gmail.com>
2022-06-28 10:22:19 +0530

    feat: add to_client_id support to transactions endpoint

--- code/go/0chain.net/smartcontract/storagesc/handler.go
+++ code/go/0chain.net/smartcontract/storagesc/handler.go
@@ -1574,6 +1574,10 @@ func (srh *StorageRestHandler) getWriteMarker(w http.ResponseWriter, r *http.Req
 //      description: restrict to transactions sent by the specified client
 //      in: query
 //      type: string
+//    + name: to_client_id
+//      description: restrict to transactions sent to a specified client
+//      in: query
+//      type: string
 //    + name: block_hash
 //      description: restrict to transactions in indicated block
 //      in: query
@@ -1606,6 +1610,7 @@ func (srh *StorageRestHandler) getWriteMarker(w http.ResponseWriter, r *http.Req
 func (srh *StorageRestHandler) getTransactionByFilter(w http.ResponseWriter, r *http.Request) {
 	var (
 		clientID      = r.URL.Query().Get("client_id")
+		toClientID    = r.URL.Query().Get("to_client_id")
 		blockHash     = r.URL.Query().Get("block_hash")
 		startBlockNum = r.URL.Query().Get("block-start")
 		endBlockNum   = r.URL.Query().Get("block-end")
@@ -1623,6 +1628,16 @@ func (srh *StorageRestHandler) getTransactionByFilter(w http.ResponseWriter, r *
 	}
 	if clientID != "" {
 		rtv, err := edb.GetTransactionByClientId(clientID, limit)
+		if err != nil {
+			common.Respond(w, r, nil, common.NewErrInternal(err.Error()))
+			return
+		}
+		common.Respond(w, r, rtv, nil)
+		return
+	}
+
+	if toClientID != "" {
+		rtv, err := edb.GetTransactionByToClientId(clientID, limit)
 		if err != nil {
 			common.Respond(w, r, nil, common.NewErrInternal(err.Error()))
 			return
