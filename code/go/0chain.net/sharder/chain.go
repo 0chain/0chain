c02b7d4d003a8477ffaee347fd66d077bd69a754
peterlimg <peterlimg@protonmail.com>
2022-06-29 15:55:02 +1000

    Check if lfb has full state on starting

--- code/go/0chain.net/sharder/chain.go
+++ code/go/0chain.net/sharder/chain.go
@@ -346,13 +346,21 @@ func (sc *Chain) walkDownLookingForLFB(iter *gorocksdb.Iterator,
 		// Don't check the state. It can be missing if the state had synced.
 		// But it works fine anyway.
 
-		// if !sc.HasClientStateStored(lfb.ClientStateHash) {
-		// 	Logger.Warn("load_lfb, missing corresponding state",
-		// 		zap.Int64("round", r.Number),
-		// 		zap.String("block_hash", r.BlockHash))
-		// 	// we can't use this block, because of missing or malformed state
-		// 	continue
-		// }
+		if !sc.HasClientStateStored(lfb.ClientStateHash) {
+			Logger.Warn("load_lfb, missing corresponding state",
+				zap.Int64("round", r.Number),
+				zap.String("block_hash", r.BlockHash))
+			// we can't use this block, because of missing or malformed state
+			continue
+		}
+
+		// check if lfb has full state
+		if !sc.HasFullState(lfb) {
+			Logger.Warn("load_lfb, missing config",
+				zap.Int64("round", r.Number),
+				zap.String("block_hash", r.BlockHash))
+			continue
+		}
 
 		return // got it
 	}
@@ -468,4 +476,26 @@ func (sc *Chain) SaveMagicBlockHandler(ctx context.Context,
 // SaveMagicBlock function.
 func (sc *Chain) SaveMagicBlock() chain.MagicBlockSaveFunc {
 	return chain.MagicBlockSaveFunc(sc.SaveMagicBlockHandler)
+}
+
+func (sc *Chain) HasFullState(b *block.Block) bool {
+	if err := sc.InitBlockState(b); err != nil {
+		Logger.Warn("load_lfb, init block state failed", zap.Int64("round", b.Round), zap.String("block", b.Hash))
+		return false
+	}
+
+	_, keys, err := b.ClientState.FindMissingNodes(context.Background())
+	if err != nil {
+		Logger.Warn("load_lfb, find missing nodes failed",
+			zap.Int64("round", b.Round), zap.String("block", b.Hash), zap.Error(err))
+		return false
+	}
+
+	if len(keys) > 0 {
+		Logger.Warn("load_lfb, lfb has missing nodes",
+			zap.Int64("round", b.Round), zap.String("block", b.Hash))
+		return false
+	}
+
+	return true
 }
