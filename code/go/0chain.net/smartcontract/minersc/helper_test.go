fa4320040655832a16e83a6626d1ca214136834d
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2022-07-04 08:46:02 +0545

    Fix/update attributes (#1398)
    * Remove authticket field
    Verify clientid against readmarker's public key
    Maker reader, the payer of reads
    
    * Regenerate msgp file
    
    * Fix client id issue
    
    * Uncomment previous line
    
    * Fix test as anyone can pay
    
    * Pre verify readmarker's client id
    
    * Add test case for readmarker cliet id verification

--- code/go/0chain.net/smartcontract/minersc/helper_test.go
+++ code/go/0chain.net/smartcontract/minersc/helper_test.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"0chain.net/chaincore/currency"
+	"0chain.net/chaincore/threshold/bls"
 
 	"0chain.net/chaincore/block"
 	cstate "0chain.net/chaincore/chain/state"
@@ -85,7 +86,9 @@ func newClient(balance currency.Coin, balances cstate.StateContextI) (
 	client.scheme = scheme
 
 	client.pk = scheme.GetPublicKey()
-	client.id = encryption.Hash(client.pk)
+	pub := bls.PublicKey{}
+	pub.DeserializeHexStr(client.pk)
+	client.id = encryption.Hash(pub.Serialize())
 
 	balances.(*testBalances).balances[client.id] = balance
 	return
