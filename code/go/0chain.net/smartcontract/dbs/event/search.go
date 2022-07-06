d1c2a953b18385c0ee5a0efabc3eab2e421f7d6d
Prateek Rathod <emaillawrathod@gmail.com>
2022-07-06 16:14:05 +0530

    fix: search.go blocks.round type bigInt error

--- code/go/0chain.net/smartcontract/dbs/event/search.go
+++ code/go/0chain.net/smartcontract/dbs/event/search.go
@@ -1,10 +1,19 @@
 package event
 
-import "database/sql"
+import (
+	"database/sql"
+	"strconv"
+)
 
 // GetGenericSearchType returns kind of search query
 func (edb *EventDb) GetGenericSearchType(query string) (string, error) {
 	var queryType string
+	var round int
+
+	round, err := strconv.Atoi(query)
+	if err != nil {
+		round = -1
+	}
 
 	res := edb.Store.Get().Raw(
 		"SELECT "+
@@ -15,7 +24,7 @@ func (edb *EventDb) GetGenericSearchType(query string) (string, error) {
 			"THEN 'BlockHash' "+
 			"WHEN EXISTS (SELECT Id FROM USERS WHERE USER_ID = @query) "+
 			"THEN 'UserId' "+
-			"WHEN EXISTS (SELECT Id FROM BLOCKS WHERE ROUND = @query) "+
+			"WHEN EXISTS (SELECT Id FROM BLOCKS WHERE ROUND = @round) "+
 			"THEN 'BlockRound' "+
 			"WHEN EXISTS (SELECT Id FROM WRITE_MARKERS WHERE CONTENT_HASH = @query) "+
 			"THEN 'ContentHash' "+
@@ -24,6 +33,7 @@ func (edb *EventDb) GetGenericSearchType(query string) (string, error) {
 			"ELSE 'Not Found' "+
 			"END AS queryType",
 		sql.Named("query", query),
+		sql.Named("round", round),
 	).Scan(&queryType)
 
 	return queryType, res.Error
