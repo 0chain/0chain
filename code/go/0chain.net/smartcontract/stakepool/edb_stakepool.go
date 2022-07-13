fd774d3a20d42064ccabb23bb6ad218ab68cbe11
Piers Shepperson <piers.shepperson@btinternet.com>
2022-07-13 17:26:57 +0100

    stake pool emit

--- code/go/0chain.net/smartcontract/stakepool/edb_stakepool.go
+++ code/go/0chain.net/smartcontract/stakepool/edb_stakepool.go
@@ -11,6 +11,18 @@ import (
 
 type StakePoolReward dbs.StakePoolReward
 
+func (sp *StakePool) EmitStakePoolBalanceUpdate(
+	pId string,
+	pType spenum.Provider,
+	balances cstate.StateContextI,
+) {
+	for id, dp := range sp.Pools {
+		dpu := dbs.NewDelegatePoolUpdate(id, pId, int(pType))
+		dpu.Updates["balance"] = dp.Balance
+		balances.EmitEvent(event.TypeStats, event.TagUpdateDelegatePool, id, *dpu)
+	}
+}
+
 func NewStakePoolReward(pId string, pType spenum.Provider) *StakePoolReward {
 	var spu StakePoolReward
 	spu.ProviderId = pId
