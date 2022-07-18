2947decf6652746708df32676a9c982673a59f07
Piers Shepperson <piers.shepperson@btinternet.com>
2022-07-18 10:55:43 +0100

    use managing wallet

--- code/go/0chain.net/smartcontract/storagesc/shut_down.go
+++ code/go/0chain.net/smartcontract/storagesc/shut_down.go
@@ -8,21 +8,36 @@ import (
 
 func (ssc *StorageSmartContract) shutDownBlobber(
 	t *transaction.Transaction,
-	_ []byte,
+	input []byte,
 	balances cstate.StateContextI,
 ) (string, error) {
-	var blobber *StorageNode
+	var req providerRequest
+	if err := req.decode(input); err != nil {
+		return "", common.NewError("shut_down_blobber_failed", err.Error())
+	}
+
 	var err error
-	if blobber, err = ssc.getBlobber(t.ClientID, balances); err != nil {
-		return "", common.NewError("blobber_health_check_failed",
+	var sp *stakePool
+	if sp, err = ssc.getStakePool(req.ID, balances); err != nil {
+		return "", common.NewError("shut_down_blobber_failed",
+			"can't get related stake pool: "+err.Error())
+	}
+	if t.ClientID != sp.Settings.DelegateWallet {
+		return "", common.NewError("shut_down_blobber_failed",
+			"access denied, allowed for delegate_wallet owner only")
+	}
+
+	var blobber *StorageNode
+	if blobber, err = ssc.getBlobber(req.ID, balances); err != nil {
+		return "", common.NewError("shut_down_blobber_failed",
 			"can't get the blobber "+t.ClientID+": "+err.Error())
 	}
 	blobber.ShutDown()
 	if err = emitUpdateBlobber(blobber, balances); err != nil {
-		return "", common.NewError("blobber_health_check_failed", err.Error())
+		return "", common.NewError("shut_down_blobber_failed", err.Error())
 	}
 	if _, err = balances.InsertTrieNode(blobber.GetKey(ssc.ID), blobber); err != nil {
-		return "", common.NewError("blobber_health_check_failed",
+		return "", common.NewError("shut_down_blobber_failed",
 			"can't save blobber: "+err.Error())
 	}
 	return "", nil
@@ -30,35 +45,50 @@ func (ssc *StorageSmartContract) shutDownBlobber(
 
 func (ssc *StorageSmartContract) shutDownValidator(
 	t *transaction.Transaction,
-	_ []byte,
+	input []byte,
 	balances cstate.StateContextI,
 ) (string, error) {
+	var req providerRequest
+	if err := req.decode(input); err != nil {
+		return "", common.NewError("shut_down_validator_failed", err.Error())
+	}
+
+	var err error
+	var sp *stakePool
+	if sp, err = ssc.getStakePool(req.ID, balances); err != nil {
+		return "", common.NewError("shut_down_validator_failed",
+			"can't get related stake pool: "+err.Error())
+	}
+	if t.ClientID != sp.Settings.DelegateWallet {
+		return "", common.NewError("shut_down_validator_failed",
+			"access denied, allowed for delegate_wallet owner only")
+	}
+
 	var validator = &ValidationNode{
 		ID: t.ClientID,
 	}
-	var err error
 	if err = balances.GetTrieNode(validator.GetKey(ssc.ID), validator); err != nil {
-		return "", common.NewError("blobber_health_check_failed",
-			"can't get the blobber "+t.ClientID+": "+err.Error())
+		return "", common.NewError("shut_down_validator_failed",
+			"can't get the validator "+t.ClientID+": "+err.Error())
 	}
 	validator.ShutDown()
 	err = validator.emitUpdate(balances)
 	if err != nil {
-		return "", common.NewErrorf("add_validator_failed", "emitting Validation node failed: %v", err.Error())
+		return "", common.NewErrorf("shut_down_validator_failed", "emitting validation node failed: %v", err.Error())
 	}
 
 	validatorPartitions, err := getValidatorsList(balances)
 	if err != nil {
-		return "", common.NewError("kill_validator_failed",
+		return "", common.NewError("shut_down_validator_failed",
 			"failed to get validator list."+err.Error())
 	}
 	if err := validatorPartitions.RemoveItem(balances, validator.PartitionPosition, validator.ID); err != nil {
-		return "", common.NewError("kill_validator_failed",
+		return "", common.NewError("shut_down_validator_failed",
 			"failed to remove validator."+err.Error())
 	}
 
 	if _, err = balances.InsertTrieNode(validator.GetKey(ssc.ID), validator); err != nil {
-		return "", common.NewError("blobber_health_check_failed",
+		return "", common.NewError("shut_down_validator_failed",
 			"can't save blobber: "+err.Error())
 	}
 	return "", nil
