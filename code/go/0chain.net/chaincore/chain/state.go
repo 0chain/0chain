f77105310ee1f29de82f91539b396a8506841862
dabasov <yuderbasov@gmail.com>
2022-07-29 18:04:29 +0300

    rollback

--- code/go/0chain.net/chaincore/chain/state.go
+++ code/go/0chain.net/chaincore/chain/state.go
@@ -709,9 +709,8 @@ func (c *Chain) emitUserEvent(sc bcstate.StateContextI, usr *event.User) error {
 		return nil
 	}
 
-	//sc.EmitEvent(event.TypeStats, event.TagAddOrOverwriteUser, "", usr, func(events []event.Event, current event.Event) []event.Event {
-	//	return append([]event.Event{current}, events...)
-	//})
-	sc.EmitEvent(event.TypeStats, event.TagAddOrOverwriteUser, "", usr)
+	sc.EmitEvent(event.TypeStats, event.TagAddOrOverwriteUser, "", usr, func(events []event.Event, current event.Event) []event.Event {
+		return append([]event.Event{current}, events...)
+	})
 	return nil
 }
