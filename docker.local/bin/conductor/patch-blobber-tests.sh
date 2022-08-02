56c2213d5addd47df5bdd220159a6a28aa0e1eaa
Fabio Ferreira <martinhoferreira10@gmail.com>
2022-08-02 18:24:32 +0200

    replace tabs by spaces on patch-blobber-tests

--- docker.local/bin/conductor/patch-blobber-tests.sh
+++ docker.local/bin/conductor/patch-blobber-tests.sh
@@ -2,4 +2,4 @@
 
 sed -i 's/rate_limit: 0 # 10/rate_limit: 1000 # 1000/' config/0chain_blobber.yaml
 
-sed -i 's/POSTGRES_USER: postgres/&\n\t\t\tPOSTGRES_PASSWORD: secret/' docker.local/b0docker-compose.yml
+sed -i 's/POSTGRES_USER: postgres/&\n      POSTGRES_PASSWORD: secret/' docker.local/b0docker-compose.yml
