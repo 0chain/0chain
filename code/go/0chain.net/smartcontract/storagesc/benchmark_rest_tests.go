68bd6377633de349546b53aedf25270f11cf57ef
Prateek Rathod <emaillawrathod@gmail.com>
2022-08-09 19:43:34 +0530

    chore: update benchmark to include `/collected_reward` changes

--- code/go/0chain.net/smartcontract/storagesc/benchmark_rest_tests.go
+++ code/go/0chain.net/smartcontract/storagesc/benchmark_rest_tests.go
@@ -2,6 +2,7 @@ package storagesc
 
 import (
 	"strconv"
+	"time"
 
 	"0chain.net/core/encryption"
 
@@ -297,9 +298,11 @@ func BenchmarkRestTests(
 			{
 				FuncName: "collected_reward",
 				Params: map[string]string{
-					"start_block": "1",
-					"end_block":   "100",
-					"client_id":   data.Clients[1],
+					"start-block": "1",
+					"end-block":   "100",
+					"start-date":  "0",
+					"end-date":    strconv.FormatInt(time.Now().AddDate(1, 0, 0).Unix(), 10),
+					"client-id":   data.Clients[1],
 				},
 				Endpoint: srh.getCollectedReward,
 			},
