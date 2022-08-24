abdebaf66a96d41495899060b68817c1ff36f19d
0xhrsh <harsh1429@gmail.com>
2022-08-25 03:25:58 +0530

    fixes failing test

--- code/go/0chain.net/miner/miner_test.go
+++ code/go/0chain.net/miner/miner_test.go
@@ -171,7 +171,7 @@ func TestBlockGeneration(t *testing.T) {
 	mc, stopAndClean := setupMinerChain()
 	defer stopAndClean()
 
-	config.SetupSmartContractConfig("")
+	config.SetupSmartContractConfig("docker.local/miner1")
 
 	gb := SetupGenesisBlock()
 	mc.AddGenesisBlock(gb)
