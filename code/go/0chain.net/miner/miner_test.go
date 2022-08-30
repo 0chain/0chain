6682c90a38d6e03843278e2e137af72259976ab3
0xhrsh <harsh1429@gmail.com>
2022-08-30 23:58:40 +0530

    sets workdir as ..

--- code/go/0chain.net/miner/miner_test.go
+++ code/go/0chain.net/miner/miner_test.go
@@ -171,7 +171,7 @@ func TestBlockGeneration(t *testing.T) {
 	mc, stopAndClean := setupMinerChain()
 	defer stopAndClean()
 
-	config.SetupSmartContractConfig("0chain.net")
+	config.SetupSmartContractConfig("../")
 
 	gb := SetupGenesisBlock()
 	mc.AddGenesisBlock(gb)
