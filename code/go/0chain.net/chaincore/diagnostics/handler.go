86500211a75a0e6b985b48679bfdec9cbf48a931
manasnisar <anasnisar980@gmail.com>
2022-10-04 11:47:12 +0500

    Created _diagnostics/txn_in_pool to show transactions currenlty in txn_pool

--- code/go/0chain.net/chaincore/diagnostics/handler.go
+++ code/go/0chain.net/chaincore/diagnostics/handler.go
@@ -21,6 +21,7 @@ func SetupHandlers() {
 	sc := chain.GetServerChain()
 	http.HandleFunc("/_diagnostics/n2n/info", common.UserRateLimit(sc.N2NStatsWriter))
 	http.HandleFunc("/_diagnostics/miner_stats", common.UserRateLimit(sc.MinerStatsHandler))
+	http.HandleFunc("/_diagnostics/txns_in_pool", common.UserRateLimit(sc.TxnsInPoolHandler))
 	http.HandleFunc("/_diagnostics/block_chain", common.UserRateLimit(sc.WIPBlockChainHandler))
 }
 
@@ -143,7 +144,7 @@ func WriteCurrentStatus(w http.ResponseWriter, c *chain.Chain) {
 	fmt.Fprintf(w, "</table>")
 }
 
-//WritePruneStats - write the last prune stats
+// WritePruneStats - write the last prune stats
 func WritePruneStats(w http.ResponseWriter, ps *util.PruneStats) {
 	fmt.Fprintf(w, "<table width='100%%'>")
 	fmt.Fprintf(w, "<tr><td>Stage</td><td>%v</td>", ps.Stage)
