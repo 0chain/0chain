f5971bd4faa03b000361be261d4ae6a96bbfb506
peterlimg <peterlimg@protonmail.com>
2022-10-07 17:31:29 +1100

    Fix error after merging staging

--- code/go/0chain.net/chaincore/chain/handler.go
+++ code/go/0chain.net/chaincore/chain/handler.go
@@ -1,7 +1,6 @@
 package chain
 
 import (
-	"0chain.net/chaincore/state"
 	"bufio"
 	"context"
 	"encoding/json"
@@ -17,6 +16,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"0chain.net/chaincore/state"
 
 	"0chain.net/chaincore/block"
 	"0chain.net/chaincore/config"
@@ -1622,16 +1623,16 @@ func (c *Chain) MinerStatsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func txnIterHandlerFunc(c *Chain, w http.ResponseWriter, s *state.State) func(context.Context, datastore.CollectionEntity) bool {
-	return func(ctx context.Context, ce datastore.CollectionEntity) bool {
+func txnIterHandlerFunc(c *Chain, w http.ResponseWriter, s *state.State) func(context.Context, datastore.CollectionEntity) (bool, error) {
+	return func(ctx context.Context, ce datastore.CollectionEntity) (bool, error) {
 		txn, ok := ce.(*transaction.Transaction)
 		if !ok {
 			logging.Logger.Error("generate block (invalid entity)", zap.Any("entity", ce))
-			return false
+			return false, nil
 		}
 
 		c.txnsInPoolTableRows(w, txn, s)
-		return true
+		return true, nil
 	}
 }
 
