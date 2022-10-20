0a0e8d1e5fe310461d174469e28b754d6382b9c0
peterlimg <peterlimg@protonmail.com>
2022-10-21 10:08:55 +1100

    Add partition item location map in partitions itself to
    make it easy to use

---
+++ code/go/0chain.net/smartcontract/partitions/partitions_test.go
@@ -0,0 +1,53 @@
+package partitions
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPartitionsSave(t *testing.T) {
+	balances := &mockStateContextI{data: make(map[string][]byte)}
+	rs, err := newRandomSelector("test_rs", 10, nil)
+	require.NoError(t, err)
+
+	parts := Partitions{rs: rs}
+
+	for i := 0; i < 20; i++ {
+		k := fmt.Sprintf("k%d", i)
+		v := fmt.Sprintf("v%d", i)
+		it := testItem{ID: k, V: v}
+		_, err = parts.AddItem(balances, &it)
+		require.NoError(t, err)
+	}
+
+	err = parts.Save(balances)
+	require.NoError(t, err)
+
+	p1, err := GetPartitions(balances, "test_rs")
+	require.NoError(t, err)
+
+	var v testItem
+	err = p1.GetItem(balances, 1, "k15", &v)
+	require.NoError(t, err)
+	require.Equal(t, "v15", v.V)
+
+	p2, err := GetPartitions(balances, "test_rs")
+	require.NoError(t, err)
+	require.Equal(t, 2, p2.rs.NumPartitions)
+
+	// update item
+	err = p1.UpdateItem(balances, 1, &testItem{"k10", "vv10"})
+	require.NoError(t, err)
+	require.NoError(t, p1.Save(balances))
+
+	p3, err := GetPartitions(balances, "test_rs")
+	require.NoError(t, err)
+	require.Equal(t, 2, p3.rs.NumPartitions)
+
+	var vv testItem
+	err = p3.GetItem(balances, 1, "k10", &vv)
+	require.NoError(t, err)
+	require.Equal(t, "vv10", vv.V)
+}
