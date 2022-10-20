c3f2579ddd83ab38343d9e6b76ab7041b9db7d97
peterlimg <peterlimg@protonmail.com>
2022-10-21 10:08:55 +1100

    Add partition item location map in partitions itself to
    make it easy to use

--- code/go/0chain.net/smartcontract/partitions/partitions.go
+++ code/go/0chain.net/smartcontract/partitions/partitions.go
@@ -1,8 +1,11 @@
 package partitions
 
 import (
+	"fmt"
 	"math/rand"
 
+	"0chain.net/core/datastore"
+	"0chain.net/core/encryption"
 	"github.com/0chain/common/core/util"
 
 	"0chain.net/chaincore/chain/state"
@@ -62,7 +65,16 @@ func (p *Partitions) GetName() string {
 
 // AddItem adds a partition item to parititons
 func (p *Partitions) AddItem(state state.StateContextI, item PartitionItem) (int, error) {
-	return p.rs.Add(state, item)
+	idx, err := p.rs.Add(state, item)
+	if err != nil {
+		return -1, err
+	}
+
+	if err := p.saveItemLoc(state, item.GetID(), idx); err != nil {
+		return -1, err
+	}
+
+	return idx, nil
 }
 
 // Save saves the partitions data into state
@@ -71,13 +83,31 @@ func (p *Partitions) Save(state state.StateContextI) error {
 }
 
 // GetItem returns partition item of given partition index and id
-func (p *Partitions) GetItem(state state.StateContextI, partIndex int, id string, v PartitionItem) error {
-	return p.rs.GetItem(state, partIndex, id, v)
+func (p *Partitions) GetItem(state state.StateContextI, id string, v PartitionItem) error {
+	loc, ok, err := p.getItemPartIndex(state, id)
+	if err != nil {
+		return err
+	}
+
+	if !ok {
+		return fmt.Errorf("partition %s not found", id)
+	}
+
+	return p.rs.GetItem(state, loc, id, v)
 }
 
 // UpdateItem updates item on given partition index
-func (p *Partitions) UpdateItem(state state.StateContextI, partIndex int, item PartitionItem) error {
-	return p.rs.UpdateItem(state, partIndex, item)
+func (p *Partitions) UpdateItem(state state.StateContextI, item PartitionItem) error {
+	loc, ok, err := p.getItemPartIndex(state, item.GetID())
+	if err != nil {
+		return err
+	}
+
+	if !ok {
+		return fmt.Errorf("partition %s not found", item.GetID())
+	}
+
+	return p.rs.UpdateItem(state, loc, item)
 }
 
 // Size returns the total item number in partitions
@@ -91,8 +121,21 @@ func (p *Partitions) Num() int {
 }
 
 // RemoveItem removes the partition item from given partIndex in Partitions
-func (p *Partitions) RemoveItem(state state.StateContextI, partIndex int, id string) error {
-	return p.rs.RemoveItem(state, id, partIndex)
+func (p *Partitions) RemoveItem(state state.StateContextI, id string) error {
+	loc, ok, err := p.getItemPartIndex(state, id)
+	if err != nil {
+		return err
+	}
+
+	if !ok {
+		return fmt.Errorf("partition %s not found", id)
+	}
+
+	if err := p.rs.RemoveItem(state, id, loc); err != nil {
+		return err
+	}
+
+	return p.removeItemLoc(state, id)
 }
 
 // GetRandomItems returns items of partition size number from random partition,
@@ -103,3 +146,30 @@ func (p *Partitions) GetRandomItems(state state.StateContextI, r *rand.Rand, v i
 }
 
 type ChangePartitionCallback = func(string, []byte, int, int, state.StateContextI) error
+
+func (p *Partitions) getLocKey(id string) datastore.Key {
+	return encryption.Hash(fmt.Sprintf("%s:%s", p.rs.Name, id))
+}
+
+func (p *Partitions) getItemPartIndex(state state.StateContextI, id string) (int, bool, error) {
+	var pl PartitionLocation
+	if err := state.GetTrieNode(p.getLocKey(id), &pl); err != nil {
+		if err == util.ErrValueNotPresent {
+			return -1, false, nil
+		}
+
+		return -1, false, err
+	}
+
+	return pl.Location, true, nil
+}
+
+func (p *Partitions) saveItemLoc(state state.StateContextI, id string, partIndex int) error {
+	_, err := state.InsertTrieNode(p.getLocKey(id), &PartitionLocation{Location: partIndex})
+	return err
+}
+
+func (p *Partitions) removeItemLoc(state state.StateContextI, id string) error {
+	_, err := state.DeleteTrieNode(p.getLocKey(id))
+	return err
+}
