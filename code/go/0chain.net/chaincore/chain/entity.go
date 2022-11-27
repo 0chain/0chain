79f57adb534594353e6c6f2e4734ad8acacf5fb3
Piers Shepperson <piers.shepperson@btinternet.com>
2022-11-27 15:17:57 +0000

    check edb nil

--- code/go/0chain.net/chaincore/chain/entity.go
+++ code/go/0chain.net/chaincore/chain/entity.go
@@ -1417,12 +1417,15 @@ func (c *Chain) updateConfig(pb *block.Block) {
 			zap.Error(err),
 		)
 	}
-	err = c.EventDb.UpdateSettings(configMap.Fields)
-	if err != nil {
-		logging.Logger.Error("updating event database settings",
-			zap.Int64("start of round", pb.Round),
-			zap.Error(err),
-		)
+
+	if c.EventDb != nil {
+		err = c.EventDb.UpdateSettings(configMap.Fields)
+		if err != nil {
+			logging.Logger.Error("updating event database settings",
+				zap.Int64("start of round", pb.Round),
+				zap.Error(err),
+			)
+		}
 	}
 
 	logging.Logger.Info("config has been updated successfully",
