182f150c92e4e6ecffc7bfcd3b9f470c1a461622
Piers Shepperson <piers.shepperson@btinternet.com>
2022-12-01 14:41:17 +0000

    check BenchDataListLength grater than zero

--- code/go/0chain.net/smartcontract/benchmark/main/cmd/options.go
+++ code/go/0chain.net/smartcontract/benchmark/main/cmd/options.go
@@ -32,12 +32,6 @@ func getTestSuites(
 	data bk.BenchData,
 	bkNames, omit []string,
 ) []bk.TestSuite {
-	var name string
-	defer func() {
-		if r := recover(); r != nil {
-			log.Println("piers Recovered benchmark in getTestSuites", r, "test name", name)
-		}
-	}()
 	var suites []bk.TestSuite
 	if len(bkNames) == 0 {
 		for _, bks := range benchmarkSources {
@@ -49,7 +43,8 @@ func getTestSuites(
 	}
 
 	common.ConfigRateLimits()
-	for _, name = range bkNames {
+
+	for _, name := range bkNames {
 		if code, ok := bk.SourceCode[name]; ok {
 			suite := benchmarkSources[code](data, &BLS0ChainScheme{})
 			suite.RemoveBenchmarks(omit)
