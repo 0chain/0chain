e38867d9fbc7c9bb5cbafb540e314360825467ba
Ebrahim Gomaa <ebrahim.gomaa.hg@gmail.com>
2022-12-02 15:59:13 +0200

    Fix blobber_id not sent in read_marker response

--- code/go/0chain.net/smartcontract/storagesc/handler.go
+++ code/go/0chain.net/smartcontract/storagesc/handler.go
@@ -1755,6 +1755,7 @@ type ReadMarkerResponse struct {
 	AuthTicket    string  `json:"auth_ticket"`   //used in readmarkers
 	BlockNumber   int64   `json:"block_number"` //used in alloc_read_size
 	ClientID	  string  `json:"client_id"`
+	BlobberID     string  `json:"blobber_id"`
 	OwnerID	      string  `json:"owner_id"`
 	TransactionID string  `json:"transaction_id"`
 	AllocationID  string  `json:"allocation_id"`
@@ -1778,6 +1779,7 @@ func toReadMarkerResponse(rm event.ReadMarker) ReadMarkerResponse {
 		AuthTicket: rm.AuthTicket,
 		BlockNumber: rm.BlockNumber,
 		ClientID: rm.ClientID,
+		BlobberID: rm.BlobberID,
 		OwnerID: rm.OwnerID,
 		TransactionID: rm.TransactionID,
 		AllocationID: rm.AllocationID,
