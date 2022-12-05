f0aca063588ef9b6b277be18c2b3897092ace701
peterlimg <peterlimg@protonmail.com>
2022-12-05 21:51:48 +1100

    Fix incorrect coeff config

--- code/go/0chain.net/chaincore/config/config.go
+++ code/go/0chain.net/chaincore/config/config.go
@@ -32,7 +32,7 @@ func SetupDefaultConfig() {
 	viper.SetDefault("server_chain.round_range", 10000000)
 	viper.SetDefault("server_chain.transaction.payload.max_size", 32)
 	viper.SetDefault("server_chain.transaction.transfer_cost", 10)
-	viper.SetDefault("server_chain.transaction.txn_cost_fee_coeff", 1000)
+	viper.SetDefault("server_chain.transaction.cost_fee_coeff", 1000)
 	viper.SetDefault("server_chain.state.prune_below_count", 100)
 	viper.SetDefault("server_chain.block.consensus.threshold_by_count", 66)
 	viper.SetDefault("server_chain.block.generation.timeout", 37)
