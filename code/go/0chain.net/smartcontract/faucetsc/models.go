81ca2e816430851208d826dd27c4de92a28a9766
Uneet7 <uneetpatel2149@gmail.com>
2022-12-07 20:56:31 +0530

    Remove global node

--- code/go/0chain.net/smartcontract/faucetsc/models.go
+++ code/go/0chain.net/smartcontract/faucetsc/models.go
@@ -46,40 +46,6 @@ func (pr *periodicResponse) decode(input []byte) error {
 	return err
 }
 
-type GlobalNode struct {
-	ID              string        `json:"id"`
-	PourAmount      state.Balance `json:"pour_amount"`
-	MaxPourAmount   state.Balance `json:"max_pour_amount"`
-	PeriodicLimit   state.Balance `json:"periodic_limit"`
-	GlobalLimit     state.Balance `json:"global_limit"`
-	IndividualReset time.Duration `json:"individual_reset"` //in hours
-	GlobalReset     time.Duration `json:"global_rest"`      //in hours
-	Used            state.Balance `json:"used"`
-	StartTime       time.Time     `json:"start_time"`
-}
-
-func (gn *GlobalNode) GetKey() datastore.Key {
-	return datastore.Key(gn.ID + gn.ID)
-}
-
-func (gn *GlobalNode) GetHash() string {
-	return util.ToHex(gn.GetHashBytes())
-}
-
-func (gn *GlobalNode) GetHashBytes() []byte {
-	return encryption.RawHash(gn.Encode())
-}
-
-func (gn *GlobalNode) Encode() []byte {
-	buff, _ := json.Marshal(gn)
-	return buff
-}
-
-func (gn *GlobalNode) Decode(input []byte) error {
-	err := json.Unmarshal(input, gn)
-	return err
-}
-
 type UserNode struct {
 	ID        string        `json:"id"`
 	StartTime time.Time     `json:"start_time"`
