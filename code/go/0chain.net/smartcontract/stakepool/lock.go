0420d57015b7310c90807b2178e52aa86f80973f
Piers Shepperson <piers.shepperson@btinternet.com>
2022-12-27 09:51:19 +0000

    staked_at

--- code/go/0chain.net/smartcontract/stakepool/lock.go
+++ code/go/0chain.net/smartcontract/stakepool/lock.go
@@ -89,6 +89,7 @@ func (sp *StakePool) LockPool(
 
 		update := newDelegatePoolUpdate(newPoolId, providerId, providerType)
 		update.Updates["balance"] = dp.Balance
+		update.Updates["staked_at"] = txn.ClientID
 		update.emitUpdate(balances)
 	}
 
