99d4816d82487be217746276defea085179795f1
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2023-01-24 16:02:38 +0545

    Add unit tests

---
+++ code/go/0chain.net/sharder/blockstore/cache_test.go
@@ -0,0 +1,189 @@
+package blockstore
+
+import (
+	"bytes"
+	"container/list"
+	"crypto/rand"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/0chain/common/core/logging"
+
+	"0chain.net/core/viper"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	logging.InitLogging("debug", ".")
+}
+
+func TestInitCache(t *testing.T) {
+	config := `
+cache:
+    path: "/path/to/cache"
+    size: "1GB"
+`
+	viper.GetViper().SetConfigType("yaml")
+	err := viper.ReadConfig(bytes.NewReader([]byte(config)))
+	require.NoError(t, err)
+
+	require.NotPanics(t, func() {
+		initCache(viper.Sub("cache"))
+	})
+
+}
+
+func TestLRUAdd(t *testing.T) {
+	l := lru{
+		list:  list.New(),
+		items: make(map[string]*list.Element),
+	}
+
+	m := map[string]int64{
+		"k1": 1,
+		"k2": 2,
+		"k3": 3,
+		"k4": 4,
+		"k5": 5,
+	}
+	for k, v := range m {
+		l.Add(k, v)
+		e := l.list.Front()
+		key := e.Value.(*listEntry).key
+		require.Equal(t, k, key)
+	}
+}
+
+func TestLRURemove(t *testing.T) {
+	l := lru{
+		list:  list.New(),
+		items: make(map[string]*list.Element),
+	}
+
+	m := map[string]int64{
+		"k1": 1,
+		"k2": 2,
+		"k3": 3,
+		"k4": 4,
+		"k5": 5,
+	}
+	for k, v := range m {
+		l.Add(k, v)
+	}
+
+	for k := range m {
+		l.Remove(k)
+		_, ok := l.items[k]
+		require.False(t, ok)
+	}
+}
+
+func TestCacheWrite(t *testing.T) {
+	p := "./cache"
+	v := viper.New()
+	v.Set("path", p)
+	v.Set("size", 500*MB)
+
+	var c cacher
+	require.NotPanics(t, func() {
+		c = initCache(v)
+	})
+
+	size := 250 * MB
+	hash1 := "hash1"
+	b := generateRandomBytes(t, size)
+	err := c.Write(hash1, b)
+	require.Nil(t, err)
+
+	finfo, err := os.Stat(filepath.Join(p, hash1))
+	require.Nil(t, err)
+
+	require.EqualValues(t, size, finfo.Size())
+
+	size = 250 * MB
+	hash2 := "hash2"
+	b = generateRandomBytes(t, size)
+	err = c.Write(hash2, b)
+	require.Nil(t, err)
+
+	size = 250 * MB
+	hash3 := "hash3"
+	b = generateRandomBytes(t, size)
+	err = c.Write(hash3, b)
+	require.Nil(t, err)
+
+	time.Sleep(time.Second)
+	finfo, err = os.Stat(filepath.Join(p, hash1))
+	require.NotNil(t, err)
+
+	finfo, err = os.Stat(filepath.Join(p, hash2))
+	require.NotNil(t, err)
+
+	finfo, err = os.Stat(filepath.Join(p, hash3))
+	require.Nil(t, err)
+	require.EqualValues(t, size, finfo.Size())
+
+	l := c.(*cache).lru.list
+	e := l.Front()
+	k := e.Value.(*listEntry).key
+	require.Equal(t, k, hash3)
+}
+
+func TestCacheRead(t *testing.T) {
+	p := "./cache"
+	v := viper.New()
+	v.Set("path", p)
+	v.Set("size", 500*MB)
+
+	var c cacher
+	require.NotPanics(t, func() {
+		c = initCache(v)
+	})
+
+	m := map[string]int{
+		"hash1": 1024,
+		"hash2": 1024,
+		"hash3": 1024,
+	}
+
+	var lastKey string
+	for k, v := range m {
+		b := generateRandomBytes(t, v)
+		err := c.Write(k, b)
+		require.Nil(t, err)
+
+		finfo, err := os.Stat(filepath.Join(p, k))
+		require.Nil(t, err)
+
+		require.EqualValues(t, v, finfo.Size())
+		lastKey = k
+	}
+
+	time.Sleep(500 * time.Millisecond)
+	e := c.(*cache).lru.list.Front()
+	k := e.Value.(*listEntry).key
+	require.Equal(t, k, lastKey)
+
+	hash1 := "hash1"
+	b, err := c.Read(hash1)
+	require.Nil(t, err)
+
+	require.Equal(t, m[hash1], len(b))
+
+	time.Sleep(500 * time.Millisecond)
+	e = c.(*cache).lru.list.Front()
+	k = e.Value.(*listEntry).key
+	require.Equal(t, k, hash1)
+}
+
+func generateRandomBytes(t *testing.T, size int) []byte {
+	b := make([]byte, size)
+	n, err := rand.Read(b)
+	require.Nil(t, err)
+	require.Equal(t, n, size)
+
+	return b
+
+}
---
+++ code/go/0chain.net/sharder/blockstore/fs_store_test.go
@@ -0,0 +1,122 @@
+package blockstore
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"0chain.net/chaincore/block"
+	"0chain.net/core/datastore"
+	"0chain.net/core/viper"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetMPrefixDir(t *testing.T) {
+
+	type input struct {
+		mPrefix        string
+		roundRemainder int64
+	}
+	tests := map[int64]*input{
+		1: {
+			mPrefix:        fmt.Sprintf("%s%d", mPrefix, 0),
+			roundRemainder: 1,
+		},
+		twoMillion: {
+			mPrefix:        fmt.Sprintf("%s%d", mPrefix, 1),
+			roundRemainder: 0,
+		},
+		twoMillion*2 + 1: {
+			mPrefix:        fmt.Sprintf("%s%d", mPrefix, 2),
+			roundRemainder: 1,
+		},
+		twoMillion*300 + 1000: {
+			mPrefix:        fmt.Sprintf("%s%d", mPrefix, 300),
+			roundRemainder: 1000,
+		},
+	}
+
+	for round := range tests {
+		t.Run(fmt.Sprintf("Test for round %d", round), func(t *testing.T) {
+			in := tests[round]
+			require.NotNil(t, in)
+			mPrefix, roundRemainder := getMPrefixDir(round)
+			require.Equal(t, in.mPrefix, mPrefix)
+			require.Equal(t, in.roundRemainder, roundRemainder)
+		})
+	}
+}
+
+func TestBlockStoreWriteReadFromDisk(t *testing.T) {
+	basePath := "block_store_path"
+	err := os.Mkdir(basePath, 0700)
+	require.NoError(t, err)
+
+	defer os.RemoveAll(basePath)
+
+	bStore := &BlockStore{
+		basePath:              basePath,
+		blockMetadataProvider: datastore.GetEntityMetadata("block"),
+	}
+
+	b := new(block.Block)
+	b.Hash = "new hash"
+	b.Round = 1
+	err = bStore.writeToDisk(b)
+	require.NoError(t, err)
+
+	bPath := filepath.Join(basePath, getBlockFilePath(b.Hash, b.Round))
+
+	_, err = os.Stat(bPath)
+	require.NoError(t, err)
+
+	b1, err := bStore.readFromDisk(b.Hash, b.Round)
+	require.NoError(t, err)
+
+	require.Equal(t, b.Hash, b1.Hash)
+	require.Equal(t, b.Round, b1.Round)
+}
+
+func TestBlockStoreWriteReadFromCache(t *testing.T) {
+	cachePath := "path_to_cache"
+	err := os.Mkdir(cachePath, 0700)
+	require.NoError(t, err)
+
+	defer os.RemoveAll(cachePath)
+	config := `
+cache:
+    path: "path_to_cache"
+    size: "1GB"
+`
+	viper.GetViper().SetConfigType("yaml")
+	err = viper.ReadConfig(bytes.NewReader([]byte(config)))
+	require.NoError(t, err)
+
+	bStore := &BlockStore{
+		blockMetadataProvider: datastore.GetEntityMetadata("block"),
+	}
+
+	require.NotPanics(t, func() {
+		bStore.cache = initCache(viper.Sub("cache"))
+	})
+
+	b := new(block.Block)
+	b.Hash = "new hash"
+	b.Round = 1
+
+	err = bStore.writeBlockToCache(b)
+	require.NoError(t, err)
+
+	data, err := bStore.cache.Read(b.Hash)
+	require.NoError(t, err)
+
+	b1 := bStore.blockMetadataProvider.Instance().(*block.Block)
+	r := bytes.NewReader(data)
+	err = datastore.ReadMsgpack(r, b1)
+	require.NoError(t, err)
+
+	require.Equal(t, b.Hash, b1.Hash)
+	require.Equal(t, b.Round, b1.Round)
+}
