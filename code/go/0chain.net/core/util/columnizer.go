d9453960762e3900740d87a104b15cf18cc88a59
devyetii <ebrahim.gomaa.hg@gmail.com>
2023-01-27 12:09:17 +0200

    Add columnize util method

---
+++ code/go/0chain.net/core/util/columnizer.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func columnize[T any](objects []T) (map[string][]interface{}, error) {
+	columns := make(map[string][]interface{})
+	
+	for _, obj := range objects {
+		v := reflect.ValueOf(obj)
+
+		if v.Kind() == reflect.Ptr {
+			v = v.Elem()
+		}
+
+		if v.Kind() != reflect.Struct {
+			return nil, fmt.Errorf("columnize error: type invalid")
+		}
+
+		t := v.Type()
+
+		for fidx := 0; fidx < t.NumField(); fidx++ {
+			f := t.Field(fidx)
+			fkey := f.Tag.Get("json")
+			if fkey == "" {
+				return nil, fmt.Errorf("columnize error: No json tag for field %v::%v", t.Name(), f.Name)
+			}
+
+			if _, ok := columns[fkey]; !ok {
+				columns[fkey] = make([]interface{}, 0, len(objects))
+			}
+			fvalue := v.Field(fidx).Interface()
+			fmt.Printf("%v => %v\n", fkey, fvalue)
+			columns[fkey] = append(columns[fkey], fvalue)
+		}
+		
+	}
+
+	return columns, nil
+}
