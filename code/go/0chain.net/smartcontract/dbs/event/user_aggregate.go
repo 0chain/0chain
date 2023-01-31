8e852a82227230e2c2dd6c983fc8bcb90d768a1a
manasnisar <anasnisar980@gmail.com>
2023-01-31 17:54:05 +0500

    fixing aggr table

--- code/go/0chain.net/smartcontract/dbs/event/user_aggregate.go
+++ code/go/0chain.net/smartcontract/dbs/event/user_aggregate.go
@@ -82,8 +82,7 @@ func (edb *EventDb) calculateUserAggregate(gs *globalSnapshot, round, limit, off
 
 	var currentUsers []User
 	result := edb.Store.Get().Model(&User{}).
-		Where("users.id in (select id from temp_user_ids ORDER BY ID limit ? offset ?)", limit, offset).
-		Joins("Rewards").
+		Where("users.id in (select id from temp_user_ids ORDER BY id limit ? offset ?)", limit, offset).
 		Find(&currentUsers)
 
 	if result.Error != nil {
