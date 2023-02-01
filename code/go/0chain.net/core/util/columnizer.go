f4c1cc253d9fe483a440e1878a7b2a59f622d2ce
devyetii <ebrahim.gomaa.hg@gmail.com>
2023-02-01 11:11:28 +0200

    Resolve comment in columnizer

--- code/go/0chain.net/core/util/columnizer.go
+++ code/go/0chain.net/core/util/columnizer.go
@@ -3,7 +3,22 @@ package util
 import (
 	"fmt"
 	"reflect"
+	"regexp"
+	"strings"
 )
+
+var (
+	firstCapRegex 	= regexp.MustCompile("(.)([A-Z][a-z]+)")
+	allCapRegex 	= regexp.MustCompile("([a-z0-9])([A-Z])")
+	gormColumRegex 	= regexp.MustCompile("(?m)column:([a-zA-Z0-9_]+);")
+)
+
+
+func toSnakeCase(str string) string {
+    snake := firstCapRegex.ReplaceAllString(str, "${1}_${2}")
+    snake = allCapRegex.ReplaceAllString(snake, "${1}_${2}")
+    return strings.ToLower(snake)
+}
 
 func Columnize[T any](objects []T) (map[string][]interface{}, error) {
 	columns := make(map[string][]interface{})
@@ -23,9 +38,14 @@ func Columnize[T any](objects []T) (map[string][]interface{}, error) {
 
 		for fidx := 0; fidx < t.NumField(); fidx++ {
 			f := t.Field(fidx)
+			
+			// Resolve field key in this order: json Tag > gorm.column Tag > snake-casing FieldName
 			fkey := f.Tag.Get("json")
 			if fkey == "" {
-				return nil, fmt.Errorf("columnize error: No json tag for field %v::%v", t.Name(), f.Name)
+				fkey = gormColumRegex.FindString(f.Tag.Get("gorm"))
+			}
+			if fkey == "" {
+				fkey = toSnakeCase(f.Name)
 			}
 
 			if _, ok := columns[fkey]; !ok {
