25b9f20f14705997574ef34bff90a4b82b29bfcf
devyetii <ebrahim.gomaa.hg@gmail.com>
2023-02-02 21:44:15 +0200

    Change default value of allocation file_options in events db

--- code/go/0chain.net/smartcontract/dbs/goose/migrations/002_add_allocation_fields_to_accept_nft.sql
+++ code/go/0chain.net/smartcontract/dbs/goose/migrations/002_add_allocation_fields_to_accept_nft.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE allocations ADD COLUMN IF NOT EXISTS third_party_extendable boolean default false;
-ALTER TABLE allocations ADD COLUMN IF NOT EXISTS file_options smallint default 0;
+ALTER TABLE allocations ADD COLUMN IF NOT EXISTS file_options smallint default 63;
 -- +goose StatementEnd
 
 -- +goose Down
