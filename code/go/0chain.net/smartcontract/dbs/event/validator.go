135895c3f03f7d6ef0c36db011b5d4a4416c67be
devyetii <ebrahim.gomaa.hg@gmail.com>
2023-02-15 11:54:22 +0200

    fix validator

--- code/go/0chain.net/smartcontract/dbs/event/validator.go
+++ code/go/0chain.net/smartcontract/dbs/event/validator.go
@@ -6,6 +6,7 @@ import (
 	"0chain.net/core/common"
 	"github.com/0chain/common/core/currency"
 	"github.com/0chain/common/core/logging"
+	"go.uber.org/zap"
 
 	common2 "0chain.net/smartcontract/common"
 	"0chain.net/smartcontract/dbs"
