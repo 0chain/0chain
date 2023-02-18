fa3b6db0f5a8d458a8f59ab7d5e087240453b6da
YarikRevich <yariksvitlitskiy81@gmail.com>
2023-02-18 10:16:14 -0500

    fix: fixed structure serialization for msgp

--- code/go/0chain.net/smartcontract/entity/entity.go
+++ code/go/0chain.net/smartcontract/entity/entity.go
@@ -1,7 +1,27 @@
 package entity
 
+import (
+	"encoding/binary"
+	"encoding/json"
+	"reflect"
+)
+
 // BurnTicketDetails represents client's burn ticket details
 type BurnTicketDetails struct {
 	Hash  string `json:"hash"`
 	Nonce int64  `json:"nonce"`
+}
+
+func (b *BurnTicketDetails) MarshalMsg(dst []byte) ([]byte, error) {
+	var err error
+	dst, err = json.Marshal(b)
+	return dst, err
+}
+
+func (b *BurnTicketDetails) UnmarshalMsg(src []byte) ([]byte, error) {
+	return src, json.Unmarshal(src, b)
+}
+
+func (b *BurnTicketDetails) Msgsize() int {
+	return binary.Size(reflect.ValueOf(*b))
 }
