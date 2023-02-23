94678b22c0c4b9d5f5db01681f712778ba5b6984
devyetii <ebrahim.gomaa.hg@gmail.com>
2023-02-23 03:33:49 +0200

    add provider count fields to snapshots table

---
+++ code/go/0chain.net/smartcontract/dbs/goose/migrations/008_add_snapshot_provider_count.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE snapshots ADD COLUMN IF NOT EXISTS miner_count bigint default 0;
+ALTER TABLE snapshots ADD COLUMN IF NOT EXISTS sharder_count bigint default 0;
+ALTER TABLE snapshots ADD COLUMN IF NOT EXISTS blobber_count bigint default 0;
+ALTER TABLE snapshots ADD COLUMN IF NOT EXISTS authorizer_count bigint default 0;
+ALTER TABLE snapshots ADD COLUMN IF NOT EXISTS validator_count bigint default 0;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE allocations DROP COLUMN IF EXISTS miner_count;
+ALTER TABLE allocations DROP COLUMN IF EXISTS sharder_count;
+ALTER TABLE allocations DROP COLUMN IF EXISTS blobber_count;
+ALTER TABLE allocations DROP COLUMN IF EXISTS authorizer_count;
+ALTER TABLE allocations DROP COLUMN IF EXISTS validator_count;
+-- +goose StatementEnd
