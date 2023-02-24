7c300bcb7cffd15c0e66c28f598d916bc709fa5b
Hitenjain14 <hitenjain146@gmail.com>
2023-02-25 00:34:30 +0530

    test fix

--- code/go/0chain.net/smartcontract/storagesc/blobber_test.go
+++ code/go/0chain.net/smartcontract/storagesc/blobber_test.go
@@ -123,7 +123,7 @@ func TestStorageSmartContract_addBlobber_preventDuplicates(t *testing.T) {
 	require.NoError(t, err)
 
 	_, err = blob.callAddBlobber(t, ssc, tp, balances)
-	require.NoError(t, err)
+	require.Error(t, err)
 
 	_, err = ssc.getBlobber(blob.id, balances)
 	require.NoError(t, err)
