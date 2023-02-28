fb9b596de9b53b9478e65063f3cdd89d3913dbb0
devyetii <ebrahim.gomaa.hg@gmail.com>
2023-03-01 00:03:09 +0200

    fix migration

--- code/go/0chain.net/smartcontract/dbs/goose/migrations/008_add_snapshot_provider_count.sql
+++ code/go/0chain.net/smartcontract/dbs/goose/migrations/008_add_snapshot_provider_count.sql
@@ -9,9 +9,9 @@ ALTER TABLE snapshots ADD COLUMN IF NOT EXISTS validator_count bigint default 0;
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE allocations DROP COLUMN IF EXISTS miner_count;
-ALTER TABLE allocations DROP COLUMN IF EXISTS sharder_count;
-ALTER TABLE allocations DROP COLUMN IF EXISTS blobber_count;
-ALTER TABLE allocations DROP COLUMN IF EXISTS authorizer_count;
-ALTER TABLE allocations DROP COLUMN IF EXISTS validator_count;
+ALTER TABLE snapshots DROP COLUMN IF EXISTS miner_count;
+ALTER TABLE snapshots DROP COLUMN IF EXISTS sharder_count;
+ALTER TABLE snapshots DROP COLUMN IF EXISTS blobber_count;
+ALTER TABLE snapshots DROP COLUMN IF EXISTS authorizer_count;
+ALTER TABLE snapshots DROP COLUMN IF EXISTS validator_count;
 -- +goose StatementEnd
