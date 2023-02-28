6b7ebe9e7424dcea0493b05b096f2e984527ba2c
manasnisar <anasnisar980@gmail.com>
2023-02-28 18:56:02 +0500

    fixing migration scripts

--- code/go/0chain.net/smartcontract/dbs/goose/migrations/002_user_aggrs.sql
+++ code/go/0chain.net/smartcontract/dbs/goose/migrations/002_user_aggrs.sql
@@ -1,17 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-
 CREATE TABLE public.user_aggregates (
     user_id text,
     round bigint,
