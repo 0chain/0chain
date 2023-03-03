814bf35b87682f235b51df7d2ded0c50047c6fd1
Hitenjain14 <hitenjain146@gmail.com>
2023-03-04 02:29:09 +0530

    blobber slash from delegate balance

--- code/go/0chain.net/smartcontract/dbs/event/enums.go
+++ code/go/0chain.net/smartcontract/dbs/event/enums.go
@@ -66,6 +66,7 @@ const (
 	TagAddOrOverwriteCurator
 	TagRemoveCurator
 	TagStakePoolReward
+	TagStakePoolPenalty
 	TagAddDelegatePool
 	TagUpdateDelegatePool
 	TagAddAllocation
@@ -165,6 +166,7 @@ func initTagString() {
 	TagString[TagAddOrOverwriteCurator] = "TagAddOrOverwriteCurator"
 	TagString[TagRemoveCurator] = "TagRemoveCurator"
 	TagString[TagStakePoolReward] = "TagStakePoolReward"
+	TagString[TagStakePoolPenalty] = "TagStakePoolPenalty"
 	TagString[TagAddDelegatePool] = "TagAddDelegatePool"
 	TagString[TagUpdateDelegatePool] = "TagUpdateDelegatePool"
 	TagString[TagAddAllocation] = "TagAddAllocation"
