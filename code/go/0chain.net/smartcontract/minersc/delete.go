c0208df94165204891062beff7f4a7b95263e29a
peterlimg <peterlimg@protonmail.com>
2023-03-07 14:31:04 +0800

    Optimize payFee txn
    - Do not load all miners and sharders for rewarding, load ids and get random ones first, then load selected nodes.
    - Do not load all miners and sharders for view change pool work to delete nodes, save node ids that will be deleted and remove them all at once.

---
+++ code/go/0chain.net/smartcontract/minersc/delete.go
@@ -0,0 +1,212 @@
+package minersc
+
+import (
+	"fmt"
+
+	"0chain.net/chaincore/chain/state"
+	"0chain.net/smartcontract/provider"
+	"0chain.net/smartcontract/stakepool/spenum"
+	"github.com/0chain/common/core/util"
+)
+
+var deleteNodeKeyMap = map[spenum.Provider]string{
+	spenum.Miner:   DeleteMinersKey,
+	spenum.Sharder: DeleteShardersKey,
+}
+
+var allNodeKeyMap = map[spenum.Provider]string{
+	spenum.Miner:   AllMinersKey,
+	spenum.Sharder: AllShardersKey,
+}
+
+func deleteNodesOnViewChange(state state.StateContextI, pType spenum.Provider) error {
+	var (
+		ids NodeIDs
+		err error
+	)
+
+	dKey, ok := deleteNodeKeyMap[pType]
+	if !ok {
+		return fmt.Errorf("get delete node key failed, invalid node type: %s", pType)
+	}
+
+	allKey, ok := allNodeKeyMap[pType]
+	if !ok {
+		return fmt.Errorf("get all node key failed, invalid node type: %s", pType)
+	}
+
+	ids, err = getDeleteNodeIDs(state, dKey)
+	if err != nil {
+		return err
+	}
+
+	if len(ids) == 0 {
+		return nil
+	}
+
+	// reset delete node ids list
+	if err := resetDeleteNodeIDs(state, dKey); err != nil {
+		return err
+	}
+
+	allNodeIDs, err := getNodeIDs(state, allKey)
+	if err != nil {
+		return err
+	}
+
+	allNodeIDs = removeIDs(allNodeIDs, ids)
+	_, err = state.InsertTrieNode(allKey, &allNodeIDs)
+	if err != nil {
+		return err
+	}
+
+	for _, id := range ids {
+		_, err := state.DeleteTrieNode(provider.GetKey(id))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// remove items b from a
+func removeIDs(a, b NodeIDs) NodeIDs {
+	if len(b) == 0 {
+		return a
+	}
+
+	toDeleteMap := make(map[string]struct{}, len(b))
+	for _, id := range b {
+		toDeleteMap[id] = struct{}{}
+	}
+
+	var j int
+	for _, id := range a {
+		if _, ok := toDeleteMap[id]; !ok {
+			a[j] = id
+			j++
+		}
+	}
+	a = a[:j]
+	return a
+}
+
+func getDeleteNodeIDs(state state.StateContextI, key string) (NodeIDs, error) {
+	ids, err := getNodeIDs(state, key)
+	if err != nil {
+		if err != util.ErrValueNotPresent {
+			return nil, err
+		}
+	}
+
+	return ids, nil
+}
+
+func saveDeleteNodeID(state state.StateContextI, pType spenum.Provider, id string) error {
+	dKey, ok := deleteNodeKeyMap[pType]
+	if !ok {
+		return fmt.Errorf("save delete node key failed, invalid node type: %s", pType)
+	}
+
+	ids, err := getDeleteNodeIDs(state, dKey)
+	if err != nil {
+		return err
+	}
+
+	for _, eid := range ids {
+		if id == eid {
+			// already exists
+			return nil
+		}
+	}
+
+	ids = append(ids, id)
+	_, err = state.InsertTrieNode(dKey, &ids)
+	return err
+}
+
+func resetDeleteNodeIDs(state state.StateContextI, key string) error {
+	_, err := state.InsertTrieNode(key, &NodeIDs{})
+	return err
+}
+
+//func resetDeleteSharderIDs(state state.StateContextI) error {
+//	_, err := state.InsertTrieNode(DeleteShardersKey, &NodeIDs{})
+//	return err
+//}
+
+//func removeDeleteMinerID(state state.StateContextI, id string) error {
+//	ids, err := getDeleteNodeIDs(state)
+//	if err != nil {
+//		return err
+//	}
+//
+//	var found bool
+//	for i, eid := range ids {
+//		if id == eid {
+//			ids[i] = ids[len(ids)-1]
+//			ids = ids[:len(ids)-1]
+//			found = true
+//			break
+//		}
+//	}
+//	if !found {
+//		return errors.New("delete miner id not found")
+//	}
+//
+//	_, err = state.InsertTrieNode(DeleteMinersKey, &ids)
+//	return err
+//}
+
+//func getDeleteShardersIDs(state state.StateContextI) (NodeIDs, error) {
+//	ids, err := getNodeIDs(state, DeleteShardersKey)
+//	if err != nil {
+//		if err != util.ErrValueNotPresent {
+//			return nil, err
+//		}
+//	}
+//
+//	return ids, nil
+//}
+
+//func saveDeleteSharderID(state state.StateContextI, id string) error {
+//	ids, err := getDeleteShardersIDs(state)
+//	if err != nil {
+//		return err
+//	}
+//
+//	for _, eid := range ids {
+//		if id == eid {
+//			// already exists
+//			return nil
+//		}
+//	}
+//
+//	ids = append(ids, id)
+//	_, err = state.InsertTrieNode(DeleteShardersKey, &ids)
+//	return err
+//}
+
+//func removeDeleteSharaderID(state state.StateContextI, id string) error {
+//	ids, err := getDeleteShardersIDs(state)
+//	if err != nil {
+//		return err
+//	}
+//
+//	var found bool
+//	for i, eid := range ids {
+//		if id == eid {
+//			ids[i] = ids[len(ids)-1]
+//			ids = ids[:len(ids)-1]
+//			found = true
+//			break
+//		}
+//	}
+//
+//	if !found {
+//		return errors.New("delete sharder id not found")
+//	}
+//
+//	_, err = state.InsertTrieNode(DeleteShardersKey, &ids)
+//	return err
+//}
