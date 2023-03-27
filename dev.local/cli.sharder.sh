ce8183716996452e406d1f41c87e4dea6b0cf7f0
Lz <imlangzi@qq.com>
2023-03-27 09:23:15 +0800

    chore(gomod): fixed lib issues for bls

--- dev.local/cli.sharder.sh
+++ dev.local/cli.sharder.sh
@@ -62,21 +62,6 @@ start_sharder(){
 
     cd ./sharder/sharder
 
-    snappy=$(brew --prefix snappy)
-    lz4=$(brew --prefix lz4)
-    gmp=$(brew --prefix gmp)
-    openssl=$(brew --prefix openssl@1.1)
-
-    export LIBRARY_PATH="/usr/local/lib:${openssl}/lib:${snappy}/lib:${lz4}/lib:${gmp}/lib"
-    export LD_LIBRARY_PATH="/usr/local/lib:${openssl}/lib:${snappy}/lib:${lz4}/lib:${gmp}/lib"
-    export DYLD_LIBRARY_PATH="/usr/local/lib:${openssl}/lib:${snappy}/lib:${lz4}/lib:${gmp}/lib"
-    export CGO_LDFLAGS="-L/usr/local/lib -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy -llz4 -lgmp"
-    export CGO_CFLAGS="-I/usr/local/include"
-    export CGO_CPPFLAGS="-I/usr/local/include"
-    export LDFLAGS="-L/usr/local/lib -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy -llz4"
-    export CFLAGS="-I/usr/local/include"
-    export CPPFLAGS="-I/usr/local/include"
-
     GIT_COMMIT="cli"
     go build -mod mod -o $root/data/sharder$i/sharder -v -tags "bn256 development dev" -gcflags "all=-N -l" -ldflags "-X 0chain.net/core/build.BuildTag=$GIT_COMMIT" 
 
--- dev.local/install.deps.sh
+++ dev.local/install.deps.sh
@@ -8,12 +8,12 @@ echo ""
 echo "1> Build and install rocksdb"
 echo ""
 
-wget -O - https://github.com/facebook/rocksdb/archive/v6.20.3.tar.gz | tar xz 
-cd ./rocksdb* && \
-PORTABLE=1 make -j 4 install-shared OPT=-g0 USE_RTTI=1
-cd /usr/local/lib/
-ln -fs librocksdb.6.20.3.dylib /usr/local/lib/-mmacosx-version-min=12.0librocksdb.6.20.dylib
-ln -fs librocksdb.6.20.3.dylib /usr/local/lib/-mmacosx-version-min=10.12librocksdb.6.20.dylib
+# wget -O - https://github.com/facebook/rocksdb/archive/v6.20.3.tar.gz | tar xz 
+# cd ./rocksdb* && \
+# PORTABLE=1 make -j 4 install-shared OPT=-g0 USE_RTTI=1
+# cd /usr/local/lib/
+# ln -fs librocksdb.6.20.3.dylib /usr/local/lib/-mmacosx-version-min=12.0librocksdb.6.20.dylib
+# ln -fs librocksdb.6.20.3.dylib /usr/local/lib/-mmacosx-version-min=10.12librocksdb.6.20.dylib
 
 
 
@@ -36,8 +36,8 @@ mv bls* bls
 make MCL_DIR=../mcl -C bls -j 4 install
 
 
-echo ""
-echo "4> install openssl@1.1"
-echo ""
-cd $root
-brew install openssl@1.1
+# echo ""
+# echo "4> install openssl@1.1"
+# echo ""
+# cd $root
+# brew install openssl@1.1
