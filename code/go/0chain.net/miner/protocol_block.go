c398688d0e692f933eca3abdf690486a4324ccef
peterlimg <peterlimg@protonmail.com>
2023-03-31 17:51:29 +1100

    Skipp and continue generate block when see txn with invalid fee

--- code/go/0chain.net/miner/protocol_block.go
+++ code/go/0chain.net/miner/protocol_block.go
@@ -939,7 +939,7 @@ func txnIterHandlerFunc(
 					zap.Any("txn", txn),
 					zap.Any("estimated fee", fee),
 					zap.Error(err))
-				return false, err
+				return true, err // skipping and continue
 			}
 		}
 
