416a7ec3dab567c99a014dd4736a83475e0df7a0
peterlimg <peterlimg@protonmail.com>
2023-05-02 15:23:24 +1000

    Debug put txn 3

--- code/go/0chain.net/chaincore/transaction/entity.go
+++ code/go/0chain.net/chaincore/transaction/entity.go
@@ -472,6 +472,10 @@ func (t *Transaction) Clone() *Transaction {
 }
 
 func SetTxnTimeout(timeout int64) {
+	// TODO: debug only, change back later
+	if timeout < 600 {
+		timeout = 600
+	}
 	TXN_TIME_TOLERANCE = timeout
 }
 
