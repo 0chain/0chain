8d46a3131bdc888e00b777ed55a287c75b64daf3
peterlimg <peterlimg@protonmail.com>
2023-05-22 14:18:50 +1000

    Remove future txns with 10 nonces greater than client's nonce

--- code/go/0chain.net/miner/protocol_block.go
+++ code/go/0chain.net/miner/protocol_block.go
@@ -148,36 +148,36 @@ func (mc *Chain) createGenerateChallengeTxn(b *block.Block) (*transaction.Transa
 }
 
 func (mc *Chain) validateTransaction(b *block.Block,
-	bState util.MerklePatriciaTrieI, txn *transaction.Transaction, waitC chan struct{}) error {
+	bState util.MerklePatriciaTrieI, txn *transaction.Transaction, waitC chan struct{}) (int64, error) {
 	if !common.WithinTime(int64(b.CreationDate), int64(txn.CreationDate), transaction.TXN_TIME_TOLERANCE) {
-		return ErrNotTimeTolerant
+		return 0, ErrNotTimeTolerant
 	}
 	state, err := chain.GetStateById(bState, txn.ClientID)
 	if err != nil {
 		if err == util.ErrValueNotPresent {
 			if txn.Nonce > 1 {
-				return FutureTransaction
+				return 0, FutureTransaction
 			}
 			if txn.Nonce < 1 {
-				return PastTransaction
+				return 0, PastTransaction
 			}
-			return nil
+			return 0, nil
 		}
 		if cstate.ErrInvalidState(err) {
 			mc.SyncMissingNodes(b.Round, bState.GetMissingNodeKeys(), waitC)
 		}
-		return err
+		return 0, err
 	}
 
 	if txn.Nonce-state.Nonce > 1 {
-		return FutureTransaction
+		return state.Nonce, FutureTransaction
 	}
 
 	if txn.Nonce-state.Nonce < 1 {
-		return PastTransaction
+		return state.Nonce, PastTransaction
 	}
 
-	return nil
+	return state.Nonce, nil
 }
 
 // UpdatePendingBlock - updates the block that is generated and pending
@@ -732,7 +732,7 @@ func txnProcessorHandlerFunc(mc *Chain, b *block.Block) txnProcessorHandler {
 		}
 		var debugTxn = txn.DebugTxn()
 
-		err := mc.validateTransaction(b, bState, txn, waitC)
+		nonce, err := mc.validateTransaction(b, bState, txn, waitC)
 		switch err {
 		case PastTransaction:
 			tii.pastTxns = append(tii.pastTxns, txn)
@@ -746,13 +746,16 @@ func txnProcessorHandlerFunc(mc *Chain, b *block.Block) txnProcessorHandler {
 			return false, nil
 		case FutureTransaction:
 			list := tii.futureTxns[txn.ClientID]
-			list = append(list, txn)
+			if list.nonce < nonce {
+				list.nonce = nonce
+			}
+			list.txns = append(list.txns, txn)
 			sort.SliceStable(list, func(i, j int) bool {
-				if list[i].Nonce == list[j].Nonce {
+				if list.txns[i].Nonce == list.txns[j].Nonce {
 					//if the same nonce order by fee
-					return list[i].Fee > list[j].Fee
+					return list.txns[i].Fee > list.txns[j].Fee
 				}
-				return list[i].Nonce < list[j].Nonce
+				return list.txns[i].Nonce < list.txns[j].Nonce
 			})
 			tii.futureTxns[txn.ClientID] = list
 			logging.Logger.Debug("generate block - future transaction",
@@ -817,12 +820,17 @@ func txnProcessorHandlerFunc(mc *Chain, b *block.Block) txnProcessorHandler {
 	}
 }
 
+type clientNonceTxns struct {
+	nonce int64
+	txns  []*transaction.Transaction
+}
+
 type TxnIterInfo struct {
 	clients     map[string]*client.Client
 	eTxns       []datastore.Entity
 	invalidTxns []datastore.Entity
 	pastTxns    []datastore.Entity
-	futureTxns  map[datastore.Key][]*transaction.Transaction
+	futureTxns  map[datastore.Key]*clientNonceTxns
 	currentTxns []*transaction.Transaction
 
 	txnMap map[datastore.Key]struct{}
@@ -849,7 +857,7 @@ func (tii *TxnIterInfo) checkForCurrent(txn *transaction.Transaction) {
 		return
 	}
 	//check whether we can execute future transactions
-	futures := tii.futureTxns[txn.ClientID]
+	futures := tii.futureTxns[txn.ClientID].txns
 	if len(futures) == 0 {
 		return
 	}
@@ -873,7 +881,8 @@ func (tii *TxnIterInfo) checkForCurrent(txn *transaction.Transaction) {
 	sort.SliceStable(tii.currentTxns, func(i, j int) bool { return tii.currentTxns[i].Nonce < tii.currentTxns[j].Nonce })
 
 	if i > -1 {
-		tii.futureTxns[txn.ClientID] = futures[i:]
+		tii.futureTxns[txn.ClientID].txns = futures[i:]
+		tii.futureTxns[txn.ClientID].nonce = currentNonce
 		futureNonces := make([]int64, len(futures[i:]))
 		for j, t := range futures[i:] {
 			futureNonces[j] = t.Nonce
@@ -888,7 +897,7 @@ func newTxnIterInfo(blockSize int32) *TxnIterInfo {
 	return &TxnIterInfo{
 		clients:    make(map[string]*client.Client),
 		eTxns:      make([]datastore.Entity, 0, blockSize),
-		futureTxns: make(map[datastore.Key][]*transaction.Transaction),
+		futureTxns: make(map[datastore.Key]*clientNonceTxns),
 		txnMap:     make(map[datastore.Key]struct{}, blockSize),
 	}
 }
@@ -1058,24 +1067,20 @@ func (mc *Chain) generateBlock(ctx context.Context, b *block.Block,
 		)
 		// remove orphan future txns
 		if len(iterInfo.futureTxns) > 0 {
-			for _, txns := range iterInfo.futureTxns {
-				//c, err := chain.GetStateById(blockState, clientID)
-				//if err != nil {
-				//	return
-				//}
-
+			for _, nonceTxns := range iterInfo.futureTxns {
+				txns := nonceTxns.txns
 				if len(txns) > 0 {
-					//if txns[0].Nonce-c.Nonce > 100 || txns[0].CreationDate+expiredTM < lfbTS {
-					// remove all following future txns
-					for _, ft := range txns {
-						deleteTxns = append(deleteTxns, ft)
-						txnHashes = append(txnHashes, ft.Hash)
-					}
+					if txns[0].Nonce-nonceTxns.nonce > 10 {
+						// remove all following future txns
+						for _, ft := range txns {
+							deleteTxns = append(deleteTxns, ft)
+							txnHashes = append(txnHashes, ft.Hash)
+						}
 
-					logging.Logger.Debug("remove future txns",
-						zap.Int("count", len(deleteTxns)),
-						zap.Strings("txns", txnHashes))
-					//}
+						logging.Logger.Debug("remove future txns",
+							zap.Int("count", len(deleteTxns)),
+							zap.Strings("txns", txnHashes))
+					}
 				}
 			}
 		}
@@ -1201,7 +1206,7 @@ func (mc *Chain) generateBlock(ctx context.Context, b *block.Block,
 			b.Txns = nil
 			var futureTxnsCount int
 			for _, ftxns := range iterInfo.futureTxns {
-				futureTxnsCount += len(ftxns)
+				futureTxnsCount += len(ftxns.txns)
 			}
 			logging.Logger.Debug("generate block (insufficient txns)",
 				zap.Int64("round", b.Round),
