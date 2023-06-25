792f50f788399b165955874365bcbe54c55ed407
devyetii <ebrahim.gomaa.hg@gmail.com>
2023-06-25 15:43:13 +0200

    migration to delete them

---
+++ code/go/0chain.net/smartcontract/dbs/goose/migrations/1687700443_remove_unused_indexes.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+-- +goose StatementBegin
+DROP INDEX IF EXISTS idx_rew_block_prov;
+DROP INDEX IF EXISTS idx_rew_del_prov;
+DROP INDEX IF EXISTS idx_event;
+DROP INDEX IF EXISTS idx_tcreation_date;
+DROP INDEX IF EXISTS idx_bcreation_date;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+-- +goose StatementEnd
