d9c7141a716f25daaf14275f824bdb6d1daa31e0
devyetii <ebrahim.gomaa.hg@gmail.com>
2023-06-25 18:21:40 +0200

    remove idx_ba_rankmetric

--- code/go/0chain.net/smartcontract/dbs/goose/migrations/1687700443_remove_unused_indexes.sql
+++ code/go/0chain.net/smartcontract/dbs/goose/migrations/1687700443_remove_unused_indexes.sql
@@ -8,6 +8,7 @@ DROP INDEX IF EXISTS idx_bcreation_date;
 DROP INDEX IF EXISTS idx_challenges_round_responded;
 DROP INDEX IF EXISTS idx_challenges_deleted_at;
 ALTER TABLE challenges DROP COLUMN deleted_at;
+DROP INDEX IF EXISTS idx_ba_rankmetric;
 -- +goose StatementEnd
 
 -- +goose Down
