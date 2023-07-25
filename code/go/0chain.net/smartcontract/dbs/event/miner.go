97edafb82d05ea0c9e92651dacebf454f92ce5b3
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2023-07-25 16:47:27 +0530

    Fix get nodes stats

--- code/go/0chain.net/smartcontract/dbs/event/miner.go
+++ code/go/0chain.net/smartcontract/dbs/event/miner.go
@@ -64,7 +64,7 @@ func (edb *EventDb) GetMinerWithDelegatePools(id string) (Miner, []DelegatePool,
 		Table("miners").
 		Joins("left join provider_rewards on miners.id = provider_rewards.provider_id").
 		Joins("left join delegate_pools on miners.id = delegate_pools.provider_id").
-		Where("miners.id = ? AND (delegate_pools IS NULL OR delegate_pools.status = 0)", id).
+		Where("miners.id = ? AND (delegate_pools.status IS NULL OR delegate_pools.status = 0)", id).
 		Scan(&minerDps)
 	if result.Error != nil {
 		return m, nil, result.Error
