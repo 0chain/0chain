41c2b581fc55e59f05dd124e39b898091e0dfd99
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2023-07-28 19:21:19 +0530

    Removed logging

--- code/go/0chain.net/smartcontract/storagesc/models.go
+++ code/go/0chain.net/smartcontract/storagesc/models.go
@@ -1182,14 +1182,10 @@ func (sa *StorageAllocation) payCancellationChargeToRemoveBlobber(sp *stakePool,
 		return fmt.Errorf("failed to get cancellation charge: %v", err)
 	}
 
-	logging.Logger.Info("Jayash 1", zap.Any("cancellationCharge", cancellationCharge), zap.Any("sa", sa), zap.Any("passRate", passRate), zap.Any("ba", ba), zap.Any("conf.CancellationCharge", conf.CancellationCharge), zap.Any("sa.BlobberAllocs", sa.BlobberAllocs))
-
 	if sa.WritePool < cancellationCharge {
 		cancellationCharge = sa.WritePool
 		logging.Logger.Error("insufficient funds, %v, for cancellation charge, %v. distributing the remaining write pool.")
 	}
-
-	logging.Logger.Info("Jayash 2", zap.Any("cancellationCharge", cancellationCharge), zap.Any("sa", sa), zap.Any("passRate", passRate), zap.Any("ba", ba), zap.Any("conf.CancellationCharge", conf.CancellationCharge), zap.Any("sa.BlobberAllocs", sa.BlobberAllocs))
 
 	totalWritePrice := currency.Coin(0)
 	for _, ba := range sa.BlobberAllocs {
@@ -1199,36 +1195,26 @@ func (sa *StorageAllocation) payCancellationChargeToRemoveBlobber(sp *stakePool,
 		}
 	}
 
-	logging.Logger.Info("Jayash 3", zap.Any("totalPrice", totalWritePrice))
-
 	totalCancellationChargePaid, err := ba.payCancellationCharge(sa, sp, balances, sc, passRate, totalWritePrice, cancellationCharge)
 	if err != nil {
 		return fmt.Errorf("2 error paying cancellation charge: %v", err)
 	}
-
-	logging.Logger.Info("Jayash 4", zap.Any("totalCancellationChargePaid", totalCancellationChargePaid))
 
 	sa.WritePool, err = currency.MinusCoin(sa.WritePool, totalCancellationChargePaid)
 	if err != nil {
 		return fmt.Errorf("failed to deduct cancellation charges from write pool: %v", err)
 	}
 
-	logging.Logger.Info("Jayash 5", zap.Any("sa.WritePool", sa.WritePool))
-
 	i, err := totalCancellationChargePaid.Int64()
 	if err != nil {
 		return fmt.Errorf("failed to convert deduction from write pool to int64: %v", err)
 	}
-
-	logging.Logger.Info("Jayash 6", zap.Any("i", i))
 
 	balances.EmitEvent(event.TypeStats, event.TagUnlockWritePool, sa.ID, event.WritePoolLock{
 		Client:       clientID,
 		AllocationId: sa.ID,
 		Amount:       i,
 	})
-
-	logging.Logger.Info("Jayash 7", zap.Any("i", i))
 
 	return nil
 }
