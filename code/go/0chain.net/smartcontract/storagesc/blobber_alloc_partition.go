8305b031997590a80bdd3ec6d6beff412cfe0bbd
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2023-08-04 03:08:10 +0530

    Fix

--- code/go/0chain.net/smartcontract/storagesc/blobber_alloc_partition.go
+++ code/go/0chain.net/smartcontract/storagesc/blobber_alloc_partition.go
@@ -59,18 +59,8 @@ func removeAllocationFromBlobberPartitions(state state.StateContextI, blobberID,
 	}
 
 	err = blobAllocsParts.Remove(state, allocID)
-	if err != nil && !partitions.ErrItemNotFound(err) {
-		logging.Logger.Error("could not remove allocation from blobber",
-			zap.Error(err),
-			zap.String("blobber", blobberID),
-			zap.String("allocation", allocID))
-		return fmt.Errorf("could not remove allocation from blobber: %v", err)
-	} else if partitions.ErrItemNotFound(err) {
-		logging.Logger.Error("allocation is not in partition",
-			zap.Error(err),
-			zap.String("blobber", blobberID),
-			zap.String("allocation", allocID))
-	} else if err == nil {
+
+	if err == nil {
 		if err := blobAllocsParts.Save(state); err != nil {
 			logging.Logger.Info("could not update blobber allocation partitions",
 				zap.Error(err),
@@ -78,33 +68,41 @@ func removeAllocationFromBlobberPartitions(state state.StateContextI, blobberID,
 				zap.String("allocation", allocID))
 			return fmt.Errorf("could not update blobber allocation partitions: %v", err)
 		}
-	} else {
-		logging.Logger.Error("unknown error",
-			zap.Error(err),
+
+		allocNum, err := blobAllocsParts.Size(state)
+		if err != nil {
+			return fmt.Errorf("could not get challenge partition size: %v", err)
+		}
+
+		logging.Logger.Info("blobber allocation partition size",
+			zap.Int("size", allocNum),
 			zap.String("blobber", blobberID),
 			zap.String("allocation", allocID))
-	}
 
-	allocNum, err := blobAllocsParts.Size(state)
-	if err != nil {
-		return fmt.Errorf("could not get challenge partition size: %v", err)
-	}
+		if allocNum > 0 {
+			return nil
+		}
 
-	logging.Logger.Info("blobber allocation partition size",
-		zap.Int("size", allocNum),
-		zap.String("blobber", blobberID),
-		zap.String("allocation", allocID))
+		// remove blobber from challenge ready partition when there's no allocation bind to it
+		err = partitionsChallengeReadyBlobbersRemove(state, blobberID)
+		if err != nil && !partitions.ErrItemNotFound(err) {
+			// it could be empty if we finalize the allocation before committing any read or write
+			return fmt.Errorf("failed to remove blobber from challenge ready partitions: %v", err)
+		}
 
-	if allocNum > 0 {
 		return nil
+	} else {
+		if !partitions.ErrItemNotFound(err) {
+			logging.Logger.Error("could not remove allocation from blobber",
+				zap.Error(err),
+				zap.String("blobber", blobberID),
+				zap.String("allocation", allocID))
+		} else {
+			logging.Logger.Error("allocation is not in partition",
+				zap.Error(err),
+				zap.String("blobber", blobberID),
+				zap.String("allocation", allocID))
+		}
+		return fmt.Errorf("could not remove allocation from blobber: %v", err)
 	}
-
-	// remove blobber from challenge ready partition when there's no allocation bind to it
-	err = partitionsChallengeReadyBlobbersRemove(state, blobberID)
-	if err != nil && !partitions.ErrItemNotFound(err) {
-		// it could be empty if we finalize the allocation before committing any read or write
-		return fmt.Errorf("failed to remove blobber from challenge ready partitions: %v", err)
-	}
-
-	return nil
 }
