afa256f560188b701a7fda2311882293a11f9186
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2023-08-04 13:41:11 +0530

    Fix

--- code/go/0chain.net/smartcontract/storagesc/blobber.go
+++ code/go/0chain.net/smartcontract/storagesc/blobber.go
@@ -877,7 +877,7 @@ func (sc *StorageSmartContract) commitBlobberConnection(
 				return "", fmt.Errorf("could not add blobber allocation to partitions: %v", err)
 			}
 		}
-	} else if alloc.Stats.UsedSize == 0 && commitConnection.WriteMarker.Size < 0 {
+	} else if blobAlloc.Stats.UsedSize == 0 && commitConnection.WriteMarker.Size < 0 {
 		if err := removeAllocationFromBlobberPartitions(balances, blobber.ID, alloc.ID); err != nil {
 			logging.Logger.Error("remove_blobber_allocation_from_partitions_error",
 				zap.String("blobber", blobber.ID),
--- code/go/0chain.net/smartcontract/storagesc/blobber_alloc_partition.go
+++ code/go/0chain.net/smartcontract/storagesc/blobber_alloc_partition.go
@@ -49,47 +49,53 @@ func partitionsBlobberAllocationsAdd(state state.StateContextI, blobberID, alloc
 
 // removeAllocationFromBlobberPartitions removes the allocation from blobber
 func removeAllocationFromBlobberPartitions(state state.StateContextI, blobberID, allocID string) error {
-	logging.Logger.Info("remove allocation from blobber partitions",
-		zap.String("blobber", blobberID),
-		zap.String("allocation", allocID))
-
 	blobAllocsParts, err := partitionsBlobberAllocations(blobberID, state)
 	if err != nil {
 		return fmt.Errorf("could not get blobber allocations partition: %v", err)
 	}
 
 	err = blobAllocsParts.Remove(state, allocID)
-	if err != nil && !partitions.ErrItemNotFound(err) {
-		logging.Logger.Error("could not remove allocation from blobber",
-			zap.Error(err),
-			zap.String("blobber", blobberID),
-			zap.String("allocation", allocID))
-		return fmt.Errorf("could not remove allocation from blobber: %v", err)
-	} else if partitions.ErrItemNotFound(err) {
-		logging.Logger.Error("allocation is not in partition",
-			zap.Error(err),
-			zap.String("blobber", blobberID),
-			zap.String("allocation", allocID))
-	} else if err == nil {
+
+	if err == nil {
 		if err := blobAllocsParts.Save(state); err != nil {
+			logging.Logger.Info("could not update blobber allocation partitions",
+				zap.Error(err),
+				zap.String("blobber", blobberID),
+				zap.String("allocation", allocID))
 			return fmt.Errorf("could not update blobber allocation partitions: %v", err)
 		}
-	}
 
-	allocNum, err := blobAllocsParts.Size(state)
-	if err != nil {
-		return fmt.Errorf("could not get challenge partition size: %v", err)
-	}
+		allocNum, err := blobAllocsParts.Size(state)
+		if err != nil {
+			return fmt.Errorf("could not get challenge partition size: %v", err)
+		}
 
-	if allocNum > 0 {
+		if allocNum > 0 {
+			return nil
+		}
+
+		// remove blobber from challenge ready partition when there's no allocation bind to it
+		err = partitionsChallengeReadyBlobbersRemove(state, blobberID)
+		if err != nil && !partitions.ErrItemNotFound(err) {
+			// it could be empty if we finalize the allocation before committing any read or write
+			return fmt.Errorf("failed to remove blobber from challenge ready partitions: %v", err)
+		}
+
 		return nil
-	}
-
-	// remove blobber from challenge ready partition when there's no allocation bind to it
-	err = partitionsChallengeReadyBlobbersRemove(state, blobberID)
-	if err != nil && !partitions.ErrItemNotFound(err) {
-		// it could be empty if we finalize the allocation before committing any read or write
-		return fmt.Errorf("failed to remove blobber from challenge ready partitions: %v", err)
+	} else {
+		if !partitions.ErrItemNotFound(err) {
+			logging.Logger.Error("allocation is not in partition",
+				zap.Error(err),
+				zap.String("blobber", blobberID),
+				zap.String("allocation", allocID))
+		} else {
+			logging.Logger.Error("error removing allocation from blobber",
+				zap.Error(err),
+				zap.String("blobber", blobberID),
+				zap.String("allocation", allocID),
+				zap.String("error", err.Error()))
+			return fmt.Errorf("could not remove allocation from blobber: %v", err)
+		}
 	}
 
 	return nil
