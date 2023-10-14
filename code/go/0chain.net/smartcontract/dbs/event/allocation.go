7474b88f5b624cd985888fba4e726f7fc085da71
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2023-10-15 03:09:25 +0530

    Fix

--- code/go/0chain.net/smartcontract/dbs/event/allocation.go
+++ code/go/0chain.net/smartcontract/dbs/event/allocation.go
@@ -103,7 +103,7 @@ func (edb *EventDb) GetExpiredAllocation(blobberID string) ([]string, error) {
 
 	err := db.Model(&AllocationBlobberTerm{}).
 		Joins("JOIN allocations ON allocation_blobber_terms.alloc_id = allocations.id").
-		Where("allocation_blobber_terms.blobber_id = ? AND allocations.finalized = ? AND allocations.expiration > ?", blobberID, false, time.Now().Unix()).
+		Where("allocation_blobber_terms.blobber_id = ? AND allocations.finalized = ? AND allocations.expiration < ?", blobberID, false, time.Now().Unix()).
 		Pluck("allocations.allocation_id", &allocationIDs).Error
 	if err != nil {
 		logging.Logger.Error("error retrieving finalized allocation for blobber", zap.Error(err))
