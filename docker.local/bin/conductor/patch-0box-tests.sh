978ca10d678255cc0850ab985af4a306dcc36b29
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2023-10-16 21:16:43 +0200

    fix: added ip address override patch

--- docker.local/bin/conductor/patch-0box-tests.sh
+++ docker.local/bin/conductor/patch-0box-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed -i 's/ipv4_address: 198.18.0.98/ipv4_address: 198.18.0.92/' docker.local/docker-compose-dev.yaml
+sed -i 's/ipv4_address: 198.18.0.150/ipv4_address: 198.18.0.150/' docker.local/docker-compose-dev.yaml
