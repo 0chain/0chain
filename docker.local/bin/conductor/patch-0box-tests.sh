d9850de5507de1c5b9dbdd951b4484abc08e2ddc
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2023-10-19 17:15:46 +0200

    fix: added access and secrets keys for 0box

--- docker.local/bin/conductor/patch-0box-tests.sh
+++ docker.local/bin/conductor/patch-0box-tests.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 sed -i 's/ipv4_address: 198.18.0.98/ipv4_address: 198.18.0.150/' docker.local/docker-compose-dev.yml
+sed -i 's/AWS_ACCESS_KEY_ID=key_id/AWS_ACCESS_KEY_ID=$1/' docker.local/docker-compose-dev.yml
+sed -i 's/AWS_SECRET_ACCESS_KEY=secret_key/AWS_SECRET_ACCESS_KEY=$2/' docker.local/docker-compose-dev.yml
