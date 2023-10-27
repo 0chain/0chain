fbfe1b36e7679907b4aa55c8f712d2f47c4ee26f
Manohar Reddy <b.manu199@gmail.com>
2023-10-27 22:39:32 +0200

    update postgres init-db script

--- docker.local/sql_script/init-db.sh
+++ docker.local/sql_script/init-db.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+set -e
+
 chown postgres:postgres $SLOW_TABLESPACE_PATH
 chmod g+rwx $SLOW_TABLESPACE_PATH
-create tablespace $SLOW_TABLESPACE owner postgres location $SLOW_TABLESPACE_PATH;
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+create tablespace $SLOW_TABLESPACE owner $POSTGRES_USER location $SLOW_TABLESPACE_PATH;
+EOSQL
