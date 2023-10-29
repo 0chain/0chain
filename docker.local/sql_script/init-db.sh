82c85f32fe0e06d5586d218f3b0e212a43652564
Manohar Reddy <b.manu199@gmail.com>
2023-10-29 17:49:12 +0100

    not mount the volume

--- docker.local/sql_script/init-db.sh
+++ docker.local/sql_script/init-db.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-chown postgres:postgres $SLOW_TABLESPACE_PATH
-chmod g+rwx $SLOW_TABLESPACE_PATH
+mkdir -p $SLOW_TABLESPACE_PATH
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 create tablespace $SLOW_TABLESPACE owner $POSTGRES_USER location $SLOW_TABLESPACE_PATH;
