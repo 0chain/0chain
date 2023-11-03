40b97ecfc787ca21e968addb555b302c86d343b4
devyetii <ebrahim.gomaa.hg@gmail.com>
2023-11-03 10:45:19 +0200

    update 0box patch script to change block_worker

--- docker.local/bin/conductor/patch-0box-tests.sh
+++ docker.local/bin/conductor/patch-0box-tests.sh
@@ -3,3 +3,4 @@
 sed -i 's/ipv4_address: 198.18.0.98/ipv4_address: 198.18.0.95/' docker.local/docker-compose-dev.yml
 sed -i "s/AWS_ACCESS_KEY_ID=key_id/AWS_ACCESS_KEY_ID=$1/" docker.local/docker-compose-dev.yml
 sed -i "s/AWS_SECRET_ACCESS_KEY=secret_key/AWS_SECRET_ACCESS_KEY=$2/" docker.local/docker-compose-dev.yml
+sed -i "s/block_worker: .*$/block_worker: $3/" docker.local/config/0box.yaml
