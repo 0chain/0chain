a8cb5d8e0518cdddd894c7872975777444969aa7
Manohar Reddy <b.manu199@gmail.com>
2023-11-03 14:41:07 +0100

    increase the cost to 10 times

--- docker.local/sql_script/000-init-db.sh
+++ docker.local/sql_script/000-init-db.sh
@@ -4,8 +4,6 @@ set -e
 mkdir -p $SLOW_TABLESPACE_PATH
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-create tablespace $SLOW_TABLESPACE location '$SLOW_TABLESPACE_PATH';
-alter tablespace $SLOW_TABLESPACE set ( seq_page_cost=5, random_page_cost=5 );
+    create tablespace $SLOW_TABLESPACE location '$SLOW_TABLESPACE_PATH';
+    alter tablespace $SLOW_TABLESPACE set ( seq_page_cost=10, random_page_cost=10 );
 EOSQL
-
-# todo: we might have to tune seq_page_cost & random_page_cost based on disk performance
