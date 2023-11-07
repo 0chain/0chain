f8f7f6e8b437411431e2bef533898328fbbbcbba
peterlimg <peterlimg@protonmail.com>
2023-11-07 15:35:34 +1100

    Auth patch fix

--- docker.local/bin/conductor/patch-authorizer-tests.sh
+++ docker.local/bin/conductor/patch-authorizer-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
- sed -i 's/ethereum_node_url:\ .*/ethereum_node_url: https://polygon-mumbai.g.alchemy.com/v2/VTT3Q9SUeQf4v38EjaHoZwsh6FPh300b/' config/config.yaml
+sed -i 's|ethereum_node_url:\ .*|ethereum_node_url: https://polygon-mumbai.g.alchemy.com/v2/VTT3Q9SUeQf4v38EjaHoZwsh6FPh300b|' config/config.yaml
