0eadfb165b60c67d94d2c881bfd7d65b1e67e4ec
devyetii <ebrahim.gomaa.hg@gmail.com>
2023-11-07 14:44:53 +0200

    basic test finalization

---
+++ docker.local/bin/conductor/download_with_verification.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+remotepath=$1
+
+# create random file
+head -c 100M < /dev/urandom > update.bin
+
+# update it
+./zboxcli/zbox --wallet testing.json download \
+    --allocation "$(cat ~/.zcn/allocation.txt)" \
+    --verifyDownload \
+    --localpath=download.bin \
+    --remotepath=$remotepath
