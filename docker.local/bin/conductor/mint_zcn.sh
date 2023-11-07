fa19675eb4c0fcccb223e3a4180083c2eda45667
peterlimg <peterlimg@protonmail.com>
2023-11-07 16:36:01 +1100

    Mint zcn for specific burn tickets

--- docker.local/bin/conductor/mint_zcn.sh
+++ docker.local/bin/conductor/mint_zcn.sh
@@ -5,4 +5,4 @@ burn_zcn_output=$(./zwalletcli/zwallet bridge-burn-eth --amount 10000000000 --wa
 tx=$(echo $burn_zcn_output | sed -n 's/.*burn \[OK\]: \(.*\)/\1/p')
 
 # get authorizers signatures
-./zwalletcli/zwallet bridge-mint-zcn --wallet wallet.json
+./zwalletcli/zwallet bridge-mint-zcn --burn-txn-hash $tx --wallet wallet.json
