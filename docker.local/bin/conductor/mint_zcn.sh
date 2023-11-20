4b3b90fc1f85cedca9a8ca053712252e75dfd0ec
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2023-11-20 17:52:26 +0100

    fix: changed mint_zcn amount to be minted to 100 ZCN

--- docker.local/bin/conductor/mint_zcn.sh
+++ docker.local/bin/conductor/mint_zcn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # burn zcn in 0chain network
-burn_zcn_output=$(./zwalletcli/zwallet bridge-burn-eth --amount 10000000000 --wallet wallet.json)
+burn_zcn_output=$(./zwalletcli/zwallet bridge-burn-eth --amount 1000000000000 --wallet wallet.json)
 
 tx=$(echo $burn_zcn_output | sed -n 's/.*burn \[OK\]: \(.*\)/\1/p')
 
