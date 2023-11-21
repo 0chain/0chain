bde9bc87da7e8564ca63c06222d23b73f5b690e6
Narasimha Murthy <jlnmurthy95@gmail.com>
2023-11-21 12:28:44 +0530

    move duplicate column migration to new file

--- code/go/0chain.net/smartcontract/dbs/goose/migrations/1700159742_remove_unused_cols_in_blobber.sql
+++ code/go/0chain.net/smartcontract/dbs/goose/migrations/1700159742_remove_unused_cols_in_blobber.sql
@@ -6,7 +6,6 @@ ALTER TABLE blobbers DROP COLUMN IF EXISTS logo_url;
 ALTER TABLE blobbers DROP COLUMN IF EXISTS description;
 ALTER TABLE blobbers DROP COLUMN IF EXISTS latitude;
 ALTER TABLE blobbers DROP COLUMN IF EXISTS longitude;
-ALTER TABLE blobbers DROP COLUMN IF EXISTS used;
 -- +goose StatementEnd
 
 -- +goose Down
@@ -17,5 +16,4 @@ ALTER TABLE blobbers ADD COLUMN IF NOT EXISTS logo_url text default '';
 ALTER TABLE blobbers ADD COLUMN IF NOT EXISTS description text default '';
 ALTER TABLE blobbers ADD COLUMN IF NOT EXISTS latitude numeric default 0;
 ALTER TABLE blobbers ADD COLUMN IF NOT EXISTS longitude numeric default 0;
-ALTER TABLE blobbers ADD COLUMN IF NOT EXISTS used bigint default 0;
 -- +goose StatementEnd
