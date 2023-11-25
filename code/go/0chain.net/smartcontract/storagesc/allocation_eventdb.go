403ad0d4e8f2f0e2bb3ef33caac96498d10e84b2
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2023-11-25 08:59:17 +0530

    Debug

--- code/go/0chain.net/smartcontract/storagesc/allocation_eventdb.go
+++ code/go/0chain.net/smartcontract/storagesc/allocation_eventdb.go
@@ -40,7 +40,8 @@ func allocationTableToStorageAllocationBlobbers(alloc *event.Allocation, eventDb
 		return nil, fmt.Errorf("error retrieving blobbers from db: %v", err)
 	}
 
-	var gbSize = sizeInGB(bSize(alloc.Size, alloc.DataShards))
+	blobberSize := bSize(alloc.Size, alloc.DataShards)
+	var gbSize = sizeInGB(blobberSize)
 
 	for _, b := range blobbers {
 		storageNodes = append(storageNodes, &storageNodeResponse{
