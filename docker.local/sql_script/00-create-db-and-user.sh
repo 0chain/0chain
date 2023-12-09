bb9920da7fbe6d40cfebfe3e173039dd37b128b8
devyetii <ebrahim.gomaa.hg@gmail.com>
2023-12-09 11:44:37 +0200

    fix create database script

--- docker.local/sql_script/00-create-db-and-user.sh
+++ docker.local/sql_script/00-create-db-and-user.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 set -e
 
+DB_NAME=events_db
+DB_USER=zchain_user
+DB_PASSWORD=zchian
+
+# Add extension
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE extension ltree;
-    CREATE DATABASE events_db;
+EOSQL
+
+# Create the db if not exists
+DB_EXISTS=$(psql -U "$POSTGRES_USER" -lqt | cut -d \| -f 1 | grep -w "$DB_NAME" || true);
+if [ -z "$DB_EXISTS" ]; then
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE $DB_NAME";
+fi
+
+# Create the user if not exists, and give permissions
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     \connect events_db;
-    CREATE USER zchain_user WITH ENCRYPTED PASSWORD 'zchian';
-    GRANT ALL PRIVILEGES ON DATABASE events_db TO zchain_user;
+
+    DO \$\$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = '$DB_USER') THEN
+            CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASSWORD';
+        END IF;
+    END
+    \$\$;
+
+    GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
 EOSQL
