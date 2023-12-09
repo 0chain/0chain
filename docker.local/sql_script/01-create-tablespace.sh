c344049ef969528cf47b9774d4f14af5d544176c
devyetii <ebrahim.gomaa.hg@gmail.com>
2023-12-09 12:26:47 +0200

    safer create tablespace script

--- docker.local/sql_script/01-create-tablespace.sh
+++ docker.local/sql_script/01-create-tablespace.sh
@@ -3,7 +3,12 @@ set -e
 
 mkdir -p $SLOW_TABLESPACE_PATH
 
+TABLESPACE_EXISTS=$(psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -tAc "SELECT 1 FROM pg_tablespace WHERE spcname = '$SLOW_TABLESPACE'")
+
+if [ -z "$TABLESPACE_EXISTS" ]; then
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "create tablespace $SLOW_TABLESPACE location '$SLOW_TABLESPACE_PATH';";
+fi
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    create tablespace $SLOW_TABLESPACE location '$SLOW_TABLESPACE_PATH';
     ALTER TABLESPACE $SLOW_TABLESPACE OWNER TO zchain_user;
 EOSQL
