a2d624f9ee7f87d0afef520f1d1da62831ad2e60
devyetii <ebrahim.gomaa.hg@gmail.com>
2023-12-09 02:37:02 +0200

    add tablespace permissions to init script

--- docker.local/sql_script/000-init-db.sh
+++ docker.local/sql_script/000-init-db.sh
@@ -5,4 +5,5 @@ mkdir -p $SLOW_TABLESPACE_PATH
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 create tablespace $SLOW_TABLESPACE location '$SLOW_TABLESPACE_PATH';
+ALTER TABLESPACE $SLOW_TABLESPACE OWNER TO zchain_user;
 EOSQL
