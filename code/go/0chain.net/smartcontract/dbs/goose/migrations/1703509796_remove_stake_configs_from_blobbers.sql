6b310d46b831afdb20259292572f24df87e68883
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2023-12-25 21:08:01 +0530

    Removed min_stake and max_stake from all providers

--- code/go/0chain.net/smartcontract/dbs/goose/migrations/1703509796_remove_stake_configs_from_blobbers.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE blobbers DROP COLUMN IF EXISTS min_stake;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE blobbers DROP COLUMN IF EXISTS max_stake;
--- +goose StatementEnd
