f3a42c0d1ffa6a7fa6a683702b46cc2c76580e24
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-01-05 20:13:43 +0530

    Debug

--- code/go/0chain.net/chaincore/chain/handler.go
+++ code/go/0chain.net/chaincore/chain/handler.go
@@ -769,6 +769,7 @@ func DiagnosticsHomepageHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "</td>")
 	fmt.Fprintf(w, "<td valign='top'>")
 	fmt.Fprintf(w, "<li><a href='_chain_stats'>/_chain_stats</a></li>")
+	fmt.Fprintf(w, "<li><a href='_txn_stats'>/_txn_stats</a></li>")
 
 	if node.NodeType(selfNodeType) == node.NodeTypeSharder {
 		fmt.Fprintf(w, "<li><a href='_transaction_errors'>/_transaction_errors</a></li>")
