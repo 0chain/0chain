5ac821caf2f6eb4c6ba22146f4dbf52ae4c40aa9
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-01-16 19:56:43 +0530

    Fix

--- code/go/0chain.net/smartcontract/storagesc/block_reward.go
+++ code/go/0chain.net/smartcontract/storagesc/block_reward.go
@@ -59,13 +59,18 @@ func (ssc *StorageSmartContract) blobberBlockRewards(t *transaction.Transaction,
 			"cannot get all blobbers list: "+err.Error())
 	}
 
-	defer func() {
+	defer cstate.WithActivation(balances, "hard_fork_1", func() {
+	}, func() {
+		logging.Logger.Info("blobber_block_rewards : cleaning older partition",
+			zap.Int64("round", balances.GetBlock().Round),
+			zap.String("block_hash", balances.GetBlock().Hash))
+
 		_, err = balances.DeleteTrieNode(BlobberRewardKey(GetPreviousRewardRound(balances.GetBlock().Round, conf.BlockReward.TriggerPeriod)))
 		if err != nil {
 			logging.Logger.Error("blobber_block_rewards_failed",
 				zap.String("deleting blobber reward node", err.Error()))
 		}
-	}()
+	})
 
 	hashString := encryption.Hash(balances.GetTransaction().Hash + balances.GetBlock().PrevHash)
 	var randomSeed int64
