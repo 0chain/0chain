dbc51904644f5f3e43a4c730b2caf5fe92b78568
devyetii <ebrahim.gomaa.hg@gmail.com>
2024-01-18 11:41:49 +0200

    initialize the column with real value

--- code/go/0chain.net/smartcontract/dbs/goose/migrations/1705569023_add_total_allocations_field_to_global_snapshot.sql
+++ code/go/0chain.net/smartcontract/dbs/goose/migrations/1705569023_add_total_allocations_field_to_global_snapshot.sql
@@ -1,6 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE snapshots ADD COLUMN total_allocations BIGINT NOT NULL DEFAULT 0;
+
+UPDATE snapshots SET total_allocations = (SELECT COUNT(*) FROM allocations);
+
 -- +goose StatementEnd
 
 -- +goose Down
