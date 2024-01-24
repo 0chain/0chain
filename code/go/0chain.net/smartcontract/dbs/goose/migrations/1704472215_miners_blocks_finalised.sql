ddd3f8fc96596a4df17efc1fb49478f8bdc3ddfa
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-01-24 19:44:02 +0530

    Fix

--- code/go/0chain.net/smartcontract/dbs/goose/migrations/1704472215_miners_blocks_finalised.sql
+++ code/go/0chain.net/smartcontract/dbs/goose/migrations/1704472215_miners_blocks_finalised.sql
@@ -1,8 +1,8 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE miners ADD COLUMN blocks_finalised BIGINT NOT NULL DEFAULT (select count(*) from blocks where miner_id = miners.id);
-ALTER TABLE miner_aggregates ADD COLUMN blocks_finalised BIGINT NOT NULL DEFAULT (select count(*) from blocks where miner_id = miner_aggregates.id);
-ALTER TABLE miner_snapshots ADD COLUMN blocks_finalised BIGINT NOT NULL DEFAULT (select count(*) from blocks where miner_id = miner_snapshots.id);
+ALTER TABLE miner_aggregates ADD COLUMN blocks_finalised BIGINT NOT NULL DEFAULT (select count(*) from blocks where miner_id = miner_aggregates.miner_id);
+ALTER TABLE miner_snapshots ADD COLUMN blocks_finalised BIGINT NOT NULL DEFAULT (select count(*) from blocks where miner_id = miner_snapshots.miner_id);
 -- +goose StatementEnd
 
 -- +goose Down
