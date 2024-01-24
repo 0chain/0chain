3b816cf2dad7d1e69bce99686ba19b08f118bc9c
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-01-24 17:09:25 +0530

    Initalise blocks finalised

--- code/go/0chain.net/smartcontract/dbs/goose/migrations/1704472215_miners_blocks_finalised.sql
+++ code/go/0chain.net/smartcontract/dbs/goose/migrations/1704472215_miners_blocks_finalised.sql
@@ -1,8 +1,8 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE miners ADD COLUMN blocks_finalised BIGINT NOT NULL DEFAULT 0;
-ALTER TABLE miner_aggregates ADD COLUMN blocks_finalised BIGINT NOT NULL DEFAULT 0;
-ALTER TABLE miner_snapshots ADD COLUMN blocks_finalised BIGINT NOT NULL DEFAULT 0;
+ALTER TABLE miners ADD COLUMN blocks_finalised BIGINT NOT NULL DEFAULT (select count(*) from blocks where miner_id = miners.id);
+ALTER TABLE miner_aggregates ADD COLUMN blocks_finalised BIGINT NOT NULL DEFAULT (select count(*) from blocks where miner_id = miners.id);
+ALTER TABLE miner_snapshots ADD COLUMN blocks_finalised BIGINT NOT NULL DEFAULT (select count(*) from blocks where miner_id = miners.id);
 -- +goose StatementEnd
 
 -- +goose Down
