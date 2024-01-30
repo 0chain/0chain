be2c3fbf103dbbbc71a9d01319b28ac421513356
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-01-30 19:57:37 +0530

    Debug

--- code/go/0chain.net/chaincore/chain/post_finalize_integration_tests.go
+++ code/go/0chain.net/chaincore/chain/post_finalize_integration_tests.go
@@ -5,6 +5,7 @@ package chain
 
 import (
 	"context"
+	"log"
 
 	"0chain.net/chaincore/block"
 	"0chain.net/chaincore/transaction"
@@ -15,27 +16,29 @@ import (
 
 type TxnHandler func(txn *transaction.Transaction, client *crpc.Entity) error
 
-var txnHandlers = map[string] TxnHandler{
-	"generate_challenge": func(txn *transaction.Transaction, client *crpc.Entity) error{
+var txnHandlers = map[string]TxnHandler{
+	"generate_challenge": func(txn *transaction.Transaction, client *crpc.Entity) error {
 		client.ChallengeGenerated(txn.Hash)
 		return nil
 	},
-	"challenge_response": func(txn *transaction.Transaction, client *crpc.Entity) error{
+	"challenge_response": func(txn *transaction.Transaction, client *crpc.Entity) error {
 		switch txn.TransactionOutput {
 		case "challenge passed by blobber":
+			log.Println("challenge failed by blobber", zap.Any("txn", txn))
 			status := 0
 			if txn.Status == 1 {
 				status = 1
 			}
 			client.SendChallengeStatus(map[string]interface{}{
 				"blobber_id": txn.ClientID,
-				"status": status,
+				"status":     status,
 			})
 		case "Challenge Failed by Blobber":
+			log.Println("challenge failed by blobber", zap.Any("txn", txn))
 			client.SendChallengeStatus(map[string]interface{}{
-				"error": txn.TransactionData,
-				"status": 0,
-				"response": txn.TransactionOutput,
+				"error":      txn.TransactionData,
+				"status":     0,
+				"response":   txn.TransactionOutput,
 				"blobber_id": txn.ClientID,
 			})
 		}
@@ -53,7 +56,7 @@ func (c *Chain) postFinalize(ctx context.Context, fb *block.Block) error {
 		logging.Logger.Info("post_finalize processing txn",
 			zap.Any("function_name", txn.FunctionName),
 			zap.Any("hash", txn.Hash),
-			zap.Any("output", txn.TransactionOutput),	
+			zap.Any("output", txn.TransactionOutput),
 		)
 		err := handler(txn, client)
 		if err != nil {
