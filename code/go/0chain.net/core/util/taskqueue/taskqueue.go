13d8ad9b5e9b378923b6758a503b405b0ac172e8
peterlimg <peterlimg@protonmail.com>
2024-02-27 19:09:33 +0800

    Increase task worker num

--- code/go/0chain.net/core/util/taskqueue/taskqueue.go
+++ code/go/0chain.net/core/util/taskqueue/taskqueue.go
@@ -98,7 +98,7 @@ type TaskExecutor struct {
 
 // NewTaskExecutor creates a new task executor
 func NewTaskExecutor(ctx context.Context) *TaskExecutor {
-	workerNum := 5
+	workerNum := 10
 	te := &TaskExecutor{
 		workerNum: workerNum,
 		scLock:    make(chan chan struct{}, workerNum),
