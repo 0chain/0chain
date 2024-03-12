74c732eac8e8fac25d392752da935f5a5fc30c24
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-03-12 22:55:19 +0530

    Debug

--- code/go/0chain.net/smartcontract/dbs/event/process.go
+++ code/go/0chain.net/smartcontract/dbs/event/process.go
@@ -278,20 +278,19 @@ func mergeEvents(round int64, block string, events []Event) ([]Event, error) {
 }
 
 func (edb *EventDb) addEventsWorker(ctx context.Context) {
-	var gs *Snapshot
-	p := int64(-1)
-	err := edb.managePartitions(0)
-	if err != nil {
-		logging.Logger.Error("can't manage partitions")
-	}
-
-	logging.Logger.Info("Jayash add events worker started")
+	//var gs *Snapshot
+	//p := int64(-1)
+	//err := edb.managePartitions(0)
+	//if err != nil {
+	//	logging.Logger.Error("can't manage partitions")
+	//}
 
 	for {
 		es := <-edb.eventsChannel
 		func() {
 			var commit bool
 			defer func() {
+				logging.Logger.Info("Jayash processing events", zap.Any("commit", commit), zap.Any("round", es.round), zap.Any("block", es.block), zap.Any("block size", es.blockSize))
 				es.done <- commit
 			}()
 
@@ -300,16 +299,16 @@ func (edb *EventDb) addEventsWorker(ctx context.Context) {
 			commit = false
 			return
 
-			s, err := Work(ctx, gs, es, &p)
-			if err != nil {
-				logging.Logger.Error("process events", zap.Error(err))
-				commit = false
-				return
-			}
-			commit = true
-			if s != nil {
-				gs = s
-			}
+			//s, err := Work(ctx, gs, es, &p)
+			//if err != nil {
+			//	logging.Logger.Error("process events", zap.Error(err))
+			//	commit = false
+			//	return
+			//}
+			//commit = true
+			//if s != nil {
+			//	gs = s
+			//}
 		}()
 	}
 }
