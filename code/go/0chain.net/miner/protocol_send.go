b3bd4b63b9441b416cc14f742e10f6a73126c7f4
peterlimg <peterlimg@protonmail.com>
2024-03-19 21:20:42 +1100

    Change back to send tickets to all

--- code/go/0chain.net/miner/protocol_send.go
+++ code/go/0chain.net/miner/protocol_send.go
@@ -88,18 +88,18 @@ func (mc *Chain) sendVerificationTicket(ctx context.Context, b *block.Block,
 		m2m = mb.Miners
 	)
 
-	// if mc.VerificationTicketsTo() == chain.Generator &&
-	// b.MinerID != node.Self.Underlying().GetKey() {
-	if b.MinerID != node.Self.Underlying().GetKey() {
+	if mc.VerificationTicketsTo() == chain.Generator &&
+		b.MinerID != node.Self.Underlying().GetKey() {
+		// if b.MinerID != node.Self.Underlying().GetKey() {
 		if _, err := m2m.SendTo(ctx, VerificationTicketSender(bvt), b.MinerID); err != nil {
 			logging.Logger.Error("send verification ticket failed", zap.Error(err))
 		}
 		return
 	}
 
-	// ts := time.Now()
-	// m2m.SendAll(ctx, VerificationTicketSender(bvt))
-	// logging.Logger.Debug("send verification tickets to all miners success",
-	// 	zap.String("block", b.Hash),
-	// 	zap.Any("duration", time.Since(ts)))
+	ts := time.Now()
+	m2m.SendAll(ctx, VerificationTicketSender(bvt))
+	logging.Logger.Debug("send verification tickets to all miners success",
+		zap.String("block", b.Hash),
+		zap.Any("duration", time.Since(ts)))
 }
