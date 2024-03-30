f0b3dd49659dd43f5308d8312672b8960c2165eb
shahnawaz-creator <117025384+shahnawaz-creator@users.noreply.github.com>
2024-03-30 07:03:12 +0530

    add 000-init-db.sh

---
+++ docker.local/sql_script/000-init-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+mkdir -p $SLOW_TABLESPACE_PATH
+# gosu chown postgres:postgres $SLOW_TABLESPACE_PATH
+# gosu chmod g+rwx $SLOW_TABLESPACE_PATH
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+create tablespace $SLOW_TABLESPACE location '$SLOW_TABLESPACE_PATH';
+EOSQL
