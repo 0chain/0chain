3facdb24ffde82cc13958326b3b4341254a254f1
shahnawaz-creator <117025384+shahnawaz-creator@users.noreply.github.com>
2024-03-30 13:10:43 +0530

    Create build.base_go20.sh

---
+++ docker.local/bin/build.base_go20.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+cmd="build"
+build_dockerfile="docker.local/build.base/Dockerfile.build_base_go20"
+run_dockerfile="docker.local/build.base/Dockerfile.run_base"
+
+docker $cmd -f $build_dockerfile . -t zchain_build_base
+docker $cmd -f $run_dockerfile docker.local/build.base -t zchain_run_base
