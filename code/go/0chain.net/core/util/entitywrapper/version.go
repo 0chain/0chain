48d05359238ea72965cf378c0de30045b7b4e88d
peterlimg <peterlimg@protonmail.com>
2024-03-30 17:20:31 +1100

    Minor change

--- code/go/0chain.net/core/util/entitywrapper/version.go
+++ code/go/0chain.net/core/util/entitywrapper/version.go
@@ -2,5 +2,5 @@ package entitywrapper
 
 //go:generate msgp -v -tests=false -io=false -unexported
 type entityVersion struct {
-	Version string `msg:"version"`
+	Version string `msg:"version" json:"version"`
 }
