452c20f41f90945ef413d037fb21948eca444c82
devyetii <ebrahim.gomaa.hg@gmail.com>
2024-04-01 01:47:17 +0200

    debug: more logs

--- code/go/0chain.net/smartcontract/dbs/benchmark/setup.go
+++ code/go/0chain.net/smartcontract/dbs/benchmark/setup.go
@@ -1,6 +1,7 @@
 package benchmark
 
 import (
+	"fmt"
 	"strconv"
 	"time"
 
@@ -63,6 +64,9 @@ func AddMockTransactions(
 	if !viper.GetBool(benchmark.EventDbEnabled) {
 		return
 	}
+
+	fmt.Printf("From AddMockTransactions, edb config = %+v\n", eventDb.Config())
+	
 	const txnTxnSmartContract = 1000
 	for blockNumber := int64(1); blockNumber <= viper.GetInt64(benchmark.NumBlocks); blockNumber++ {
 		_ = eventDb.ManagePartitions(blockNumber)
