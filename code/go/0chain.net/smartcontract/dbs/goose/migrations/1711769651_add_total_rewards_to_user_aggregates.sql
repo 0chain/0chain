c7490b0b1f6a3c4abd7a4d4630db02f562a0ee7b
devyetii <ebrahim.gomaa.hg@gmail.com>
2024-04-04 05:53:24 +0200

    use rename in the migration

--- code/go/0chain.net/smartcontract/dbs/goose/migrations/1711769651_add_total_rewards_to_user_aggregates.sql
+++ code/go/0chain.net/smartcontract/dbs/goose/migrations/1711769651_add_total_rewards_to_user_aggregates.sql
@@ -1,9 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE user_aggregates ADD COLUMN total_reward BIGINT DEFAULT 0;
-ALTER TABLE user_snapshots ADD COLUMN total_reward BIGINT DEFAULT 0;
-ALTER TABLE user_aggregates DROP COLUMN claimable_reward;
-ALTER TABLE user_snapshots DROP COLUMN claimable_reward;
+ALTER TABLE user_aggregates RENAME COLUMN claimable_reward TO total_reward;
+ALTER TABLE user_snapshots RENAME COLUMN claimable_reward TO total_reward;
 
 UPDATE user_snapshots SET total_reward = original.total_reward FROM 
     (SELECT delegate_id, SUM(total_reward) AS total_reward FROM delegate_pools GROUP BY delegate_id)
@@ -12,8 +10,6 @@ AS original WHERE user_snapshots.user_id = original.delegate_id;
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE user_aggregates DROP COLUMN total_reward;
-ALTER TABLE user_snapshots DROP COLUMN total_reward;
-ALTER TABLE user_aggregates ADD COLUMN claimable_reward BIGINT DEFAULT 0;
-ALTER TABLE user_snapshots ADD COLUMN claimable_reward BIGINT DEFAULT 0;
+ALTER TABLE user_aggregates RENAME COLUMN total_reward TO claimable_reward;
+ALTER TABLE user_snapshots RENAME COLUMN total_reward TO claimable_reward;
 -- +goose StatementEnd
