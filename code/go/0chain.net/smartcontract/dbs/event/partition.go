c9159c6bf6eccae6d502d6c1d48f5a6f99181fbe
dabasov <yuderbasov@gmail.com>
2024-04-20 12:43:57 +0300

    changed permanent table name

--- code/go/0chain.net/smartcontract/dbs/event/partition.go
+++ code/go/0chain.net/smartcontract/dbs/event/partition.go
@@ -37,7 +37,8 @@ func (edb *EventDb) addPermanentPartition(current int64, table string) error {
 
 	timeout, cancelFunc := context.WithTimeout(context.Background(), 10*time.Minute)
 	defer cancelFunc()
-	raw := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %v_%v PARTITION OF %v FOR VALUES FROM (%v) TO (%v)", table, current, table, from, to)
+	partTableName := edb.partTableName(table, from, to)
+	raw := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %v PARTITION OF %v FOR VALUES FROM (%v) TO (%v)", partTableName, table, from, to)
 	logging.Logger.Debug("adding partition for",
 		zap.String("table", table),
 		zap.Int64("current", current),
@@ -46,17 +47,20 @@ func (edb *EventDb) addPermanentPartition(current int64, table string) error {
 	return edb.Store.Get().WithContext(timeout).Exec(raw).Error
 }
 
+func (edb *EventDb) partTableName(table string, from int64, to int64) string {
+	return fmt.Sprintf("%v_part_%v_%v", table, from, to)
+}
+
 func (edb *EventDb) movePartitionToSlowTableSpace(current int64, table string) error {
-	toMove := current - edb.settings.PermanentPartitionKeepCount
-	if toMove < 0 {
-		return nil
-	}
+	from := current * edb.settings.PermanentPartitionChangePeriod
+	to := (current + 1) * edb.settings.PermanentPartitionChangePeriod
 
 	timeout, cancelFunc := context.WithTimeout(context.Background(), 10*time.Minute)
 	defer cancelFunc()
 
 	tablespace := edb.dbConfig.Slowtablespace
+	partTableName := edb.partTableName(table, from, to)
 	// identify the partition table that needs to be moved to slow partition
-	raw := fmt.Sprintf("ALTER TABLE %v_%v SET TABLESPACE %v", table, toMove, tablespace)
+	raw := fmt.Sprintf("ALTER TABLE %v SET TABLESPACE %v", partTableName, tablespace)
 	return edb.Store.Get().WithContext(timeout).Exec(raw).Error
 }
