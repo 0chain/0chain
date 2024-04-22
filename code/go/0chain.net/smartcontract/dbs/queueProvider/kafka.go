c3c6f14be445ba24b8ea0d385b55fde02aff0cb2
peterlimg <peterlimg@protonmail.com>
2024-04-22 16:21:33 +1000

    Debug enable idempotent but not wait for success

--- code/go/0chain.net/smartcontract/dbs/queueProvider/kafka.go
+++ code/go/0chain.net/smartcontract/dbs/queueProvider/kafka.go
@@ -39,14 +39,14 @@ func NewKafkaProvider(host, username, password string, writeTimeout time.Duratio
 	config.Net.SASL.User = username
 	config.Net.SASL.Password = password
 	config.Net.SASL.Mechanism = sarama.SASLTypePlaintext
-	config.Producer.Return.Successes = true
+	// config.Producer.Return.Successes = true
 	// config.Net.MaxOpenRequests = 1
 
 	// config idempotent producer
-	// config.Producer.Idempotent = true
-	// config.Producer.RequiredAcks = sarama.WaitForAll
-	// config.Producer.Retry.Max = 5
-	// config.Metadata.AllowAutoTopicCreation = true
+	config.Producer.Idempotent = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	config.Metadata.AllowAutoTopicCreation = true
 
 	return &KafkaProvider{
 		Host:         host,
@@ -80,6 +80,7 @@ func (k *KafkaProvider) PublishToKafka(topic string, key, message []byte) error 
 	// defer cancel()
 
 	writer.Input() <- msg
+	writer.Successes()
 	// select {
 	// case writer.Input() <- msg:
 	// fmt.Println("push message success:")
