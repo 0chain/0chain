bd7014b6c0e0d03d923e969688839749e2d57cdd
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-04-30 04:52:44 +0530

    Fix

--- code/go/0chain.net/smartcontract/dbs/goose/migrations/1714430847_provider_active_delegates.sql
+++ code/go/0chain.net/smartcontract/dbs/goose/migrations/1714430847_provider_active_delegates.sql
@@ -15,23 +15,6 @@ WHERE EXISTS (
       AND delegate_pools.status = 0
 );
 
-ALTER TABLE blobber_aggregates ADD COLUMN IF NOT EXISTS active_delegates boolean;
-UPDATE blobber_aggregates
-SET active_delegates = (
-    SELECT COUNT(*)
-    FROM delegate_pools
-    WHERE delegate_pools.provider_id = blobber_aggregates.id
-      AND delegate_pools.status = 0
-)
-WHERE EXISTS (
-    SELECT 1
-    FROM delegate_pools
-    WHERE delegate_pools.provider_id = blobber_aggregates.id
-      AND delegate_pools.status = 0
-);
-
-
-
 ALTER TABLE validators ADD COLUMN IF NOT EXISTS active_delegates boolean;
 UPDATE validators
 SET active_delegates = (
@@ -46,23 +29,6 @@ WHERE EXISTS (
     WHERE delegate_pools.provider_id = validators.id
       AND delegate_pools.status = 0
 );
-
-ALTER TABLE validator_aggregates ADD COLUMN IF NOT EXISTS active_delegates boolean;
-UPDATE validator_aggregates
-SET active_delegates = (
-    SELECT COUNT(*)
-    FROM delegate_pools
-    WHERE delegate_pools.provider_id = validator_aggregates.id
-      AND delegate_pools.status = 0
-)
-WHERE EXISTS (
-    SELECT 1
-    FROM delegate_pools
-    WHERE delegate_pools.provider_id = validator_aggregates.id
-      AND delegate_pools.status = 0
-);
-
-
 
 ALTER TABLE miners ADD COLUMN IF NOT EXISTS active_delegates boolean;
 UPDATE miners
@@ -79,23 +45,6 @@ WHERE EXISTS (
       AND delegate_pools.status = 0
 );
 
-ALTER TABLE miner_aggregates ADD COLUMN IF NOT EXISTS active_delegates boolean;
-UPDATE miner_aggregates
-SET active_delegates = (
-    SELECT COUNT(*)
-    FROM delegate_pools
-    WHERE delegate_pools.provider_id = miner_aggregates.id
-      AND delegate_pools.status = 0
-)
-WHERE EXISTS (
-    SELECT 1
-    FROM delegate_pools
-    WHERE delegate_pools.provider_id = miner_aggregates.id
-      AND delegate_pools.status = 0
-);
-
-
-
 ALTER TABLE sharders ADD COLUMN IF NOT EXISTS active_delegates boolean;
 UPDATE sharders
 SET active_delegates = (
@@ -110,23 +59,6 @@ WHERE EXISTS (
     WHERE delegate_pools.provider_id = sharders.id
       AND delegate_pools.status = 0
 );
-
-ALTER TABLE sharder_aggregates ADD COLUMN IF NOT EXISTS active_delegates boolean;
-UPDATE sharder_aggregates
-SET active_delegates = (
-    SELECT COUNT(*)
-    FROM delegate_pools
-    WHERE delegate_pools.provider_id = sharder_aggregates.id
-      AND delegate_pools.status = 0
-)
-WHERE EXISTS (
-    SELECT 1
-    FROM delegate_pools
-    WHERE delegate_pools.provider_id = sharder_aggregates.id
-      AND delegate_pools.status = 0
-);
-
-
 
 ALTER TABLE authorizers ADD COLUMN IF NOT EXISTS active_delegates boolean;
 UPDATE authorizers
@@ -143,20 +75,13 @@ WHERE EXISTS (
       AND delegate_pools.status = 0
 );
 
+
+
+ALTER TABLE blobber_aggregates ADD COLUMN IF NOT EXISTS active_delegates boolean;
+ALTER TABLE validator_aggregates ADD COLUMN IF NOT EXISTS active_delegates boolean;
+ALTER TABLE miner_aggregates ADD COLUMN IF NOT EXISTS active_delegates boolean;
+ALTER TABLE sharder_aggregates ADD COLUMN IF NOT EXISTS active_delegates boolean;
 ALTER TABLE authorizer_aggregates ADD COLUMN IF NOT EXISTS active_delegates boolean;
-UPDATE authorizer_aggregates
-SET active_delegates = (
-    SELECT COUNT(*)
-    FROM delegate_pools
-    WHERE delegate_pools.provider_id = authorizer_aggregates.id
-      AND delegate_pools.status = 0
-)
-WHERE EXISTS (
-    SELECT 1
-    FROM delegate_pools
-    WHERE delegate_pools.provider_id = authorizer_aggregates.id
-      AND delegate_pools.status = 0
-);
 
 
 -- +goose StatementEnd
@@ -164,17 +89,16 @@ WHERE EXISTS (
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE blobbers DROP COLUMN IF EXISTS active_delegates;
-ALTER TABLE blobber_aggregates DROP COLUMN IF EXISTS active_delegates;
-
 ALTER TABLE validators DROP COLUMN IF EXISTS active_delegates;
-ALTER TABLE validator_aggregates DROP COLUMN IF EXISTS active_delegates;
-
 ALTER TABLE miners DROP COLUMN IF EXISTS active_delegates;
-ALTER TABLE miner_aggregates DROP COLUMN IF EXISTS active_delegates;
-
 ALTER TABLE sharders DROP COLUMN IF EXISTS active_delegates;
-ALTER TABLE sharder_aggregates DROP COLUMN IF EXISTS active_delegates;
-
 ALTER TABLE authorizers DROP COLUMN IF EXISTS active_delegates;
+
+
+ALTER TABLE blobber_aggregates DROP COLUMN IF EXISTS active_delegates;
+ALTER TABLE validator_aggregates DROP COLUMN IF EXISTS active_delegates;
+ALTER TABLE miner_aggregates DROP COLUMN IF EXISTS active_delegates;
+ALTER TABLE sharder_aggregates DROP COLUMN IF EXISTS active_delegates;
 ALTER TABLE authorizer_aggregates DROP COLUMN IF EXISTS active_delegates;
+
 -- +goose StatementEnd
