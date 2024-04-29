569bf11c84a049cfc24e110a59ac041bbaad8b78
shubham <shubam.198sk@gmail.com>
2024-04-29 11:59:41 +0530

    Added edge case and more scenario

--- code/go/0chain.net/smartcontract/zcnsc/stakepool_test.go
+++ code/go/0chain.net/smartcontract/zcnsc/stakepool_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"testing"
+     "strconv"
 
 	"github.com/stretchr/testify/require"
 	"0chain.net/core/encryption"
@@ -86,7 +87,6 @@ func TestDelegatePoolOperations(t *testing.T) {
 		// Assertion
 		require.NoError(t, err1)
 		require.NotNil(t, resp1)
-		// Add more assertions to verify stake pool lock operation and delegate pool balance
 	})
 
 	t.Run("AddToDelegatePool_Failure_GlobalNodeError", func(t *testing.T) {
@@ -137,9 +137,227 @@ func TestDelegatePoolOperations(t *testing.T) {
 		// Assertion
 		require.Error(t, err1)
 		require.Empty(t, resp1)
-		// Add more assertions to verify the error
+	})
+	t.Run("AddToDelegatePool_MaxStakeAmountExceeded", func(t *testing.T) {
+		// Setup
+		ctx := MakeMockStateContext() // Create a mock state context
+		// Mock the GetGlobalNode function to return a valid global node
+		mockGlobalNode := &GlobalNode{
+			ZCNSConfig : &ZCNSConfig {
+			MinStakeAmount:       1000,
+			MaxStakeAmount:       5000,
+			MaxDelegates:   10,
+			MinStakePerDelegate: 100,},
+
+		}
+		mockGetGlobalNode := func(balances state.StateContextI) (*GlobalNode, error) {
+			return mockGlobalNode, nil
+		}
+	
+		zcn := &ZCNSmartContract{}
+		resp1, err1 := mockGetGlobalNode(ctx.StateContextI)
+		require.NotNil(t, resp1)
+		require.NoError(t, err1)
+		stake := 6000
+		stakeString := strconv.Itoa(stake)
+        stakeBytes := []byte(stakeString)
+		resp, err := zcn.AddToDelegatePool(nil, stakeBytes, ctx.StateContextI)
+	
+		// Assertion
+		require.Error(t, err)
+		require.Empty(t, resp)
+	})
+	t.Run("AddToDelegatePool_MaxDelegatesReached", func(t *testing.T) {
+		// Setup
+		ctx := MakeMockStateContext() // Create a mock state context
+		// Mock the GetGlobalNode function to return a valid global node
+		mockGlobalNode := &GlobalNode{
+			ZCNSConfig : &ZCNSConfig {
+			MinStakeAmount:       1000,
+			MaxStakeAmount:       5000,
+			MaxDelegates:   1,
+			MinStakePerDelegate: 100,},
+
+		}
+		mockGetGlobalNode := func(balances state.StateContextI) (*GlobalNode, error) {
+			return mockGlobalNode, nil
+		}
+	
+		zcn := &ZCNSmartContract{}
+		resp1, err1 := mockGetGlobalNode(ctx.StateContextI)
+		require.NotNil(t, resp1)
+		require.NoError(t, err1)
+		stake := 2000
+		stakeString := strconv.Itoa(stake)
+        stakeBytes := []byte(stakeString)
+		resp, err := zcn.AddToDelegatePool(nil, stakeBytes, ctx.StateContextI)
+	
+		// Assertion
+		require.Error(t, err)
+		require.Empty(t, resp)
+	})
+	t.Run("AddToDelegatePool_MinStakeAmount", func(t *testing.T) {
+		// Setup
+		ctx := MakeMockStateContext() // Create a mock state context
+		// Mock the GetGlobalNode function to return a valid global node
+		mockGlobalNode := &GlobalNode{
+			ZCNSConfig : &ZCNSConfig {
+			MinStakeAmount:       1000,
+			MaxStakeAmount:       5000,
+			MaxDelegates:   10,
+			MinStakePerDelegate: 100,},
+
+		}
+		mockGetGlobalNode := func(balances state.StateContextI) (*GlobalNode, error) {
+			return mockGlobalNode, nil
+		}
+	
+		zcn := &ZCNSmartContract{}
+		resp1, err1 := mockGetGlobalNode(ctx.StateContextI)
+		require.NotNil(t, resp1)
+		require.NoError(t, err1)
+		// Attempt to add stake equal to the minimum allowed
+		stake := 1000
+		stakeString := strconv.Itoa(stake)
+        stakeBytes := []byte(stakeString)
+	
+		resp, err := zcn.AddToDelegatePool(nil, stakeBytes, ctx.StateContextI)
+	
+		// Assertion
+		require.NoError(t, err)
+		require.NotNil(t, resp)
+	})
+	t.Run("AddToDelegatePool_MinStakePerDelegate", func(t *testing.T) {
+		// Setup
+		ctx := MakeMockStateContext() // Create a mock state context
+		// Mock the GetGlobalNode function to return a valid global node
+		mockGlobalNode := &GlobalNode{
+			ZCNSConfig : &ZCNSConfig {
+			MinStakeAmount:       1000,
+			MaxStakeAmount:       5000,
+			MaxDelegates:   10,
+			MinStakePerDelegate: 100,},
+
+		}
+		mockGetGlobalNode := func(balances state.StateContextI) (*GlobalNode, error) {
+			return mockGlobalNode, nil
+		}
+	
+		zcn := &ZCNSmartContract{}
+		resp1, err1 := mockGetGlobalNode(ctx.StateContextI)
+		require.NotNil(t, resp1)
+		require.NoError(t, err1)
+		
+		stake := 100
+		stakeString := strconv.Itoa(stake)
+        stakeBytes := []byte(stakeString)
+	
+		
+		resp, err := zcn.AddToDelegatePool(nil, stakeBytes, ctx.StateContextI)
+	
+		// Assertion
+		require.NoError(t, err)
+		require.NotNil(t, resp)
+	})
+	t.Run("AddToDelegatePool_InvalidStakeAmount", func(t *testing.T) {
+		// Setup
+		ctx := MakeMockStateContext() // Create a mock state context
+		// Mock the GetGlobalNode function to return a valid global node
+		mockGlobalNode := &GlobalNode{
+			ZCNSConfig : &ZCNSConfig {
+			MinStakeAmount:       1000,
+			MaxStakeAmount:       5000,
+			MaxDelegates:   10,
+			MinStakePerDelegate: 100,},
+
+		}
+		mockGetGlobalNode := func(balances state.StateContextI) (*GlobalNode, error) {
+			return mockGlobalNode, nil
+		}
+	
+		zcn := &ZCNSmartContract{}
+		resp1, err1 := mockGetGlobalNode(ctx.StateContextI)
+		require.NotNil(t, resp1)
+		require.NoError(t, err1)
+		
+		stake :=-100
+		stakeString := strconv.Itoa(stake)
+        stakeBytes := []byte(stakeString)
+	
+		
+		resp, err := zcn.AddToDelegatePool(nil, stakeBytes, ctx.StateContextI)
+	
+		// Assertion
+		require.Error(t, err)
+		require.Empty(t, resp)
+	})
+	t.Run("AddToDelegatePool_InvalidContext", func(t *testing.T) {
+		// Attempt to add stake with an invalid context
+		zcn := &ZCNSmartContract{}
+
+		stake :=100
+		stakeString := strconv.Itoa(stake)
+        stakeBytes := []byte(stakeString)
+		resp, err := zcn.AddToDelegatePool(nil, stakeBytes, nil)
+	
+		// Assertion
+		require.Error(t, err)
+		require.Empty(t, resp)
+	})
+	
+	t.Run("AddToDelegatePool_MaxStakeReached", func(t *testing.T) {
+		// Setup
+		ctx := MakeMockStateContext() // Create a mock state context
+		// Mock the GetGlobalNode function to return a valid global node
+		mockGlobalNode := &GlobalNode{
+			ZCNSConfig : &ZCNSConfig {
+			MinStakeAmount:       1000,
+			MaxStakeAmount:       5000,
+			MaxDelegates:   10,
+			MinStakePerDelegate: 100,},
+
+		}
+		mockGetGlobalNode := func(balances state.StateContextI) (*GlobalNode, error) {
+			return mockGlobalNode, nil
+		}
+		
+	
+		zcn := &ZCNSmartContract{}
+		
+		resp1, err1 := mockGetGlobalNode(ctx.StateContextI)
+		require.NotNil(t, resp1)
+		require.NoError(t, err1)
+		stake :=6000
+		stakeString := strconv.Itoa(stake)
+        stakeBytes := []byte(stakeString)
+	
+		
+		resp, err := zcn.AddToDelegatePool(nil, stakeBytes, ctx.StateContextI)
+	
+		// Assertion
+		require.Error(t, err)
+		require.Empty(t, resp)
 	})
 
+
+	t.Run("UpdateAuthorizerStakePool_AuthorizerNotFound", func(t *testing.T) {
+		// Setup
+		ctx := MakeMockStateContext() // Create a mock state context
+		// Attempt to update stake pool for a non-existing authorizer
+		authorizerID := "nonexistent_auth"
+		payload := CreateAuthorizerStakingPoolParamPayload(authorizerID)
+		tr, err := CreateTransaction(authorizerID, UpdateAuthorizerStakePoolFunc, payload, ctx.StateContextI)
+		require.NoError(t, err)
+		sc := CreateZCNSmartContract()
+		
+		// Call UpdateAuthorizerStakePool with invalid authorizer ID
+		resp, err := sc.UpdateAuthorizerStakePool(tr, payload, ctx.StateContextI)
+	
+		// Assertion
+		require.Error(t, err)
+		require.Empty(t, resp)
+	})
+			
 	// Test cases for DeleteFromDelegatePool
 	t.Run("DeleteFromDelegatePool_Success", func(t *testing.T) {
 		// Setup
@@ -153,7 +371,6 @@ func TestDelegatePoolOperations(t *testing.T) {
 		// Assertion
 		require.NoError(t, err)
 		require.NotNil(t, resp)
-		// Add more assertions to verify stake pool unlock operation and delegate pool balance
 	})
 
 	t.Run("DeleteFromDelegatePool_Failure_StakePoolUnlockError", func(t *testing.T) {
@@ -168,7 +385,64 @@ func TestDelegatePoolOperations(t *testing.T) {
 		// Assertion
 		require.Error(t, err)
 		require.Empty(t, resp)
-		// Add more assertions to verify the error
+	})
+	t.Run("DeleteFromDelegatePool_NotEnoughBalanceForUnlock", func(t *testing.T) {
+		// Setup
+		ctx := MakeMockStateContext() // Create a mock state context
+		// Mock the GetGlobalNode function to return a valid global node
+		mockGlobalNode := &GlobalNode{
+			ZCNSConfig : &ZCNSConfig {
+			MinStakeAmount:       1000,
+			MaxStakeAmount:       5000,
+			MaxDelegates:   10,
+			MinStakePerDelegate: 100,},
+
+		}
+		mockGetGlobalNode := func(balances state.StateContextI) (*GlobalNode, error) {
+			return mockGlobalNode, nil
+		}
+	
+		zcn := &ZCNSmartContract{}
+		resp1, err1 := mockGetGlobalNode(ctx.StateContextI)
+		require.NotNil(t, resp1)
+		require.NoError(t, err1)
+		stake := 3000
+		stakeString := strconv.Itoa(stake)
+        stakeBytes := []byte(stakeString)
+
+		zcn.AddToDelegatePool(nil, stakeBytes, ctx.StateContextI)
+		unlockAmount := 4000
+		unlockAmountString := strconv.Itoa(unlockAmount)
+        unlockAmountBytes := []byte(unlockAmountString)
+		resp, err := zcn.DeleteFromDelegatePool(nil, unlockAmountBytes, ctx.StateContextI)
+	
+		// Assertion
+		require.Error(t, err)
+		require.Empty(t, resp)
+	})
+	
+	
+	t.Run("DeleteFromDelegatePool_DelegateNotFound", func(t *testing.T) {
+		// Setup
+		ctx := MakeMockStateContext() // Create a mock state context with a delegate pool containing a sufficient balance
+		// Mock the DeleteDelegate function to return an error indicating delegate not found
+		mockDeleteDelegate := func(id string, balances state.StateContextI) error {
+			// Simulate delegate not found error
+			return errors.New("delegate not found")
+		}
+	
+		zcn := &ZCNSmartContract{}
+
+		invalidDelegateID := "invalid_delegate_id"
+		invalidDelegateBytes := []byte(invalidDelegateID)
+		err1:=mockDeleteDelegate(invalidDelegateID,ctx.StateContextI)
+		require.Error(t,err1)
+		// Call DeleteFromDelegatePool with invalid delegate ID
+		resp, err := zcn.DeleteFromDelegatePool(nil,invalidDelegateBytes, ctx.StateContextI)
+	
+		// Assertion
+		require.Error(t, err)
+		require.Empty(t, resp)
 	})
 
 	t.Run("DeleteFromDelegatePool_InvalidInputData", func(t *testing.T) {
@@ -183,6 +457,5 @@ func TestDelegatePoolOperations(t *testing.T) {
 		// Assertion
 		require.Error(t, err)
 		require.Empty(t, resp)
-		// Add more assertions to verify the error
 	})
 }
