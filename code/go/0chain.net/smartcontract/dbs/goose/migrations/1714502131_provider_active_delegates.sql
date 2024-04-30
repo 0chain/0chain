ec3b7aaaab6e17f0e9bde51a4affbfedf16c9ff9
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-05-01 00:14:31 +0530

    Fix migration script and unit tests

--- code/go/0chain.net/smartcontract/dbs/goose/migrations/1714502131_provider_active_delegates.sql
+++ code/go/0chain.net/smartcontract/dbs/goose/migrations/1714502131_provider_active_delegates.sql
@@ -14,6 +14,8 @@ WHERE EXISTS (
     WHERE delegate_pools.provider_id = blobbers.id
       AND delegate_pools.status = 0
 );
+ALTER TABLE blobbers ADD COLUMN IF NOT EXISTS num_delegates bigint;
+
 
 ALTER TABLE validators ADD COLUMN IF NOT EXISTS active_delegates bigint;
 UPDATE validators
@@ -29,6 +31,7 @@ WHERE EXISTS (
     WHERE delegate_pools.provider_id = validators.id
       AND delegate_pools.status = 0
 );
+ALTER TABLE validators ADD COLUMN IF NOT EXISTS num_delegates bigint;
 
 ALTER TABLE miners ADD COLUMN IF NOT EXISTS active_delegates bigint;
 UPDATE miners
@@ -44,6 +47,7 @@ WHERE EXISTS (
     WHERE delegate_pools.provider_id = miners.id
       AND delegate_pools.status = 0
 );
+ALTER TABLE miners ADD COLUMN IF NOT EXISTS num_delegates bigint;
 
 ALTER TABLE sharders ADD COLUMN IF NOT EXISTS active_delegates bigint;
 UPDATE sharders
@@ -59,6 +63,7 @@ WHERE EXISTS (
     WHERE delegate_pools.provider_id = sharders.id
       AND delegate_pools.status = 0
 );
+ALTER TABLE sharders ADD COLUMN IF NOT EXISTS num_delegates bigint;
 
 ALTER TABLE authorizers ADD COLUMN IF NOT EXISTS active_delegates bigint;
 UPDATE authorizers
@@ -74,6 +79,7 @@ WHERE EXISTS (
     WHERE delegate_pools.provider_id = authorizers.id
       AND delegate_pools.status = 0
 );
+ALTER TABLE authorizers ADD COLUMN IF NOT EXISTS num_delegates bigint;
 
 
 
@@ -88,8 +94,6 @@ ALTER TABLE validator_aggregates ADD COLUMN IF NOT EXISTS num_delegates bigint;
 ALTER TABLE miner_aggregates ADD COLUMN IF NOT EXISTS num_delegates bigint;
 ALTER TABLE sharder_aggregates ADD COLUMN IF NOT EXISTS num_delegates bigint;
 ALTER TABLE authorizer_aggregates ADD COLUMN IF NOT EXISTS num_delegates bigint;
-
-
 -- +goose StatementEnd
 
 -- +goose Down
@@ -100,11 +104,23 @@ ALTER TABLE miners DROP COLUMN IF EXISTS active_delegates;
 ALTER TABLE sharders DROP COLUMN IF EXISTS active_delegates;
 ALTER TABLE authorizers DROP COLUMN IF EXISTS active_delegates;
 
+ALTER TABLE blobbers DROP COLUMN IF EXISTS num_delegates;
+ALTER TABLE validators DROP COLUMN IF EXISTS num_delegates;
+ALTER TABLE miners DROP COLUMN IF EXISTS num_delegates;
+ALTER TABLE sharders DROP COLUMN IF EXISTS num_delegates;
+ALTER TABLE authorizers DROP COLUMN IF EXISTS num_delegates;
+
+
+ALTER TABLE blobber_aggregates DROP COLUMN IF EXISTS active_delegates;
+ALTER TABLE validator_aggregates DROP COLUMN IF EXISTS num_delegates;
+ALTER TABLE miner_aggregates DROP COLUMN IF EXISTS num_delegates;
+ALTER TABLE sharder_aggregates DROP COLUMN IF EXISTS num_delegates;
+ALTER TABLE authorizer_aggregates DROP COLUMN IF EXISTS num_delegates;
+
 
 ALTER TABLE blobber_aggregates DROP COLUMN IF EXISTS num_delegates;
 ALTER TABLE validator_aggregates DROP COLUMN IF EXISTS num_delegates;
 ALTER TABLE miner_aggregates DROP COLUMN IF EXISTS num_delegates;
 ALTER TABLE sharder_aggregates DROP COLUMN IF EXISTS num_delegates;
 ALTER TABLE authorizer_aggregates DROP COLUMN IF EXISTS num_delegates;
-
 -- +goose StatementEnd
