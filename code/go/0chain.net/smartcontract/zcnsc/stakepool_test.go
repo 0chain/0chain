fff747cff193935247202f4c14b76785261afe7e
shubham <shubam.198sk@gmail.com>
2024-05-14 10:38:05 +0530

    Adding cases

--- code/go/0chain.net/smartcontract/zcnsc/stakepool_test.go
+++ code/go/0chain.net/smartcontract/zcnsc/stakepool_test.go
@@ -2,9 +2,15 @@ package zcnsc_test
 
 import (
 	"encoding/hex"
+	"errors"
 	"testing"
 
+	"0chain.net/chaincore/chain/state"
+	"0chain.net/chaincore/transaction"
 	"0chain.net/core/encryption"
+	"0chain.net/smartcontract/stakepool"
+	"0chain.net/smartcontract/stakepool/spenum"
+	"0chain.net/smartcontract/zcnsc"
 	. "0chain.net/smartcontract/zcnsc"
 	"github.com/stretchr/testify/require"
 )
@@ -51,4 +57,35 @@ func Test_WhenAuthorizerDoesNotExists_StakePool_IsNotUpdatedOrCreated(t *testing
 	require.Error(t, err)
 	require.EqualError(t, err, "authorizer(authorizerID: "+authorizerID+") not found")
 	require.Empty(t, resp)
+}
+func Test_StakePoolLock_NoGetFuncProvided(t *testing.T) {
+     // Mock necessary dependencies
+	 const authorizerID = "auth0"
+	 ctx:= MakeMockStateContext()
+	//  tInput := &transaction.Transaction{} // Provide a valid transaction object
+	//  input := []byte{}                    // Provide a valid input byte array
+	//  bal := &mockStateContext{}      // Provide a mock implementation of StateContextI
+	 
+	 zcn := CreateZCNSmartContract() // Initialize ZCNSmartContract
+     
+	//  payload := CreateAuthorizerStakingPoolParamPayload(authorizerID)
+    //  tr,err := CreateTransaction(authorizerID, UpdateAuthorizerStakePoolFunc, payload, ctx)
+    //  require.NoError(t,err)
+	//  resp,err := zcn.UpdateAuthorizerConfig(tr,payload,ctx)
+	//  require.Error(t,err)
+	//  require.EqualError(t, err, "authorizer(authorizerID: "+authorizerID+") not found")
+	//  require.Empty(t,resp)
+	 
+    // Create a transaction for updating authorizer config
+    payload := CreateAuthorizerStakingPoolParamPayload(authorizerID)
+    tr, err := CreateTransaction(authorizerID, UpdateAuthorizerStakePoolFunc, payload, ctx)
+    require.NoError(t, err)
+    
+    // Call the AddToDelegatePool function
+    resp, err := zcn.UpdateAuthorizerConfig(tr, payload, ctx)
+    
+    // Check if an error occurred because no get function was provided
+    require.Error(t, err)
+    require.EqualError(t, err, "provide get func")
+    require.Empty(t, resp)
 }
