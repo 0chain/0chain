0946fa16ff64ffbb862a9adf5a91d6e62e584dd4
peterlimg <peterlimg@protonmail.com>
2024-05-28 20:44:44 +1000

    Tune wait time for block finalization

--- code/go/0chain.net/chaincore/chain/protocol_round.go
+++ code/go/0chain.net/chaincore/chain/protocol_round.go
@@ -336,6 +336,7 @@ func (c *Chain) finalizeRound(ctx context.Context, r round.RoundI) {
 			zap.Int64("round", roundNumber),
 			zap.Int64("lfb round", lfb.Round),
 			zap.String("lfb block", lfb.Hash))
+		fbSteadyStateTM := SteadyStateFinalizationTimer.Mean()
 		for idx := range frchain {
 			fb := frchain[len(frchain)-1-idx]
 			// if roundNumber-fb.Round < 2 {
@@ -343,8 +344,8 @@ func (c *Chain) finalizeRound(ctx context.Context, r round.RoundI) {
 			// 	logging.Logger.Debug("finalize round - block has less than 3 confirmation")
 			// 	continue
 			// }
-			if time.Since(fb.CreateTime) < 200*time.Millisecond {
-				logging.Logger.Debug("finalize round - block has less than 3 confirmation")
+			if time.Since(fb.CreateTime) < time.Duration(1.5*fbSteadyStateTM) {
+				logging.Logger.Debug("finalize round - block is too new")
 				continue
 			}
 
