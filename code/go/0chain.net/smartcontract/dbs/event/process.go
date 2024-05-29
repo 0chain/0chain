ea5fb89af95c725cf2d384d451ec7f1d9e9001c4
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-05-29 19:20:20 +0530

    Revert "Merge pull request #3278 from 0chain/kafka/migration-testing"
    This reverts commit d770390c381d1119a860585f0bbe4e1f9cdb0ff2, reversing
    changes made to 96239111be48206e9584c847f6feee362ff70e86.

--- code/go/0chain.net/smartcontract/dbs/event/process.go
+++ code/go/0chain.net/smartcontract/dbs/event/process.go
@@ -461,10 +461,6 @@ func (edb *EventDb) WorkEvents(
 		logging.Logger.Warn("work events - lost connection")
 	}
 
-	if blockEvents.round == edb.Config().KafkaTriggerRound {
-		logging.Logger.Panic("panic after " + fmt.Sprintf("round %v", blockEvents.round))
-	}
-
 	currentPermanentPartition := blockEvents.round / edb.settings.PermanentPartitionChangePeriod
 	if blockEvents.round%edb.settings.PermanentPartitionChangePeriod == 0 {
 		edb.managePermanentPartitionsAsync(currentPermanentPartition)
