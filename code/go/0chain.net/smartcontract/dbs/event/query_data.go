c6c0e5e8daec8ab7835ccacbb9f448f3fdec995c
shohan2001 <shohanmondal3@gmail.com>
2024-07-05 23:25:00 +0530

    adding changes

--- code/go/0chain.net/smartcontract/dbs/event/query_data.go
+++ code/go/0chain.net/smartcontract/dbs/event/query_data.go
@@ -7,35 +7,35 @@ func (edb *EventDb) GetQueryData(fields string, table interface{}) (interface{},
 	switch t := table.(type) {
 	case *Miner:
 		var miners []Miner
-		err := edb.Get().Model(&t).Select(fields).Find(&miners).Error
+		err := edb.Get().Model(&Miner{}).Select(fields).Find(&miners).Error
 		if err != nil {
 			return nil, err
 		}
 		result = miners
 	case *Blobber:
 		var blobbers []Blobber
-		err := edb.Get().Model(&t).Select(fields).Find(&blobbers).Error
+		err := edb.Get().Model(&Blobber{}).Select(fields).Find(&blobbers).Error
 		if err != nil {
 			return nil, err
 		}
 		result = blobbers
 	case *Sharder:
 		var sharders []Sharder
-		err := edb.Get().Model(&t).Select(fields).Find(&sharders).Error
+		err := edb.Get().Model(&Sharder{}).Select(fields).Find(&sharders).Error
 		if err != nil {
 			return nil, err
 		}
 		result = sharders
 	case *Authorizer:
 		var authorizers []Authorizer
-		err := edb.Get().Model(&t).Select(fields).Find(&authorizers).Error
+		err := edb.Get().Model(&Authorizer{}).Select(fields).Find(&authorizers).Error
 		if err != nil {
 			return nil, err
 		}
 		result = authorizers
 	case *Validator:
 		var validators []Validator
-		err := edb.Get().Model(&t).Select(fields).Find(&validators).Error
+		err := edb.Get().Model(&Validator{}).Select(fields).Find(&validators).Error
 		if err != nil {
 			return nil, err
 		}
