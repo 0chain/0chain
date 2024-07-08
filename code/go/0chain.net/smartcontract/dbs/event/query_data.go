a8173a04a3d982f994b10508cea8025e264ed7ae
shohan2001 <shohanmondal3@gmail.com>
2024-07-09 01:51:08 +0530

    editing GetQueryData function

--- code/go/0chain.net/smartcontract/dbs/event/query_data.go
+++ code/go/0chain.net/smartcontract/dbs/event/query_data.go
@@ -40,6 +40,62 @@ func (edb *EventDb) GetQueryData(fields string, table interface{}) (interface{},
 			return nil, err
 		}
 		result = validators
+	case *User:
+		var users []User
+		err := edb.Get().Model(&User{}).Select(fields).Find(&users).Error
+		if err != nil {
+			return nil, err
+		}
+		result = users
+	case *UserSnapshot:
+		var userSnapshots []UserSnapshot
+		err := edb.Get().Model(&UserSnapshot{}).Select(fields).Find(&userSnapshots).Error
+		if err != nil {
+			return nil, err
+		}
+		result = userSnapshots
+	case *MinerSnapshot:
+		var minerSnapshots []MinerSnapshot
+		err := edb.Get().Model(&MinerSnapshot{}).Select(fields).Find(&minerSnapshots).Error
+		if err != nil {
+			return nil, err
+		}
+		result = minerSnapshots
+	case *BlobberSnapshot:
+		var blobberSnapshots []BlobberSnapshot
+		err := edb.Get().Model(&BlobberSnapshot{}).Select(fields).Find(&blobberSnapshots).Error
+		if err != nil {
+			return nil, err
+		}
+		result = blobberSnapshots
+	case *SharderSnapshot:
+		var sharderSnapshots []SharderSnapshot
+		err := edb.Get().Model(&SharderSnapshot{}).Select(fields).Find(&sharderSnapshots).Error
+		if err != nil {
+			return nil, err
+		}
+		result = sharderSnapshots
+	case *ValidatorSnapshot:
+		var validatorSnapshots []ValidatorSnapshot
+		err := edb.Get().Model(&ValidatorSnapshot{}).Select(fields).Find(&validatorSnapshots).Error
+		if err != nil {
+			return nil, err
+		}
+		result = validatorSnapshots
+	case *AuthorizerSnapshot:
+		var authorizerSnapshots []AuthorizerSnapshot
+		err := edb.Get().Model(&AuthorizerSnapshot{}).Select(fields).Find(&authorizerSnapshots).Error
+		if err != nil {
+			return nil, err
+		}
+		result = authorizerSnapshots
+	case *ProviderRewards:
+		var providerRewards []ProviderRewards
+		err := edb.Get().Model(&ProviderRewards{}).Select(fields).Find(&providerRewards).Error
+		if err != nil {
+			return nil, err
+		}
+		result = providerRewards
 	default:
 		return nil, fmt.Errorf("unsupported type: %T", t)
 	}
