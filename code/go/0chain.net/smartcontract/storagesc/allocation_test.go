705daf87e2e7666cfd58469e930d48c336e77aa2
jals413 <jalsthakor413@gmail.com>
2024-07-30 01:47:05 +0530

    added test where blobbers increase / decrease the price

--- code/go/0chain.net/smartcontract/storagesc/allocation_test.go
+++ code/go/0chain.net/smartcontract/storagesc/allocation_test.go
@@ -2040,6 +2040,57 @@ func TestUpdateAllocationRequest(t *testing.T) {
 		compareAllocationData(t, *expectedAlloc, *afterAlloc)
 	})
 
+	t.Run("Upgrade size in unused allocation (20 blobbers in allocation) where 5 blobbers increased price to 2x and 2 blobbers decreased it to 0.5x", func(t *testing.T) {
+		var (
+			tp                  = int64(0)
+			client              = newClient(2000*x10, balances)
+			mockBlobberCapacity = 200 * GB
+		)
+
+		beforeAlloc, _ := setupAllocationWithMockStats(t, ssc, client, tp, balances, mockBlobberCapacity, true)
+		allocID := beforeAlloc.ID
+
+		// Update blobber prices: 5 blobbers increase price to 2x, 2 blobbers decrease price to 0.5x
+		for i, ba := range beforeAlloc.BlobberAllocs {
+			if i < 5 {
+				ba.Terms.WritePrice *= 2
+			} else if i < 7 {
+				ba.Terms.WritePrice /= 2
+			}
+		}
+
+		// upgrade
+		var uar updateAllocationRequest
+		uar.ID = allocID
+		uar.Size = 10 * GB
+		tp += int64(360 * time.Hour / 1e9)
+		resp, err := uar.callUpdateAllocReq(t, client.id, 100*x10, tp, ssc, balances)
+		require.NoError(t, err)
+
+		var deco StorageAllocation
+		require.NoError(t, deco.Decode([]byte(resp)))
+
+		afterAlloc, err := ssc.getAllocation(allocID, balances)
+		require.NoError(t, err)
+
+		require.EqualValues(t, afterAlloc, &deco, "Response and allocation in MPT should be same")
+		assert.NotEqual(t, beforeAlloc.Tx, afterAlloc.Tx, "Transaction should be updated")
+
+		assert.Equal(t, int64(20*GB), afterAlloc.Size, "Allocation size should be increased")
+		require.Equal(t, 200*x10, int(afterAlloc.WritePool), "Write pool should be updated")
+		assert.Equal(t, common.Timestamp(tp+int64(720*time.Hour/1e9)), afterAlloc.Expiration, "Allocation expiration should be increased")
+
+		expectedAlloc := beforeAlloc
+		expectedAlloc.Tx = afterAlloc.Tx
+		expectedAlloc.Expiration = afterAlloc.Expiration
+		expectedAlloc.WritePool = afterAlloc.WritePool
+		expectedAlloc.Size = afterAlloc.Size
+		for _, ba := range expectedAlloc.BlobberAllocs {
+			ba.Size += uar.Size / int64(afterAlloc.DataShards)
+		}
+		compareAllocationData(t, *expectedAlloc, *afterAlloc)
+	})
+
 	t.Run("Upgrade size in used allocation should work", func(t *testing.T) {
 		var (
 			tp     = int64(10)
