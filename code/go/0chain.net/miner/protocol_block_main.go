9240549e65b8d6bd8cd659ddf830142e0096e2ed
peterlimg <peterlimg@protonmail.com>
2024-08-23 21:13:46 +1000

    Fix lint error

--- code/go/0chain.net/miner/protocol_block_main.go
+++ code/go/0chain.net/miner/protocol_block_main.go
@@ -29,7 +29,9 @@ func (mc *Chain) hashAndSignGeneratedBlock(ctx context.Context,
 
 /*UpdateFinalizedBlock - update the latest finalized block */
 func (mc *Chain) UpdateFinalizedBlock(ctx context.Context, b *block.Block) error {
-	go mc.updateFinalizedBlock(ctx, b)
+	go func() {
+		mc.updateFinalizedBlock(ctx, b) // nolint: errcheck
+	}()
 	return nil
 }
 
