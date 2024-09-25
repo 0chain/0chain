25c7f6e45fb0a2b41296ee8faf794b8b7c0e6bd6
peterlimg <peterlimg@protonmail.com>
2024-09-25 11:00:20 +0900

    Stop delete node if they are in register list

--- code/go/0chain.net/smartcontract/minersc/miner.go
+++ code/go/0chain.net/smartcontract/minersc/miner.go
@@ -354,6 +354,21 @@ func (msc *MinerSmartContract) deleteNode(
 		zap.String("node type", nodeType.String()),
 		zap.String("id", deleteNode.ID))
 
+	// check if the node is in register list
+	rids, err := getRegisterNodes(balances, nodeType)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, rid := range rids {
+		if rid == deleteNode.ID {
+			logging.Logger.Error("delete node failed, node is in register list",
+				zap.String("node type", nodeType.String()),
+				zap.String("id", deleteNode.ID))
+			return nil, common.NewError("delete_node", "node is in register list")
+		}
+	}
+
 	err = saveDeleteNodeID(balances, nodeType, deleteNode.ID)
 	if err != nil {
 		return nil, err
