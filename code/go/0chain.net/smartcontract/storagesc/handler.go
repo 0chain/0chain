b3de594876cf0d4cd0b7f90f4d6cd5f566d5e790
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-10-02 21:47:05 +0530

    Debug

--- code/go/0chain.net/smartcontract/storagesc/handler.go
+++ code/go/0chain.net/smartcontract/storagesc/handler.go
@@ -1988,16 +1988,6 @@ func requiredTokensForUpdateAllocation(sa *storageAllocationBase, cpBalance curr
 		tokensRequiredToLock = 0
 	}
 
-	var costOfUnusedAlloc currency.Coin
-
-	costOfUnusedAlloc, err = sa.unUsedAllocCost()
-	if err != nil {
-		return 0, fmt.Errorf("failed to get unused allocation cost: %v", err)
-	}
-	if costOfUnusedAlloc > sa.WritePool && costOfUnusedAlloc-sa.WritePool > tokensRequiredToLock {
-		tokensRequiredToLock = costOfUnusedAlloc - sa.WritePool
-	}
-
 	logging.Logger.Info("requiredTokensForUpdateAllocation",
 		zap.Any("costOfAllocAfterUpdate", costOfAllocAfterUpdate),
 		zap.Any("totalWritePool", totalWritePool),
@@ -2007,7 +1997,6 @@ func requiredTokensForUpdateAllocation(sa *storageAllocationBase, cpBalance curr
 		zap.Any("sa", sa),
 		zap.Any("cpBalance", cpBalance),
 		zap.Any("now", now),
-		zap.Any("costOfUnusedAlloc", costOfUnusedAlloc),
 	)
 
 	return tokensRequiredToLock, nil
