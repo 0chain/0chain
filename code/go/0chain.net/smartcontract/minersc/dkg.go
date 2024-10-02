398b84b47fade2597fdbf916fc4f6f98408c28bd
peterlimg <peterlimg@protonmail.com>
2024-10-02 16:21:25 +0800

    Fix error

--- code/go/0chain.net/smartcontract/minersc/dkg.go
+++ code/go/0chain.net/smartcontract/minersc/dkg.go
@@ -424,7 +424,7 @@ func (msc *MinerSmartContract) createDKGMinersForContribute(
 	if err != nil {
 		return err
 	}
-	logging.Logger.Debug("[mvc] sharder delete list", zap.Strings("ids", deleteMinersIDs))
+	logging.Logger.Debug("[mvc] sharder delete list", zap.Strings("ids", deleteShardersIDs))
 
 	var toDeleteSharder string
 	if len(deleteShardersIDs) > 0 {
