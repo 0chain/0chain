9a4913fc46a39c96b5f8570d209ca3f431ea4505
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-11-19 03:45:20 +0530

    Fix build tag

--- quickstart/1s_2m_4b/base/rebuild_sharders.sh
+++ quickstart/1s_2m_4b/base/rebuild_sharders.sh
@@ -21,7 +21,7 @@ sudo rm -rf docker.local/sharder*/log/*
 # docker.local/bin/build.sharders.sh
 set -e
 
-GIT_COMMIT=$(git rev-list -1 HEAD)
+GIT_COMMIT=$(git describe --tags --dirty --always)
 echo $GIT_COMMIT
 
 docker build --build-arg GIT_COMMIT=$GIT_COMMIT -f docker.local/build.sharder/Dockerfile . -t sharder
