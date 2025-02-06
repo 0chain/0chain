f9317d490710ce8f377e2bee9ff5ec43e50493d4
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2025-02-07 00:14:36 +0530

    Debug

--- code/go/0chain.net/smartcontract/storagesc/allocation.go
+++ code/go/0chain.net/smartcontract/storagesc/allocation.go
@@ -1,19 +1,17 @@
 package storagesc
 
 import (
+	"0chain.net/core/maths"
 	"0chain.net/core/util/entitywrapper"
+	"encoding/base64"
 	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"math"
 	"math/rand"
-	"reflect"
 	"strings"
 	"time"
-	"unsafe"
-
-	"0chain.net/core/maths"
 
 	"0chain.net/smartcontract/dbs/event"
 	"0chain.net/smartcontract/stakepool/spenum"
@@ -49,27 +47,6 @@ func (sc *StorageSmartContract) getAllocation(allocID string,
 	return
 }
 
-func sizeOf(v interface{}) uintptr {
-	val := reflect.ValueOf(v)
-	size := unsafe.Sizeof(v)
-
-	// Recursively calculate the size of struct fields if applicable
-	switch val.Kind() {
-	case reflect.Struct:
-		for i := 0; i < val.NumField(); i++ {
-			size += sizeOf(val.Field(i).Interface())
-		}
-	case reflect.Slice:
-		size += uintptr(val.Cap()) * unsafe.Sizeof(val.Index(0).Interface())
-	case reflect.String:
-		size += uintptr(len(val.String()))
-	default:
-		panic("unhandled default case")
-	}
-
-	return size
-}
-
 func (sc *StorageSmartContract) addAllocation(alloc *StorageAllocation,
 	balances chainstate.StateContextI) (string, error) {
 	ta := &StorageAllocation{}
@@ -84,12 +61,11 @@ func (sc *StorageSmartContract) addAllocation(alloc *StorageAllocation,
 	}
 
 	largeData := make([]byte, 2*1024*1024+1) // 2MB + 1 byte
+
 	alloc.mustUpdateBase(func(sab *storageAllocationBase) error {
-		sab.Owner = string(largeData)
+		sab.Owner = base64.StdEncoding.EncodeToString(largeData) // Convert efficiently
 		return nil
 	})
-
-	logging.Logger.Info("Jayash Alloc size", zap.Any("size", sizeOf(alloc)))
 
 	_, err = balances.InsertTrieNode(alloc.GetKey(sc.ID), alloc)
 	if err != nil {
