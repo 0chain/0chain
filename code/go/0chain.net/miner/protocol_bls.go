3a38489a249faa1e263b8a4878672b2d9b4a0615
peterlimg <peterlimg@protonmail.com>
2025-02-17 21:10:42 +1100

    Skip invalid dkg

--- code/go/0chain.net/miner/protocol_bls.go
+++ code/go/0chain.net/miner/protocol_bls.go
@@ -113,7 +113,8 @@ func (mc *Chain) SetDKGSFromStore(ctx context.Context, mb *block.MagicBlock, wor
 			}
 		} else {
 			logging.Logger.Error("[mvc2] failed to verify dkg summary", zap.Int64("mb number", mb.MagicBlockNumber), zap.Int64("start_round", mb.StartingRound))
-			return errors.New("invalid dkg summary")
+			// return errors.New("invalid dkg summary")
+			return nil
 		}
 	}
 
