a6507d281d9a321716fbe56a6f306612a8de1408
peterlimg <peterlimg@protonmail.com>
2025-03-05 22:59:00 +1100

    Do not print all dkg summary

--- code/go/0chain.net/miner/protocol_bls.go
+++ code/go/0chain.net/miner/protocol_bls.go
@@ -113,8 +113,9 @@ func (mc *Chain) SetDKGSFromStore(ctx context.Context, mb *block.MagicBlock) (
 		zap.Int("miners num", len(mb.Miners.CopyNodesMap())),
 		zap.Int("T", mb.T),
 		zap.Int("N", mb.N),
-		zap.Any("mb", mb),
-		zap.Any("summary", summary))
+		zap.Int64("mb", mb.MagicBlockNumber),
+		zap.Int64("sr", mb.StartingRound),
+	)
 
 	for k := range mb.Miners.CopyNodesMap() {
 		logging.Logger.Debug("[mvc] set dkg key", zap.String("key", ComputeBlsID(k)))
