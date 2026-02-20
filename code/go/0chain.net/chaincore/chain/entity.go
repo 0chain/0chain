1fd9db362f284b308b5ca770da129e1e1b9e1fad
guruhubb <sbasu999@gmail.com>
2026-02-20 14:57:21 -0800

    fix: correct block_events entity metadata name in getBlockEvents
    Was using 'last_block_events' which doesn't match the registered entity name.

--- code/go/0chain.net/chaincore/chain/entity.go
+++ code/go/0chain.net/chaincore/chain/entity.go
@@ -967,9 +967,9 @@ func (c *Chain) SetupEventDatabase() error {
 }
 
 func (c *Chain) getBlockEvents(round int64) (int64, []event.Event, error) {
-	meta := datastore.GetEntityMetadata("last_block_events")
+	meta := datastore.GetEntityMetadata("block_events")
 	if meta == nil {
-		return 0, nil, fmt.Errorf("last_block_events entity metadata not registered")
+		return 0, nil, fmt.Errorf("block_events entity metadata not registered")
 	}
 	blockEvents := meta.Instance().(*block.BlockEvents)
 	key := strconv.FormatInt(round%int64(block.EventsRingSize), 10)
